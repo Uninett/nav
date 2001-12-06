6667f1cc75ceae8dcbce469d8c487186e4b74901
Sigurd Gartmann <sigurd@brogar.org>
2001-12-06 11:07:05 +0000

    kollekt har blitt collect

---
+++ cron/collect/bokser.pl
@@ -0,0 +1,125 @@
+#!/usr/bin/perl -w
+
+#use Socket;
+use SNMP_util;
+use strict;
+
+my $vei = "/usr/local/nav/navme/lib";
+require "$vei/database.pl";
+require "$vei/snmplib.pl";
+require "$vei/fil.pl";
+
+my $conn = &db_connect("manage","navall","uka97urgf");
+
+my $mib_sysname = ".1.3.6.1.2.1.1.5.0";
+my $mib_type =     ".1.3.6.1.2.1.1.2.0";
+
+my (%server,%db_server,%nettel,%db_nettel,%alle,%db_alle);
+
+#sysname-endelser
+#leses inn fra fil og legges i kolonseparert skalar
+    my $fil_endelser = "/usr/local/nav/etc/conf/endelser.txt";
+    my $endelser = &fil_endelser($fil_endelser);
+    my %type = &db_hent_enkel($conn,"SELECT sysobjectid,typeid FROM type");
+#-----------------------
+#FILLESING: server.txt
+my @felt_server = ("ip","romid","sysname","orgid","kat","kat2","ro");
+my $fil_server = "/usr/local/nav/etc/server.txt";
+%server = &fil_server($fil_server,scalar(@felt_server),$endelser);
+
+#----------------------------------
+#DATABASELESING
+
+%db_server = &db_hent_hash($conn,"SELECT ".join(",", @felt_server )." FROM boks");
+#legge til alle
+for my $a (keys %db_server) {
+    $db_alle{$a} = 1;
+}
+
+&db_endring($conn,\%server,\%db_server,\@felt_server,"boks");
+
+#------------------------------
+#FILLESING: nettel.txt
+my @felt_nettel = ("ip","typeid","romid","sysname","orgid","kat","kat2","ro","rw");
+my $fil_nettel = "/usr/local/nav/etc/nettel.txt";
+%nettel = &fil_nettel($fil_nettel,scalar(@felt_nettel),$endelser);
+
+#----------------------------------
+#DATABASELESING
+#felter som skal leses ut av databasen
+
+%db_nettel = &db_hent_hash($conn,"SELECT ".join(",", @felt_nettel )." FROM boks");
+
+#legge til i alle
+for my $a (keys %db_nettel) {
+    $db_alle{$a} = 1;
+}
+&db_endring($conn,\%nettel,\%db_nettel,\@felt_nettel,"boks");
+
+#-----------------------------------
+#DELETE
+    my @felt_alle = ("ip");
+    &db_sletting($conn,\%alle,\%db_alle,\@felt_alle,"boks");
+
+sub fil_endelser {
+    my $fil = $_[0];
+    open (FIL, "<$fil") || die ("kunne ikke åpne $fil");
+    my @endelser;
+    while (<FIL>) {
+	next unless /^\./; 
+	my $endelse = rydd($_);
+	@endelser=(@endelser,$endelse);
+    }
+    close FIL;
+    return join(":",@endelser);
+}
+sub fil_nettel{
+    my ($fil,$felt,$endelser) = @_;
+    
+    open (FIL, "<$fil") || die ("kunne ikke åpne $fil");
+    while (<FIL>) {
+	@_ = &fil_hent_linje($felt,$_);
+
+	if(my $ip = $_[1]){
+	    my $ro = $_[5];
+	    my $temptype;
+	    if($temptype = &snmp_type($ip,$ro,$mib_type)) {
+		my $type = $type{$temptype};
+		my $sysname = &snmp_sysname(1,$ip,$ro,$mib_sysname,$endelser);
+#		if($type) {
+		    @_ = ($ip,$type,$_[0],$sysname,$_[2],@_[3..6]);
+		    @_ = map rydd($_), @_;
+		    
+		    $nettel{$_[0]} = [ @_ ];
+		    $alle{$_[0]} = 1;
+#		}
+	    }
+	}
+	
+    }
+    close FIL;
+    return %nettel;
+}
+sub fil_server{
+    my ($fil,$felt,$endelser) = @_;
+
+    open (FIL, "<$fil") || die ("kunne ikke åpne $fil");
+    while (<FIL>) {
+
+	@_ = &fil_hent_linje($felt,$_);
+
+	my $ip;
+	if($ip = &hent_ip($_[1])) {
+	    @_ = ($ip,@_[0..1],lc($_[2]),uc($_[3]),@_[4..5]);
+	    @_ = map rydd($_), @_;
+	    
+	    my $sysname = &fjern_endelse($_[2],$endelser);
+	    
+	    $server{$_[0]} = [ @_[0..1],$sysname,@_[3..6] ];
+	    $alle{$_[0]} = 1;
+	}
+    }
+    close FIL;
+    return %server;
+}
+return 1;
---
+++ cron/collect/tekstfiler.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $vei = "/usr/local/nav/navme/lib";
+require "$vei/database.pl";
+require "$vei/fil.pl";
+#-------------ALLE-------------
+my $db = &db_connect("manage","navall","uka97urgf");
+my ($fil,$tabell,@felt);
+#--------------ANV-------------
+$fil = "/usr/local/nav/etc/anv.txt";
+$tabell = "anv";
+@felt = ("anvid","descr");
+&db_endring_med_sletting($db,$fil,$tabell,join(":",@felt));
+#--------------STED------------
+$fil = "/usr/local/nav/etc/sted.txt";
+$tabell = "sted";
+@felt = ("stedid","descr");
+&db_endring_med_sletting($db,$fil,$tabell,join(":",@felt));
+#--------------ROM-------------
+$fil = "/usr/local/nav/etc/rom.txt";
+$tabell = "rom";
+@felt = ("romid","stedid","descr","rom2","rom3","rom4","rom5");
+&db_endring_med_sletting($db,$fil,$tabell,join(":",@felt));
+#--------------ORG-------------
+$fil = "/usr/local/nav/etc/org.txt";
+$tabell = "org";
+@felt = ("orgid","forelder","descr","org2","org3","org4");
+&spesiell_endring_org($db,$fil,$tabell,join(":",@felt),join(":",@felt));
+#--------------TYPE------------
+$fil = "/usr/local/nav/etc/type.txt";
+$tabell = "type";
+@felt = ("typeid","typegruppe","sysObjectID","descr");
+&db_endring_med_sletting($db,$fil,$tabell,join(":",@felt));
+#--------------SLUTT-----------
+
+
+sub spesiell_endring_org {
+    my ($db,$fil,$tabell,$felt) = @_;
+    my @felt = split(/:/,$felt);
+    my %ny = &fil_hent($fil,scalar(@felt));
+    #leser fra database
+    my %gammel = &db_hent_hash($db,"SELECT ".join(",", @felt )." FROM $tabell ORDER BY $felt[0]");
+    for my $feltnull (keys %ny) {
+	unless($ny{$feltnull}[1]){
+	    &db_endring_per_linje($db,\@{$ny{$feltnull}},\@{$gammel{$feltnull}},\@felt,$tabell,$feltnull);
+	}
+    }
+    &db_endring($db,\%ny,\%gammel,\@felt,$tabell);
+
+    &db_sletting($db,\%ny,\%gammel,\@felt,$tabell);
+}
