1a1a72c3e956f1ed86be0c4f67c39b052237f21f
Vidar Faltinsen <vidar.faltinsen@uninett.no>
2002-03-21 10:36:03 +0000

    Lagt til noen rapportlenker

--- apache/shtdocs/res/ragen/reports.php
+++ apache/shtdocs/res/ragen/reports.php
@@ -11,24 +11,26 @@ navstart("Rapporter",$bruker);
 <h1>Rapporter</h1>
  
 <ul>
-<li> <a href="/ragen/?rapport=boks"> Utstyrsregister</a> (alt
+<li> <a href="/res/ragen/?rapport=boks"> Utstyrsregister</a> (alt
 utstyr)
   <ul>
-  <li> <a href="/ragen/?rapport=gw"> Kun rutere (GW)</a>
-  <li> <a href="/ragen/?rapport=sw"> Kun svitsjer (SW)</a>
-  <li> <a href="/ragen/?rapport=kant"> Kun kantutstyr (KANT)</a>
-  <li> <a href="/ragen/?rapport=srv"> Kun servere (SRV)</a>
-  <li> <a href="/ragen/?rapport=oppsop"> Resten</a>
+  <li> <a href="/res/ragen/?rapport=gw"> Kun rutere (GW)</a>
+  <li> <a href="/res/ragen/?rapport=sw"> Kun svitsjer (SW)</a>
+  <li> <a href="/res/ragen/?rapport=kant"> Kun kantutstyr (KANT)</a>
+  <li> <a href="/res/ragen/?rapport=srv"> Kun servere (SRV)</a>
+  <li> <a href="/res/ragen/?rapport=oppsop"> Resten</a>
+  <li> <a href="/res/ragen/?rapport=type"> Liste over utstyrstyper</a>
   </ul>
-<li> <a href="/ragen/?rapport=gwport"> Ruterporter</a> og
-     <a href="/ragen/?rapport=prefiks"> brukte prefiks</a>
-<li> <a href="/ragen/?rapport=swportv"> Svitsjeporter</a>, kun
-     <a href="/ragen/?rapport=swportt"> svitsjeporter som er trunk</a>
-<li> <a href="/ragen/?rapport=modules"> moduler</a>,
-     <a href="/ragen/?rapport=mem"> minne/flash</a>
-<li> <a href="/ragen/?rapport=rom"> Rom</a>
-<li> <a href="/ragen/?rapport=sted"> Sted</a>
-<li> <a href="/ragen/?rapport=org"> Organisasjon</a>
+<li> <a href="/res/ragen/?rapport=gwport"> Ruterporter</a> og
+     <a href="/res/ragen/?rapport=prefiks"> brukte prefiks</a>
+<li> <a href="/res/ragen/?rapport=swportv"> Svitsjeporter</a>, kun
+     <a href="/res/ragen/?rapport=swportt"> svitsjeporter som er trunk</a>
+<li> <a href="/res/ragen/?rapport=modules"> moduler</a>,
+     <a href="/res/ragen/?rapport=mem"> minne/flash</a>
+<li> <a href="/res/ragen/?rapport=rom"> Rom</a>
+<li> <a href="/res/ragen/?rapport=sted"> Sted</a>
+<li> <a href="/res/ragen/?rapport=org"> Organisasjon</a>
+<li> <a href="/res/ragen/?rapport=anv"> Anvendelsestyper for subnett</a>
  
  </ul>
 
