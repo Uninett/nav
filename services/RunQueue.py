53dbc297a6d2ea2e235b9f4d162aae4e9bf3b5e7
Magnus Nordseth <magnus@ntnu.no>
2002-06-04 21:13:04 +0000

    Arver threading.Thread i RunQueue

--- services/RunQueue.py
+++ services/RunQueue.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2.2
 """
 $Author: magnun $
-$Id: RunQueue.py,v 1.1 2002/06/04 09:26:44 magnun Exp $
+$Id: RunQueue.py,v 1.2 2002/06/04 21:13:46 magnun Exp $
 $Source: /usr/local/cvs/navbak/navme/services/Attic/RunQueue.py,v $
 """
 from threading import *
@@ -28,41 +28,62 @@ class observer:
 class TerminateException(Exception):
     pass
 
-def runStrategy(runnable, rq):
-    runnable.starttime=time.time()
-    rq.ob.observees.append(runnable)
-    print 'kaller run() på objektet'
-    runnable.run()
-    print 'ting er ferdig :)'
+#def runStrategy(runnable, rq):
+#    runnable.starttime=time.time()
+#    rq.ob.observees.append(runnable)
+#    print 'kaller run() på objektet i tråd '
+#    print traceback.print_exc()
+#    runnable.run()
 
-def simpleRunStrategy(runnable,rq):
-    runnable.run()
 
-def execute(runQueue,runStrategy):
-    while 1:
-        try:
-            r=runQueue.deq()
-            runStrategy(r,runQueue)
-        except TerminateException:
-            runQueue.numThreads-=1
-            return
-        except:
-            traceback.print_exc()
+#def simpleRunStrategy(runnable,rq):
+#    runnable.run()
+
+#def execute(runQueue,runStrategy):
+#    while 1:
+#        try:
+#            r=runQueue.deq()
+#            runStrategy(r,runQueue)
+#        except TerminateException:
+#            runQueue.numThreads-=1
+#            return
+#        except:
+#            traceback.print_exc()
+
+class worker(threading.Thread):
+    def __init__(self, rq):
+        threading.Thread.__init__(self)
+        self.runQueue=rq
+
+    def run(self):
+        while 1:
+            try:
+                self.job=self.runQueue.deq()
+                self.execute()
+            except TerminateException:
+                runQueue.numThreads-=1
+                return
+            except:
+                traceback.print_exc()
+
+    def execute(self):
+        print "Skal kjøre jobb nå..."
+        self.job.run()
+
 
 class RunQueue:
     def __init__(self,**kwargs):
-        self.runStrategy=kwargs.get('runstrategy', runStrategy)
         self.maxThreads=kwargs.get('maxthreads', sys.maxint)
         self.numThreads=0
         self.numThreadsWaiting=0
         self.rq=kwargs.get('queue')
+        self.workers=[]
         if self.rq is None:
             self.rq=DEQueue.DEQueue()
             self.lock=RLock()
             self.awaitWork=Condition(self.lock)
             self.stop=0
             self.makeDaemon=1
-            self.threadfunction=kwargs.get('threadfunction', execute)
             self.startObserver()
 
     def startObserver(self):
@@ -73,19 +94,31 @@ class RunQueue:
     def enq(self,*r):
         self.lock.acquire()
         self.rq.put(*r)
+        print 'Elementer i køen: '+ str(len(self.rq))
         if self.numThreadsWaiting>0:
             self.numThreadsWaiting-=1
+            print 'Har ventende tråd. Kaller notify()'
             self.awaitWork.notify()
         elif self.numThreads < self.maxThreads:
-            t=Thread(target=self.threadfunction, args=(self,self.runStrategy))
+            t=worker(self)
+            print 'lager nytt trådobjekt'
             t.setDaemon(self.makeDaemon)
             self.numThreads+=1
+            if self.numThreads==1:
+                t.setName('observer')
+            else:
+                t.setName('worker'+str(self.numThreads))
+            self.workers.append(t)
+
+            print "numThreads: "+ str(self.numThreads)
+            print "len(workers): "+ str(len(self.workers))
             t.start()
         self.lock.release()
 
     def deq(self):
         self.lock.acquire()
         while len(self.rq)==0:
+            print 'Jobbkøen er tom. Venter...'
             if self.stop:
                 self.numThreads-=1 
                 self.lock.release()
