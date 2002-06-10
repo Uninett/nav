80d7c4da0cc0b9f24033f170ad37f35ba0b739d7
Sigurd Gartmann <sigurd@brogar.org>
2002-06-10 09:37:04 +0000

    N� med logging.

--- cron/fyll_db.pl
+++ cron/fyll_db.pl
@@ -2,53 +2,58 @@
 use strict;
 
 require "/usr/local/nav/navme/etc/conf/path.pl";
+my $localkilde = &localkilde();
+require "$lib/fil.pl";
+&log_open;
 
 my @tid = localtime(time);
 
-#kommer på mail nå.
-#pen(STDERR,'>>','/usr/local/nav/local/log/collect/sterr-'.($tid[5]+1900).'-'.($tid[4]+1).'-'.$tid[3].'-'.$tid[2].'_'.$tid[1].'.log');
-#pen(STDOUT,'>>','/usr/local/nav/local/log/collect/stout-'.($tid[5]+1900).'-'.($tid[4]+1).'-'.$tid[3].'-'.$tid[2].'_'.$tid[1].'.log');
-
-=cut
-open(DBERR,'>>','/usr/local/nav/local/log/collect/collecterror-'.($tid[5]+1900).'-'.($tid[4]+1).'-'.$tid[3].'-'.$tid[2].'_'.$tid[1].'.log');
-open(DBOUT,'>>','/usr/local/nav/local/log/collect/collectupdate-'.($tid[5]+1900).'-'.($tid[4]+1).'-'.$tid[3].'-'.$tid[2].'_'.$tid[1].'.log');
-
-open(SNERR,'>>','/usr/local/nav/local/log/collect/snmperror-'.($tid[5]+1900).'-'.($tid[4]+1).'-'.$tid[3].'-'.$tid[2].'_'.$tid[1].'.log');
-open(SNOUT,'>>','/usr/local/nav/local/log/collect/sysnameerror-'.($tid[5]+1900).'-'.($tid[4]+1).'-'.$tid[3].'-'.$tid[2].'_'.$tid[1].'.log');
-
-open(GWERR,'>>','/usr/local/nav/local/log/collect/gwporterror-'.($tid[5]+1900).'-'.($tid[4]+1).'-'.$tid[3].'-'.$tid[2].'_'.$tid[1].'.log');
-open(GWOUT,'>>','/usr/local/nav/local/log/collect/gwport-'.($tid[5]+1900).'-'.($tid[4]+1).'-'.$tid[3].'-'.$tid[2].'_'.$tid[1].'.log');
-
-open(SWERR,'>>','/usr/local/nav/local/log/collect/swporterror-'.($tid[5]+1900).'-'.($tid[4]+1).'-'.$tid[3].'-'.$tid[2].'_'.$tid[1].'.log');
-open(SWOUT,'>>','/usr/local/nav/local/log/collect/swport-'.($tid[5]+1900).'-'.($tid[4]+1).'-'.$tid[3].'-'.$tid[2].'_'.$tid[1].'.log');
-=cut
-
 my $collect = &collect();
 
-my $tid = localtime(time); 
-print $tid.": START. Starter først oppdateringer fra tekstfilene.\n";
+&skriv("RUN-START","program=tekstfiler");
+#my $tid = localtime(time); 
+#print $tid.": START. Starter først oppdateringer fra tekstfilene.\n";
 
 system    "$collect/tekstfiler.pl";
-$tid = localtime(time); 
-print $tid.": ferdig med tekstfilene, starter oppdatering av boksene.\n";
+&skriv("RUN-END","program=tekstfiler");
+
+&skriv("RUN-START","program=bokser");
+#$tid = localtime(time); 
+#print $tid.": ferdig med tekstfilene, starter oppdatering av boksene.\n";
 
 system    "$collect/bokser.pl";
-$tid = localtime(time); 
-print $tid.": ferdig med boksene, starter oppdatering av prefiks og gwporter.\n";
+&skriv("RUN-END","program=bokser");
+
+&skriv("RUN-START","program=gwporter");
+#$tid = localtime(time); 
+#print $tid.": ferdig med boksene, starter oppdatering av prefiks og gwporter.\n";
 
 system    "$collect/gwporter.pl";
-$tid = localtime(time); 
-print $tid.": ferdig med gwporter, starter oppdatering av swporter og hjelpetabeller.\n";
+&skriv("RUN-END","program=gwporter");
+
+&skriv("RUN-START","program=swporter");
+#$tid = localtime(time); 
+#print $tid.": ferdig med gwporter, starter oppdatering av swporter og hjelpetabeller.\n";
 
 system    "$collect/swporter.pl";
-$tid = localtime(time); 
-print $tid.": ferdig med swporter.pl, starter get_boksdata.\n";
+&skriv("RUN-END","program=swporter");
+
+&skriv("RUN-START","program=get_boksdata");
+#$tid = localtime(time); 
+#print $tid.": ferdig med swporter.pl, starter get_boksdata.\n";
 
 system    "$collect/get_boksdata.pl";
-$tid = localtime(time); 
-print $tid.": ferdig med get_boksdata.pl, starter sletting fra trapdetectunntakene.\n";
+&skriv("RUN-END","program=get_boksdata");
+
+&skriv("RUN-START","program=slett_fra_trapdetect_unntak");
+#$tid = localtime(time); 
+#print $tid.": ferdig med get_boksdata.pl, starter sletting fra trapdetectunntakene.\n";
 
 system "$collect/slett_fra_trapdet_unntak.pl";
-$tid = localtime(time); 
-print $tid.": ferdig med slett_fra_trapdet_unntak.pl, og dermed helt FERDIG!\n";
+&skriv("RUN-END","program=slett_fra_trapdetect_unntak");
 
+
+#$tid = localtime(time); 
+#print $tid.": ferdig med slett_fra_trapdet_unntak.pl, og dermed helt FERDIG!\n";
+
+&log_close;
