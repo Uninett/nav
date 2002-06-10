b87bfd5881ae654dcc701b87d0f685f1eda71eb8
Magnus Nordseth <magnus@ntnu.no>
2002-06-10 13:26:03 +0000

    *** empty log message ***

--- services/controller.py
+++ services/controller.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 """
 $Author: magnun $
-$Id: controller.py,v 1.3 2002/06/10 13:26:17 magnun Exp $
+$Id: controller.py,v 1.4 2002/06/10 13:26:37 magnun Exp $
 $Source: /usr/local/cvs/navbak/navme/services/controller.py,v $
 
 """
 
 import RunQueue, types, os, time, job, getopt
 
-__version__ = """$Version$"""
+__version__ = """$Version: $"""
 
 class controller:
     def __init__(self, **kwargs):
