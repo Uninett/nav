148f725fd97afbb17d054af71d496014cbc99a24
Erik Gorset <erikgors@stud.ntnu.no>
2002-06-13 13:04:00 +0000

    f�rste test for � se om det funker

--- services/lib/database.py
+++ services/lib/database.py
@@ -2,7 +2,7 @@
 database
 
 $Author: erikgors $
-$Id: database.py,v 1.1 2002/06/13 09:57:58 erikgors Exp $
+$Id: database.py,v 1.2 2002/06/13 13:04:01 erikgors Exp $
 $Source: /usr/local/cvs/navbak/navme/services/lib/Attic/database.py,v $
 """
 from psycopg import connect
@@ -18,3 +18,12 @@ def newEvent(event):
 	print 'new event:',event.id,event.status,event.info
 def newVersion(id,version):
 	print 'new version:',id,version
+def getJobs():
+	query = 'SELECT serviceid, handler, version, ip FROM service NATURAL JOIN boks order by serviceid'
+	c = db.cursor()
+	c.execute(query)
+	for i in c.getall():
+		print i
+if __name__ == '__main__':
+	startup('host = localhost user = jee dbname = weather password = oops')
+	getJobs()
