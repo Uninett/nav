4a0b0c868c50033fa37e0c65c051b4c83d4709f9
Kristian Eide <kreide@gmail.com>
2002-06-13 20:24:05 +0000

    - Add SimpleSnmp

---
+++ src/SimpleSnmp/src/no/ntnu/nav/SimpleSnmp/SimpleSnmp.java
@@ -0,0 +1,191 @@
+package no.ntnu.nav.SimpleSnmp;
+
+import java.io.*;
+import java.util.*;
+
+import uk.co.westhawk.snmp.stack.*;
+import uk.co.westhawk.snmp.pdu.*;
+
+
+public class SimpleSnmp
+{
+	private final int TIMEOUT_LIMIT = 4;
+
+	private String host = "127.0.0.1";
+	private String cs_ro = "community";
+	private String baseOid = "1.3";
+	private SnmpContext context = null;
+	private int timeoutCnt = 0;
+	private boolean gotTimeout = false;
+	private int getCnt;
+
+	public SimpleSnmp() { }
+
+	public SimpleSnmp(String host, String cs_ro, String baseOid)
+	{
+		setParams(host, cs_ro, baseOid);
+	}
+
+	public void setHost(String host) { this.host = host; }
+	public void setCs_ro(String cs_ro) { this.cs_ro = cs_ro; }
+	public void setBaseOid(String baseOid) { this.baseOid = baseOid; }
+	public void setParams(String host, String cs_ro, String baseOid)
+	{
+		setHost(host);
+		setCs_ro(cs_ro);
+		setBaseOid(baseOid);
+	}
+
+	/*
+	public boolean resetGotTimeout() {
+		boolean b = gotTimeout;
+		gotTimeout = false;
+		return b;
+	}
+	*/
+
+	/**
+	 * Snmpwalk the given OID and return the entire subtree
+	 *
+	 * @param cnt The maximum number of OIDs to get; 0 or less means get as much as possible
+	 * @param decodeHex try to decode returned hex to ASCII
+	 * @return an ArrayList containing String arrays of two elements; OID and value
+	 * @throws TimeoutException if the hosts times out
+	 */
+	public ArrayList getNext(int cnt, boolean decodeHex) throws TimeoutException
+	{
+		getCnt = (cnt < 0) ? 0 : cnt;
+		return getAll(decodeHex);
+	}
+
+	/**
+	 * Snmpwalk the given OID and return the entire subtree
+	 *
+	 * @return an ArrayList containing String arrays of two elements; OID and value
+	 * @throws TimeoutException if the hosts times out
+	 */
+	public ArrayList getAll() throws TimeoutException
+	{
+		return getAll(false);
+	}
+
+	/**
+	 * Snmpwalk the given OID and return the entire subtree
+	 *
+	 * @param decodeHex try to decode returned hex to ASCII
+	 * @return an ArrayList containing String arrays of two elements; OID and value
+	 * @throws TimeoutException if the hosts times out
+	 */
+	public ArrayList getAll(boolean decodeHex) throws TimeoutException
+	{
+		ArrayList l = new ArrayList();
+		if (baseOid.charAt(0) == '.') baseOid = baseOid.substring(1, baseOid.length());
+
+		try {
+			if (context == null || !context.getHost().equals(host)) {
+				if (context != null) context.destroy();
+				//outl("Switched context, host: " + host);
+				context = new SnmpContext(host, 161);
+				context.setCommunity(cs_ro);
+				timeoutCnt = 0;
+			} else if (!context.getCommunity().equals(cs_ro)) {
+				//outl("Community changed: " + cs_ro);
+				context.setCommunity(cs_ro);
+			}
+
+			//SnmpContext context = new SnmpContext(host, 161);
+			BlockPdu pdu = new BlockPdu(context);
+
+			pdu.setPduType(BlockPdu.GETNEXT);
+			pdu.addOid(baseOid);
+
+			// Should we get the entire subtree?
+			boolean getAll = getCnt == 0;
+
+			while (true) {
+				try {
+					varbind vb;
+					while ( (vb=pdu.getResponseVariableBinding()) != null) {
+						String oid = vb.getOid().getValue();
+						if (!oid.startsWith(baseOid) || (!getAll && getCnt-- == 0)) break;
+
+						// Reset timeoutCnt
+						timeoutCnt = 0;
+
+						// Behandle svaret vi har fått
+						String data;
+						{
+							AsnObject o = vb.getValue();
+							if (o instanceof AsnOctets) {
+								AsnOctets ao = (AsnOctets)o;
+								data = (decodeHex ? ao.toDisplayString() : ao.toHex());
+
+								//outl("OID: " + oid + " S: " + data + " HEX: " + ao.toHex() );
+							} else {
+								 data = o.toString();
+							}
+						}
+						//outl("OID: " + oid + " = " + data );
+
+						String[] s = {
+							oid.substring(baseOid.length()+1, oid.length()).trim(),
+							//String.valueOf(vb.getValue()).trim()
+							data.trim()
+						};
+						l.add(s);
+
+						pdu = new BlockPdu(context);
+						pdu.setPduType(BlockPdu.GETNEXT);
+						pdu.addOid(oid);
+					}
+
+				} catch (PduException e) {
+					String m = e.getMessage();
+					if (m.equals("Timed out")) {
+						gotTimeout = true;
+						timeoutCnt++;
+						if (timeoutCnt >= TIMEOUT_LIMIT) {
+							throw new TimeoutException("Too many timeouts, giving up");
+						} else {
+							// Re-try operation
+							continue;
+						}
+					} else {
+						outl("  *ERROR*: Host: " + host + " PduExecption: " + e.getMessage() );
+					}
+				}
+
+				// Ferdig å hente meldinger
+				break;
+
+			} // while
+		} catch (IOException e) {
+			outl("  *ERROR*: Host: " + host + " IOException: " + e.getMessage() );
+		}
+		return l;
+	}
+
+	/*
+	public void destroy()
+	{
+		if (context != null) {
+			outl("Context distroyed..");
+			context.destroy();
+			context = null;
+		}
+	}
+	*/
+
+	public void finalize()
+	{
+		if (context != null) {
+			outl("Context distroyed by finalize.");
+			context.destroy();
+			context = null;
+		}
+	}
+
+	private static void out(String s) { System.out.print(s); }
+	private static void outl(String s) { System.out.println(s); }
+}
+
---
+++ src/SimpleSnmp/src/no/ntnu/nav/SimpleSnmp/TimeoutException.java
@@ -0,0 +1,9 @@
+package no.ntnu.nav.SimpleSnmp;
+
+public class TimeoutException extends RuntimeException
+{
+	public TimeoutException(String s)
+	{
+		super(s);
+	}
+}
