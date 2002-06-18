ce21180d7511e024b81c3dad8a68ab11aa420045
Kristian Eide <kreide@gmail.com>
2002-06-18 20:17:02 +0000

    - canHandleDevice now returns int.

--- src/getDeviceData-PluginAPI/src/no/ntnu/nav/getDeviceData/plugins/DeviceHandler.java
+++ src/getDeviceData-PluginAPI/src/no/ntnu/nav/getDeviceData/plugins/DeviceHandler.java
@@ -4,7 +4,18 @@ import no.ntnu.nav.SimpleSnmp.*;
 
 public interface DeviceHandler
 {
-	public boolean canHandleDevice(BoksData bd);
+	/**
+	 * This method should check if the implementing plugin can handle the device specified
+	 * by the BoksData argument, and return a value greater than zero if it does.
+	 *
+	 * All plugins will be asked if they can handle a given device, and the one that returns
+	 * the highest value greater than zero will be picked.
+	 *
+	 * @argument bd The data describing the device
+	 * @return a value greater than zero (0) if the plugin can handle the given device
+	 */
+	public int canHandleDevice(BoksData bd);
+
 	public void handle(BoksData bd, SimpleSnmp sSnmp, DeviceDataList ddList) throws TimeoutException;
 
 }
