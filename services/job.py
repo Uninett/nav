5017177bc5ea90b52d9d9e33d02bb0939c0596a5
Magnus Nordseth <magnus@ntnu.no>
2002-06-20 23:59:03 +0000

    Da skal PopHandler fungere riktig.

--- services/job.py
+++ services/job.py
@@ -3,7 +3,7 @@
 Overvåkeren
 
 $Author: magnun $
-$Id: job.py,v 1.31 2002/06/20 18:46:31 magnun Exp $
+$Id: job.py,v 1.32 2002/06/20 23:59:33 magnun Exp $
 $Source: /usr/local/cvs/navbak/navme/services/Attic/job.py,v $
 """
 import time,socket,sys,types
@@ -369,24 +369,27 @@ class PopConnection(poplib.POP3):
 		#self.welcome=self._getresp()
 
 class PopHandler(JobHandler):
+	"""
+	args:
+	username
+	password
+	port
+	"""
 	def __init__(self, serviceid, boksid, ip, args, version):
 		port = args.get("port", 110)
 		JobHandler.__init__(self, "pop3", serviceid, boksid, (ip, port), args, version)
 
 	def execute(self):
-		"""
-		hihi
-		"""
 		args = self.getArgs()
 		user = args.get("username","")
 		passwd = args.get("password", "")
-		print self.getAddress()
 		ip, port = self.getAddress()
 		p = PopConnection(self.getTimeout(), ip, port)
 		p.user(user)
 		p.pass_(passwd)
 		nummessages = len(p.list()[1])
 		p.quit()
+		return Event.UP, "Ok"
 
 
 
