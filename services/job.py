6dc26dedf742f3dca3283f6dcb4afa104b651689
Magnus Nordseth <magnus@ntnu.no>
2002-06-20 16:23:01 +0000

    *** empty log message ***

--- services/job.py
+++ services/job.py
@@ -3,7 +3,7 @@
 Overvåkeren
 
 $Author: magnun $
-$Id: job.py,v 1.29 2002/06/20 14:41:29 magnun Exp $
+$Id: job.py,v 1.30 2002/06/20 16:23:17 magnun Exp $
 $Source: /usr/local/cvs/navbak/navme/services/Attic/job.py,v $
 """
 import time,socket,sys,types
@@ -354,7 +354,38 @@ class ImapHandler(JobHandler):
 		m.login(user, passwd)
 		m.logout()
 		return Event.UP, "Ok"
-		
+
+
+import poplib
+class PopConnection(poplib.POP3):
+	def __init__(self, timeout, ip, port):
+		self.ip=ip
+		self.port=port
+
+		self.sock=Socket(timeout)
+		self.sock.connect((self.ip, self.port))
+		self.file=self.sock.makefile('rb')
+		self._debugging=0
+		#self.welcome=self._getresp()
+
+class PopHandler(JobHandler):
+	def __init__(self, serviceid, boksid, ip, args, version):
+		port = args.get("port", 110)
+		JobHandler.__init__(self, "pop3", serviceid, boksid, (ip, port), args, version)
+
+	def execute(self):
+		args = self.getArgs()
+		user = args.get("username","")
+		passwd = args.get("password", "")
+		print self.getAddress()
+		ip, port = self.getAddress()
+		p = PopConnection(self.getTimeout(), ip, port)
+		p.user(user)
+		p.pass_(passwd)
+		nummessages = len(p.list()[1])
+		p.quit()
+
+
 
 class SmbHandler(JobHandler):
 	"""
