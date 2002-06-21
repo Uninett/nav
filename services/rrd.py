17f180beba816002a66ee43babef2ab6073f823c
Erik Gorset <erikgors@stud.ntnu.no>
2002-06-21 09:25:00 +0000

    rengj�re koden + legge til noen kommentarer

--- services/rrd.py
+++ services/rrd.py
@@ -1,7 +1,7 @@
 """
 create and update rrd-objects
 
-$Id: rrd.py,v 1.4 2002/06/19 10:37:19 erikgors Exp $
+$Id: rrd.py,v 1.5 2002/06/21 09:25:05 erikgors Exp $
 $Source: /usr/local/cvs/navbak/navme/services/Attic/rrd.py,v $
 """
 import os
@@ -14,12 +14,11 @@ def create(serviceid):
 		os.mkdir(RRDDIR)
 	rrd.create(('%s/%i.rrd' % (RRDDIR,serviceid),'-s 300','DS:STATUS:GAUGE:600:0:1','DS:REPONSETIME:GAUGE:600:0:300','RRA:AVERAGE:0.5:1:288','RRA:AVERAGE:0.5:6:336','RRA:AVERAGE:0.5:12:720','RRA:MAX:0.5:12:720','RRA:AVERAGE:0.5:288:365','RRA:MAX:0.5:288:365','RRA:AVERAGE:0.5:288:1095','RRA:MAX:0.5:288:1095'))
 def update(serviceid,time,status,responsetime):
-	if status == Event.UP:
-		status = 0
-		responsetime = str(responsetime)
-	else:
-		status = 1
-		responsetime = ''
-	if not os.path.exists('%s/%i.rrd' % (RRDDIR,serviceid)):
-		create(serviceid)
-	rrd.update(('%s/%i.rrd' % (RRDDIR,serviceid),'%s:%i:%s' % (str(time), status, responsetime)))
+	"""
+	time: 'N' or time.time()
+	status: 'UP' or 'DOWN' (from Event.status)
+	responsetime: 0-300 or '' (undef)
+	"""
+	filename = '%s/%i.rrd' % (RRDDIR,serviceid)
+	os.path.exists(filename) or create(serviceid)
+	rrd.update((filename,'%s:%i:%s' % (time, status == Event.UP and 100, responsetime)))
