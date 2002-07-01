482e83a4083c1bd21134c89dfbfc7f1808c2ee5d
cricket <devnull@localhost>
2002-07-01 13:59:02 +0000

    la til disksjekking fra config fil

--- src/getDeviceData/plugins/handlerServer/src/HandlerServer.py
+++ src/getDeviceData/plugins/handlerServer/src/HandlerServer.py
@@ -6,15 +6,20 @@ http://www.nav.ntnu.no/
 (c) Stian Søiland <stain@itea.ntnu.no> 2002
 """
 
-__version__ = "$Id: HandlerServer.py,v 1.3 2002/06/19 13:12:13 stain Exp $"
+__version__ = "$Id: HandlerServer.py,v 1.4 2002/07/01 13:59:21 cricket Exp $"
 
 import UserDict
 import re
+from array import array
+import string
 
 import no.ntnu.nav.Database.Database as db 
 import no.ntnu.nav.SimpleSnmp as snmp
 from no.ntnu.nav.getDeviceData.plugins import DeviceHandler
 from no.ntnu.nav.getDeviceData.plugins import DeviceData
+
+#temp code
+file = "/usr/local/nav/local/etc/conf/diskException.conf"
 
 class OID:
   """Constants for typical used OIDs"""
@@ -23,6 +28,7 @@ class OID:
   interfaceTypes = "1.3.6.1.2.1.2.2.1.3" 
   interfaceLoopback = "24" # really an Integer, not an OID =)
   diskDescriptions = "1.3.6.1.2.1.25.2.3.1.3" 
+  diskBlockSize ="1.3.6.1.2.1.25.2.3.1.4"
   diskFilesystems = "1.3.6.1.2.1.25.3.8.1.4"
   filesystemNFS = "1.3.6.1.2.1.25.3.9.14"
   filesystemUnknown = "1.3.6.1.2.1.25.3.9.2"
@@ -36,6 +42,35 @@ class Unit:
   def __repr__(self):
     info = (self.unitID, repr(self.type), repr(self.description))
     return "<unit id=%s type=%s description=%s>" % info
+
+class Checking:
+  """ Checking wh1.3.6.1.2.1.25.2.3.1.4 and if it should 
+      be recursive or not """
+  def rec(self , line , splitter):
+    li2 = string.expandtabs(line,1)
+    #print li2
+    li = (string.split(li2,splitter))
+    #print li
+    j = 1
+    for i in li[1:]:
+      j = j + 1
+      i = string.strip(i)
+      if (i == ''):
+        continue
+      else:
+        #print 'fant noe'
+        partition = i
+        break
+    moveon = None
+    print j
+    try:
+      for i in li[j:]:
+        i = string.strip(i)
+        if (i == 'recursive'):
+          moveon = 'recursive'
+      return (partition,moveon)
+    except:
+      return (partition,moveon)
 
 class Units(UserDict.UserDict):
   """And a nice dictionary creating those unit for us on-the-fly"""
@@ -97,11 +132,48 @@ class HandlerServer(DeviceHandler):
 
 
     self.getSnmpAgent(box, snmp, deviceData)
-    self.getDisks(box, snmp, deviceData)
+    check_array = self.getDiskconf(box)
+    keep_on = 1
+    for i in check_array:
+      if i[0] == '*':
+        keep_on = 0
+        break
+    if keep_on:
+      self.getDisks(box, snmp, deviceData, check_array)
     self.getInterfaces(box, snmp, deviceData)
 
     # We're done! Submit to the old large database
     deviceDataList.setDeviceData(deviceData)
+
+  def getDiskconf(self,box):
+    """ We open the config file and insert all the disks we don't want
+        to monitor in an array """
+
+    s = Checking()
+    sep = ' '
+    arr = []
+    conf_file = open(file ,'r')
+    lines = conf_file.readlines()
+    for line in lines:
+      line2 = string.expandtabs(line,1)
+      #line = string.strip(line)
+      #her må den fjerne whitespaces på begge sider
+      if ((line[0] == '#') or (line == " ") or (line == "\n") or (line=="\t")):
+        continue
+      elif string.find(line,'"') != -1:
+        if ((line[0] == '*') or (string.split(line2,' ')[0]==box)):
+          on = s.rec(line,'"')
+          arr.append((on[0],on[1]))
+          continue
+      elif (line[0] == '*'):
+        on = s.rec(line,sep)
+        arr.append((on[0],on[1]))
+        continue
+      elif (string.split(line2,' ')[0]==box):
+        on = s.rec(line,sep)
+        arr.append((on[0],on[1]))
+        continue
+
 
   def getSnmpAgent(self, box, snmp, deviceData):
     """Retrieve SNMP agent version and store it in the database directly
@@ -127,7 +199,7 @@ class HandlerServer(DeviceHandler):
       db.update(sql)
 
 
-  def getDisks(self, box, snmp, deviceData):
+  def getDisks(self, box, snmp, deviceData, check_array):
     """Retrieve disk data and prepare to store in deviceData.
     
        box -- getDeviceData.plugins.BoksData instance of the box
@@ -182,9 +254,14 @@ class HandlerServer(DeviceHandler):
         del disks[disk.unitID]
       
       # /dev and /proc and their childs are not interresting
-      if(re.match(r"^/(dev|proc)", disk.description)):
-        del disks[disk.unitID]
-
+      # Here we will sort out the partitions we don't want to monitor
+      for i in check_array:
+        if i[1] == 'recursive':
+          if(re.match(r"^"+i[0], disk.description)):
+            del disks[disk.unitID]
+        else:
+          if(i[0] == disk.description):
+            del disks[disk.unitID]
 
     # Insert into database
     for disk in disks.values():
