98a685381a5ce8a5c4e9536e1a954289e16d7689
Erik Gorset <erikgors@stud.ntnu.no>
2002-07-02 10:38:02 +0000

    rpchandler, kan sjekke om programmer er registert i mapperen med argumentet "requried"

---
+++ services/lib/handler/RpcHandler.py
@@ -0,0 +1,43 @@
+"""
+$Id: RpcHandler.py,v 1.1 2002/07/02 10:38:20 erikgors Exp $
+$Source: /usr/local/cvs/navbak/navme/services/lib/handler/RpcHandler.py,v $
+"""
+import os
+from job import JobHandler, Event
+class RpcHandler(JobHandler):
+	"""
+	args:
+	requried
+	ex: nfs,nlockmgr
+	"""
+	def __init__(self, serviceid, boksid, ip, args, version):
+		port = args.get("port", 111)
+		JobHandler.__init__(self, "rpc", serviceid, boksid, (ip, port), args, version)
+
+	def execute(self):
+		args = self.getArgs()
+		required = args.get('required','').split(',')
+
+		ip, port = self.getAddress()
+
+		output = os.popen('./Timeout.py -t %s rpcinfo -p %s' % (self.getTimeout(),ip)).read()
+		if not output:
+			return Event.DOWN,'timeout'
+
+		missing = []
+		for i in required:
+			i = i.strip()
+			if output.find(i) == -1:
+				missing += [i]
+		if missing:
+			return Event.DOWN,'missing: ' + ', '.join(missing)
+		else:
+			return Event.UP, "Ok"
+
+def getRequiredArgs():
+	"""
+	Returns a list of required arguments
+	"""
+	requiredArgs = ['required']
+	return requiredArgs
+			
