4e8e78c83f3ffd18ab56c1468268da5dffe76d80
Kristian Eide <kreide@gmail.com>
2002-07-04 21:49:02 +0000

    - JavaDoc

--- src/getDeviceData-PluginAPI/src/no/ntnu/nav/getDeviceData/plugins/DeviceHandler.java
+++ src/getDeviceData-PluginAPI/src/no/ntnu/nav/getDeviceData/plugins/DeviceHandler.java
@@ -7,16 +7,33 @@ public interface DeviceHandler
 {
 	/**
 	 * This method should check if the implementing plugin can handle the device specified
-	 * by the BoksData argument, and return a value greater than zero if it does.
+	 * by the BoksData argument, and return a value different from zero if it does.
 	 *
-	 * All plugins will be asked if they can handle a given device, and the one that returns
-	 * the highest value greater than zero will be picked.
+	 * If the value returned is greater than zero, no plugins with a lower absolute value
+	 * will be asked to handle the device. The plugins will then be asked to handle the
+	 * device in order from highest to lowest value, ignoring sign.
+	 *
+	 * If two (or more) plugins return the same positive value, only one of them will be
+	 * asked to handle the device, but it is not defined which one. If two (or more)
+	 * plugins return the same absolute value, but with different sign, the plugins who
+	 * returned negative values will not be asked.
+	 *
+	 * For example, if there are five plugins, returning values -4, 1, -3, 4, and -6, only
+	 * the plugins returning -6 and 4 will be asked (in that order).
 	 *
 	 * @param bd The data describing the device
-	 * @return a value greater than zero (0) if the plugin can handle the given device
+	 * @return a value different from zero (0) if the plugin can handle the given device
 	 */
 	public int canHandleDevice(BoksData bd);
 
+	/**
+	 * Actually handle the device specified by the BoksData argument.
+	 *
+	 * @param bd The data describing the device
+	 * @param sSnmp An instance of SimpleSnmp the plugins can use to do SNMP queries
+	 * @param cp A ConfigParser instance reading data from nav.conf
+	 * @param ddList The object used to return the data collected
+	 */
 	public void handle(BoksData bd, SimpleSnmp sSnmp, ConfigParser cp, DeviceDataList ddList) throws TimeoutException;
 
 }
