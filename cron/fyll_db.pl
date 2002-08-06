354c2a7fe9c2b0b0e9fb3bc6bc47399dc0248746
Sigurd Gartmann <sigurd@brogar.org>
2002-08-06 08:01:03 +0000

    med fill�sing, s� det ikke krasjer.

--- cron/fyll_db.pl
+++ cron/fyll_db.pl
@@ -3,57 +3,79 @@ use strict;
 
 require "/usr/local/nav/navme/etc/conf/path.pl";
 my $localkilde = &localkilde();
+#print "$lib\n";
+my $lib = &lib;
 require "$lib/fil.pl";
-&log_open;
+
 
 my @tid = localtime(time);
 
 my $collect = &collect();
 
+&log_open;
 &skriv("RUN-START","program=tekstfiler");
+&log_close;
 #my $tid = localtime(time); 
 #print $tid.": START. Starter først oppdateringer fra tekstfilene.\n";
 
 system    "$collect/tekstfiler.pl";
+
+&log_open;
 &skriv("RUN-END","program=tekstfiler");
 
 &skriv("RUN-START","program=bokser");
+&log_close;
 #$tid = localtime(time); 
 #print $tid.": ferdig med tekstfilene, starter oppdatering av boksene.\n";
 
 system    "$collect/bokser.pl";
+
+&log_open;
 &skriv("RUN-END","program=bokser");
 
 &skriv("RUN-START","program=gwporter");
+&log_close;
 #$tid = localtime(time); 
 #print $tid.": ferdig med boksene, starter oppdatering av prefiks og gwporter.\n";
 
 system    "$collect/gwporter.pl";
+
+&log_open;
 &skriv("RUN-END","program=gwporter");
 
 &skriv("RUN-START","program=swporter");
+&log_close;
 #$tid = localtime(time); 
 #print $tid.": ferdig med gwporter, starter oppdatering av swporter og hjelpetabeller.\n";
 
 system    "$collect/swporter.pl";
+
+&log_open;
 &skriv("RUN-END","program=swporter");
 
 &skriv("RUN-START","program=get_boksdata");
+&log_close;
 #$tid = localtime(time); 
 #print $tid.": ferdig med swporter.pl, starter get_boksdata.\n";
 
 system    "$collect/get_boksdata.pl";
+
+&log_open;
 &skriv("RUN-END","program=get_boksdata");
 
 &skriv("RUN-START","program=slett_fra_trapdetect_unntak");
+&log_close;
 #$tid = localtime(time); 
 #print $tid.": ferdig med get_boksdata.pl, starter sletting fra trapdetectunntakene.\n";
 
 system "$collect/slett_fra_trapdet_unntak.pl";
+
+&log_open;
 &skriv("RUN-END","program=slett_fra_trapdetect_unntak");
+&log_close;
 
 
 #$tid = localtime(time); 
 #print $tid.": ferdig med slett_fra_trapdet_unntak.pl, og dermed helt FERDIG!\n";
 
-&log_close;
+
