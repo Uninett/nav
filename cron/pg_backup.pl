cc0bb87d188553c9997c792bbdae8f2ddcf05274
Knut-Helge Vindheim <knut-helge.vindheim@ntnu.no>
2002-10-01 08:49:02 +0000

    Bytta i fra %e (et siffer med leading space) til %d som er to siffer. .

--- cron/pg_backup.pl
+++ cron/pg_backup.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# $Id: pg_backup.pl,v 1.7 2002/07/24 12:34:00 mortenv Exp $
+# $Id: pg_backup.pl,v 1.8 2002/10/01 08:49:21 knutvi Exp $
 ##############################################################
 # Et script som tar full backup av databasen hver
 #  * Dag	Roterer pr uke
@@ -17,7 +17,7 @@ delete @ENV{'IFS', 'CDPATH', 'ENV', 'BASH_ENV'};
 
 my ($res, $filnavn);
 
-my $now_string = strftime "%w:%e:%A:%B:%V", localtime;
+my $now_string = strftime "%w:%d:%A:%B:%V", localtime;
 my ($dayofweek, $dayofmonth, $weekday, $month, $weeknumber) = split(/\:/, $now_string);
 
 my $conf = '/usr/local/nav/local/etc/conf/pgpasswd.conf';
@@ -32,7 +32,7 @@ my $logfil = $config{'logfil'};
 
 
 # Sjekker om det er den første i måneden
-if ($dayofmonth eq '1')
+if ($dayofmonth eq '01')
 {
 	$filnavn = "fullbackup_postgres_01\.$month";
 }
@@ -52,24 +52,24 @@ open(LOGFIL,">>$logfil") || die "Kan ikke åpne loggfilen ($logfil): $!";
 
 
 # Kjøre backup
-$now_string = strftime "%a %e %b %Y %T", localtime;
+$now_string = strftime "%a %d %b %Y %T", localtime;
 print LOGFIL "$now_string\tBackup: pg_dumpall > $sti$filnavn\n";
 $res = `PGPASSWORD=$passord PGUSER=$brukernavn pg_dumpall > $sti$filnavn`;
 if ($res ne '') { print LOGFIL "$res\n\n"; }
 
 # Pakker ned filen
-$now_string = strftime "%a %e %b %Y %T", localtime;
+$now_string = strftime "%a %d %b %Y %T", localtime;
 print LOGFIL "$now_string\tbzip2 $sti$filnavn\n";
 $res = `nice bzip2 -f $sti$filnavn`;
 if ($res ne '') { print LOGFIL "$res\n\n"; }
 
 #VacuumDB
-$now_string = strftime "%a %e %b %Y %T", localtime;
+$now_string = strftime "%a %d %b %Y %T", localtime;
 print LOGFIL "$now_string\tVacuumdb ...\n";
 $res = `PGPASSWORD=$passord  vacuumdb -U postgres -a -z`;
 if ($res ne '') { print LOGFIL "$res"; }
 
-$now_string = strftime "%a %e %b %Y %T", localtime;
+$now_string = strftime "%a %d %b %Y %T", localtime;
 print LOGFIL "$now_string\tFerdig.\n\n";
 
 close(LOGFIL);
