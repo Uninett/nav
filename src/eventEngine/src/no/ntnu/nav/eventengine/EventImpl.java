a302bdd215acf3187d2af2752cbeed9620282835
Kristian Eide <kreide@gmail.com>
2002-10-23 20:52:03 +0000

    *** empty log message ***

--- src/eventEngine/src/no/ntnu/nav/eventengine/EventImpl.java
+++ src/eventEngine/src/no/ntnu/nav/eventengine/EventImpl.java
@@ -288,6 +288,10 @@ class AlertmsgParser
 				String var = msg.substring(i, e).trim();
 				if (var.length() == 0) continue;
 				if (varMap.containsKey(var)) {
+					Object o = varMap.get(var);
+					if (!o instanceof String) {
+						outld("formatMsgs: Unknown object: " + o);
+					}
 					msg.replace(i-1, e, (String)varMap.get(var));
 				}
 			}
