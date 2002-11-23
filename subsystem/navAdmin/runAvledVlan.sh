330cdca80f622ecbeedb988bf246b7f59d3fae96
Kristian Eide <kreide@gmail.com>
2002-11-23 00:38:01 +0000

    - Initial checkin

---
+++ subsystem/navAdmin/runAvledVlan.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+NAV_ROOT="/usr/local/nav"
+NAV_CONF="$NAV_ROOT/local/etc/conf/nav.conf"
+
+if [ "$JAVA_HOME" == "" ]; then
+        JAVA_HOME=`awk -F= '/JAVA_HOME/ && $1!~/#.*/{gsub("[\t ]", "", $2); print $2}' $NAV_CONF`
+fi
+
+CONF_FILE="$NAV_ROOT/local/etc/conf/navAdmin.conf"
+NAVME_ROOT="$NAV_ROOT/navme"
+CUR_DIR=$NAVME_ROOT/subsystem/navAdmin
+REPORT_DIR=$NAV_ROOT/local/log/navAdmin/report
+
+cd $CUR_DIR
+$JAVA_HOME/bin/java -jar NavUtils.jar $CONF_FILE -avledVlan > "$REPORT_DIR/avledVlan.html"
