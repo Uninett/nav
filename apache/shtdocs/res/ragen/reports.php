ccc8ac687e910401a48218cd007ecaf4a6a1d30d
Vidar Faltinsen <vidar.faltinsen@uninett.no>
2002-12-18 13:13:05 +0000

    tok v2.1.3 og la til tjenesterapport .

--- apache/shtdocs/res/ragen/reports.php
+++ apache/shtdocs/res/ragen/reports.php
@@ -11,26 +11,33 @@ navstart("Rapporter",$bruker);
 <h1>Rapporter</h1>
  
 <ul>
-<li> <a href="/res/ragen/?rapport=boks"> Utstyrsregister</a> (alt
+<li> <a href="/res/ragen/?rapport=netbox"> Utstyrsregister</a> (alt
 utstyr)
   <ul>
-  <li> <a href="/res/ragen/?rapport=gw"> Kun rutere (GW)</a>
-  <li> <a href="/res/ragen/?rapport=sw"> Kun svitsjer (SW)</a>
-  <li> <a href="/res/ragen/?rapport=kant"> Kun kantutstyr (KANT)</a>
-  <li> <a href="/res/ragen/?rapport=srv"> Kun servere (SRV)</a>
-  <li> <a href="/res/ragen/?rapport=oppsop"> Resten</a>
   <li> <a href="/res/ragen/?rapport=type"> Liste over utstyrstyper</a>
+  <li> <a href="/res/ragen/?rapport=gw"> Kun rutere (GW/GSW)</a>
+  <li> <a href="/res/ragen/?rapport=sw"> Kun svitsjer (SW/GSW)</a>
+  <li> <a href="/res/ragen/?rapport=netbox&kat=KANT"> Kun kantutstyr 
+(KANT)</a>
+  <li> <a href="/res/ragen/?rapport=wlan"> Kun trådløst (WLAN)</a>
+  <li> <a href="/res/ragen/?rapport=srv"> Kun servere (SRV)</a>
+  <li> <a href="/res/ragen/?rapport=servicemon"> Tjenester som overvåkes </a>
+  <li> <a href="/res/ragen/?rapport=oppsop"> Resten</a>
   </ul>
-<li> <a href="/res/ragen/?rapport=gwport"> Ruterporter</a> og
-     <a href="/res/ragen/?rapport=prefiks"> brukte prefiks</a>
+<li> 
+     <a href="/res/ragen/?rapport=prefix"> Brukte subnett 
+/ prefiks / vlan</a>. Liste over 
+<a href="/res/ragen/?rapport=gwport"> ruterporter</a>.
 <li> <a href="/res/ragen/?rapport=swportv"> Svitsjeporter</a>, kun
      <a href="/res/ragen/?rapport=swportt"> svitsjeporter som er trunk</a>
-<li> <a href="/res/ragen/?rapport=modules"> moduler</a>,
+<li> 
+<a href="/res/ragen/?rapport=modultype"> modultyper</a>,
+<a href="/res/ragen/?rapport=modules"> moduler</a>,
      <a href="/res/ragen/?rapport=mem"> minne/flash</a>
-<li> <a href="/res/ragen/?rapport=rom"> Rom</a>
-<li> <a href="/res/ragen/?rapport=sted"> Sted</a>
-<li> <a href="/res/ragen/?rapport=org"> Organisasjon</a>
-<li> <a href="/res/ragen/?rapport=anv"> Anvendelsestyper for subnett</a>
+<li> <a href="/res/ragen/?rapport=room"> Rom</a>,
+ <a href="/res/ragen/?rapport=location"> Sted</a>
+<li> <a href="/res/ragen/?rapport=org"> Organisasjon</a>,
+ <a href="/res/ragen/?rapport=usage"> Anvendelsestyper for subnett</a>
  
  </ul>
 
@@ -39,16 +46,16 @@ utstyr)
 <b>Søk etter:</b>
 <table>
 <tr>
-  <form action="./?rapport=boks" method=POST>
+  <form action="./?rapport=netbox" method=POST>
   <td>utstyr i telematikkrom:</td>
-  <td><input type=text name=romid size=15></td>
+  <td><input type=text name=roomid size=15></td>
   <td><input type=submit value="Søk"></td>
   </form>
 </tr>
 <tr>
-  <form action="./?rapport=prefiks" method=POST>
-  <td>nettadresse (ikke wildcard):</td> 
-  <td><input type=text name=nettadr size=15></td>
+  <form action="./?rapport=netbox" method=POST>
+  <td>ip-adresse (% wildcard):</td>
+  <td><input type=text name=host(ip) size=15></td>
   <td><input type=submit value="Søk"></td>
   </form>
 </tr>
