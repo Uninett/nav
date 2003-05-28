12418e9fcb24a3a632f1fe73dcef4f83a2d15f75
Kristian Eide <kreide@gmail.com>
2003-05-28 14:54:05 +0000

    - Put .pid file in correct place.

--- subsystem/eventEngine/eventEngine.sh
+++ subsystem/eventEngine/eventEngine.sh
@@ -1,27 +1,28 @@
 #!/bin/sh
 
 NAV_ROOT="/usr/local/nav"
-#NAV_ROOT="/home/kristian/devel"
 
 NAV_CONF="$NAV_ROOT/local/etc/conf/nav.conf"
 
 if [ "$JAVA_HOME" == "" ]; then
-	JAVA_HOME=`awk -F= '/JAVA_HOME/ && $1!~/#.*/{gsub("[\t ]", "", $2); print $2}' $NAV_CONF`
+  JAVA_HOME=`awk -F= '/JAVA_HOME/ && $1!~/#.*/{gsub("[\t ]", "", $2); print $2}' $NAV_CONF`
 fi
 
 CUR_DIR=$NAV_ROOT/navme/subsystem/eventEngine
 
 LOG_DIR="$NAV_ROOT/local/log/eventEngine"
-#LOG_DIR=$CUR_DIR
 
 COUNT=`ps wwwwx|grep "eventEngine"|grep -v eventEngine.sh|grep -v grep|wc -l|sed s/" "//g`
 if [ "$COUNT" == "0" ]; then
         cd $CUR_DIR
         # Now run script
-        $JAVA_HOME/bin/java -jar eventEngine.jar $1 > "$LOG_DIR/eventEngine-`/bin/date +%Y-%m-%d_%H-%M`.log" &
+        $JAVA_HOME/bin/java -jar eventEngine.jar $1 >> "$LOG_DIR/eventEngine.log" 2> error-log &
         PID="$!"
-        echo $PID >last.pid
-        wait $PID
+        echo $PID >$NAV_ROOT/local/var/run/eventEngine.pid
+        #wait $PID
 else
-        echo "eventEngine already running"
+        echo -n "eventEngine already running"
+        exit 1
 fi
+
+exit 0
