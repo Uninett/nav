0d04243eaee64c7312238da42d0ae8571fdbfcce
Kristian Eide <kreide@gmail.com>
2003-06-10 18:18:04 +0000

    - Update doc

--- src/getDeviceData/src/no/ntnu/nav/getDeviceData/dataplugins/DataContainer.java
+++ src/getDeviceData/src/no/ntnu/nav/getDeviceData/dataplugins/DataContainer.java
@@ -1,21 +1,32 @@
 package no.ntnu.nav.getDeviceData.dataplugins;
 
 /**
- * Used by DeviceHandler plugins to return data to DataHandlers. The methods
- * provided by the actuall object are not specified; check the documentation
- * for each plugin.
+ * <p> Each data handler must include a class which implements
+ * DataContainer; this is the interface presented to the device
+ * plugins. There is no restriction on how this interface should look;
+ * each device plugin will need to cast the given DataHandler object
+ * to the actual implementing class in order to gain access to the
+ * methods used for storing collected data.  </p>
+ *
+ * <p> For details on the interface for the data handler plugin in
+ * interest, look at the documentation for the class implementing this
+ * interface.  </p>
  */
 
 public interface DataContainer {
 	
 	/**
 	 * Get the name of the container.
+	 *
+	 * @return the name of this DataContainer
 	 */
 	public String getName();
 
 	/**
 	 * Get a data-handler for this container; this is typically a reference
 	 * to the same object which created the container.
+	 *
+	 * @return reference to a DataHandler object which can handle this DataContainer
 	 */
 	public DataHandler getDataHandler();
 
