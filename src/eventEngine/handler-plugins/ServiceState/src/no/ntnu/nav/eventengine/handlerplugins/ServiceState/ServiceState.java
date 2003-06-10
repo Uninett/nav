4cce6fde5897bd11b8d91560ed95474431a65712
Kristian Eide <kreide@gmail.com>
2003-06-10 17:52:00 +0000

    - Set 'up' field in the service table - Update to new API

--- src/eventEngine/handler-plugins/ServiceState/src/no/ntnu/nav/eventengine/handlerplugins/ServiceState/ServiceState.java
+++ src/eventEngine/handler-plugins/ServiceState/src/no/ntnu/nav/eventengine/handlerplugins/ServiceState/ServiceState.java
@@ -5,6 +5,7 @@ import no.ntnu.nav.eventengine.deviceplugins.Box.*;
 import no.ntnu.nav.eventengine.deviceplugins.Netel.*;
 
 import no.ntnu.nav.Database.*;
+import no.ntnu.nav.ConfigParser.*;
 
 import java.util.*;
 import java.sql.ResultSet;
@@ -21,35 +22,50 @@ public class ServiceState implements EventHandler
 		return new String[] { "serviceState" };
 	}
 
-	public void handle(DeviceDB ddb, Event e)
+	public void handle(DeviceDB ddb, Event e, ConfigParser cp)
 	{
 		outld("ServiceState handling event: " + e);
 
 		// Create alert
 		Alert a = ddb.alertFactory(e);
 		a.addEvent(e);
-
+		
 		outld("  added alert: " + a);
 
 		// Lookup the server
 		Device d = ddb.getDevice(e.getDeviceid());
 		if (d != null) {
-		    String deviceup;
-		    if (!d.isUp()) {
-			// Lower the severity by 30 points, but not below zero
-			a.setSeverity(Math.max(e.getSeverity()-30,0));
-			deviceup = "No";
-		    } else {
-			deviceup = "Yes";
-		    }
-		    a.addVar("deviceup", deviceup);
+			String deviceup;
+			if (!d.isUp()) {
+				// Lower the severity by 30 points, but not below zero
+				a.setSeverity(Math.max(e.getSeverity()-30,0));
+				deviceup = "No";
+			} else {
+				deviceup = "Yes";
+			}
+			a.addVar("deviceup", deviceup);
 		} else {
-		    outld("  warning: device for deviceid("+e.getDeviceid()+") not found!");
+			outld("  warning: device for deviceid("+e.getDeviceid()+") not found!");
 		}
 
-		// Set time
-		a.addVar("time", e.getTimeS());
-
+		char up = 'x';
+		if (e.getState() == Event.STATE_START) {
+			up = 'n';
+		}
+		else if (e.getState() == Event.STATE_END) {
+			up = 'y';
+		}
+		
+		if (up != 'x') {
+			// Update up in service
+			try {
+				Database.update("UPDATE service SET up = '" + up + "' WHERE serviceid = " + e.getSubid());
+				Database.commit();
+			} catch (SQLException exp) {
+				errl("ServiceState: SQLException when trying to update up-field (set to " + up + ") in service: " + exp.getMessage());
+			}
+		}
+		
 		// Update varMap from database
 		try {
 			ResultSet rs = Database.query("SELECT * FROM service LEFT JOIN serviceproperty USING(serviceid) WHERE serviceid = " + e.getSubid());
@@ -71,9 +87,9 @@ public class ServiceState implements EventHandler
 		} catch (PostAlertException exp) {
 			errl("ServiceState.classback: While posting service alert, PostAlertException: " + exp.getMessage());
 		}
-
+		
 	}
-
+	
 	private static void outd(Object o) { if (DEBUG_OUT) System.out.print(o); }
 	private static void outld(Object o) { if (DEBUG_OUT) System.out.println(o); }
 
