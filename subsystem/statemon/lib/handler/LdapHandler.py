802b3ee8145ae53826d2b9428dc4c8f384353adf
Bjørn Ove Grøtan <bjorn.grotan@itea.ntnu.no>
2003-06-15 15:56:01 +0000

    Added some documentation of what arguments can be sent to the Handler with examples for each argument.

--- subsystem/statemon/lib/handler/LdapHandler.py
+++ subsystem/statemon/lib/handler/LdapHandler.py
@@ -1,5 +1,5 @@
 """
-$Id: LdapHandler.py,v 1.2 2003/06/15 13:25:16 bgrotan Exp $
+$Id: LdapHandler.py,v 1.3 2003/06/15 15:56:12 bgrotan Exp $
 $Source: /usr/local/cvs/navbak/navme/subsystem/statemon/lib/handler/LdapHandler.py,v $
 """
 
@@ -9,6 +9,25 @@ import ldap,base64,string
 
 class LdapHandler(JobHandler):
 	"""
+	Handle LDAP-servers. It can be quite simple (just try anonymous bind) or quite
+	sophisticated with additional search and retrieve specific dn's with specific attributes.
+
+	Arguments to this handler (with explenations):
+	port:	remote tcp-port where ldap-server is living. Default is 389
+	hostname: accessible from self.getAddress() as pure FQDN hostname
+	url:    if this is defined as arguments this will override hostname/port
+		example-url: ldap://myserver.mydomain.com:389
+	version: implies protocol-version of LDAP. Default is 3 - though 2 is also supported
+	username: leave this blank if you want to bind anonymously
+	password: leave this blank if you want to bind anonymously
+	authtype: meant to handle authentication-types. Not implemented yet. Only simple auth yet
+		types will be: TLS, SASL, simple, Kerberos
+	base:	the server's basedn (e.g. dc=mydomain,dc=com)
+	scope:	what level of the search will be needed? BASE, ONELEVEL and SUBTREE can be used
+	filter: dn to search for. May have regexp and more. Example: cn=monitor
+	attrs:  specific attributes you want to retrieve from searching filter (not implemented in this version)
+	attr_val: regexp for matching attrs. If matched - Event.UP is returned - else Event.DOWN (not implemented in this version)
+	
 	"""
 
 	def __init__(self,service, **kwargs):
@@ -24,8 +43,14 @@ class LdapHandler(JobHandler):
 				l = ldap.initialize(args["url"])
 		else:
 			l = ldap.open(self.getAddress())
-		username = ""
-		pwd = ""
+		if args.has_key("username"):
+			username = args["username"]
+		else:
+			username = ""
+		if args.has_key("password"):
+			pwd = args["password"]
+		else:
+			pwd = ""
 
 		timeout = self.getTimeout()
 		try:
