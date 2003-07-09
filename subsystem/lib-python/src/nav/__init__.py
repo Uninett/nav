6c21cd027ea7d5b548e3328a30331d2d2d7737fd
Morten Brekkevold <morten.brekkevold@uninett.no>
2003-07-09 09:07:00 +0000

    - Added proper comment header to nav and nav.db modules. - New CachedObject class in nav module. - nav.db.getConnection() now uses a simple cache of connection objects.  Each   Apache thread/process will retain an (almost) constant set of reusable   database connections, hopefully resulting in better performance and reduced   hogging of database resources. The cache still needs functionality to detect   and delete rotten connection objects (disconnected/hung sessions etc.). - Set the svn:keywords property = "Id" on more source files.

--- subsystem/lib-python/src/nav/__init__.py
+++ subsystem/lib-python/src/nav/__init__.py
@@ -1,1 +1,44 @@
-__all__ = ["config", "users", "web"]
+"""
+$Id$
+
+This file is part of the NAV project.
+
+Provides a common root package for the NAV python library.
+
+Copyright (c) 2003 by NTNU, ITEA nettgruppen
+Authors: Morten Vold <morten.vold@itea.ntnu.no>
+"""
+# We import some sub-modules because of bugs in mod_python
+import db
+try:
+    # This actually belongs in another subsystem
+    import web
+except:
+    pass
+
+import time
+
+class CachedObject:
+    """
+    A simple class to wrap objects for 'caching'.  It contains the
+    object reference and the time the object was cached.
+    """
+    def __init__(self, object=None, loadTime=None):
+        if not loadTime:
+            loadTime = time.time()
+
+        self.loadTime = loadTime
+        self.object = object
+
+    def age(self):
+        """
+        Return the age of this object
+        """
+        return time.time() - self.loadTime
+
+    def __repr__(self):
+        return "<%s cached at %s>" % (repr(self.object),
+                                      time.asctime(time.localtime(self.loadTime)))
+    
+    def __str__(self):
+        return self.object.__str__()
--- subsystem/lib-python/src/nav/db/__init__.py
+++ subsystem/lib-python/src/nav/db/__init__.py
@@ -1,13 +1,19 @@
 """
 $Id: x1$
 
-Database functionality for NAV.
+This file is part of the NAV project.
+
+Provides common database functionality for NAV.
+
+Copyright (c) 2003 by NTNU, ITEA nettgruppen
+Authors: Morten Vold <morten.vold@itea.ntnu.no>
 """
 import psycopg
 from nav import config
 
 db = None
 driver = psycopg
+_connectionCache = {}
 
 def escape(string):
     return str(psycopg.QuotedString(string))
@@ -15,25 +21,45 @@ def escape(string):
 def getConnection(scriptName, database='nav'):
     """
     Returns an open database connection, as configured in db.conf for
-    the given scriptName.
+    the given scriptName.  Connections are cached, so that future
+    calls using the same parameters will receive an already open
+    connection.
     """
-    conf = config.readConfig('db.conf')
-    dbname = conf['db_%s' % database]
-    user   = conf['script_%s' % scriptName]
-    pw     = conf['userpw_%s' % user]
+    import nav
+    if not hasattr(nav, 'CachedObject'):
+        reload(nav)
+        import sys
+        sys.stderr.write('MORTEN-DEBUG: CachedObject not found, module nav reloaded;' +
+                         'Extremely strange mod_python bug forces this ugly hack\n')
+                  
+    from nav import CachedObject
+    global _connectionCache
+    cacheKey = '%s_%s' % (scriptName, database)
 
-    connection = psycopg.connect('host=%s dbname=%s user=%s password=%s' %
-                                 (conf['dbhost'], dbname, user, pw))
-    return connection
+    # If the connection object already exists in the connection cache,
+    # we return this instead.  Here we should also perform some checks
+    # as to whether the connection is still valid.
+    if _connectionCache.has_key(cacheKey):
+        return _connectionCache[cacheKey].object
+    else:
+        conf = config.readConfig('db.conf')
+        dbname = conf['db_%s' % database]
+        user   = conf['script_%s' % scriptName]
+        pw     = conf['userpw_%s' % user]
+        
+        connection = psycopg.connect('host=%s dbname=%s user=%s password=%s' %
+                                     (conf['dbhost'], dbname, user, pw))
+
+        _connectionCache[cacheKey] = CachedObject(connection)
+        return connection
 
 def setDefaultConnection(conn):
     global db, cursor
     db = conn
     cursor = db.cursor
-#    db.autocommit(1)
+    db.autocommit(1)
 
 ###### Initialization ######
 
 if not db:
     setDefaultConnection(getConnection('default'));
-
