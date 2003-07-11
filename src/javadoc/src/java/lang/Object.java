67cacddbe61b674f0563e852ce2abb008b2dccc7
Kristian Eide <kreide@gmail.com>
2003-07-11 13:15:04 +0000

    - Add dummy objects - Add more projects to javadoc

---
+++ src/javadoc/src/java/lang/Object.java
@@ -0,0 +1,11 @@
+package java.lang;
+
+/**
+ * Dummy file to make the javadoc look nicer.
+ *
+ * Look at the Sun Java API for the real documentation of this class.
+ */
+
+public class Object {
+	Object() { }
+}
