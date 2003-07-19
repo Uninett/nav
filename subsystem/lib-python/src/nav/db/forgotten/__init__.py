2caa8f21f3dd68996d68c80e38866d2f80264a4f
Stian Søiland <stian.soiland@ntnu.no>
2003-07-19 16:43:00 +0000

    Added modules for SQL table manage. forgotten/init got some docs.

---
+++ subsystem/lib-python/src/nav/db/forgotten/__init__.py
@@ -0,0 +1,14 @@
+"""
+$Id$
+
+This file is part of the NAV project.
+
+Contains forgetSQL-generated modules, one for each 
+table in the database.
+
+Use the extended version of these modules from 
+nav.db instead of these.
+
+Copyright (c) 2003 by NTNU, ITEA nettgruppen
+Authors: Stian Søiland <stain@itea.ntnu.no>
+"""
