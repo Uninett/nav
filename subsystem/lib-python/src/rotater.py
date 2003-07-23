609af4841427a82d9fefa88a4fc04eb6a6867381
Magnus Nordseth <magnus@ntnu.no>
2003-07-23 16:10:01 +0000

    Added an example

--- subsystem/lib-python/src/rotater.py
+++ subsystem/lib-python/src/rotater.py
@@ -7,8 +7,10 @@ class RotaterPlugin:
         self.rotation = -1
         self.rotatedelay = rotdelay
     def rotate(self, noDelay=None):
+        if self.rotation > -1:
+            sys.stdout.write("\010")
         self.rotation = (self.rotation + 1) % len(ROTATION)
-        sys.stdout.write("\010" + ROTATION[self.rotation])
+        sys.stdout.write(ROTATION[self.rotation])
         sys.stdout.flush()
         if(not noDelay):
             time.sleep(self.rotatedelay)
@@ -18,3 +20,22 @@ class RotaterPlugin:
         print ('%s '*len(msg) % msg)[:-1]
         sys.stdout.write(" ")
 
+
+def __test():
+    import random
+    foo = RotaterPlugin()
+    messages = ['Calculation gravitation...',
+                'Getting nuclear blast ratio...',
+                ]
+    try:
+        while 1:
+            if random.random() > 0.9:
+                foo.write(random.choice(messages))
+            foo.rotate()
+    except:
+        sys.exit(0)
+
+if __name__ == '__main__':
+    __test()
+
+            
