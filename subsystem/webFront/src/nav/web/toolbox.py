dc96b9601efdd16e4f55a2966c649952abc452bf
Morten Brekkevold <morten.brekkevold@uninett.no>
2003-08-06 12:35:05 +0000

    - Added standard NAV module header - The tool list is now sorted before it is returned.  The priority attribute   was added to assign a sorting priority to tools; secondary sorting is done by   tool name for equal priorities.

--- subsystem/webFront/src/nav/web/toolbox.py
+++ subsystem/webFront/src/nav/web/toolbox.py
@@ -1,9 +1,31 @@
+"""
+$Id$
+
+This file is part of the NAV project.
+
+This module contains functionality related to the web toolbox.
+
+Copyright (c) 2003 by NTNU, ITEA nettgruppen
+Authors: Morten Vold <morten.vold@itea.ntnu.no>
+"""
 from nav import config
 import os
 import os.path
 
 webfrontConfig = config.readConfig('webfront.conf')
 
+def _compareTools(x, y):
+    """
+    Internal function to compare two tools for sorting purposes
+    """
+    # Do a standard comparison of priority values (to accomplish an
+    # ascendingg sort, we negate the priorities)
+    ret = cmp(-x.priority, -y.priority)
+    # If priorities were equal, sort by name instead
+    if not ret:
+        ret = cmp(x.name.upper(), y.name.upper())
+    return ret
+    
 def getToolList():
     """Searches the TOOLPATH search path for *.tool files and returns
     a list of Tool objects representing these files"""
@@ -22,8 +44,10 @@ def getToolList():
                     fullpath = os.path.join(path, filename)
                     list.append(Tool().load(fullpath))
 
+    # Sort the tool list according to the _cmpTool function
+    list.sort(_compareTools)
     return list
-    
+
 class Tool:
     def __init__(self):
         self.name = ''
@@ -39,5 +63,9 @@ class Tool:
         self.uri         = dict['uri']
         self.description = dict['description']
         self.icon        = dict['icon']
+        if dict.has_key('priority'):
+            self.priority = int(dict['priority'])
+        else:
+            self.priority = 0
         
         return self
