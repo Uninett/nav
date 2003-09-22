b9df7c80d4d23d669e6c69e1c633342ee18f1d5f
John-Magne Bredal <john.m.bredal@ntnu.no>
2003-09-22 12:06:04 +0000

    - added INSTALL-file, which guides you through the install of thresholdMon subsystem. - added fillthresholds.py, the script that does an initial setup of the database for the subsystem.

---
+++ subsystem/thresholdMon/fillthresholds.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+##################################################
+# fillthreshold.py
+#
+# Author: John Magne Bredal, ITEA / NTNU © 2003
+##################################################
+
+import psycopg
+import forgetSQL
+import re
+import nav.db.forgotten
+from nav import db
+
+conn = db.getConnection('thresholdmon','manage')
+
+from nav.db import manage
+
+nav.db.forgotten.manage._Wrapper.cursor = conn.cursor
+nav.db.forgotten.manage._Wrapper._dbModule = psycopg
+
+def setData (datasource,threshold,max):
+    datasource.threshold = threshold
+    datasource.max = max
+    datasource.delimiter = ">"
+    datasource.thresholdstate = "inactive"
+    datasource.save()
+
+# setting default threshold
+default = "90"
+
+#for datasource in manage.Rrd_datasource.getAllIterator(where="threshold IS NULL"):
+for datasource in manage.Rrd_datasource.getAllIterator():
+    if datasource.units == '%' or datasource.units == '-%':
+        print "Found percent %s: %s, setting threshold=%s, max=100" %(datasource.descr,datasource.units, default)
+        setData(datasource,default,"100")
+    elif re.compile("octets",re.I).search(datasource.descr):
+        # Finds the speed of the interface
+        rrdfile = datasource.rrd_file
+
+	if (rrdfile.key == 'swport'):
+            port = manage.Swport(rrdfile.value)
+	else:
+	    port = manage.Gwport(rrdfile.value)
+
+        try:
+            port.load()
+        except forgetSQL.NotFound:
+            #print "Faen! %s-%s finnes ikke i databasen???" %(rrdfile.key,rrdfile.value)
+            continue
+
+        if port.speed:
+            speed = int(port.speed * 2 ** 20)
+        
+        print "Found octets: %s, setting threshold to %s, max=%s" %(datasource.descr, default+"%", speed);
+        setData(datasource,default+"%",speed)        
+    else:
+	pass
+        #print "Fant ingen måte å sette threshold og max på for %s" %datasource.descr
+        
