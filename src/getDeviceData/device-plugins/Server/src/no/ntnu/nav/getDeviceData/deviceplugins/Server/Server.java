a7fa297db9daaf074a6d142f18e510c115f35ad9
Stian Søiland <stian.soiland@ntnu.no>
2003-09-26 02:07:02 +0000

    Space*8 - not tab

--- src/getDeviceData/device-plugins/Server/src/no/ntnu/nav/getDeviceData/deviceplugins/Server/Server.java
+++ src/getDeviceData/device-plugins/Server/src/no/ntnu/nav/getDeviceData/deviceplugins/Server/Server.java
@@ -13,21 +13,23 @@ import no.ntnu.nav.getDeviceData.dataplugins.NetboxInfo.*;
 public class Server implements DeviceHandler
 {
     public int canHandleDevice(Netbox nb) {
-	if (nb.getCat().equalsIgnoreCase("srv") &&
-	    nb.getSnmpMajor() > 0) {
-	    Log.d("ServerPlugin", "canHandleDevice", "We are ready to serve " + nb.getSysname());
-	    return DeviceHandler.ALWAYS_HANDLE;
-	} 
-	Log.d("ServerPlugin", "canHandleDevice", "We cannot serve " + nb.getSysname());
-	return DeviceHandler.NEVER_HANDLE;
+        if (nb.getCat().equalsIgnoreCase("srv") 
+        // snmpMajor is not set yet...
+        // && nb.getSnmpMajor() > 0
+        ) {
+            Log.d("ServerPlugin", "canHandleDevice", "We are ready to serve " + nb.getSysname());
+            return DeviceHandler.ALWAYS_HANDLE;
+        } 
+        Log.d("ServerPlugin", "canHandleDevice", "We cannot serve " + nb.getSysname());
+        return DeviceHandler.NEVER_HANDLE;
     }
     public void handleDevice(Netbox nb, SimpleSnmp sSnmp, ConfigParser cp, DataContainers containers) throws TimeoutException {
-	Log.setDefaultSubsystem("ServerPlugin");
-	NetboxInfoContainer infoContainer = (NetboxInfoContainer)containers.getContainer("NetboxInfoContainer");
-	sSnmp.setHost(nb.getIp());
-	sSnmp.setCs_ro(nb.getCommunityRo());
-	infoContainer.put("blipp", "blapp", "hihi");
-	infoContainer.commit();
+        Log.setDefaultSubsystem("ServerPlugin");
+        NetboxInfoContainer infoContainer = (NetboxInfoContainer)containers.getContainer("NetboxInfoContainer");
+        sSnmp.setHost(nb.getIp());
+        sSnmp.setCs_ro(nb.getCommunityRo());
+        infoContainer.put("blipp", "blapp", "hihi");
+        infoContainer.commit();
     }
 }
     
