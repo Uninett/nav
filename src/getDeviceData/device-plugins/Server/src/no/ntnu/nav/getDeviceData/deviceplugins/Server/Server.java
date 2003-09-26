2e4216f2d1cca8d3bc506510707e11f77ec04470
Stian Søiland <stian.soiland@ntnu.no>
2003-09-26 00:34:04 +0000

    Vi går over til java igjen vi

---
+++ src/getDeviceData/device-plugins/Server/src/no/ntnu/nav/getDeviceData/deviceplugins/Server/Server.java
@@ -0,0 +1,33 @@
+package no.ntnu.nav.getDeviceData.deviceplugins.Server;
+
+import java.util.*;
+
+import no.ntnu.nav.logger.*;
+import no.ntnu.nav.SimpleSnmp.*;
+import no.ntnu.nav.ConfigParser.*;
+import no.ntnu.nav.getDeviceData.Netbox;
+import no.ntnu.nav.getDeviceData.deviceplugins.DeviceHandler;
+import no.ntnu.nav.getDeviceData.dataplugins.*;
+import no.ntnu.nav.getDeviceData.dataplugins.NetboxInfo.*;
+
+public class Server implements DeviceHandler
+{
+    public int canHandleDevice(Netbox nb) {
+	if (nb.getCat().equalsIgnoreCase("srv") &&
+	    nb.getSnmpMajor() > 0) {
+	    Log.d("ServerPlugin", "canHandleDevice", "We are ready to serve " + nb.getSysname());
+	    return DeviceHandler.ALWAYS_HANDLE;
+	} 
+	Log.d("ServerPlugin", "canHandleDevice", "We cannot serve " + nb.getSysname());
+	return DeviceHandler.NEVER_HANDLE;
+    }
+    public void handleDevice(Netbox nb, SimpleSnmp sSnmp, ConfigParser cp, DataContainers containers) throws TimeoutException {
+	Log.setDefaultSubsystem("ServerPlugin");
+	NetboxInfoContainer infoContainer = (NetboxInfoContainer)containers.getContainer("NetboxInfoContainer");
+	sSnmp.setHost(nb.getIp());
+	sSnmp.setCs_ro(nb.getCommunityRo());
+	infoContainer.put("blipp", "blapp", "hihi");
+	infoContainer.commit();
+    }
+}
+    
