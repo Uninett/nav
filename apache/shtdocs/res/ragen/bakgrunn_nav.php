2aa4d33c11d56eec8c5ca539763d9266ba9e3aed
Morten Brekkevold <morten.brekkevold@uninett.no>
2003-10-02 08:30:05 +0000

    * Removed many old and useless files stemming from v2, but discontinued in v3.
    * Moved subnet2web.pl from the old v2-structure to the report subsystem (where
      Sigurd will massage it into a new script later).

--- apache/shtdocs/res/ragen/bakgrunn_nav.php
+++
@@ -1,5 +0,0 @@
-<?php Header ("Content-type: image/png"); 
-$im = imagecreate (1,1); 
-$color_nav = ImageColorAllocate ($im, 71, 100, 144); 
-imagefill($im,0,0,$color_nav);
-Imagepng($im); ImageDestroy ($im); ?>
