69ec32b71ace26cba4c8c1dea4b4755eed912163
Kristian Eide <kreide@gmail.com>
2003-11-25 10:03:01 +0000

    - Added methods for printing class hierarchys

--- src/eventEngine/src/no/ntnu/nav/eventengine/Device.java
+++ src/eventEngine/src/no/ntnu/nav/eventengine/Device.java
@@ -33,7 +33,25 @@ public abstract class Device
 	public abstract void up();
 	public abstract boolean isUp();
 
+	/**
+	 * Return the class hierarchy for the class of this object as a String.
+	 */
+	public String getClassH() {
+		return getClassH(getClass());
+	}
 
+	/**
+   * Return the class hierarchy for the class as a String.
+   */
+	public static String getClassH(Class c) {
+		if (c.getSuperclass() == null) return "";
+		return getClassH(c.getSuperclass()) + " <- " + c.getName();
+	}
+
+	/**
+	 * Return true of the names of the classes of the two objects are
+	 * equal.
+	 */
 	public static boolean classEq(Object o1, Object o2)
 	{
 		if (o1 == null || o2 == null) return false;
