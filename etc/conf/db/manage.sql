8f171bd44df73eac46144571d97181c0d67fda83
Morten Brekkevold <morten.brekkevold@uninett.no>
2003-12-04 13:45:04 +0000

    * Table emotd_related is now defined before the view that depends on it.

--- etc/conf/db/manage.sql
+++ etc/conf/db/manage.sql
@@ -864,13 +864,6 @@ CREATE TABLE maintenance (
     state VARCHAR CHECK (state IN ('scheduled','active','passed','overridden'))   
 );
 
--- maintenanceview, joined maintenance with emotd_related
-CREATE OR REPLACE VIEW maintenance_view AS 
-    SELECT maintenance.maintenanceid, emotd_related.emotdid, 
-    emotd_related.key, emotd_related.value, maintenance.maint_start, 
-    maintenance.maint_end, maintenance.state FROM maintenance, 
-    emotd_related WHERE (emotd_related.emotdid = maintenance.emotdid);
-
 -- references to netbox/room/etc.
 CREATE TABLE emotd_related (
     emotdid INT REFERENCES emotd ON UPDATE CASCADE ON DELETE CASCADE,
@@ -880,6 +873,13 @@ CREATE TABLE emotd_related (
     value VARCHAR NOT NULL,
     PRIMARY KEY(emotdid,key,value)
 );
+
+-- maintenanceview, joined maintenance with emotd_related
+CREATE OR REPLACE VIEW maintenance_view AS 
+    SELECT maintenance.maintenanceid, emotd_related.emotdid, 
+    emotd_related.key, emotd_related.value, maintenance.maint_start, 
+    maintenance.maint_end, maintenance.state FROM maintenance, 
+    emotd_related WHERE (emotd_related.emotdid = maintenance.emotdid);
 
 
 
