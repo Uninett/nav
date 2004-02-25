300a338c9e8a172883e43b02f0a167f2827be7ab
Morten Brekkevold <morten.brekkevold@uninett.no>
2004-02-25 11:07:01 +0000

    Reworked and renamed the convertsql.py script into dumpsnmpoid.py - it now connects to a live database and dumps the contents of the snmpoid table.

---
+++ tools/dumpsnmpoid.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import psycopg, os, sys
+
+#
+# This script will connect to a live manage database and dump the
+# contents of the snmpoid table in a format suitable for re-insertion
+# at the same or a different site.
+#
+# Make sure the environment variable PGASSWORD is set before running the script.
+# Should be rewritten to ask for a password if none is known.
+#
+
+dbname = "manage"
+dbuser = os.getenv('PGUSER')
+if not dbuser:
+    dbuser = "postgres"
+dbpasswd = os.getenv('PGPASSWORD')
+
+connection = psycopg.connect('host=localhost dbname=%s user=%s password=%s' %
+                             (dbname, dbuser, dbpasswd))
+cursor = connection.cursor()
+cursor.execute("SELECT oidkey, snmpoid, descr, oidsource, getnext, match_regex, decodehex FROM snmpoid")
+
+print "BEGIN;"
+for row in cursor.fetchall():
+    newrow = []
+    for col in row:
+        if col is None:
+            newrow.append('NULL')
+        else:
+            newrow.append("'%s'" % str(col))
+    print "INSERT INTO snmpoid (oidkey, snmpoid, descr, oidsource, getnext, match_regex, decodehex)",
+    print "VALUES (%s);" % ",".join(newrow)
+    print
+    
+print "COMMIT;"
