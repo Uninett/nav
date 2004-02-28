69e12d256d8fbf98a9f255f0f0ebe18bf9489a8d
Magnus Nordseth <magnus@ntnu.no>
2004-02-28 19:03:00 +0000

    * added fuzzy search for netboxes

--- subsystem/devBrowser/src/lib/netbox.py
+++ subsystem/devBrowser/src/lib/netbox.py
@@ -18,6 +18,27 @@ _statusTranslator = {'y':'Up',
                      's':'Shadow'
                      }
 
+def distance(a,b):
+    """
+    Calculates the Levenshtein distance between a and b.
+    """
+    n, m = len(a), len(b)
+    if n > m:
+        a,b = b,a
+        n,m = m,n
+        
+    current = range(n+1)
+    for i in range(1,m+1):
+        previous, current = current, [i]+[0]*m
+        for j in range(1,n+1):
+            add, delete = previous[j]+1, current[j-1]+1
+            change = previous[j-1]
+            if a[j-1] != b[i-1]:
+                change = change + 1
+            current[j] = min(add, delete, change)
+            
+    return current[n]
+
 def findNetboxes(hostname):
     """
     Finds a netbox from sysname or partial sysname. Returns
@@ -40,6 +61,15 @@ def findNetboxes(hostname):
         raise RedirectError, urlbuilder.createUrl(matches[0])
     elif matches:
         return matches
+    # try mr. levenshtein...
+    a=hostname.count('.')
+    for sysname in sysnames:
+        # should be fixed in a better way...
+        if not sysname.find('129.241') >= 0:
+            shortname = '.'.join(sysname.split('.')[:a+1])
+            matches.append((distance(hostname, shortname), sysname))
+    matches.sort()
+    return [manage.getNetbox(x[1]) for x in matches[:20]]
     
     raise apache.SERVER_RETURN, apache.HTTP_NOT_FOUND
     
