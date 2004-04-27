c12c0f2fef62ef578ddd9397fcecdf1b370651d8
Kristian Eide <kreide@gmail.com>
2004-04-27 22:33:02 +0000

    - Added GPL notice

--- src/Database/src/no/ntnu/nav/Database/Database.java
+++ src/Database/src/no/ntnu/nav/Database/Database.java
@@ -1,13 +1,28 @@
-/*******************
-*
-* $Id: Database.java,v 1.4 2003/03/03 10:44:53 kristian Exp $
-* This file is part of the NAV project.
-* Database interface class
-*
-* Copyright (c) 2002 by NTNU, ITEA nettgruppen
-* Authors: Kristian Eide <kreide@online.no>
-*
-*******************/
+/*
+ * Database
+ * 
+ * $LastChangedRevision$
+ *
+ * $LastChangedDate$
+ *
+ * Copyright 2002-2004 Norwegian University of Science and Technology
+ * 
+ * This file is part of Network Administration Visualized (NAV)
+ * 
+ * NAV is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * NAV is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with NAV; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 package no.ntnu.nav.Database;
 
@@ -30,8 +45,12 @@ import no.ntnu.nav.logger.Log;
  * database. This class contains only static methods any may thus be
  * used without needing a reference to it. It is also
  * thread-safe. However, it only supports connecting to one database
- * at a time.  * </p>
+ * at a time.  </p>
+ *
+ * @version $LastChangedRevision$ $LastChangedDate$
+ * @author Kristian Eide <kreide@online.no>
  */
+
 public class Database
 {
 	private static final int ST_BUFFER = 40;
