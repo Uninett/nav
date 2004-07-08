ba8cc5b42c0100384038005b2f6d4f0d43e71bbf
Kristian Eide <kreide@gmail.com>
2004-07-08 12:43:05 +0000

    - Added necessary escapes for match_regex fields

--- doc/sql/snmpoid.sql
+++ doc/sql/snmpoid.sql
@@ -21,7 +21,7 @@ VALUES ('3cHwVer','1.3.6.1.4.1.43.10.27.1.1.1.11','Hardware version number','3co
 
 DELETE FROM snmpoid WHERE oidkey='3cIfDescr';
 INSERT INTO snmpoid (oidkey, snmpoid, descr, oidsource, getnext, match_regex, decodehex, oidname, mib)
-VALUES ('3cIfDescr','1.3.6.1.2.1.2.2.1.2','3Com ifDescr for port and unit','3com','0','.*(Unit|Port) (\d+)\b.*','1','ifDescr','IF-MIB');
+VALUES ('3cIfDescr','1.3.6.1.2.1.2.2.1.2','3Com ifDescr for port and unit','3com','0','.*\(Unit|Port\) \(\\d+\)\\b.*','1','ifDescr','IF-MIB');
 
 DELETE FROM snmpoid WHERE oidkey='3cIfMauType';
 INSERT INTO snmpoid (oidkey, snmpoid, descr, oidsource, getnext, match_regex, decodehex, oidname, mib)
@@ -105,7 +105,7 @@ VALUES ('cCardIndex','1.3.6.1.4.1.9.3.6.11.1.1','Slot card type','Cisco','0',NUL
 
 DELETE FROM snmpoid WHERE oidkey='cCardSerial';
 INSERT INTO snmpoid (oidkey, snmpoid, descr, oidsource, getnext, match_regex, decodehex, oidname, mib)
-VALUES ('cCardSerial','1.3.6.1.4.1.9.3.6.11.1.4','Slot card serial','Cisco','0','[^0]|\w{2,}','0','cardSerial','OLD-CISCO-CHASSIS-MIB');
+VALUES ('cCardSerial','1.3.6.1.4.1.9.3.6.11.1.4','Slot card serial','Cisco','0','[^0]|\\w{2,}','0','cardSerial','OLD-CISCO-CHASSIS-MIB');
 
 DELETE FROM snmpoid WHERE oidkey='cCardSlotNumber';
 INSERT INTO snmpoid (oidkey, snmpoid, descr, oidsource, getnext, match_regex, decodehex, oidname, mib)
