63486a3b484d265b00d7d58ae32be0f7ef6c64fe
Kristian Eide <kreide@gmail.com>
2004-09-18 18:59:01 +0000

    - ';' and '_' are no longer considered delimiter characters in alertmsg.conf (i.e. they can now be used as part of a variable name)

--- src/eventEngine/src/EventImpl.java
+++ src/eventEngine/src/EventImpl.java
@@ -360,7 +360,7 @@ class AlertmsgParser
 			while ( (i=msg.indexOf("$", i)) != -1) {
 				if (++i == msg.length()) break;
 				int e = i;
-				while (e < msg.length() && Character.isLetterOrDigit(msg.charAt(e))) e++;
+				while (e < msg.length() && (Character.isLetterOrDigit(msg.charAt(e)) || e == ';' || e == '_')  ) e++;
 				String var = msg.substring(i, e).trim();
 				if (var.length() == 0) continue;
 				if (varMap.containsKey(var)) {
