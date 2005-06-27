0efbd32d067adb5940219163b77990c4a9261ec8
Kristian Eide <kreide@gmail.com>
2005-06-27 02:01:00 +0000

    - Remember to convert port number to ifindex to avoid skew

--- src/getDeviceData/device-plugins/CiscoSwIOSaddon/src/no/ntnu/nav/getDeviceData/deviceplugins/CiscoSwIOSaddon/CiscoSwIOSaddon.java
+++ src/getDeviceData/device-plugins/CiscoSwIOSaddon/src/no/ntnu/nav/getDeviceData/deviceplugins/CiscoSwIOSaddon/CiscoSwIOSaddon.java
@@ -67,33 +67,41 @@ public class CiscoSwIOSaddon implements DeviceHandler
 		String netboxid = nb.getNetboxidS();
 		String ip = nb.getIp();
 		String cs_ro = nb.getCommunityRo();
-		String type = nb.getType();
 		this.sSnmp = sSnmp;
 
-		processIOSaddon(nb, netboxid, ip, cs_ro, type, sc);
+		processIOSaddon(nb, netboxid, ip, cs_ro, sc);
 
 	}
 
-	private void processIOSaddon(Netbox nb, String netboxid, String ip, String cs_ro, String typeid, SwportContainer sc) throws TimeoutException
+	private void processIOSaddon(Netbox nb, String netboxid, String ip, String cs_ro, SwportContainer sc) throws TimeoutException
 	{
-		typeid = typeid.toLowerCase();
+		// Map port to ifindex
+		Map ifindexMap = sSnmp.getAllMap(nb.getOid("iosPortIfindex"), false);
+		if (ifindexMap != null) {
+			List l;
 		
-		List l;
-		
-		l = sSnmp.getAll(nb.getOid("iosTrunk"));
-		if (l != null) {
-			for (Iterator it = l.iterator(); it.hasNext();) {
-				String[] s = (String[])it.next();
-				boolean trunk = (s[1].equals("1") ? false : true);
-				sc.swportFactory(s[0]).setTrunk(trunk);
+			l = sSnmp.getAll(nb.getOid("iosTrunk"));
+			if (l != null) {
+				for (Iterator it = l.iterator(); it.hasNext();) {
+					String[] s = (String[])it.next();
+					String ifindex = (String)ifindexMap.get(s[0]);
+					if (ifindex == null) continue;
+
+					boolean trunk = (s[1].equals("1") ? false : true);
+					sc.swportFactory(ifindex).setTrunk(trunk);
+				}
 			}
-		}
-		l = sSnmp.getAll(nb.getOid("iosDuplex"));
-		if (l != null) {
-			for (Iterator it = l.iterator(); it.hasNext();) {
-				String[] s = (String[])it.next();
-				char duplex = (s[1].equals("1") ? 'f' : 'h');
-				sc.swportFactory(s[0]).setDuplex(duplex);
+			l = sSnmp.getAll(nb.getOid("iosDuplex"));
+			if (l != null) {
+				for (Iterator it = l.iterator(); it.hasNext();) {
+					String[] s = (String[])it.next();
+					String ifindex = (String)ifindexMap.get(s[0]);
+					if (ifindex == null) continue;
+
+					char duplex = (s[1].equals("1") ? 'f' : 'h');
+					sc.swportFactory(ifindex).setDuplex(duplex);
+					sc.swportFactory(ifindex).setPort(Integer.valueOf(s[0]));
+				}
 			}
 		}
 	}
