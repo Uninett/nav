66840d09759a0a1988463b127e3415bd9afb8248
Morten Brekkevold <morten.brekkevold@uninett.no>
2005-12-13 12:48:01 +0000

    Fix confusing dnsMismatch alerts.
     * MibIISw.java
       Post sysname read from SNMP as "snmpsysname" to the event's variable map.
    
     * alertmsg.conf
       Use snmpsysname variable instead of sysname to indicate the sysname
       collected by SNMP.

--- src/getDeviceData/device-plugins/MibIISw/src/no/ntnu/nav/getDeviceData/deviceplugins/MibIISw/MibIISw.java
+++ src/getDeviceData/device-plugins/MibIISw/src/no/ntnu/nav/getDeviceData/deviceplugins/MibIISw/MibIISw.java
@@ -279,7 +279,7 @@ public class MibIISw implements DeviceHandler
 				Map varMap = new HashMap();
 				varMap.put("alerttype", "dnsMismatch");
 				varMap.put("dnsname", String.valueOf(nb.getSysname()));
-				varMap.put("sysname", String.valueOf(s[1]));
+				varMap.put("snmpsysname", String.valueOf(s[1]));
 				EventQ.createAndPostEvent("getDeviceData", "eventEngine", nb.getDeviceid(), nb.getNetboxid(), 0, "info", Event.STATE_NONE, 0, 0, varMap);
 			} else {
 				Log.d("HANDLE", "Correct: Sysname (DNS) ("+nb.getSysname()+") starts with the collected sysname ("+s[1]+")");
