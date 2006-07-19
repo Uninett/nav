3e3db71fc8205254f48fd56ef8d445c5e5188612
Stein Magnus Jodal <stein.magnus.jodal@uninett.no>
2006-07-19 13:59:05 +0000

    - Create framework for Maintenance2 subsystem.

---
+++ subsystem/maintenance2/maintenance2.tool
@@ -0,0 +1,5 @@
+name=Maintenance
+uri=/maintenance2/
+icon=/images/toolbox/tools/emotd.gif
+description=Create and edit equipment maintenance schedules.
+priority=1
---
+++ subsystem/maintenance2/nav/web/maintenance2/handler.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+# -*- coding: ISO8859-1 -*-
+#
+# Copyright 2006 UNINETT AS
+#
+# This file is part of Network Administration Visualized (NAV)
+#
+# NAV is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# NAV is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NAV; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+"""
+FIXME
+"""
+
+__copyright__ = "Copyright 2006 UNINETT AS"
+__license__ = "GPL"
+__author__ = "Stein Magnus Jodal (stein.magnus@jodal.no)"
+__id__ = "$Id$"
+
+from mod_python import apache
+
+import nav.db
+from nav.web.URI import URI
+from nav.web.templates.Maintenance2Template import Maintenance2Template
+
+dbconn = nav.db.getConnection('webfront', 'manage')
+db = dbconn.cursor()
+
+def handler(req):
+    """Handler for the Maintenance 2 subsystem."""
+
+    args = URI(req.unparsed_uri)
+
+    # Setup template
+    page = Maintenance2Template()
+    req.content_type = 'text/html'
+    req.send_http_header()
+
+   
+    req.write(page.respond())
+    return apache.OK
+
