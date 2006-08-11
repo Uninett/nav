dae6e4ae8948315c2c0e039a8bdf7ce7777383a2
Stein Magnus Jodal <stein.magnus.jodal@uninett.no>
2006-08-11 13:47:03 +0000

    General: * Add author fields * Add documentation * svn propset svn:keyword Id
    messages2.py
    * Make setMsg() also support updating of messages
    * Make setMsgTask() check if connection exists before creating it
    * Add removeMsgsTasks()
    * Add expireMsg()
    
    handler.py (almost completly rewritten)
    * Do not show replaced messages under planned and active
    * Do show replaced messages under historic, even if not expired
    * Make 'new' section also handle 'edit' and 'followup' (large diff)
    * Fix indentation and breaking of long lines
    * Add comments where missing
    
    Messages2Template.tmpl
    * Add infomsgs list before errors list
    * Remove javascript 'go back' link
    
    Messages2NewTemplate.tmpl (also handles edit and followup)
    * Remove form action, so it submits to itself
    * Add edit_messageid hidden field
    * Fix task.description stripping if to long
    * Fix reselection of selected tasks at the event of errors or edit
    * Add 'replaces message' information (uneditable)
    
    Messages2ListTemplate.tmpl
    * Only show five first messages fully, then list the rest
    * Remove seconds from timestamps
    * In description fields, replace newlines with <br>-tags and more than one
      space with &nbsp;-entities
    * Use 'expires' or 'expired' depending on when message expires
    * Add 'replaces message' and 'replaces by' fields

--- subsystem/messages2/nav/web/messages2/handler.py
+++ subsystem/messages2/nav/web/messages2/handler.py
@@ -19,9 +19,12 @@
 # along with NAV; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+#
+# Author: Stein Magnus Jodal <stein.magnus@jodal.no>
+#
 
 """
-FIXME
+mod_python handler for the Message2 subsystem.
 """
 
 __copyright__ = "Copyright 2006 UNINETT AS"
@@ -57,34 +60,107 @@ def handler(req):
         section = args.path.split('/')[-1]
     else:
         section = 'active'
-    
-    # Create section page
+ 
+    ### SECTIONS
+    # Planned messages (not yet reached publishing time)
     if section == 'planned':
         page = Messages2ListTemplate()
         page.title = 'Planned Messages'
-        page.msgs = messages2.getMsgs(['publish_start > now()'])
+        page.msgs = messages2.getMsgs('publish_start > now() AND publish_end > now() AND replaced_by IS NULL')
+
+    # Historic and replaced messages
     elif section == 'historic':
         page = Messages2ListTemplate()
         page.title = 'Historic Messages'
-        page.msgs = messages2.getMsgs(['publish_end < now()'])
+        page.msgs = messages2.getMsgs('publish_end < now() OR replaced_by IS NOT NULL', 'publish_end DESC')
+
+    # View a message
     elif section == 'view' and args.get('id').isdigit():
         page = Messages2ListTemplate()
         page.title = 'Message'
-        viewid = int(args.get('id'))
-        page.msgs = messages2.getMsgs(['messageid = %d' % viewid])
-    elif section == 'new':
+        msgid = int(args.get('id'))
+        page.msgs = messages2.getMsgs('messageid = %d' % msgid)
+
+    # Expire a message
+    elif section == 'expire' and args.get('id').isdigit():
+        page = Messages2ListTemplate()
+        page.title = 'Expire message'
+        page.infomsgs = []
+        msgid = int(args.get('id'))
+        messages2.expireMsg(msgid)
+        page.infomsgs.append('The following message was expired.')
+        page.msgs = messages2.getMsgs('messageid = %d' % msgid)
+
+    # New, followup and edit message
+    elif section == 'new' or section == 'edit' or section == 'followup':
         page = Messages2NewTemplate()
         page.title = 'Create New Message'
-        page.tasks = maintenance2.getTasks(['maint_end > now()'])
-        page.submit = req.form.has_key('new-do')
+        page.tasks = maintenance2.getTasks('maint_end > now()')
+        page.errors = []
+
+        # Followup
+        if section == 'followup':
+            page.current = 'new' # Just to mark the menu tab
+            if not args.get('id').isdigit():
+                page.errors.append('Message ID in request is not a digit.')
+            else:
+                msgid = int(args.get('id'))
+                page.replaces_messageid = msgid
+                page.replaces_message = messages2.getMsgs('messageid = %d' \
+                    % msgid)[0]
+
+        # Edit
+        if section == 'edit':
+            page.title = 'Edit Message'
+            page.submittext = 'Save Message'
+            if not args.get('id').isdigit():
+                page.errors.append('Message ID in request is not a digit.')
+            else:
+                msgid = int(args.get('id'))
+                msg = messages2.getMsgs('messageid = %d' % msgid)[0]
+
+                page.edit_messageid = msgid
+                page.formtitle = msg['title']
+                page.description = msg['description']
+                page.tech_description = msg['tech_description']
+
+                page.start_year = int(msg['publish_start'].strftime('%Y'))
+                page.start_month = int(msg['publish_start'].strftime('%m'))
+                page.start_day = int(msg['publish_start'].strftime('%d'))
+                page.start_hour = int(msg['publish_start'].strftime('%H'))
+                page.start_min = int(msg['publish_start'].strftime('%M'))
+
+                page.end_year = int(msg['publish_end'].strftime('%Y'))
+                page.end_month = int(msg['publish_end'].strftime('%m'))
+                page.end_day = int(msg['publish_end'].strftime('%d'))
+                page.end_hour = int(msg['publish_end'].strftime('%H'))
+                page.end_min = int(msg['publish_end'].strftime('%M'))
+
+                if type(msg['replaces_message']) is int:
+                    page.replaces_messageid = msg['replaces_message']
+                    page.replaces_message = messages2.getMsgs('messageid = %d' \
+                        % page.replaces_messageid)[0]
+                else:
+                    page.replaces_messageid = False
+
+                if type(msg['tasks']) is list:
+                    page.maint_tasks = []
+                    for task in msg['tasks']:
+                        page.maint_tasks.append(str(task['maint_taskid']))
+                else:
+                    page.maint_tasks = []
+
+        # Form submitted
+        page.submit = req.form.has_key('submit')
         if page.submit:
-            page.errors = []
+            # Get and control form data
             if req.form.has_key('title') and req.form['title']:
                 title = req.form['title']
                 page.formtitle = title
             else:
                 page.errors.append('You did not supply a title.')
 
+            # Descriptions
             if req.form.has_key('description') and req.form['description']:
                 description = req.form['description']
                 page.description = description
@@ -97,15 +173,16 @@ def handler(req):
             else:
                 tech_description = False
 
+            # Publish times
             if (req.form.has_key('start_year') and req.form['start_year']
                 and req.form.has_key('start_month') and req.form['start_month']
                 and req.form.has_key('start_day') and req.form['start_day']
                 and req.form.has_key('start_hour') and req.form['start_hour']
                 and req.form.has_key('start_min') and req.form['start_min']):
                 publish_start = '%4d-%02d-%02d %02d:%02d' % (
-                 int(req.form['start_year']), int(req.form['start_month']),
-                 int(req.form['start_day']), int(req.form['start_hour']),
-                 int(req.form['start_min']))
+                    int(req.form['start_year']), int(req.form['start_month']),
+                    int(req.form['start_day']), int(req.form['start_hour']),
+                    int(req.form['start_min']))
                 publish_start = time.strptime(publish_start, '%Y-%m-%d %H:%M')
 
                 page.start_year = int(req.form['start_year'])
@@ -122,9 +199,9 @@ def handler(req):
                 and req.form.has_key('end_hour') and req.form['end_hour']
                 and req.form.has_key('end_min') and req.form['end_min']):
                 publish_end = '%4d-%02d-%02d %02d:%02d' % (
-                 int(req.form['end_year']), int(req.form['end_month']),
-                 int(req.form['end_day']), int(req.form['end_hour']),
-                 int(req.form['end_min']))
+                    int(req.form['end_year']), int(req.form['end_month']),
+                    int(req.form['end_day']), int(req.form['end_hour']),
+                    int(req.form['end_min']))
                 publish_end = time.strptime(publish_end, '%Y-%m-%d %H:%M')
 
                 page.end_year = int(req.form['end_year'])
@@ -136,8 +213,10 @@ def handler(req):
                 publish_end = time.localtime(int(time.time()) + 7*24*60*60)
 
             if publish_start > publish_end:
-                page.errors.append('Publish end is before start.')
-
+                page.errors.append('Publish end is before start, ' \
+                    + 'message will never be published.')
+            
+            # Maintenance tasks
             if req.form.has_key('maint_tasks'):
                 maint_tasks = req.form['maint_tasks']
                 if type(maint_tasks) is not list:
@@ -147,40 +226,78 @@ def handler(req):
                 except ValueError, error:
                     pass
                 page.maint_tasks = maint_tasks
+            else:
+                maint_tasks = []
 
+            # Followup
+            if req.form.has_key('replaces_messageid') \
+                and req.form['replaces_messageid']:
+                replaces_messageid = int(req.form['replaces_messageid'])
+                page.replaces_messageid = replaces_messageid
+            else:
+                replaces_messageid = False
+
+            # Get ID of message edited
+            if section == 'edit':
+                if req.form.has_key('edit_messageid') \
+                    and req.form['edit_messageid']:
+                    edit_messageid = int(req.form['edit_messageid'])
+                else:
+                    page.errors.append('ID of edited message is missing.')
+
+            # Get session data
             author = req.session['user'].login
-            replaces_message = False
 
-            if len(page.errors) == 0:
-                # Insert message
-                msgid = messages2.setMsg(title, description, tech_description,
-                 publish_start, publish_end, author, replaces_message)
+            # If any data not okay, form is showed with list of errors on top.
+            # There is no need to do anything further here.
+            if len(page.errors):
+                pass
+            # No errors, update database
+            else:
+                if section == 'edit':
+                    msgid = edit_messageid
+                else:
+                    msgid = False
+
+                # Update/Insert message
+                msgid = messages2.setMsg(msgid, title, description,
+                    tech_description, publish_start, publish_end, author,
+                    replaces_messageid)
+
+                # For updates, remove all existing task connections
+                if section == 'edit':
+                    messages2.removeMsgTasks(msgid)
 
                 # Connect with task
                 for taskid in maint_tasks:
                     messages2.setMsgTask(msgid, int(taskid))
+
+                # Expire replaced messages
+                # If a msg is "unreplaced" it will still be expired
+                #if replaces_messageid:
+                #    messages2.expireMsg(replaces_messageid)
 
                 # Redirect to view?id=$newid and exit
                 req.headers_out['location'] = 'view?id=' + str(msgid)
                 req.status = apache.HTTP_MOVED_TEMPORARILY
                 req.send_http_header()
                 return apache.OK
-    elif section == 'expire':
-        page = Messages2ListTemplate()
-        page.title = 'Create New Message'
-        page.tasks = maintenance2.getTasks(['maint_end > now()'])
-        page.submit = req.form.has_key('new-do')
+
+    # Default: Show active messages (public messages)
     else:
         page = Messages2ListTemplate()
         page.title = 'Active Messages'
-        page.msgs = messages2.getMsgs(['publish_start < now()', 'publish_end > now()'])
+        page.msgs = messages2.getMsgs('publish_start < now() AND publish_end > now() AND replaced_by IS NULL')
 
     # Create menu
     page.menu = [{'link': 'active', 'text': 'Active', 'admin': False},
                 {'link': 'planned', 'text': 'Planned', 'admin': False},
                 {'link': 'historic', 'text': 'Historic', 'admin': False},
                 {'link': 'new', 'text': 'Create new', 'admin': True}]
-    page.current = section
+    if not page.hasVar('current'):
+        page.current = section
+    if not page.hasVar('submittext'):
+        page.submittext = page.title
   
     # Done, output the page
     req.content_type = 'text/html'
--- subsystem/messages2/nav/web/messages2/messages2.py
+++ subsystem/messages2/nav/web/messages2/messages2.py
@@ -19,6 +19,9 @@
 # along with NAV; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+#
+# Author: Stein Magnus Jodal <stein.magnus@jodal.no>
+#
 
 """
 Common methods for message management
@@ -36,20 +39,42 @@ import nav.db
 dbconn = nav.db.getConnection('webfront', 'manage')
 db = dbconn.cursor()
 
-def getMsgs(where = []):
-    select = """SELECT messageid, title, description, tech_description,
-        publish_start, publish_end, author, last_changed, replaces_message
-        FROM message"""
-    where = " AND ".join(where)
-    order = "publish_start DESC"
+def getMsgs(where = False, order = 'publish_start DESC'):
+    """
+    Get message with connected tasks
 
-    if len(where):
+    Input:
+        where   Where clause for the query
+        order   Result order
+
+    Returns:
+        If messages found, returns dictionary with results
+        If no messages found, returns false
+
+    """
+
+    select = """SELECT
+        messageid, title, description, tech_description,
+        publish_start, publish_end, author, last_changed,
+        replaces_message, replaces_message_title, replaces_message_description,
+        replaces_message_tech_description, replaces_message_publish_start,
+        replaces_message_publish_end, replaces_message_author,
+        replaces_message_last_changed,
+        replaced_by, replaced_by_title, replaced_by_description,
+        replaced_by_tech_description, replaced_by_publish_start,
+        replaced_by_publish_end, replaced_by_author,
+        replaced_by_last_changed
+        FROM message_with_replaced"""
+
+    if where:
         sql = "%s WHERE %s ORDER BY %s" % (select, where, order)
     else:
         sql = "%s ORDER BY %s" % (select, order)
 
     # FIXME: Log query
     db.execute(sql)
+    if not db.rowcount:
+        return False
     results = db.dictfetchall()
     # FIXME: Log result
 
@@ -59,18 +84,48 @@ def getMsgs(where = []):
     
     return results
 
-def setMsg(title, description, tech_description, publish_start,
- publish_end, author, replaces_message):
-    sql = """INSERT INTO message (
-            title, description, tech_description, publish_start, publish_end,
-            author, replaces_message
-        ) VALUES (
-            %(title)s, %(description)s, %(tech_description)s,
-            %(publish_start)s, %(publish_end)s, %(author)s,
-            %(replaces_message)s
-        )"""
+def setMsg(msgid, title, description, tech_description, publish_start,
+    publish_end, author, replaces_message):
+    """
+    Insert or update a message
+
+    Input:
+        msgid               Message ID if update, set to false if new message
+        title               Message title
+        description         The message itself
+        tech_description    A more detailed technical description
+        publish_start       Time to publish message
+        publish_end         Time to expire message
+        author              Username of author
+        replaces_message    Message ID of old message replaced by this one
+
+    Returns:
+        msgid               ID of updated or inserted message
+    """
+
+    if msgid:
+        sql = """UPDATE message SET
+                title = %(title)s,
+                description = %(description)s,
+                tech_description = %(tech_description)s,
+                publish_start = %(publish_start)s,
+                publish_end = %(publish_end)s,
+                author = %(author)s,
+                replaces_message = %(replaces_message)d
+            WHERE
+                messageid = %(messageid)d"""
+    else:
+        sql = """INSERT INTO message (
+                title, description, tech_description, publish_start,
+                publish_end, author, replaces_message
+            ) VALUES (
+                %(title)s, %(description)s, %(tech_description)s,
+                %(publish_start)s, %(publish_end)s, %(author)s,
+                %(replaces_message)d
+            )"""
 
     data = {
+        'messageid': msgid or None,
         'title': title,
         'description': description,
         'tech_description': tech_description or None,
@@ -82,12 +137,25 @@ def setMsg(title, description, tech_description, publish_start,
 
     # FIXME: Log query
     db.execute(sql, data)
-    db.execute("SELECT CURRVAL('message_messageid_seq')")
-    id = db.dictfetchone()['currval']
+    if not msgid:
+        db.execute("SELECT CURRVAL('message_messageid_seq')")
+        msgid = db.dictfetchone()['currval']
     # FIXME: Log result
-    return id
+    return msgid
 
 def getMsgTasks(msgid):
+    """
+    Get tasks connected to a message
+
+    Input:
+        msgid   Message ID of message
+
+    Returns:
+        If messages found, returns dictionary with results
+        If no messages found, returns false
+
+    """
+
     sql = """SELECT maint_taskid, maint_start, maint_end, description,
         author, state
         FROM maint_task NATURAL JOIN message_to_maint_task
@@ -97,19 +165,93 @@ def getMsgTasks(msgid):
 
     # FIXME: Log query
     db.execute(sql, data)
+    if not db.rowcount:
+        return False
     result = db.dictfetchall()
     # FIXME: Log result
     return result
 
 def setMsgTask(msgid, taskid):
-    sql = """INSERT INTO message_to_maint_task (
-            messageid, maint_taskid
-        ) VALUES (
-            %(messageid)d, %(maint_taskid)d
-        )"""
+    """
+    Create connection between message and maintenance task
+
+    Input:
+        msgid   Message ID
+        taskid  Maintenance task ID
+
+    Returns:
+        If connection already exists, returns False
+        If connection was created, reeturns True
+
+    """
 
     data = {'messageid': msgid, 'maint_taskid': taskid}
+
+    # Check if message and task is already linked
+    sql = """SELECT messageid, maint_taskid
+        FROM message_to_maint_task
+        WHERE messageid = %(messageid)d AND maint_taskid = %(maint_taskid)d"""
+    # FIXME: Log query
+    db.execute(sql, data)
+    # FIXME: Log result
+
+    if db.rowcount:
+        return False
+    else:
+        sql = """INSERT INTO message_to_maint_task (
+                messageid, maint_taskid
+            ) VALUES (
+                %(messageid)d, %(maint_taskid)d
+            )"""
+
+        # FIXME: Log query
+        db.execute(sql, data)
+        # FIXME: Log result
+        return True
+
+def removeMsgTasks(msgid):
+    """
+    Remove connection between given message and all tasks
+
+    Input:
+        msgid   Message ID
+
+    Returns:
+        If connections removed, returns True
+        If no connections removed, returns False
+
+    """
+    
+    data = {'messageid': msgid}
+    sql = "DELETE FROM message_to_maint_task WHERE messageid = %(messageid)d"
 
     # FIXME: Log query
     db.execute(sql, data)
     # FIXME: Log result
+
+    if db.rowcount:
+        return True
+    else:
+        return False
+
+def expireMsg(msgid):
+    """
+    Expire a message by setting publish_end to current time
+
+    Input:
+        msgid   ID of message to be expired
+
+    Returns:
+        Always returns true, unless some error occurs.
+
+    """
+
+    sql = """UPDATE message SET publish_end = now()
+        WHERE messageid = %(messageid)d"""
+
+    data = {'messageid': msgid}
+
+    # FIXME: Log query
+    db.execute(sql, data)
+    # FIXME: Log result
+    return True
