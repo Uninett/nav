310390b50d4c294418d0d7cf786e21e819305e22
Stein Magnus Jodal <stein.magnus.jodal@uninett.no>
2006-08-25 13:17:04 +0000

    - Add access for unauthenticated users to messages2/maintenance2.

--- doc/sql/navprofiles.sql
+++ doc/sql/navprofiles.sql
@@ -818,7 +818,8 @@ INSERT INTO Privilege VALUES (4, 'report_access');
 /*
   Set some default web_access privileges
 */
--- Anonymous users need access to a few things, like the login page and images and soforth
+-- Anonymous users need access to a few things, like the login page and images
+-- and soforth
 INSERT INTO AccountGroupPrivilege (accountgroupid, privilegeid, target) VALUES (2, 2, '^/images/.*');
 INSERT INTO AccountGroupPrivilege (accountgroupid, privilegeid, target) VALUES (2, 2, '^/js/.*');
 INSERT INTO AccountGroupPrivilege (accountgroupid, privilegeid, target) VALUES (2, 2, '^/style/.*');
@@ -828,6 +829,8 @@ INSERT INTO AccountGroupPrivilege (accountgroupid, privilegeid, target) VALUES (
 INSERT INTO AccountGroupPrivilege (accountgroupid, privilegeid, target) VALUES (2, 2, '^/index(.py)?/(index|login|logout|userinfo|passwd)\\b');
 INSERT INTO AccountGroupPrivilege (accountgroupid, privilegeid, target) VALUES (2, 2, '^/messages/(main\\.py|rss|historic|active|planned|view|maintenance)\\b');
 INSERT INTO AccountGroupPrivilege (accountgroupid, privilegeid, target) VALUES (2, 2, '^/messages/?$');
+INSERT INTO AccountGroupPrivilege (accountgroupid, privilegeid, target) VALUES (2, 2, '^/messages2/(active|historic|planned|view|rss)\\b');
+INSERT INTO AccountGroupPrivilege (accountgroupid, privilegeid, target) VALUES (2, 2, '^/maintenance2/(calendar|active|historic|planned|view)\\b');
 INSERT INTO AccountGroupPrivilege (accountgroupid, privilegeid, target) VALUES (2, 2, '^/vlanPlot\\b');
 INSERT INTO AccountGroupPrivilege (accountgroupid, privilegeid, target) VALUES (2, 2, '^/vPServer/servlet/vPServer\\b');
 
