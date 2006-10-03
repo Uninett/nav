473ade1405176571ffbcb0af00f5f4b87a35bdfb
Morten Brekkevold <morten.brekkevold@uninett.no>
2006-10-03 08:21:03 +0000

    Backported security fix from r3641 on trunk.

--- subsystem/webFront/lib/nav/web/state.py
+++ subsystem/webFront/lib/nav/web/state.py
@@ -38,6 +38,11 @@ import sys
 import fcntl
 import nav.errors
 import nav.web
+import sre as re
+try:
+    from mod_python import apache
+except:
+    pass
 
 sessionCookieName = 'nav_sessid'
 tempDir = '/tmp'
@@ -51,6 +56,9 @@ def getUniqueString(entropy=''):
     'entropy' parameter.  It's not really magic, it just picks the
     current system time and a pseudo random number and returns the md5
     digest of these put together."""
+    # This function returns a 32 character md5 hash string.  The validate_sid
+    # function will validate ids accordingly.  If you change this
+    # implementation, you must also change the validate_sid implementation.
     hash = md5.new()
     hash.update(str(time.time()))
     hash.update(str(random.random()))
@@ -59,6 +67,16 @@ def getUniqueString(entropy=''):
         hash.update(entropy)
     return hash.hexdigest()
 
+session_valid_re = re.compile('[0-9a-f]{32}$')
+def validate_sid(sid):
+    """Verifies the validity of a session id.
+
+    A valid session ID consists of exactly 32 characters, and must only
+    contain the characters 0-9 and a-f.  Invalid session identifiers supplied
+    by clients may result in directory traversal attacks (they might contain /
+    and .. sequences).
+    """    
+    return session_valid_re.match(sid)
 
 def setupSession(req):
     """
@@ -174,6 +192,12 @@ def sessionFilename(session):
         sessid = session
     else:
         sessid = session.id
+
+    if not validate_sid(sessid):
+        apache.log_error("Invalid session ID: %s" % sessid,
+                         apache.APLOG_ERROR)
+        raise apache.SERVER_RETURN, apache.HTTP_INTERNAL_SERVER_ERROR
+
     return path.join(tempDir, '%s%s' % (serialPrefix, sessid))
 
 class Session(dict):
