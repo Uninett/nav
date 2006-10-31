ab6946f8b2b3ab042616525cecc2929c0f19ea1b
Morten Brekkevold <morten.brekkevold@uninett.no>
2006-10-31 10:38:04 +0000

    Further refactoring of vprrd.py, and a hack to fix the "black load" syndrome.
    The hack is called a timewarp; if the data values retrieved from an RRD are
    NaN values and timewarping is enabled, we skew the start/stop times by -60
    seconds and try to extract those numbers instead.  This will allow Cricket up
    to 60 seconds to process its targets before updating an RRD file, and should
    ensure that no unnecessary "black load" appears in the Traffic Map applet.
    
    * vprrd.py
      Allow querying any number of datasources per RRD.  Introduce the concept of
      timewarping.
      (rrdvalues): New function.

--- src/webapps/vPServer/vprrd.py
+++ src/webapps/vPServer/vprrd.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: ISO8859-1 -*-
 # Copyright 2004 Norwegian University of Science and Technology
+# Copyright 2006 UNINETT AS
 #
 # This file is part of Network Administration Visualized (NAV)
 #
@@ -20,9 +21,11 @@
 #
 #
 # Authors: Kristian Eide <kreide@online.no>
+#          Morten Brekkevold <morten.brekkevold@uninett.no>
 #
-"""
-Fetch RRD data (used by vPServer)
+"""Helper program to allow vPServer to fetch RRD data.
+
+vPServer establishes communication with this program through a pipe.
 """
 
 import rrdtool
@@ -30,27 +33,42 @@ from sys import stdin
 import os.path
 import shlex
 
+doWarp = True # Should we perform timewarping?
+timewarp = 60 # Seconds
+
+def rrdvalues(start, stop, rrdfile, datasources, consolidation):
+    """Extract values from RRD files and return as a list."""
+    defs = [ 'DEF:value' + str(ds[0]) + '=' + rrdfile + ':' + ds[1] +
+             ':AVERAGE'
+             for ds in enumerate(datasources) ]
+    prints = [ 'PRINT:value' + str(ds[0]) + ':' + consolidation + ':%6.2lf'
+               for ds in enumerate(datasources) ]
+
+    result = rrdtool.graph('-', '-s ' + str(start) + ':' + str(stop),
+                           *(defs+prints))
+    return [v.strip() for v in result[2]]
+
 consolidation = stdin.readline().strip()
 time = stdin.readline().strip().split(",")
+start = long(time[0])
+stop = long(time[1])
 
 for line in stdin:
     if len(line.strip()) == 0: continue
     t = shlex.split(line)
-    if os.path.isfile(t[0]):
-        if (len(t) == 2):
-            r = rrdtool.graph('-', '-s ' + time[0] + ':' + time[1],
-                              'DEF:value=' + t[0] + ':' + t[1] + ':AVERAGE',
-                              'PRINT:value:' + consolidation + ':%6.2lf')
-            value = r[2][0].strip()
-            print value
-        elif (len(t) == 3):
-            r = rrdtool.graph('-', '-s ' + time[0] + ':' + time[1],
-                              'DEF:value1=' + t[0] + ':' + t[1] + ':AVERAGE',
-                              'DEF:value2=' + t[0] + ':' + t[2] + ':AVERAGE',
-                              'PRINT:value1:' + consolidation + ':%6.2lf',
-                              'PRINT:value2:' + consolidation + ':%6.2lf')
-            value1 = r[2][0].strip()
-            value2 = r[2][1].strip()
-            print value1 + ' ' + value2
+    rrdfile = t[0]
+    dss = t[1:]
+    
+    if os.path.isfile(rrdfile):
+        values = rrdvalues(start, stop, rrdfile, dss, consolidation)
+        if doWarp and stop == 0:
+            if "nan" in [v.lower() for v in values]:
+                # The RRD file's last data point might not have been inserted
+                # yet, so we look `timewarp` seconds further back in time in a
+                # desperate attempt to avoid so-called "black load" on the
+                # traffic map.
+                values = rrdvalues(start-timewarp, stop-timewarp, rrdfile,
+                                   dss, consolidation)
+        print " ".join(values)
     else:
         print 'fnf'
