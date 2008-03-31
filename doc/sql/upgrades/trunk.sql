0c904b42c68c57d27973ba15c8958785b6944216
John-Magne Bredal <john.m.bredal@ntnu.no>
2008-03-31 11:18:03 +0000

    manage: - La til felt contact i org-tabellen
    arnold:
    - La til endringer som må til for at nye versjonen av Arnold skal kunne kjøre.

--- doc/sql/upgrades/trunk.sql
+++ doc/sql/upgrades/trunk.sql
@@ -15,3 +15,54 @@
  *
 */
 
+
+--
+-- Add field 'contact' to org-table in manage-database
+--
+
+\connect manage
+
+ALTER TABLE org ADD contact VARCHAR;
+
+
+--
+-- The following changes to the arnold-database are necessary to migrate to new version of Arnold.
+--
+
+\connect arnold
+
+-- Create new table for storing of quarantine vlans.
+CREATE TABLE quarantine_vlans (
+quarantineid SERIAL PRIMARY KEY,
+vlan INT,
+description VARCHAR
+);
+
+-- Changes regarding table blocked_reasons
+ALTER TABLE blocked_reason RENAME text TO name;
+
+-- Changes regarding table identity
+ALTER TABLE identity DROP CONSTRAINT identity_blocked_status;
+ALTER TABLE identity ADD CONSTRAINT identity_blocked_status CHECK (blocked_status='disabled' OR blocked_status='enabled' OR blocked_status='quarantined');
+
+ALTER TABLE identity DROP COLUMN swsysname;
+ALTER TABLE identity DROP COLUMN swvendor;
+ALTER TABLE identity DROP COLUMN swip;
+ALTER TABLE identity DROP COLUMN swmodule;
+ALTER TABLE identity DROP COLUMN swport;
+ALTER TABLE identity DROP COLUMN swifindex;
+ALTER TABLE identity DROP COLUMN community;
+ALTER TABLE identity DROP COLUMN multiple;
+ALTER TABLE identity DROP COLUMN secret;
+ALTER TABLE identity DROP COLUMN userlock;
+
+ALTER TABLE identity ADD fromvlan INT;
+ALTER TABLE identity ADD tovlan INT;
+
+-- Changes regarding table block
+ALTER TABLE block DROP COLUMN private;
+ALTER TABLE block DROP COLUMN userid;
+
+ALTER TABLE block ADD activeonvlans VARCHAR;
+ALTER TABLE block ADD detainmenttype VARCHAR CHECK (detainmenttype='disable' OR detainmenttype='quarantine');
+ALTER TABLE block ADD quarantineid INT REFERENCES quarantine_vlans ON UPDATE CASCADE ON DELETE CASCADE;
