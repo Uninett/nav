38c36cfea1080255c83a5d8bf92fecb925a2d631
Morten Brekkevold <morten.brekkevold@uninett.no>
2008-05-20 11:20:37 +0200

    Added new trunk.sql upgrade script for upgrading from 3.4.0
    The "trunk" name was kept for historic reasons.

---
+++ doc/sql/upgrades/trunk.sql
@@ -0,0 +1,19 @@
+/*
+ *
+ * This preliminary SQL script is designed to upgrade your NAV
+ * database from version 3.4 to the current trunk revision (i.e. the
+ * tip of the default development branch).  Please update this with
+ * every change you make to the database initialization scripts.  It
+ * will eventually become the update script for the next release.
+ *
+ * Also, if you are keeping your installation in sync with the default
+ * branch, you should watch this file for changes and run them when
+ * updating (check the diffs!)
+ *
+ * Connect to PostgreSQL as the postgres superuser or the nav database user
+ * like this:
+ *
+ *  psql -f trunk.sql manage <username>
+ *
+*/
+
