529872fb2316f3c39d027099720e7e3efadec7ee
Magnus Eide <magnus.eide@uninett.no>
2008-06-16 13:43:01 +0200

    Some cleanup

--- subsystem/alertprofiles/error.php
+++ subsystem/alertprofiles/error.php
@@ -8,7 +8,7 @@ class Error {
 	var $message;
 	var $type_name;
 	var $sev; 
-	
+
 	function Error ($errtype, $sev = 0) {
 		$this->type_name = array(
 				gettext('Uknown error'),
@@ -17,23 +17,23 @@ class Error {
 				gettext('Security error'),
 				gettext('IO error'),
 				gettext('AlertProfiles PHP Errorhandler')
-			);
+				);
 		$this->type = $errtype;
 		$this->sev = $sev;
 	}
-	
+
 	function getHeader () {
 		return $this->type_name[$this->type];
 	}
-	
+
 	function setMessage ($msg) {
 		$this->message = $msg;
 	}
-	
+
 	function isSevere() {
 		return ($this->sev == 1);
 	}
-	
+
 	function getHTML () {
 		$html =  "<table width=\"100%\" class=\"feilWindow\"><tr><td class=\"mainWindowHead\"><h2>";
 		$html .= $this->GetHeader();
@@ -60,51 +60,51 @@ function myErrorHandler($errno, $errstr, $errfile, $errline)
 				echo "AlertProfiles error-handler:<b>FATAL</b> [$errno] <h3>$errstr</h3><p>\n";
 				echo "  Fatal error in line $errline of file $errfile";
 				echo ", PHP " . PHP_VERSION . " (" . PHP_OS . ")<br />
-				$errfile [$errline]\n";
+					$errfile [$errline]\n";
 				echo "Aborting...<br />\n";
 			}
 			exit(1);
-		break;
+			break;
 		case E_ERROR:
 			$ne = new Error(5, 1);
 			$ne->message = gettext("<b>ERROR</b> [$errno] <h3>$errstr</h3><p>
-			$errfile<br>on line [$errline]");
+					$errfile<br>on line [$errline]");
 			$error[] = $ne;
-		break;
+			break;
 		case E_NOTICE:
 			$ne = new Error(5);
 			$ne->message = gettext("AlertProfiles error-handler:<b>WARNING</b> [$errno] <h3>$errstr</h3><p>
-			$errfile<br>on line [$errline]\n");
+					$errfile<br>on line [$errline]\n");
 			$error[] = $ne;
-		break;
+			break;
 		default:
 			$ne = new Error(5, 1);
 			$ne->message = gettext("AlertProfiles error-handler:Unkown error type: [$errno] <h3>$errstr</h3><p>
-			$errfile<br>on line [$errline]\n");
+					$errfile<br>on line [$errline]\n");
 			$error[] = $ne;
-		break;
+			break;
 	}
 }
 
 function flusherrors() {
 	global $error;
-/* 	print "<pre>ERRORS:"; */
-/* 	print_r($error); */
-/* 	print "</pre>"; */
+	/* 	print "<pre>ERRORS:"; */
+	/* 	print_r($error); */
+	/* 	print "</pre>"; */
 
 	while (is_array($error) && $err = array_pop($error)) {
 
 		$errorlvl = isset($_GET['debug']) ? $_GET['debug'] : AP_DEBUG_LEVEL;
-		
+
 		if ( ($err->isSevere()  and $errorlvl > 0 ) or 
-			($errorlvl > 1) ) {
+				($errorlvl > 1) ) {
 			if (AP_DEBUG_TYPE == AP_DEBUG_INLINE) {
 				print '<table width="100%" class="feilWindow">
 					<tr><td class="mainWindowHead" colspan="2">
-						<h2>';
+					<h2>';
 				print $err->GetHeader();
 				print "</h2></td></tr>";
-				
+
 				print '<tr><td><img alt="error" src="images/warning.png"></td>
 					<td><p>';
 				print $err->message . "</td></tr></table>";
@@ -115,7 +115,7 @@ function flusherrors() {
 				print "<tr><td><p>" . $err->message . "</td></tr></table>";
 			}
 		}
-		
+
 	}
 }
 
