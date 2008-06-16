bf8a3965f3279ed82b5816f211c5325616c62498
Magnus Eide <magnus.eide@uninett.no>
2008-06-16 13:27:36 +0200

    Separate error functions

--- subsystem/alertprofiles/index.php
+++ subsystem/alertprofiles/index.php
@@ -33,157 +33,26 @@
 // Report all errors except E_NOTICE
 error_reporting (E_ALL ^ E_NOTICE);
 
+require_once 'config.php';
+require_once 'error.php';
+require_once 'php4support.php';
+require_once 'databaseHandler.php';
+require_once 'session.php';
 
+require_once 'dbinit.php';
 
-/*
-	******************
-	ERROR  Handling
-*/
+require_once 'leading_zero.function.php';
+require_once 'hasPrivilege.function.php';
+require_once 'check_syntax.function.php';
 
+require_once 'auth.php';
 
-
-
-/* 
- * Dette er en generell feilmeldingsklasse. 
- */
-class Error {
-	var $type;
-	var $message;
-	var $type_name;
-	var $sev; 
-	
-	function Error ($errtype, $sev = 0) {
-		$this->type_name = array(
-				gettext('Uknown error'),
-				gettext('Log in error'), 
-				gettext('Database error'),
-				gettext('Security error'),
-				gettext('IO error'),
-				gettext('AlertProfiles PHP Errorhandler')
-			);
-		$this->type = $errtype;
-		$this->sev = $sev;
-	}
-	
-	function getHeader () {
-		return $this->type_name[$this->type];
-	}
-	
-	function setMessage ($msg) {
-		$this->message = $msg;
-	}
-	
-	function isSevere() {
-		return ($this->sev == 1);
-	}
-	
-	function getHTML () {
-		$html =  "<table width=\"100%\" class=\"feilWindow\"><tr><td class=\"mainWindowHead\"><h2>";
-		$html .= $this->GetHeader();
-		$html .= "</h2></td></tr>";
-		$html .= "<tr><td><p>" . $this->message . "</td></tr></table>";
-		return $html;
-	}
-
-}
-
-global $error;
-
-// set the error reporting level for this script
-//error_reporting(E_ALL);
-
-
-// error handler function
-function myErrorHandler($errno, $errstr, $errfile, $errline) 
-{
-	global $error;
-	switch ($errno) {
-		case E_ERROR:
-			if (AP_DEBUG_LEVEL > 0) {
-				echo "AlertProfiles error-handler:<b>FATAL</b> [$errno] <h3>$errstr</h3><p>\n";
-				echo "  Fatal error in line $errline of file $errfile";
-				echo ", PHP " . PHP_VERSION . " (" . PHP_OS . ")<br />
-				$errfile [$errline]\n";
-				echo "Aborting...<br />\n";
-			}
-			exit(1);
-		break;
-		case E_ERROR:
-			$ne = new Error(5, 1);
-			$ne->message = gettext("<b>ERROR</b> [$errno] <h3>$errstr</h3><p>
-			$errfile<br>on line [$errline]");
-			$error[] = $ne;
-		break;
-		case E_NOTICE:
-			$ne = new Error(5);
-			$ne->message = gettext("AlertProfiles error-handler:<b>WARNING</b> [$errno] <h3>$errstr</h3><p>
-			$errfile<br>on line [$errline]\n");
-			$error[] = $ne;
-		break;
-		default:
-			$ne = new Error(5, 1);
-			$ne->message = gettext("AlertProfiles error-handler:Unkown error type: [$errno] <h3>$errstr</h3><p>
-			$errfile<br>on line [$errline]\n");
-			$error[] = $ne;
-		break;
-	}
-}
-
-function flusherrors() {
-	global $error;
-/* 	print "<pre>ERRORS:"; */
-/* 	print_r($error); */
-/* 	print "</pre>"; */
-
-	while (is_array($error) && $err = array_pop($error)) {
-
-		$errorlvl = isset($_GET['debug']) ? $_GET['debug'] : AP_DEBUG_LEVEL;
-		
-		if ( ($err->isSevere()  and $errorlvl > 0 ) or 
-			($errorlvl > 1) ) {
-			if (AP_DEBUG_TYPE == AP_DEBUG_INLINE) {
-				print '<table width="100%" class="feilWindow">
-					<tr><td class="mainWindowHead" colspan="2">
-						<h2>';
-				print $err->GetHeader();
-				print "</h2></td></tr>";
-				
-				print '<tr><td><img alt="error" src="images/warning.png"></td>
-					<td><p>';
-				print $err->message . "</td></tr></table>";
-			} elseif (AP_DEBUG_TYPE == AP_DEBUG_FILE)  {
-				print "<table width=\"100%\" class=\"feilWindow\"><tr><td class=\"mainWindowHead\"><h2>";
-				print "FILE";
-				print "</h2></td></tr>";
-				print "<tr><td><p>" . $err->message . "</td></tr></table>";
-			}
-		}
-		
-	}
-}
+require_once 'listing.php';
 
 // set to the user defined error handler
 $old_error_handler = set_error_handler("myErrorHandler");
 
-
-
-require("config.php");
-require("php4support.php");
-require("databaseHandler.php");
-require("session.php");
-
-require("dbinit.php");
-
-require("leading_zero.function.php");
-require("hasPrivilege.function.php");
-require("check_syntax.function.php");
-
-require("auth.php");
-
 header("Content-Type: text/html; charset=utf-8");
-
-
-
 
 
 // I18N support information here
@@ -208,9 +77,6 @@ $domain = 'messages';
 bindtextdomain($domain, "./locale/");
 textdomain($domain);
 
-
-
-require("listing.php");
 ?>
 
 <!-- DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
