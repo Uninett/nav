973fd811dc17a84b988b3831c29f08724381b323
Magnus Eide <magnus.eide@uninett.no>
2008-06-18 13:46:43 +0200

    Add alertprofiles to urls.py

--- subsystem/lib-python/src/nav/django/urls.py
+++ subsystem/lib-python/src/nav/django/urls.py
@@ -33,6 +33,8 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     # Give the ipdevinfo namespace to the IP Device Info subsystem
     (r'^ipdevinfo/', include('nav.web.ipdevinfo.urls')),
+    (r'^networkexplorer/', include('nav.web.networkexplorer.urls')),
+    (r'^alertprofiles-django/', include('nav.web.alertprofiles.urls')),
 )
 
 ### URL patterns for non-Django subsystems
