8aa10d84ecdff3b8b9ff4c6ed603f923b263e805
Magnus Eide <magnus.eide@uninett.no>
2008-06-27 16:06:56 +0200

    Utility methods is_admin, resolve_account_admin_and_owner and order_filter_group_content added to utils.py

--- subsystem/alertprofiles/nav/web/alertprofiles/utils.py
+++ subsystem/alertprofiles/nav/web/alertprofiles/utils.py
@@ -28,7 +28,8 @@ __license__ = "GPL"
 __author__ = "Magnus Motzfeldt Eide (magnus.eide@uninett.no)"
 __id__ = "$Id$"
 
-from nav.models.profiles import Filter, FilterGroup, Account
+from nav.django.utils import get_account
+from nav.models.profiles import Filter, FilterGroup, FilterGroupContent, Account
 
 # Admingroup is identified by having id/primary key 1.
 ADMINGROUP = 1
@@ -55,3 +56,47 @@ def account_owns_filters(account, *filters):
                     return True
                 else:
                     return False
+
+def is_admin(account):
+    """Check if user is a member of the administrator group"""
+    return account.accountgroup_set.filter(pk=ADMINGROUP).count() > 0;
+
+def resolve_account_admin_and_owner(request):
+    """Primarily used before saving filters and filter groups.
+    Gets account, checks if user is admin, and sets owner to a appropriate
+    value.
+    """
+    account = get_account(request)
+    admin = is_admin(account)
+
+    owner = Account()
+    if request.POST.get('owner') or not admin:
+        owner = account
+
+    return (account, admin, owner)
+
+def order_filter_group_content(filter_group):
+    """Filter group content is ordered by priority where each filters priority
+    is the previous filters priority incremented by one, starting at 1. Here we
+    loop through all the filters and check if they are ordered that way, and if
+    they are not, we order them that way.
+
+    Returns the last filters priority (0 if there are no filters)
+    """
+    filter_group_content = FilterGroupContent.objects.filter(
+            filter_group=filter_group.id
+        ).order_by('priority')
+
+    if len(filter_group_content) > 0:
+        prev_priority = 0
+        for f in filter_group_content:
+            priority = f.priority
+            if priority - prev_priority != 1:
+                f.priority = prev_priority + 1
+                f.save()
+            prev_priority = f.priority
+
+        return prev_priority
+    else:
+        return 0
+
