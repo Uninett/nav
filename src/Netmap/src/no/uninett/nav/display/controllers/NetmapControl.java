786a9b7c47f3b1cb40eec385cd615f46f652b866
Kristian Klette <kristian.klette@uninett.no>
2008-07-07 10:07:49 +0200

    Fixed parsing of cpu-load when its unknown (non-double value)

--- src/Netmap/src/no/uninett/nav/display/controllers/NetmapControl.java
+++ src/Netmap/src/no/uninett/nav/display/controllers/NetmapControl.java
@@ -57,12 +57,18 @@ public class NetmapControl extends ControlAdapter {
 	    item.setFixed(true);
 	    JPopupMenu t = new JPopupMenu();
 	    if (item.isInGroup("graph.nodes")){
+		    String load;
+		    try {
+			load = String.format("%.2f", Double.parseDouble(item.getString("cpuload"))) + " %";
+		    } catch (java.lang.NumberFormatException parseException){
+			    load = "unknown";
+		    }
 		    t.add(new netboxTooltip(
 					    item.getString("sysname"),
 					    item.getString("category"),
 					    item.getString("type"),
 					    item.getString("room"),
-					    String.format("%.2f", Double.parseDouble(item.getString("cpuload"))) + " %"
+					    load
 					   ));
 	    } else if (item.isInGroup("graph.edges")){
 		    Double cap = -1.0;
