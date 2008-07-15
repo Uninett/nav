35b1d62ea58c5a25356b00b52b267b8f8bc2aff0
Magnus Eide <magnus.eide@uninett.no>
2008-07-15 15:55:32 +0200

    Add breadcrumb path component to django/cheeta render functions

--- subsystem/lib-python/src/nav/django/shortcuts.py
+++ subsystem/lib-python/src/nav/django/shortcuts.py
@@ -33,7 +33,7 @@ from django.template import RequestContext
 from django.template.loader import render_to_string
 from django.views.generic import list_detail
 
-def _cheetah_render(cheetah_template_func, rendered_by_django):
+def _cheetah_render(cheetah_template_func, rendered_by_django, path=[]):
     """Inserts a rendered template from Django into a Cheetah template"""
 
     # Insert the result into content_string in the given Cheetah template
@@ -45,21 +45,35 @@ def _cheetah_render(cheetah_template_func, rendered_by_django):
         rendered_by_django = rendered_by_django.encode('utf-8')
     cheetah_template.content_string = rendered_by_django
 
+    # Insert path into cheetah
+    if path:
+        # Same unicode fixing as above, only for the path this time
+        for i, path_part in enumerate(path):
+            # Path_part is a tuple with the title of the link in position 0 and
+            # the url itself in position 1
+            if isinstance(path_part[0], unicode):
+                path[i] = (path_part[0].encode('utf-8'), path_part[1])
+
+        cheetah_template.path = path
+
     # Return a Django HttpResponse with the Cheetah template result
     return HttpResponse(cheetah_template.respond())
 
 def render_to_response(cheetah_template_func, template_name, context,
-        context_instance=None):
+        context_instance=None, path=[]):
     """Mixes Django's render_to_response shortcut with a Cheetah template"""
 
     # Render a Django template with the given context
     rendered = render_to_string(template_name, context, context_instance)
 
     # Pass it on to the Cheetah template
-    return _cheetah_render(cheetah_template_func, rendered)
+    return _cheetah_render(cheetah_template_func, rendered, path)
 
 def object_list(cheetah_template_func, *args, **kwargs):
     """Mixes Django's generic view object_list with a Cheetah template"""
+
+    # Pop path component
+    path = kwargs.pop('path', None)
 
     # Pass on call to original object_list view
     response = list_detail.object_list(*args, **kwargs)
@@ -68,10 +82,13 @@ def object_list(cheetah_template_func, *args, **kwargs):
     rendered = response.content
 
     # Pass it on to the Cheetah template
-    return _cheetah_render(cheetah_template_func, rendered)
+    return _cheetah_render(cheetah_template_func, rendered, path)
 
 def object_detail(cheetah_template_func, *args, **kwargs):
     """Mixes Django's generic view object_detail with a Cheetah template"""
+
+    # Pop path component
+    path = kwargs.pop('path', None)
 
     # Pass on call to original object_detail view
     response = list_detail.object_detail(*args, **kwargs)
@@ -80,4 +97,4 @@ def object_detail(cheetah_template_func, *args, **kwargs):
     rendered = response.content
 
     # Pass it on to the Cheetah template
-    return _cheetah_render(cheetah_template_func, rendered)
+    return _cheetah_render(cheetah_template_func, rendered, path)
