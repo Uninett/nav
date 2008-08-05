e33a2803bdf4fd30f2f43055fb2a75b8c655a4cc
Magnus Eide <magnus.eide@uninett.no>
2008-08-05 15:33:46 +0200

    Add url import for device history

--- subsystem/lib-python/src/nav/django/urls.py
+++ subsystem/lib-python/src/nav/django/urls.py
@@ -33,6 +33,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     # Give the ipdevinfo namespace to the IP Device Info subsystem
     (r'^ipdevinfo/', include('nav.web.ipdevinfo.urls')),
+    (r'^devicehist/', include('nav.web.devicehistory.urls')),
 )
 
 ### URL patterns for non-Django subsystems
