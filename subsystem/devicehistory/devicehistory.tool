2e73ad93ae97246161d46270142d1ea0e6637fc0
Magnus Eide <magnus.eide@uninett.no>
2008-08-05 15:34:02 +0200

    Add directory for device history

---
+++ subsystem/devicehistory/devicehistory.tool
@@ -0,0 +1,5 @@
+name=Device History (beta)
+uri=/devicehist/
+icon=/images/toolbox/tools/deviceman.png
+description=Beta version of the future replacement for Device Management.
+priority=0
---
+++ subsystem/devicehistory/nav/web/devicehistory/urls.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008 UNINETT AS
+#
+# This file is part of Network Administration Visualized (NAV)
+#
+# NAV is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# NAV is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NAV; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Authors: Magnus Motzfeldt Eide <magnus.eide@uninett.no>
+#
+
+__copyright__ = "Copyright 2008 UNINETT AS"
+__license__ = "GPL"
+__author__ = "Magnus Motzfeldt Eide (magnus.eide@uninett.no)"
+__id__ = "$Id$"
+
+from django.conf.urls.defaults import *
+
+from nav.web.devicehistory.views import *
+
+# The patterns are relative to the base URL of the subsystem
+urlpatterns = patterns('',
+    # Search
+    url(r'^$', 'devicehistory',
+        name='devicehistory'),
+)
+
