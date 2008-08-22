e916f2434999f4adab8ee22abfef6161b387112d
Stein Magnus Jodal <stein.magnus.jodal@uninett.no>
2008-08-22 16:57:30 +0200

    Allow authenticated users to visit ipdevinfo

--- doc/sql/upgrades/trunk.sql
+++ doc/sql/upgrades/trunk.sql
@@ -33,3 +33,6 @@ ALTER SEQUENCE logger.message_id_seq RENAME TO log_message_id_seq;
 
 ALTER TABLE logger.type RENAME TO message_type;
 ALTER SEQUENCE logger.type_type_seq RENAME TO message_type_type_seq;
+
+-- Allow authenticated users to visit ipdevinfo
+UPDATE accountgroupprivilege SET target = '^/(preferences|status|navAdmin|report|browse|stats|cricket|machinetracker|ipinfo|l2trace|logger|alertprofiles|ipdevinfo)/?' WHERE target = '^/(preferences|status|navAdmin|report|browse|stats|cricket|machinetracker|ipinfo|l2trace|logger|alertprofiles)/?';
