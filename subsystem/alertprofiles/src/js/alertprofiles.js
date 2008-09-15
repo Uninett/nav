0e19bb3df4a121a1e7abc4ba328098d225b53579
Magnus Eide <magnus.eide@uninett.no>
2008-09-15 13:17:26 +0200

    I obviously know a lot about jquery. Now it should work :)

--- subsystem/alertprofiles/src/js/alertprofiles.js
+++ subsystem/alertprofiles/src/js/alertprofiles.js
@@ -32,18 +32,18 @@ $(function() {
 		var shared_id = $(this).attr('class').split(' ').slice(-1);
 		$("tr." + shared_id).removeClass('hilight');
 	});
-});
 
-$("select#id_operator").ready(function() {
-    if ($(this).val() == 0) {
-        $("select#id_value").removeAttr('multiple');
-    }
-});
+	$("select#id_operator").ready(function() {
+	    if ($(this).val() == 0) {
+		$("select#id_value").removeAttr('multiple');
+	    }
+	});
 
-$("select#id_operator").change(function() {
-    if ($(this).val() == 0) {
-        $("select#id_value").removeAttr('multiple');
-    } else {
-        $("select#id_value").attr('multiple', 'multiple');
-    }
+	$("select#id_operator").change(function() {
+	    if ($(this).val() == 0) {
+		$("select#id_value").removeAttr('multiple');
+	    } else {
+		$("select#id_value").attr('multiple', 'multiple');
+	    }
+	});
 });
