dd356bfcff9295dd592a99cdc68fadf5d66f5d24
Magnus Eide <magnus.eide@uninett.no>
2008-09-23 13:42:46 +0200

    Assign functions in the JS to variables, and use those variables in the jquery events instead of having the whole function there

--- subsystem/alertprofiles/js/alertprofiles.js
+++ subsystem/alertprofiles/js/alertprofiles.js
@@ -25,6 +25,27 @@
  */
 
 $(function() {
+	// Highlights shared time-periods
+	var doHighlight = function() {
+		var shared_id = $(this).attr('class').split(' ').slice(-1);
+		$("tr." + shared_id + " td:first-child").addClass('hilight');
+	}
+
+	// Removes highlight from shared time-periods
+	var removeHighlight = function() {
+		var shared_id = $(this).attr('class').split(' ').slice(-1);
+		$("tr." + shared_id + " td:first-child").removeClass('hilight');
+	}
+
+	// Switch between multiple and single select list in the expression form
+	var switchMultiple = function() {
+		if ($("select#id_operator").val() == 11) {
+			$("select#id_value").attr('multiple', 'multiple');
+		} else {
+			$("select#id_value").removeAttr('multiple');
+		}
+	}
+
 	// Display some help text to the "highlight shared periods" js
 	$("#timeperiods_table_container").prepend("<div class=\"boxes infobox\"><p>" +
 		"If, when hovering over one period in one of the tables, two rows " +
@@ -32,26 +53,14 @@ $(function() {
 		"actually the one and same period. It's just an \"all days\" period." +
 	"</p></div>");
 
+
 	// Highlight shared periods
-	$("#timeperiods_table_container tr.all_days_period").hover(function () {
-		var shared_id = $(this).attr('class').split(' ').slice(-1);
-		$("tr." + shared_id).addClass('hilight');
-	}, function() {
-		var shared_id = $(this).attr('class').split(' ').slice(-1);
-		$("tr." + shared_id).removeClass('hilight');
-	});
+	$("#timeperiods_table_container tr.all_days_period").hover(doHighlight, removeHighlight);
 
-	$("select#id_operator").ready(function() {
-		if ($(this).val() != 11) {
-			$("select#id_value").removeAttr('multiple');
-		}
-	});
+	// Set the right attribute for select list on page load
+	$("select#id_operator").ready(switchMultiple);
 
-	$("select#id_operator").change(function() {
-		if ($(this).val() == 11) {
-			$("select#id_value").attr('multiple', 'multiple');
-		} else {
-			$("select#id_value").removeAttr('multiple');
-		}
-	});
+	// If operator switch is changed, check if we have to add/remove 'multiple'
+	// attribute
+	$("select#id_operator").change(switchMultiple);
 });
