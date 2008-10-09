3942c69747ff5fc3f703fd51966a1b74adebaaf3
Magnus Eide <magnus.eide@uninett.no>
2008-10-09 17:01:51 +0200

    Refactored register_error_events() some. Made a more general (double) loop, instead of looping over each type (location, room, netbox, module) first and then loop another time to actually store the errors.

--- subsystem/devicehistory/nav/web/devicehistory/utils/error.py
+++ subsystem/devicehistory/nav/web/devicehistory/utils/error.py
@@ -39,14 +39,22 @@ STATE_END = 'e'
 _ = lambda a: a
 
 def register_error_events(request, **kwargs):
-    events = []
     messages = Messages(request)
+
+    # Get the username of this user.
     username = request._req.session['user'].login
 
+    # Selection must be a dictionary.
+    # Recognized keys are 'location', 'room', 'netbox' and 'module'.
+    # The values should be simple lists with the ID of the device.
     selection = kwargs.pop('selection', [])
+
+    # The comment for this error.
     comment = kwargs.pop('comment', None)
 
-    source = kwargs.pop('source', 'devicehistory')
+    # Additional information for the event. Default values should do the trick
+    # here, unless you really know what you are doing.
+    source = kwargs.pop('source', 'deviceManagement')
     target = kwargs.pop('target', 'eventEngine')
     severity = kwargs.pop('severity', 0)
     state = kwargs.pop('state', STATE_NONE)
@@ -56,7 +64,8 @@ def register_error_events(request, **kwargs):
     for key in kwargs.keys():
         raise TypeError('register_error_events() got an unexpected keyword argument %s' % key)
 
-    eventq_data = {
+    # Data that will be inserted into the eventq table.
+    default_eventq_data = {
         'source': Subsystem.objects.get(name=source),
         'target': Subsystem.objects.get(name=target),
         'device': None,
@@ -67,106 +76,67 @@ def register_error_events(request, **kwargs):
         'severity': severity,
     }
 
-    eventqvar_data = {
+    # Data that will be inserted into the eventqvar table.
+    default_eventqvar_data = {
         'alerttype': alert_type,
         'comment': comment,
         'username': username,
     }
 
-    if 'location' in selection and len(selection['location']) > 0:
-        for dev in selection['location']:
-            var_data = eventqvar_data
-            var_data['unittype'] = 'location'
-            var_data['locationid'] = dev
+    for type in selection:
+        if type == 'location':
+            devices = selection[type]
+        elif type == 'room':
+            devices = Room.objects.select_related(
+                'location'
+            ).filter(id__in=selection[type])
+        elif type == 'netbox':
+            devices = Netbox.objects.select_related(
+                'device', 'room', 'room__location'
+            ).filter(id__in=selection[type])
+        elif type == 'module':
+            devices = Module.objects.select_related(
+                'device', 'netbox', 'netbox__room', 'netbox__room__location'
+            ).filter(id__in=selection[type])
+        else:
+            continue
 
-            events.append({
-                'meta': {
-                    'type': 'location',
-                    'name': dev,
-                },
-                'eventq_data': eventq_data,
-                'eventqvar_data': var_data,
-            })
-    if 'room' in selection and len(selection['room']) > 0:
-        rooms = Room.objects.select_related(
-            'location'
-        ).filter(id__in=selection['room'])
-        for room in rooms:
-            var_data = eventqvar_data
-            var_data['unittype'] = 'room'
-            var_data['roomid'] = room.id
-            var_data['locationid'] = room.location.id
+        for device in devices:
+            eventq_data = default_eventq_data.copy()
+            eventqvar_data = default_eventqvar_data.copy()
+            eventqvar_data['unittype'] = type
 
-            events.append({
-                'meta': {
-                    'type': 'room',
-                    'name': room,
-                },
-                'eventq_data': eventq_data,
-                'eventqvar_data': var_data,
-            })
-    if 'netbox' in selection and len(selection['netbox']) > 0:
-        boxes = Netbox.objects.select_related(
-            'device', 'room', 'room__location'
-        ).filter(id__in=selection['netbox'])
-        for netbox in boxes:
-            data = eventq_data
-            data['netbox'] = netbox
-            data['device'] = netbox.device
+            if type == 'location':
+                eventqvar_data['locationid'] = device
+            elif type == 'room':
+                eventqvar_data['roomid'] = device.id
+                eventqvar_data['locationid'] = device.location.id
+            elif type == 'netbox':
+                eventq_data['netbox'] = device
+                eventq_data['device'] = device.device
+                eventqvar_data['roomid'] = device.room.id
+                eventqvar_data['locationid'] = device.room.location.id
+            elif type == 'module':
+                eventq_data['subid'] = device.id
+                eventq_data['netbox'] = device.netbox
+                eventq_data['device'] = device.device
+                eventqvar_data['roomid'] = device.netbox.room.id
+                eventqvar_data['locationid'] = device.netbox.room.location.id
 
-            var_data = eventqvar_data
-            var_data['unittype'] = 'netbox'
-            var_data['roomid'] = netbox.room.id
-            var_data['locationid'] = netbox.room.location.id
+            new_event = EventQueue.objects.create(**eventq_data)
+            for key in eventqvar_data:
+                event_vars = EventQueueVar.objects.create(
+                    event_queue=new_event,
+                    variable=key,
+                    value=eventqvar_data[key]
+                )
 
-            events.append({
-                'meta': {
-                    'type': 'box',
-                    'name': netbox,
-                },
-                'eventq_data': data,
-                'eventqvar_data': var_data,
-            })
-    if 'module' in selection and len(selection['module']) > 0:
-        modules = Module.objects.select_related(
-            'device', 'netbox', 'netbox__room', 'netbox__room__location'
-        ).filter(id__in=selection['module'])
-        for module in modules:
-            data = eventq_data
-            data['subid'] = module.id
-            data['netbox'] = module.netbox
-            data['device'] = module.device
-
-            var_data = eventqvar_data
-            var_data['unittype'] = 'module'
-            var_data['roomid'] = module.netbox.room.id
-            var_data['locationid'] = module.netbox.room.location.id
-
-            events.append({
-                'meta': {
-                    'type': 'module',
-                    'name': module,
-                },
-                'eventq_data': data,
-                'eventqvar_data': var_data
-            })
-
-    for event in events:
-        new_event = EventQueue(**event['eventq_data'])
-        new_event.save()
-        for key in event['eventqvar_data']:
-            event_vars = EventQueueVar(
-                event_queue=new_event,
-                variable=key,
-                value=event['eventqvar_data'][key]
-            )
-            event_vars.save()
-        messages.append({
-            'message': _('Registered error on %(type)s %(device)s.') % {
-                'type': event['meta']['type'],
-                'device': event['meta']['name'],
-            },
-            'type': Messages.SUCCESS,
-        })
+                messages.append({
+                    'message': _('Registered error on %(type)s %(device)s.') % {
+                        'type': type,
+                        'device': device,
+                    },
+                    'type': Messages.SUCCESS,
+                })
 
     messages.save()
