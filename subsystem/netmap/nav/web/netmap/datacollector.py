6f0a9fae7f042373d2389c1a4ee73caa593048d3
Kristian Klette <kristian.klette@uninett.no>
2008-11-03 16:24:03 +0100

    Removed mamoth query and split it into several smaller queries. Reduces complexity and should yield better results.

--- subsystem/netmap/nav/web/netmap/datacollector.py
+++ subsystem/netmap/nav/web/netmap/datacollector.py
@@ -35,139 +35,192 @@ def getData(db_cursor = None):
         raise nav.errors.GeneralException("No db-cursor given")
 
     netboxes = {}
-    tmpConnections = {}
-    connections = []
+    connections = {}
 
-    # Get links from the database (give postgresql something to chew on :)
-    #TODO: Remove union stuff as soon as we get a common port-model.
-    query = """
-SELECT DISTINCT ON (swport.swportid, to_swport)
-        netbox.netboxid,
-        connection.netboxid AS to_netboxid,
-        netbox.sysname AS from_sysname,
-        connection.to_sysname,
-        CASE WHEN swport.speed > connection.to_speed
-          THEN swport.speed
-          ELSE connection.to_speed
-        END AS speed,
-        interface AS from_interface,
-        connection.to_interface,
-        module.model AS from_module_model,
-        module.descr AS from_module_desc,
-        connection.model AS to_module_model,
-        connection.descr AS to_module_descr,
-        rrd.path || '/' || rrd.filename AS rrdfile,
-        CASE
-          WHEN rrd.value::integer = swport.swportid THEN 'n'
-          ELSE 'y'
-        END AS reversed,
-        netident,
-        nettype,
-        0 AS is_gwport,
-        swport.swportid AS from_portid,
-        connection.to_swport AS to_portid
-FROM netbox
-  JOIN module USING (netboxid)
-  JOIN swport USING (moduleid)
+    layer_2_query = """
+SELECT gwportid,speed, ifindex, interface, sysname, netbox.netboxid, conn.*, nettype, netident, path || filename AS rrdfile,
+2 AS layer, NULL AS from_swportid
+FROM gwportprefix
   JOIN (
-        SELECT  swport.swportid AS to_swport,
-                swport.speed AS to_speed,
-                netbox.sysname AS to_sysname,
-                netbox.netboxid,
-                interface AS to_interface,
-                module.model,
-                module.descr
-        FROM swport
-          JOIN module USING (moduleid)
-          JOIN netbox USING (netboxid)
-        ) AS connection
-    ON (connection.to_swport = to_swportid)
-    LEFT JOIN swportvlan USING(swportid)
-    LEFT JOIN vlan USING (vlanid)
-  LEFT JOIN (SELECT path, filename, value FROM rrd_file) AS rrd ON (rrd.value::integer = swport.swportid OR rrd.value::integer = connection.to_swport)
+     SELECT DISTINCT ON (gwportprefix.prefixid)
+       gwportid AS from_gwportid,
+       gwportprefix.prefixid,
+       ifindex AS from_ifindex,
+       interface AS from_interface,
+       sysname AS from_sysname,
+       speed AS from_speed,
+       netboxid AS from_netboxid
 
-  UNION
-
-SELECT DISTINCT ON (gwport.gwportid, to_gwport)
-        netbox.netboxid,
-        connection.netboxid AS to_netboxid,
-        netbox.sysname AS from_sysname,
-        connection.sysname AS to_sysname,
-        CASE WHEN gwport.speed > connection.to_speed
-          THEN gwport.speed
-          ELSE connection.to_speed
-        END AS speed,
-        interface AS from_interface,
-        connection.to_interface,
-        module.model AS from_module_model,
-        module.descr AS from_module_desc,
-        connection.model AS to_module_model,
-        connection.descr AS to_module_descr,
-        rrd.path || '/' || rrd.filename AS rrdfile,
-        CASE
-                WHEN rrd.value::integer = gwport.gwportid THEN 'n'
-                ELSE 'y'
-        END AS reversed,
-        netident,
-        nettype,
-        1 AS is_gwport,
-        gwport.gwportid AS from_portid,
-        connection.to_gwport AS to_portid
-  FROM gwportprefix
-   JOIN gwport USING (gwportid)
-   JOIN module USING (moduleid)
-   JOIN netbox USING (netboxid)
-   JOIN (SELECT gwportprefix.prefixid,
-                netbox.netboxid,
-                gwport.gwportid AS to_gwport,
-                speed AS to_speed,
-                sysname,
-                interface AS to_interface,
-                module.model,
-                module.descr
-         FROM gwportprefix
-           JOIN gwport USING (gwportid)
-           JOIN module USING (moduleid)
-           JOIN netbox USING (netboxid)
-          ) AS connection
-    ON (gwportprefix.prefixid = connection.prefixid AND connection.sysname != netbox.sysname)
-    LEFT JOIN prefix ON (prefix.prefixid = gwportprefix.prefixid)
-    LEFT JOIN vlan USING (vlanid)
-   LEFT JOIN (SELECT path, filename, value FROM rrd_file) AS rrd ON (rrd.value::integer = gwport.gwportid OR rrd.value::integer = connection.to_gwport)
+       FROM gwport
+       JOIN module USING (moduleid)
+       JOIN netbox USING (netboxid)
+       JOIN gwportprefix USING (gwportid)
+       ) AS conn USING (prefixid)
+  JOIN gwport USING (gwportid)
+  JOIN module USING (moduleid)
+  JOIN netbox USING (netboxid)
+  LEFT JOIN prefix ON  (prefix.prefixid = gwportprefix.prefixid)
+  LEFT JOIN vlan USING (vlanid)
+  LEFT JOIN rrd_file ON (key='gwport' AND value=conn.from_gwportid::varchar)
+WHERE gwportid <> from_gwportid
+ORDER BY sysname, speed DESC
 """
 
-    db_cursor.execute(query)
-    results = db_cursor.dictfetchall()
+    layer_3_query_1 = """
+SELECT DISTINCT ON (swport.swportid)
+gwport.gwportid AS from_gwportid,
+gwport.speed,
+gwport.ifindex AS from_ifindex,
+gwport.interface AS from_interface,
+netbox.sysname AS from_sysname,
+netbox.netboxid AS from_netboxid,
+gwport.to_swportid AS to_swportid,
 
+swport.swportid AS swportid,
+swport.interface  AS interface,
+swport_netbox.sysname AS sysname,
+swport_netbox.netboxid AS netboxid,
+swport.ifindex AS ifindex,
+
+3 AS layer,
+path || filename AS rrdfile,
+nettype, netident,
+NULL AS gwportid,
+NULL AS from_gwportid,
+NULL AS from_swportid
+
+FROM gwport
+ JOIN module ON (gwport.moduleid = module.moduleid)
+ JOIN netbox USING (netboxid)
+ 
+ LEFT JOIN gwportprefix ON (gwportprefix.gwportid = gwport.gwportid)
+ LEFT JOIN prefix ON  (prefix.prefixid = gwportprefix.prefixid)
+ LEFT JOIN vlan USING (vlanid)
+ LEFT JOIN rrd_file ON (key='gwport' AND value=gwport.gwportid::varchar)
+
+ JOIN swport ON (swport.swportid = gwport.to_swportid)
+ JOIN module AS swport_module ON (swport.moduleid = swport.moduleid)
+ JOIN netbox AS swport_netbox ON (swport_module.netboxid = swport_netbox.netboxid)
+
+
+ WHERE gwport.to_swportid IS NOT NULL AND gwport.to_swportid = swport.swportid AND swport_module.moduleid = swport.moduleid
+    """
+
+    layer_3_query_2 = """
+SELECT DISTINCT ON (from_sysname, sysname)
+swport.swportid AS from_swportid,
+swport.speed,
+swport.ifindex AS from_ifindex,
+swport.interface AS from_interface,
+netbox.sysname AS from_sysname,
+netbox.netboxid AS from_netboxid,
+swport.to_swportid AS to_swportid,
+3 AS layer,
+foo.*,
+vlan.*,
+path || filename AS rrdfile,
+NULL AS gwportid,
+NULL AS from_gwportid,
+NULL AS from_swportid
+
+
+FROM swport
+ JOIN module ON (swport.moduleid = module.moduleid)
+ JOIN netbox USING (netboxid)
+
+ JOIN (
+
+ SELECT
+ swport.swportid AS swportid,
+ swport.speed,
+ swport.ifindex AS ifindex,
+ swport.interface AS interface,
+ netbox.sysname AS sysname,
+ netbox.netboxid AS netboxid
+
+ FROM swport
+  JOIN module ON (swport.moduleid = module.moduleid)
+   JOIN netbox USING (netboxid)
+   ) AS foo ON (foo.swportid = to_swportid)
+
+
+LEFT JOIN swportvlan ON (swport.swportid = swportvlan.swportid)
+LEFT JOIN vlan USING (vlanid)
+
+LEFT JOIN rrd_file  ON (key='swport' AND value=swport.swportid::varchar)
+
+ORDER BY from_sysname, sysname, swport.speed DESC
+    """
+
+    layer_3_query_3 = """
+SELECT DISTINCT ON (from_sysname, sysname)
+
+swport.swportid AS from_swportid,
+swport.speed,
+swport.ifindex AS from_ifindex,
+swport.interface AS from_interface,
+netbox.sysname AS from_sysname,
+netbox.netboxid AS from_netboxid,
+3 AS layer,
+conn.*,
+vlan.*,
+path || filename AS rrdfile,
+NULL AS gwportid,
+NULL AS from_gwportid,
+NULL AS to_swportid
+
+
+FROM swport
+ JOIN module ON (swport.moduleid = module.moduleid)
+ JOIN netbox USING (netboxid)
+
+ JOIN (
+
+ SELECT *, NULL AS interface, NULL AS swportid
+FROM netbox
+   ) AS conn ON (conn.netboxid = to_netboxid)
+
+
+LEFT JOIN swportvlan ON (swport.swportid = swportvlan.swportid)
+LEFT JOIN vlan USING (vlanid)
+
+LEFT JOIN rrd_file  ON (key='swport' AND value=swport.swportid::varchar)
+
+ORDER BY from_sysname, sysname, swport.speed DESC
+    """
+
+    db_cursor.execute(layer_2_query)
+    results = db_cursor.dictfetchall()
+    db_cursor.execute(layer_3_query_1)
+    results.extend(db_cursor.dictfetchall())
+    db_cursor.execute(layer_3_query_2)
+    results.extend(db_cursor.dictfetchall())
+    db_cursor.execute(layer_3_query_3)
+    results.extend(db_cursor.dictfetchall())
     for res in results:
-        data = get_rrd_link_load(res['rrdfile'])
-        if data[0] == -1:
-            if res['reversed'] == 'y':
-                if res['is_gwport'] == 1:
-                    db_cursor.execute("""SELECT path || '/' || filename FROM rrd_file WHERE key = 'gwport' AND value::integer = %s""" % res['from_portid'])
-                else:
-                    db_cursor.execute("""SELECT path || '/' || filename FROM rrd_file WHERE key = 'swport' AND value::integer = %s""" % res['from_portid'])
-                res['reversed'] = 'n'
-            else:
-                if res['is_gwport'] == 1:
-                    db_cursor.execute("""SELECT path || '/' || filename FROM rrd_file WHERE key = 'gwport' AND value::integer = %s""" % res['to_portid'])
-                else:
-                    db_cursor.execute("""SELECT path || '/' || filename AS file FROM rrd_file WHERE key = 'swport' AND value::integer = %s""" % res['to_portid'])
-            new_rrd = db_cursor.fetchone()
-            if new_rrd:
-                data = get_rrd_link_load(new_rrd[0])
-        if res['reversed'] == 'y':
-            res['load'] = (data[1],data[0])
-        else:
+        if res['rrdfile']:
+            data = get_rrd_link_load(res['rrdfile'])
             res['load'] = (data[0],data[1])
+        else:
+            res['load'] = (-1,-1)
+
+        connection_id = "%s-%s" % (res['sysname'], res['from_sysname'])
+        connection_rid = "%s-%s" % (res['from_sysname'], res['sysname'])
+        if connection_id not in connections and connection_rid not in connections:
+            connections[connection_id] = res
+        else:
+            for conn in connections.keys():
+                if conn == connection_id or conn == connection_rid:
+                    if connections[conn]['speed'] < res['speed']:
+                        connections[conn] = res
 
 
     query = """
         SELECT DISTINCT ON (netboxid) *,  path || '/' || filename AS rrd
         FROM netbox
-        JOIN room using (roomid)
-        JOIN location USING (locationid)
-        JOIN type USING (typeid)
+        LEFT JOIN room using (roomid)
+        LEFT JOIN location USING (locationid)
+        LEFT JOIN type USING (typeid)
         LEFT JOIN (SELECT netboxid,path,filename FROM rrd_file NATURAL JOIN rrd_datasource WHERE descr = 'cpu5min') AS rrd USING (netboxid)
         LEFT JOIN netmap_position USING (sysname)"""
     db_cursor.execute(query)
@@ -181,7 +234,11 @@ SELECT DISTINCT ON (gwport.gwportid, to_gwport)
         else:
             netbox['load'] = 'unknown'
 
-    return (netboxes, results)
+
+
+
+
+    return (netboxes, connections)
 
 def get_rrd_link_load(rrdfile):
     """ Returns the ds1 and ds2 fields of an rrd-file (ifInOctets, ifOutOctets)"""
