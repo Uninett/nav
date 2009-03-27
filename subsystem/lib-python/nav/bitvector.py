d7ea1bd2ca2df1de73c86db4797cc0b3648cff32
Morten Brekkevold <morten.brekkevold@uninett.no>
2009-03-27 13:17:44 +0100

    Update legacy lib-python modules according to new source code header policy.
    Also, fix some obvious missing/incorrect copyright remarks.

--- subsystem/lib-python/nav/bitvector.py
+++ subsystem/lib-python/nav/bitvector.py
@@ -1,30 +1,20 @@
+# -*- coding: utf-8 -*-
 #
-# Copyright 2007 Norwegian University of Science and Technology
+# Copyright (C) 2007 UNINETT AS
 #
-# This file is part of Network Administration Visualized (NAV)
+# This file is part of Network Administration Visualized (NAV).
 #
-# NAV is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# NAV is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
 #
-# NAV is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details. 
+# You should have received a copy of the GNU General Public License along with
+# NAV. If not, see <http://www.gnu.org/licenses/>.
 #
-# You should have received a copy of the GNU General Public License
-# along with NAV; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-"""
-Bit vector manipulation.
-"""
-__author__ = 'Morten Brekkevold <morten.brekkevold@uninett.no>'
-__copyright__ = '2007 UNINETT AS'
-__license__ = 'GPLv2'
-__credits__ = 'Josiah Carlson <jcarlson@uci.edu>'
-__version__ = '$Id$'
+"""Bit vector manipulation."""
 
 import array
 
