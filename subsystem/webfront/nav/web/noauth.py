6df0eaac428afcba451948dffb8b4e068745971c
Morten Brekkevold <morten.brekkevold@uninett.no>
2009-05-04 12:56:54 +0200

    webfront: Update python source code headers according to policy.
    Removed copyright notice entirely from Makefile.  Does it really need
    one?  Do stylesheets need one?

--- subsystem/webfront/nav/web/noauth.py
+++ subsystem/webfront/nav/web/noauth.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008 UNINETT AS
+#
+# This file is part of Network Administration Visualized (NAV).
+#
+# NAV is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.  You should have received a copy of the GNU General Public
+# License along with NAV. If not, see <http://www.gnu.org/licenses/>.
+#
 """Handler module to bypass NAV's auth system.
 
 Apache will by default be configured to pass all requests under the
@@ -22,9 +38,6 @@ following line:
 PythonHeaderParserHandler nav.web.noauth
 
 """
-__author__ = 'Morten Brekkevold (morten.brekkevold@uninett.no)'
-__copyright__ = '2008 UNINETT AS'
-__license__ = 'GPLv2'
 
 try:
     from mod_python import apache
--- subsystem/webfront/nav/web/quickselect.py
+++ subsystem/webfront/nav/web/quickselect.py
@@ -1,30 +1,20 @@
-# -*- coding: ISO8859-1 -*-
-# Copyright 2008 UNINETT AS
+# -*- coding: utf-8 -*-
 #
-# This file is part of Network Administration Visualized (NAV)
+# Copyright (C) 2008 UNINETT AS
 #
-# NAV is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# This file is part of Network Administration Visualized (NAV).
 #
-# NAV is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# NAV is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
 #
-# You should have received a copy of the GNU General Public License
-# along with NAV; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.  You should have received a copy of the GNU General Public
+# License along with NAV. If not, see <http://www.gnu.org/licenses/>.
 #
-#
-# Author: Thomas Adamcik <thomas.adamcik@uninett.no>
-#
-
-__copyright__ = "Copyright 2008 UNINETT AS"
-__license__ = "GPL"
-__author__ = "Thomas Adamcik (thomas.adamcik@uninett.no)"
-__id__ = "$Id$"
+"""QuickSelect widget for use in various web forms."""
 
 from django.template.loader import get_template
 from django.template import Context
