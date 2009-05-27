deb94ccef1eabc671f40f7f8143a0d34da1f929a
Morten Brekkevold <morten.brekkevold@uninett.no>
2009-05-27 12:19:10 +0200

    Refactor nav.config module, adding unit tests.
    * subsystem/lib-python/nav/config.py
      Clean up imports.
    
      (readConfig, getconfig): Interpret the first argument as either a
      file object or a file name, allowing us to insert a mocked file
      object for testing.  Update docstrings.
    
    * subsystem/lib-python/nav/tests.py
      (ConfigTestCase): New test case for the nav.config module.

--- subsystem/lib-python/nav/tests.py
+++ subsystem/lib-python/nav/tests.py
@@ -158,6 +158,51 @@ class UtilTestCase(unittest.TestCase):
             self.assertFalse(util.isValidIP(ip),
                              msg="%s should be invalid" % ip)
 
+
+import StringIO
+import config
+class ConfigTestCase(unittest.TestCase):
+    def setUp(self):
+        mockfile = StringIO.StringIO("".join([
+            '# mock config file\n',
+            'foo1=bar1\n',
+            'foo2 =  bar2  \n',
+            '#foo3=invalid\n',
+            'foo4 = bar4 # comment\n',
+            '# eof',
+            ]))
+        self.mockfile = mockfile
+
+        mockinifile = StringIO.StringIO("".join([
+            '# mock config file\n',
+            '[section1]\n',
+            'foo1=bar1\n',
+            'foo2 =  bar2  \n',
+            '[section2] \n',
+            '#foo3=invalid\n',
+            'foo4 = bar4 \n',
+            '# eof',
+         ]))
+        self.mockinifile = mockinifile
+
+    def test_readConfig(self):
+        values = config.readConfig(self.mockfile)
+        self.assertEquals(values['foo1'], 'bar1')
+        self.assertEquals(values['foo2'], 'bar2')
+        self.assertEquals(values['foo4'], 'bar4')
+        self.assertFalse(values.has_key('foo3'))
+
+    def test_getconfig(self):
+        values = config.getconfig(self.mockinifile)
+        self.assertEquals(2, len(values.keys()))
+        self.assert_(values.has_key('section1'))
+        self.assert_(values.has_key('section2'))
+
+        self.assertEquals(values['section1']['foo1'], 'bar1')
+        self.assertEquals(values['section1']['foo2'], 'bar2')
+        self.assertEquals(values['section2']['foo4'], 'bar4')
+        self.assertFalse(values['section2'].has_key('foo3'))
+
 # Run all tests if run as a program
 if __name__ == '__main__':
     unittest.main()
