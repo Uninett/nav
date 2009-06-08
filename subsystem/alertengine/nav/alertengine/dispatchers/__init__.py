24cdb3631ca8701fc04728e5246fe6ed4ef3cb75
Morten Brekkevold <morten.brekkevold@uninett.no>
2009-06-08 15:27:04 +0200

    Update alertengine headers in accordance with policy.
    Also updated its README file.

--- subsystem/alertengine/nav/alertengine/dispatchers/__init__.py
+++ subsystem/alertengine/nav/alertengine/dispatchers/__init__.py
@@ -1,25 +1,20 @@
 #! /usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 #
-# Copyright 2008 UNINETT AS
+# Copyright (C) 2008 UNINETT AS
 #
-# This file is part of Network Administration Visualized (NAV)
+# This file is part of Network Administration Visualized (NAV).
 #
-# NAV is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# NAV is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License version 2 as published by the Free
+# Software Foundation.
 #
-# NAV is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.  You should have received a copy of the GNU General Public
+# License along with NAV. If not, see <http://www.gnu.org/licenses/>.
 #
-# You should have received a copy of the GNU General Public License
-# along with NAV; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-
 """
 The dispatchers package contains all the methods that alertengine can use to
 send out alerts. Adding new messaging channels is a simple matter of writting
@@ -37,10 +32,6 @@ For your dispatchers logging please use `logging.getlogger('nav.alertengine.disp
 and try to use sensible log messages, look at the modules that ship with NAV
 for examples.
 """
-
-__copyright__ = "Copyright 2008 UNINETT AS"
-__license__ = "GPL"
-__author__ = "Thomas Adamcik (thomas.adamcik@uninett.no)"
 
 import logging
 import os
