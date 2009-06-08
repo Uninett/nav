717f033405b09f75799a3491483548b7ba5a01ff
Morten Brekkevold <morten.brekkevold@uninett.no>
2009-06-08 21:36:06 +0200

    Add storage layer for ipdevpoll.
    Storage consists of container classes that shadow (or mimic, if you will)
    Django model classes.  The classes are automatically built using a metaclass,
    and will be "dumb" data containers resembling their original Django model
    counterparts.

---
+++ subsystem/ipdevpoll/nav/ipdevpoll/storage.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009 UNINETT AS
+#
+# This file is part of Network Administration Visualized (NAV).
+#
+# NAV is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.  You should have received a copy of the GNU General Public
+# License along with NAV. If not, see <http://www.gnu.org/licenses/>.
+#
+"""Storage layer for ipdevpoll"""
+
+from nav.models import manage
+
+class MetaShadow(type):
+    """Metaclass for building storage container classes.
+
+    A shadow class built by this metaclass will look mostly like its
+    Django model counterpart, but will be a dumb container.  Usage of
+    attributes in such a container will guarantee that no database
+    access occurs, and should therefore not introduce unwanted delays
+    inside an asynchronous event loop.
+
+    """
+
+    def __init__(cls, name, bases, dct):
+        try:
+            shadowclass = dct['__shadowclass__']
+        except KeyError, error:
+            raise AttributeError("No shadow attribute in class %s" % name)
+
+        super(MetaShadow, cls).__init__(name, bases, dct)
+        
+        if shadowclass is None:
+            return
+
+        field_names = [f.name for f in shadowclass._meta.fields]
+        setattr(cls, '_fields', field_names)
+        for f in field_names:
+            setattr(cls, f, None)
+
+class Shadow(object):
+    """Base class to shadow Django model classes.
+
+    To create a "dumb" container of values, whose attribute list will
+    be equal to that of a Django model class, define a descendant of
+    this class and set its __shadowclass__ attribute to the Django
+    model class.
+
+    Using shadow containers will ensure no synchronous database calls
+    take place behind the scenes.
+
+    Example:
+
+    >>> from nav.models import manage
+    >>> class Netbox(Shadow):
+    ...     __shadowclass__ = manage.Netbox
+    ...
+    >>>
+
+    """
+    __shadowclass__ = None
+    __metaclass__ = MetaShadow
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a shadow container.
+
+        To copy the values of a Django model object, supply the object
+        as the first argument.  Any keyword arguments will be used to
+        initialize the attributes of the container object.
+
+        """
+        if args:
+            obj = args[0]
+            if isinstance(obj, self.__class__.__shadowclass__):
+                for field in self.__class__._fields:
+                    setattr(self, field, getattr(obj, field))
+            else:
+                raise ValueError("First argument is not a %s instance" %
+                                 self.__class__.__shadowclass__.__name__)
+        else:
+            for key, val in kwargs.items():
+                if not hasattr(self.__class__, key):
+                    raise AttributeError("Invalid keyword argument %s" % key)
+                setattr(self, key, val)
+
+        self._touched = set()
+
+    def __repr__(self):
+        attrs = [field for field in self._fields
+                 if getattr(self, field) is not None or 
+                 field in self._touched]
+        varbinds = ["%s=%r" % (field, getattr(self, field))
+                    for field in attrs]
+        return "%s(%s)" % (self.__class__.__name__, ", ".join(varbinds))
+
+    def __setattr__(self, attr, value):
+        """Set attribute and register it as having been touched.
+
+        See the get_touched() method for more info.
+
+        """
+        # The _touched attribute will not exist during initialization
+        # of the object, so ignore AttributeErrors
+        try:
+            self._touched.add(attr)
+        except AttributeError:
+            pass
+        return super(Shadow, self).__setattr__(attr, value)
+
+    def get_touched(self):
+        """Get list of touched attributes.
+
+        Returns a list of attributes that have been modified since
+        this container's creation.
+
+        """
+        return list(self._touched)
+
+class Netbox(Shadow):
+    __shadowclass__ = manage.Netbox
+
+class Module(Shadow):
+    __shadowclass__ = manage.Module
+
+class Device(Shadow):
+    __shadowclass__ = manage.Device
+
+class Interface(Shadow):
+    __shadowclass__ = manage.Interface
