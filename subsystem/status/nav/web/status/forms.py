6bd9ea2e0f998a7d84648d05d7fce2d838ffd0bd
Magnus Eide <magnus.eide@uninett.no>
2009-06-19 17:16:46 +0200

    StatusPreference form can now save (at least netbox and service type)

--- subsystem/status/nav/web/status/forms.py
+++ subsystem/status/nav/web/status/forms.py
@@ -15,24 +15,40 @@
 # NAV. If not, see <http://www.gnu.org/licenses/>.
 #
 
+from itertools import chain
 from django import forms
+from django.utils.encoding import force_unicode
 
 from nav.models.profiles import StatusPreference
 from nav.models.manage import Netbox
 from nav.web import serviceHelper
 
-class NoneWidget(forms.widgets.Widget):
-    '''Does not render a form widget, only a simple paragraph.'''
+class StatusTypeWidget(forms.widgets.HiddenInput):
+    '''Users should not change status type, so we have a special widget for it.
+    
+    Renders a hidden input field with the status type and a simple textual
+    representation.
+    '''
     def render(self, name, value, attrs=None, choices=()):
-        for choice in self.choices:
-            if choice[0] == value:
-                value = choice[1]
-        return forms.widgets.mark_safe(u'<p>%s</p>' % value)
+        if value is None:
+            value = ''
+        final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
+        final_attrs['value'] = force_unicode(value)
+        output = [u'<input%s>' % forms.util.flatatt(final_attrs)]
+
+        for option_value, option_label in chain(self.choices, choices):
+            if option_value == value:
+                break
+        output.append(u'%s<br />' % force_unicode(option_label))
+        return forms.widgets.mark_safe(u'\n'.join(output))
 
 class SectionForm(forms.ModelForm):
     name = forms.CharField()
-    type = forms.ChoiceField(choices=StatusPreference.SECTION_CHOICES,
-        widget=NoneWidget)
+    type = forms.ChoiceField(
+        choices=StatusPreference.SECTION_CHOICES,
+        widget=StatusTypeWidget()
+    )
+    position = forms.IntegerField(widget=forms.HiddenInput())
 
     class Meta:
         model = StatusPreference
@@ -58,12 +74,41 @@ class SectionForm(forms.ModelForm):
             if selected_services:
                 selected_services = selected_services.split(',')
 
-            self.fields['categories'] = None
+            del self.fields['categories']
             self.fields['service_choice'] = forms.MultipleChoiceField(
                 choices=services,
                 initial=selected_services
             )
 
-    def save(self, *args, **kwargs):
-        '''Probably a bad idea to save this form automatically.'''
-        raise Exception('Oh no you didn\'t!')
+    def clean(self, *args, **kwargs):
+        # Different requirements for different types
+        type = self.cleaned_data.get('type')
+        categories = self.cleaned_data.get('categories')
+        services = self.cleaned_data.get('service_choice')
+        if type == 'netbox':
+            # Netbox needs categories and should not have services
+            if not categories:
+                raise forms.ValidationError('No categories selected')
+            else:
+                self.cleaned_data['categories'] = categories
+                self.cleaned_data['services'] = ''
+        elif type == 'service':
+            # Service needs service and should not have categories.
+            if not services:
+                raise forms.ValidationError('No services selected')
+            else:
+                # Services are stored as a comma separated string.
+                # Also, the options from the form are in service_state.
+                #
+                # Join the service_choice list and pass it to services.
+                self.cleaned_data['services'] = ",".join(services)
+                self.cleaned_data['categories'] = []
+
+        # States are stored as a comma separated string.
+        # Also, the options from the form are in state_choice.
+        #
+        # Join the state_choice list and pass it to states.
+        states = self.cleaned_data.get('state_choice')
+        self.cleaned_data['states'] = ",".join(states)
+
+        return self.cleaned_data
