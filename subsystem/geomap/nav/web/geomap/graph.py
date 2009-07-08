7235e19f46a77d4a6a515958b120fc40760c9852
Øystein Skartsæterhagen <oysteini@pvv.ntnu.no>
2009-07-08 09:33:35 +0200

    Use lazy_dict for aggregated properties

--- subsystem/geomap/nav/web/geomap/graph.py
+++ subsystem/geomap/nav/web/geomap/graph.py
@@ -284,7 +284,7 @@ def aggregate_properties(objects, aggregators):
             fun = aggr
             lst = objects
         return fun(lst)
-    return map_dict(apply_aggregator, aggregators)
+    return map_dict_lazy(apply_aggregator, aggregators)
 
 #     return dict(map(lambda (property, aggregator):
 #                         (property,
