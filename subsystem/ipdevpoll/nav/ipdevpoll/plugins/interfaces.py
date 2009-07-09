919b8bea7c4e666c3832705045765f92d0c0ccef
Morten Brekkevold <morten.brekkevold@uninett.no>
2009-07-09 16:26:43 +0200

    Make Interfaces plugin discover and mark missing interfaces.

--- subsystem/ipdevpoll/nav/ipdevpoll/plugins/interfaces.py
+++ subsystem/ipdevpoll/nav/ipdevpoll/plugins/interfaces.py
@@ -23,13 +23,15 @@ by polling will be marked as missing with a timestamp (gone_since).
 
 import logging
 import pprint
+import datetime
 
-from twisted.internet import defer
+from twisted.internet import defer, threads
 from twisted.python.failure import Failure
 
 from nav.mibs import IfMib
 from nav.ipdevpoll import Plugin, FatalPluginError
 from nav.ipdevpoll import storage
+from nav.models import manage
 
 class Interfaces(Plugin):
     def __init__(self, *args, **kwargs):
@@ -56,6 +58,7 @@ class Interfaces(Plugin):
                 'ifAlias',
                 ])
         df.addCallback(self._got_interfaces)
+        df.addCallback(self._check_missing_interfaces)
         df.addErrback(self._error)
         return self.deferred
 
@@ -74,13 +77,12 @@ class Interfaces(Plugin):
 
         self.logger.debug("Found %d interfaces", len(result))
 
-        # Now save stuff to containers and signal our exit
+        # Now save stuff to containers and pass the list of containers
+        # to the next callback
         netbox = self.job_handler.container_factory(storage.Netbox, key=None)
-        for (ifindex,),row in result.items():
-            self._convert_row_to_container(netbox, ifindex, row)
-
-        self.deferred.callback(True)
-        return result
+        interfaces = [self._convert_row_to_container(netbox, ifindex, row)
+                      for (ifindex,),row in result.items()]
+        return interfaces
     
     def _convert_row_to_container(self, netbox, ifindex, row):
         """Convert a collected ifTable/ifXTable row into a container object."""
@@ -103,10 +105,63 @@ class Interfaces(Plugin):
         interface.ifname = row['ifName']
         interface.ifconnectorpresent = row['ifConnectorPresent'] == 1
         interface.ifalias = row['ifAlias']
+        
+        interface.gone_since = None
 
         interface.netbox = netbox
         return interface
 
+    def _check_missing_interfaces(self, interfaces):
+        """Check if any known interfaces are missing from a result set.
+
+        This method will load the known interfaces of this netbox from
+        the database.  A new container will be created for any known
+        interface missing from the result set, and its gone_since
+        timestamp will be set.
+
+        NOTE: The comparisons are only made using ifindex values.  If
+        a netbox has re-assigned ifindices to its interfaces since the
+        last collection, this may cause trouble.
+
+        TODO: Make a deletion algorithm.  Missing interfaces that do
+        not correspond to a module known to be down should be deleted.
+        If all interfaces belonging to a specific module is down, we
+        may have detected that the module is down as well.
+
+        """
+        def mark_as_gone(ifindices):
+            now = datetime.datetime.now()
+            netbox = self.job_handler.container_factory(storage.Netbox, 
+                                                        key=None)
+
+            if ifindices:
+                self.logger.info("Marking interfaces as gone.  Ifindex: %r", 
+                                 ifindices)
+
+            for ifindex in ifindices:
+                interface = self.job_handler.container_factory(
+                    storage.Interface, key=ifindex)
+                interface.ifindex = ifindex
+                interface.gone_since = now
+                interface.netbox = netbox
+
+        def do_comparison(known_interfaces):
+            known_ifindices = set(i.ifindex for i in known_interfaces)
+            found_ifindices = set(i.ifindex for i in interfaces)
+            missing_ifindices = known_ifindices.difference(found_ifindices)
+
+            mark_as_gone(missing_ifindices)
+
+            self.deferred.callback(True)
+            
+        # pick only the ones not known to be missing already
+        queryset = manage.Interface.objects.filter(netbox=self.netbox.id,
+                                                   gone_since__isnull=True)
+        deferred = threads.deferToThread(storage.shadowify_queryset, 
+                                         queryset)
+        deferred.addCallback(do_comparison)
+
+# FIXME This utility function should probably be someplace else?
 def binary_mac_to_hex(binary_mac):
     """Convert a binary string MAC address to hex string."""
     if binary_mac:
