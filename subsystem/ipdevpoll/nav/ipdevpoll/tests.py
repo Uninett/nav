64b0b492ea7fabedb253d7cc57e1b2427b1c140d
Magnus Eide <magnus.eide@uninett.no>
2009-07-10 13:53:31 +0200

    Move non arp tests from test_arp

--- subsystem/ipdevpoll/nav/ipdevpoll/tests.py
+++ subsystem/ipdevpoll/nav/ipdevpoll/tests.py
@@ -17,14 +17,16 @@
 
 import unittest
 import os
+from IPy import IP
 
 os.environ['PYSNMP_API_VERSION'] = 'v3'
 os.environ['DJANGO_SETTINGS_MODULE'] = 'nav.django.settings'
 
-from IPy import IP
-
 from nav.models.manage import Prefix
-from nav.ipdevpoll.plugins.arp import *
+from nav.ipdevpoll.utils import find_prefix
+from nav.mibs.ip_mib import IpMib, IndexToIpException
+from nav.mibs.ipv6_mib import Ipv6Mib
+from nav.mibs.cisco_ietf_ip_mib import CiscoIetfIpMib
 
 class ArpPluginTest(unittest.TestCase):
     def setUp(self):
@@ -34,61 +36,61 @@ class ArpPluginTest(unittest.TestCase):
     def test_ipmib_index(self):
         # This is what we expect, ifIndex + IP
         ip_tuple = (1L, 192, 0L, 2L, 1L)
-        ip = ipmib_index_to_ip(ip_tuple)
+        ip = IpMib.index_to_ip(ip_tuple)
         self.assertEquals(ip, self.correct_ipv4)
 
         # Three other things, but the four last are still an IP, should work
         # fine.
         ip_tuple = (1, 2, 3, 192, 0, 2, 1)
-        ip = ipmib_index_to_ip(ip_tuple)
+        ip = IpMib.index_to_ip(ip_tuple)
         self.assertEquals(ip, self.correct_ipv4)
 
         # Just IP
         ip_tuple = (192, 0L, 2L, 1L)
-        ip = ipmib_index_to_ip(ip_tuple)
+        ip = IpMib.index_to_ip(ip_tuple)
         self.assertEquals(ip, self.correct_ipv4)
 
         # To few parts, should fail
         ip_tuple = (1L, 2L, 3L)
-        self.assertRaises(IndexToIpException, ipmib_index_to_ip, ip_tuple)
+        self.assertRaises(IndexToIpException, IpMib.index_to_ip, ip_tuple)
 
     def test_ipv6mib_index(self):
         # This is what we expect, ifIndex + IP
         ip_tuple = (1L, 32L, 1L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 1L)
-        ip = ipv6mib_index_to_ip(ip_tuple)
+        ip = Ipv6Mib.index_to_ip(ip_tuple)
         self.assertEquals(ip, self.correct_ipv6)
 
         # Three other things, but the last 16 parts are still an IP, should
         # work fine.
         ip_tuple = (1, 2, 3, 32, 1, 13, 184, 18, 52, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1)
-        ip = ipv6mib_index_to_ip(ip_tuple)
+        ip = Ipv6Mib.index_to_ip(ip_tuple)
         self.assertEquals(ip, self.correct_ipv6)
 
         # Just an IP
         ip_tuple = (32L, 1L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 1L)
-        ip = ipv6mib_index_to_ip(ip_tuple)
+        ip = Ipv6Mib.index_to_ip(ip_tuple)
         self.assertEquals(ip, self.correct_ipv6)
 
         # To few parts, should fail
         ip_tuple = (1L, 2L, 3L)
-        self.assertRaises(IndexToIpException, ipv6mib_index_to_ip, ip_tuple)
+        self.assertRaises(IndexToIpException, Ipv6Mib.index_to_ip, ip_tuple)
 
     def test_ciscomib_index(self):
         ip_tuple = (1L, 2L, 16L, 32L, 1L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L,
                     0L, 0L, 0L, 0L, 0L, 1L)
-        ip = ciscomib_index_to_ip(ip_tuple)
+        ip = CiscoIetfIpMib.index_to_ip(ip_tuple)
         self.assertEquals(ip, self.correct_ipv6)
 
         ip_tuple = (1L, 1L, 16L, 32L, 1L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L,
                     0L, 0L, 0L, 0L, 0L, 1L)
-        self.assertRaises(IndexToIpException, ciscomib_index_to_ip, ip_tuple)
+        self.assertRaises(IndexToIpException, CiscoIetfIpMib.index_to_ip, ip_tuple)
 
         ip_tuple = (1L, 1L, 4L, 192, 0L, 2L, 1L)
-        ip = ciscomib_index_to_ip(ip_tuple)
+        ip = CiscoIetfIpMib.index_to_ip(ip_tuple)
         self.assertEquals(ip, self.correct_ipv4)
 
         ip_tuple = (1L, 2L, 4L, 192, 0L, 2L, 1L)
-        self.assertRaises(IndexToIpException, ciscomib_index_to_ip, ip_tuple)
+        self.assertRaises(IndexToIpException, CiscoIetfIpMib.index_to_ip, ip_tuple)
 
     def test_find_prefix(self):
         loose_prefix = Prefix(
