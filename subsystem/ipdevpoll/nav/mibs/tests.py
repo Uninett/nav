993f6f36e6eeb4c0a327bdd8f47b4d075bc8df98
Magnus Eide <magnus.eide@uninett.no>
2009-07-10 14:37:19 +0200

    Change tests to work with new index_to_ip functions

--- subsystem/ipdevpoll/nav/mibs/tests.py
+++ subsystem/ipdevpoll/nav/mibs/tests.py
@@ -32,63 +32,49 @@ class ArpPluginTest(unittest.TestCase):
         self.correct_ipv6 = IP('2001:db8:1234::1')
 
     def test_ipmib_index(self):
-        # This is what we expect, ifIndex + IP
-        ip_tuple = (1L, 192, 0L, 2L, 1L)
-        ip = IpMib.index_to_ip(ip_tuple)
-        self.assertEquals(ip, self.correct_ipv4)
-
-        # Three other things, but the four last are still an IP, should work
-        # fine.
-        ip_tuple = (1, 2, 3, 192, 0, 2, 1)
-        ip = IpMib.index_to_ip(ip_tuple)
-        self.assertEquals(ip, self.correct_ipv4)
-
-        # Just IP
         ip_tuple = (192, 0L, 2L, 1L)
         ip = IpMib.index_to_ip(ip_tuple)
         self.assertEquals(ip, self.correct_ipv4)
 
-        # To few parts, should fail
+        ip_tuple = (300, 300, 300, 300)
+        self.assertRaises(ValueError, IpMib.index_to_ip, ip_tuple)
+
+        # To few parts
         ip_tuple = (1L, 2L, 3L)
         self.assertRaises(IndexToIpException, IpMib.index_to_ip, ip_tuple)
 
+        # To many parts
+        ip_tuple = (32L, 1L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 1L)
+        self.assertRaises(IndexToIpException, IpMib.index_to_ip, ip_tuple)
+
     def test_ipv6mib_index(self):
-        # This is what we expect, ifIndex + IP
-        ip_tuple = (1L, 32L, 1L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 1L)
-        ip = Ipv6Mib.index_to_ip(ip_tuple)
-        self.assertEquals(ip, self.correct_ipv6)
-
-        # Three other things, but the last 16 parts are still an IP, should
-        # work fine.
-        ip_tuple = (1, 2, 3, 32, 1, 13, 184, 18, 52, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1)
-        ip = Ipv6Mib.index_to_ip(ip_tuple)
-        self.assertEquals(ip, self.correct_ipv6)
-
-        # Just an IP
         ip_tuple = (32L, 1L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 1L)
         ip = Ipv6Mib.index_to_ip(ip_tuple)
         self.assertEquals(ip, self.correct_ipv6)
 
+        ip_tuple = (32L, 500L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 1L)
+        self.assertRaises(ValueError, Ipv6Mib.index_to_ip, ip_tuple)
+
         # To few parts, should fail
-        ip_tuple = (1L, 2L, 3L)
+        ip_tuple = (192, 0L, 2L, 1L)
+        self.assertRaises(IndexToIpException, Ipv6Mib.index_to_ip, ip_tuple)
+
+        # To many parts
+        ip_tuple = (1L, 32L, 1L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 1L)
         self.assertRaises(IndexToIpException, Ipv6Mib.index_to_ip, ip_tuple)
 
     def test_ciscomib_index(self):
-        ip_tuple = (1L, 2L, 16L, 32L, 1L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L,
-                    0L, 0L, 0L, 0L, 0L, 1L)
-        ip = CiscoIetfIpMib.index_to_ip(ip_tuple)
-        self.assertEquals(ip, self.correct_ipv6)
-
-        ip_tuple = (1L, 1L, 16L, 32L, 1L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L,
-                    0L, 0L, 0L, 0L, 0L, 1L)
-        self.assertRaises(IndexToIpException, CiscoIetfIpMib.index_to_ip, ip_tuple)
-
-        ip_tuple = (1L, 1L, 4L, 192, 0L, 2L, 1L)
-        ip = CiscoIetfIpMib.index_to_ip(ip_tuple)
+        ip_tuple = (192, 0L, 2L, 1L)
+        ip = CiscoIetfIpMib.index_to_ip(1, ip_tuple)
         self.assertEquals(ip, self.correct_ipv4)
 
-        ip_tuple = (1L, 2L, 4L, 192, 0L, 2L, 1L)
-        self.assertRaises(IndexToIpException, CiscoIetfIpMib.index_to_ip, ip_tuple)
+        self.assertRaises(IndexToIpException, CiscoIetfIpMib.index_to_ip, 2, ip_tuple)
+
+        ip_tuple = (32L, 1L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 1L)
+        ip = CiscoIetfIpMib.index_to_ip(2, ip_tuple)
+        self.assertEquals(ip, self.correct_ipv6)
+
+        self.assertRaises(IndexToIpException, CiscoIetfIpMib.index_to_ip, 1, ip_tuple)
 
 if __name__ == '__main__':
     unittest.main()
