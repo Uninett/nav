493042cb239325f8deeb20fcf75c77b8f88ac34c
Magnus Eide <magnus.eide@uninett.no>
2009-07-10 13:36:46 +0200

    Test for prefixes in test_arp.py

--- subsystem/ipdevpoll/nav/ipdevpoll/plugins/test_arp.py
+++ subsystem/ipdevpoll/nav/ipdevpoll/plugins/test_arp.py
@@ -16,17 +16,20 @@
 #
 
 import unittest
-from IPy import IP
-
 import os
+
 os.environ['PYSNMP_API_VERSION'] = 'v3'
 os.environ['DJANGO_SETTINGS_MODULE'] = 'nav.django.settings'
+
+from IPy import IP
+
+from nav.models.manage import Prefix
 from nav.ipdevpoll.plugins.arp import *
 
 class ArpPluginTest(unittest.TestCase):
     def setUp(self):
         self.correct_ipv4 = IP('192.0.2.1')
-        self.correct_ipv6 = IP('2001:db8::1')
+        self.correct_ipv6 = IP('2001:db8:1234::1')
 
     def test_ipmib_index(self):
         # This is what we expect, ifIndex + IP
@@ -51,18 +54,18 @@ class ArpPluginTest(unittest.TestCase):
 
     def test_ipv6mib_index(self):
         # This is what we expect, ifIndex + IP
-        ip_tuple = (1L, 32L, 1L, 13L, 184L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 1L)
+        ip_tuple = (1L, 32L, 1L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 1L)
         ip = ipv6mib_index_to_ip(ip_tuple)
         self.assertEquals(ip, self.correct_ipv6)
 
         # Three other things, but the last 16 parts are still an IP, should
         # work fine.
-        ip_tuple = (1, 2, 3, 32, 1, 13, 184, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1)
+        ip_tuple = (1, 2, 3, 32, 1, 13, 184, 18, 52, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1)
         ip = ipv6mib_index_to_ip(ip_tuple)
         self.assertEquals(ip, self.correct_ipv6)
 
         # Just an IP
-        ip_tuple = (32L, 1L, 13L, 184L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 1L)
+        ip_tuple = (32L, 1L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 1L)
         ip = ipv6mib_index_to_ip(ip_tuple)
         self.assertEquals(ip, self.correct_ipv6)
 
@@ -71,12 +74,12 @@ class ArpPluginTest(unittest.TestCase):
         self.assertRaises(IndexToIpException, ipv6mib_index_to_ip, ip_tuple)
 
     def test_ciscomib_index(self):
-        ip_tuple = (1L, 2L, 16L, 32L, 1L, 13L, 184L, 0L, 0L, 0L, 0L, 0L, 0L,
+        ip_tuple = (1L, 2L, 16L, 32L, 1L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L,
                     0L, 0L, 0L, 0L, 0L, 1L)
         ip = ciscomib_index_to_ip(ip_tuple)
         self.assertEquals(ip, self.correct_ipv6)
 
-        ip_tuple = (1L, 1L, 16L, 32L, 1L, 13L, 184L, 0L, 0L, 0L, 0L, 0L, 0L,
+        ip_tuple = (1L, 1L, 16L, 32L, 1L, 13L, 184L, 18L, 52L, 0L, 0L, 0L, 0L,
                     0L, 0L, 0L, 0L, 0L, 1L)
         self.assertRaises(IndexToIpException, ciscomib_index_to_ip, ip_tuple)
 
@@ -86,6 +89,33 @@ class ArpPluginTest(unittest.TestCase):
 
         ip_tuple = (1L, 2L, 4L, 192, 0L, 2L, 1L)
         self.assertRaises(IndexToIpException, ciscomib_index_to_ip, ip_tuple)
+
+    def test_find_prefix(self):
+        loose_prefix = Prefix(
+            net_address='2001:db8::/32'
+        )
+        tight_prefix = Prefix(
+            net_address='2001:db8:1234::/48'
+        )
+
+        prefix1 = find_prefix(self.correct_ipv6, [loose_prefix, tight_prefix])
+        prefix2 = find_prefix(self.correct_ipv6, [tight_prefix, loose_prefix])
+
+        self.assertEqual(prefix1, tight_prefix)
+        self.assertEqual(prefix2, tight_prefix)
+
+        loose_prefix = Prefix(
+            net_address='192.0.2/24'
+        )
+        tight_prefix = Prefix(
+            net_address='192.0.2.0/26'
+        )
+
+        prefix1 = find_prefix(self.correct_ipv4, [loose_prefix, tight_prefix])
+        prefix2 = find_prefix(self.correct_ipv4, [tight_prefix, loose_prefix])
+
+        self.assertEqual(prefix1, tight_prefix)
+        self.assertEqual(prefix2, tight_prefix)
 
 if __name__ == '__main__':
     unittest.main()
