286973a1a4aa9a7ba8fd3157c70813a34b0b3fe9
Jørgen Abrahamsen <jorgen.abrahamsen@uninett.no>
2009-08-24 10:56:22 +0200

    Cleanup of Python file headers, according to new policy.

--- subsystem/smsd/nav/smsd/gammudispatcher.py
+++ subsystem/smsd/nav/smsd/gammudispatcher.py
@@ -1,27 +1,20 @@
-#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006 UNINETT AS
+# Copyright (C) 2006 UNINETT AS
 #
-# This file is part of Network Administration Visualized (NAV)
+# This file is part of Network Administration Visualized (NAV).
 #
-# NAV is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# NAV is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
 #
-# NAV is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.  You should have received a copy of the GNU General Public License
+# along with NAV. If not, see <http://www.gnu.org/licenses/>.
 #
-# You should have received a copy of the GNU General Public License
-# along with NAV; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-
-"""
-The smsd dispatcher for Gammu.
+"""The smsd dispatcher for Gammu.
 
 This dispatcher takes care of all communication between smsd and Gammu. Gammu
 is used to send SMS messages via a cell phone connected to the server with a
@@ -29,11 +22,8 @@ serial cable, USB cable, IR or Bluetooth. See http://www.gammu.org/ for more
 information.
 
 Depends on python-gammu.
-"""
 
-__copyright__ = "Copyright 2006 UNINETT AS"
-__license__ = "GPL"
-__author__ = "Stein Magnus Jodal (stein.magnus.jodal@uninett.no)"
+"""
 
 from nav.smsd.dispatcher import *
 
