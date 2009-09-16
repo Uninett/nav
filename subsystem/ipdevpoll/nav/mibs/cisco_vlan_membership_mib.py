c35b63b8dcad966d95c9716c32eed535f40de4e5
Morten Brekkevold <morten.brekkevold@uninett.no>
2009-09-16 15:44:54 +0200

    nav/mibs: Add VLAN related helper methods to CiscoVtpMib and CiscoVlanMembershipMib.
    * subsystem/ipdevpoll/nav/mibs/cisco_vlan_membership_mib.py
      (CiscoVlanMembershipMib.get_vlan_membership): New method.
    
    * subsystem/ipdevpoll/nav/mibs/cisco_vtp_mib.py
      (CiscoVTPMib.get_trunk_native_vlans,
       CiscoVTPMib.get_trunk_enabled_vlans): New methods.

--- subsystem/ipdevpoll/nav/mibs/cisco_vlan_membership_mib.py
+++ subsystem/ipdevpoll/nav/mibs/cisco_vlan_membership_mib.py
@@ -1,4 +1,16 @@
+from twisted.internet import defer
 import mibretriever
 
 class CiscoVlanMembershipMib(mibretriever.MibRetriever):
     from nav.smidumps.cisco_vlan_membership_mib import MIB as mib
+
+    @defer.deferredGenerator
+    def get_vlan_membership(self):
+        """Get a mapping of access port ifindexes->VLAN"""
+        dw = defer.waitForDeferred(self.retrieve_column('vmVlan'))
+        yield dw
+        vlans = dw.getResult()
+
+        result = dict([(index[0], vlan)
+                      for index, vlan in vlans.items()])
+        yield result
--- subsystem/ipdevpoll/nav/mibs/cisco_vtp_mib.py
+++ subsystem/ipdevpoll/nav/mibs/cisco_vtp_mib.py
@@ -1,4 +1,59 @@
+from twisted.internet import defer
+
+from nav.bitvector import BitVector
 import mibretriever
 
 class CiscoVTPMib(mibretriever.MibRetriever):
     from nav.smidumps.cisco_vtp_mib import MIB as mib
+
+    @defer.deferredGenerator
+    def get_trunk_native_vlans(self):
+        """Get a mapping of the native VLANs of trunk ports."""
+        dw = defer.waitForDeferred(
+            self.retrieve_columns((
+                    'vlanTrunkPortNativeVlan',
+                    'vlanTrunkPortDynamicStatus',
+                    ))
+            )
+        yield dw
+        trunkports = self.translate_result(dw.getResult())
+        
+        result = dict([(index[0], row['vlanTrunkPortNativeVlan'])
+                       for index, row in trunkports.items()
+                       if row['vlanTrunkPortDynamicStatus'] == 'trunking'])
+        yield result
+
+
+    @defer.deferredGenerator
+    def get_trunk_enabled_vlans(self, as_bitvector=False):
+        """Get a list of enabled VLANs for each trunk port.
+
+        If as_bitvector=True, the result will be a raw BitVector object.
+
+        """
+
+        def get_vlan_list(row):
+            enabled = BitVector(
+                row['vlanTrunkPortVlansEnabled'] +
+                row['vlanTrunkPortVlansEnabled2k'] +
+                row['vlanTrunkPortVlansEnabled3k'] +
+                row['vlanTrunkPortVlansEnabled4k']
+                )
+            return as_bitvector and enabled or enabled.get_set_bits()
+
+        dw = defer.waitForDeferred(
+            self.retrieve_columns((
+                    'vlanTrunkPortVlansEnabled',
+                    'vlanTrunkPortDynamicStatus',
+                    'vlanTrunkPortVlansEnabled2k',
+                    'vlanTrunkPortVlansEnabled3k',
+                    'vlanTrunkPortVlansEnabled4k',
+                    ))
+            )
+        yield dw
+        trunkports = self.translate_result(dw.getResult())
+
+        result = dict([(index[0], get_vlan_list(row))
+                       for index, row in trunkports.items()
+                       if row['vlanTrunkPortDynamicStatus'] == 'trunking'])
+        yield result
