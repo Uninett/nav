f64b6dcc8a1ebc928291060e86a8daca8e5aa7da
Magnus Eide <magnus.eide@uninett.no>
2009-11-03 11:02:50 +0100

    Instead of using one magic form for all the different forms, we now use different forms for different tasks.

--- subsystem/status/nav/web/status/forms.py
+++ subsystem/status/nav/web/status/forms.py
@@ -20,112 +20,45 @@ from django import forms
 from django.utils.encoding import force_unicode
 
 from nav.models.profiles import StatusPreference
-from nav.models.manage import Netbox
+from nav.models.manage import Netbox, Organization, Category
 from nav.web import serviceHelper
 
-class StatusTypeWidget(forms.widgets.HiddenInput):
-    '''Users should not change status type, so we have a special widget for it.
-    
-    Renders a hidden input field with the status type and a simple textual
-    representation.
-    '''
-    def render(self, name, value, attrs=None, choices=()):
-        if value is None:
-            value = ''
-        final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
-        final_attrs['value'] = force_unicode(value)
-        output = [u'<input%s>' % forms.util.flatatt(final_attrs)]
+def _organization_choices():
+    org = [(org.id, org.description) for org in Organization.objects.all()]
+    org.insert(0, ('', '(all)'))
+    return org
 
-        for option_value, option_label in chain(self.choices, choices):
-            if option_value == value:
-                break
-        output.append(u'%s<br />' % force_unicode(option_label))
-        return forms.widgets.mark_safe(u'\n'.join(output))
+def _category_choices():
+    cat = [(cat.id,cat.description) for cat in Category.objects.all()]
+    cat.insert(0, ('', '(all)'))
+    return cat
 
-class SectionForm(forms.ModelForm):
-    name = forms.CharField()
-    type = forms.ChoiceField(
-        choices=StatusPreference.SECTION_CHOICES,
-        widget=StatusTypeWidget()
+def _service_choices():
+    service = [(s, s) for s in serviceHelper.getCheckers()]
+    service.insert(0, ('', '(all)'))
+    return service
+
+def _state_choices():
+    return (
+        (Netbox.UP_DOWN, 'down'),
+        (Netbox.UP_SHADOW, 'shadow'),
     )
-    position = forms.IntegerField(widget=forms.HiddenInput(), required=False)
-    account = forms.IntegerField(required=False)
 
-    class Meta:
-        model = StatusPreference
+class SectionForm(forms.Form):
+    id = forms.IntegerField(widget=forms.HiddenInput(), required=False)
+    name = forms.CharField()
+    type = forms.CharField(widget=forms.HiddenInput())
+    organizations = forms.MultipleChoiceField(choices=_organization_choices())
 
-    def __init__(self, *args, **kwargs):
-        type = kwargs.pop('type', None)
-        super(SectionForm, self).__init__(*args, **kwargs)
+class SectionWithCategoryForm(SectionForm):
+    categories = forms.MultipleChoiceField(choices=_category_choices())
 
-        if not type:
-            type = self.instance.type
-        
-        # All section types except threshold let's the user choose state
-        if type != StatusPreference.SECTION_THRESHOLD:
-            selected_states = self.instance.states
-            if selected_states:
-                selected_states = selected_states.split(',')
+class SectionWithCategoryAndStateForm(SectionWithCategoryForm):
+    states = forms.MultipleChoiceField(choices=_state_choices())
 
-            self.fields['state_choice'] = forms.MultipleChoiceField(
-                choices=Netbox.UP_CHOICES,
-                initial=selected_states
-            )
-
-        # No categories choice for services, instead it's a service choice
-        if type in (StatusPreference.SECTION_SERVICE, StatusPreference.SECTION_SERVICE_MAINTENANCE):
-            services = [(s,s) for s in serviceHelper.getCheckers()]
-            selected_services = self.instance.services
-            if selected_services:
-                selected_services = selected_services.split(',')
-
-            del self.fields['categories']
-            self.fields['service_choice'] = forms.MultipleChoiceField(
-                choices=services,
-                initial=selected_services
-            )
-
-    def clean(self, *args, **kwargs):
-        # Different requirements for different types
-        type = self.cleaned_data.get('type')
-        categories = self.cleaned_data.get('categories')
-        services = self.cleaned_data.get('service_choice')
-        if type == 'service' or type == 'service_maintenance':
-            # Service needs service and should not have categories.
-            if not services:
-                raise forms.ValidationError('No services selected')
-            else:
-                # Services are stored as a comma separated string.
-                # Also, the options from the form are in service_state.
-                #
-                # Join the service_choice list and pass it to services.
-                self.cleaned_data['services'] = ",".join(services)
-                self.cleaned_data['categories'] = []
-        else:
-            # Netbox needs categories and should not have services
-            if not categories:
-                raise forms.ValidationError('No categories selected')
-            else:
-                self.cleaned_data['categories'] = categories
-                self.cleaned_data['services'] = ''
-
-        if type != StatusPreference.SECTION_THRESHOLD:
-            # States are stored as a comma separated string.
-            # Also, the options from the form are in state_choice.
-            #
-            # Join the state_choice list and pass it to states.
-            states = self.cleaned_data.get('state_choice')
-            self.cleaned_data['states'] = ",".join(states)
-
-        return self.cleaned_data
-
-    def save(self, account=None, position=None, commit=True):
-        # Look away!
-        self.cleaned_data['account'] = account
-        self.cleaned_data['position'] = position
-        # You can look again.
-
-        return super(SectionForm, self).save(commit=commit)
+class SectionWithServiceAndStateForm(SectionForm):
+    services = forms.MultipleChoiceField(choices=_service_choices())
+    states = forms.MultipleChoiceField(choices=_state_choices())
 
 class AddSectionForm(forms.Form):
     section = forms.ChoiceField(
