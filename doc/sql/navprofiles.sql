f88f73e14148b8107f7dc2f815c8a11703e1a26b
Magnus Eide <magnus.eide@uninett.no>
2009-11-03 14:54:42 +0100

    services and states should be NOT NULL. Empty values are stored as empty strings. Add default status preferences to default user.

--- doc/sql/navprofiles.sql
+++ doc/sql/navprofiles.sql
@@ -728,8 +728,8 @@ CREATE TABLE statuspreference (
 	type varchar NOT NULL,
 	accountid integer NOT NULL,
 
-	services varchar,
-	states varchar,
+	services varchar NOT NULL DEFAULT '',
+	states varchar NOT NULL DEFAULT 'n,s',
 
 	CONSTRAINT statuspreference_pkey PRIMARY KEY(id),
 	CONSTRAINT statuspreference_accountid_fkey
@@ -1102,10 +1102,11 @@ INSERT INTO filtergroup_group_permission (accountgroup_id, filtergroup_id) VALUE
 
 -- StatusPreferences for Default user
 
-INSERT INTO statuspreference (name, position, type, accountid, states) VALUES ('IP devices down', 1, 'netbox', 0, 'n');
-INSERT INTO statuspreference (name, position, type, accountid, states) VALUES ('IP devices in shadow', 2, 'netbox', 0, 's');
-INSERT INTO statuspreference (name, position, type, accountid, states) VALUES ('Modules down/in shadow', 3, 'module', 0, 'n,s');
-INSERT INTO statuspreference (name, position, type, accountid, states) VALUES ('Services down', 4, 'service', 0, 'n,s');
+INSERT INTO statuspreference (id, name, position, type, accountid, states) VALUES (1, 'IP devices down', 1, 'netbox', 0, 'n');
+INSERT INTO statuspreference (id, name, position, type, accountid, states) VALUES (2, 'IP devices in shadow', 2, 'netbox', 0, 's');
+INSERT INTO statuspreference (id, name, position, type, accountid, states) VALUES (3, 'IP devices on maintenance', 3, 'netbox_maintenance', 0, 'n,s');
+INSERT INTO statuspreference (id, name, position, type, accountid, states) VALUES (4, 'Modules down/in shadow', 4, 'module', 0, 'n,s');
+INSERT INTO statuspreference (id, name, position, type, accountid, states) VALUES (5, 'Services down', 5, 'service', 0, 'n,s');
 
 /*
 ------------------------------------------------------
