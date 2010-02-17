a8dc07dc68d51db94848b833e226fd5eed078bbe
Magnus Eide <magnus.eide@uninett.no>
2010-02-17 14:59:06 +0100

    IP Search works just the same as old machinetracker ip search

--- subsystem/machinetracker/nav/web/machinetracker/views.py
+++ subsystem/machinetracker/nav/web/machinetracker/views.py
@@ -29,13 +29,24 @@ from nav.models.manage import Arp
 from nav.web.machinetracker import forms
 from nav.web.machinetracker.utils import hostname
 
+NAVBAR = [('Home', '/'), ('Machinetracker', None)]
+IP_TITLE = 'NAV - Machinetracker - IP Search'
+MAC_TITLE = 'NAV - Machinetracker - MAC Search'
+SWP_TITLE = 'NAV - Machinetracker - Switch Search'
+IP_DEFAULTS = {'title': IP_TITLE, 'navpath': NAVBAR}
+MAC_DEFAULTS = {'title': MAC_TITLE, 'navpath': NAVBAR}
+SWP_DEFAULTS = {'title': SWP_TITLE, 'navpath': NAVBAR}
+
+
 def ip(request):
+    info_dict = {
+        'active': {'ip': True},
+        'form': forms.IpTrackerForm(),
+    }
+    info_dict.update(IP_DEFAULTS)
     return render_to_response(
         'machinetracker/ip_search.html',
-        {
-            'active': {'ip': True},
-            'form': forms.IpTrackerForm(),
-        },
+        info_dict,
         RequestContext(request)
     )
 
@@ -61,37 +72,61 @@ def ip_search(request):
 
         from_time = date.today() - timedelta(days=7)
 
-        result = Arp.objects.filter(
-            end_time__gt=from_time,
-        ).extra(
-            where=['ip BETWEEN %s and %s'],
-            params=[unicode(from_ip), unicode(to_ip)],
-        ).order_by('ip', 'mac', '-start_time')
+        ip_result = SortedDict()
+        if active:
+            result = Arp.objects.filter(
+                end_time__gt=from_time,
+            ).extra(
+                where=['ip BETWEEN %s and %s'],
+                params=[unicode(from_ip), unicode(to_ip)],
+            ).order_by('ip', 'mac', '-start_time')
+
+            for row in result:
+                ip = IP(row.ip)
+                if ip not in ip_result:
+                    ip_result[ip] = []
+                ip_result[ip].append(row)
+
+        ip_range = []
+        if inactive:
+            ip_range = [IP(ip) for ip in range(from_ip.int(), to_ip.int() + 1)]
+        else:
+            ip_range = [key for key in ip_result]
 
         tracker = SortedDict()
-        for row in result:
-            ip = row.ip
-            mac = row.mac
-            if row.end_time > datetime.now():
-                row.still_active = "Still active"
-            if (ip, mac) in tracker:
+        for ip_key in ip_range:
+            if ip_key in ip_result:
+                rows = ip_result[ip_key]
+                for row in rows:
+                    ip = row.ip
+                    mac = row.mac
+                    if row.end_time > datetime.now():
+                        row.still_active = "Still active"
+                    if (ip, mac) not in tracker:
+                        if dns:
+                            row.dns_lookup = hostname(row.ip) or ""
+                        tracker[(ip, mac)] = [row]
+                    else:
+                        row.ip = ""
+                        row.mac = ""
+                        row.dns_lookup = "--"
+                        tracker[(ip, mac)].append(row)
+            elif inactive:
+                ip = unicode(ip_key)
+                row = {'ip': ip}
                 if dns:
-                    row.dns_lookup = "--"
-                row.ip = ""
-                row.mac = ""
-                tracker[(ip, mac)].append(row)
-            else:
-                if dns:
-                    row.dns_lookup = hostname(row.ip)
-                tracker[(ip, mac)] = [row]
+                    row['dns_lookup'] = hostname(ip) or ""
+                tracker[(ip, "")] = [row]
 
+    info_dict = {
+        'active': {'ip': True},
+        'form': form,
+        'tracker': tracker,
+    }
+    info_dict.update(IP_DEFAULTS)
     return render_to_response(
         'machinetracker/ip_search.html',
-        {
-            'active': {'ip': True},
-            'form': form,
-            'tracker': tracker,
-        },
+        info_dict,
         RequestContext(request)
     )
 
