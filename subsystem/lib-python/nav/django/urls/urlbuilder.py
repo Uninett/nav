b7ded009553c725724aca5ee8ed33ca75cdcf27c
Magnus Eide <magnus.eide@uninett.no>
2010-02-17 12:35:58 +0100

    Make ready for new machinetracker.

--- subsystem/lib-python/nav/django/urls/urlbuilder.py
+++ subsystem/lib-python/nav/django/urls/urlbuilder.py
@@ -51,10 +51,10 @@ def get_urlpatterns():
             dummy, name='maintenance-new-service'),
 
         # Subsystem: Machine Tracker
-        url(r'^machinetracker/$',
-            dummy, name='machinetracker'),
-        url(r'^machinetracker/swp\?switch=(?P<netbox_sysname>[\w\d._-]+)&module=(?P<module_number>\d+)&port=(?P<port_interface>[\w\d/._-]+)$',
-            dummy, name='machinetracker-swport'),
+#        url(r'^machinetracker/$',
+#            dummy, name='machinetracker'),
+#        url(r'^machinetracker/swp\?switch=(?P<netbox_sysname>[\w\d._-]+)&module=(?P<module_number>\d+)&port=(?P<port_interface>[\w\d/._-]+)$',
+#            dummy, name='machinetracker-swport'),
 
         # Subsystem: Report
         # Naming convention: report-<result>-<query>
