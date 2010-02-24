df9dda905dc92e08cc64e09fcb2a2df28db1083f
Magnus Eide <magnus.eide@uninett.no>
2010-02-24 13:18:07 +0100

    Utility methods for mac search

--- subsystem/machinetracker/nav/web/machinetracker/utils.py
+++ subsystem/machinetracker/nav/web/machinetracker/utils.py
@@ -17,7 +17,11 @@
 # NAV. If not, see <http://www.gnu.org/licenses/>.
 #
 
+import re
+from datetime import date, datetime, timedelta
 from socket import gethostbyaddr, herror
+
+from django.utils.datastructures import SortedDict
 
 def hostname(addr):
     dns = _Hostname()
@@ -35,3 +39,44 @@ class _Hostname:
             return False
         self.cache[addr] = dns[0]
         return dns[0]
+
+def min_max_mac(mac):
+    """If max is shorter than 12 characters we pad the mac with 0 for the
+    min_mac and f for the max_mac.
+
+    Ie. if mac is 00:10:20:40
+    mac_min will be 00:10:20:40:00:00
+    mac_max will be 00:10:20:40:ff:ff
+    """
+    mac = re.sub("[^0-9a-fA-F]+", "", mac).lower()
+    if len(mac) < 12:
+        mac_min = mac + '0' * (12 - len(mac))
+        mac_max = mac + 'f' * (12 - len(mac))
+    else:
+        mac_min = mac
+        mac_max = mac
+    return (mac_min, mac_max)
+    
+
+def track_mac(keys, resultset, dns):
+    """Groups results from Query for the mac_search page.
+
+        keys        - a tuple/list with strings that identifies the fields the
+                      result should be grouped by
+        resultset   - a QuerySet
+        dns         - should we lookup the hostname?
+    """
+    tracker = SortedDict()
+    for row in resultset:
+        if row['end_time'] > datetime.now():
+            row['still_active'] = "Still active"
+        if dns:
+            row['dns_lookup'] = hostname(row['ip'])
+        key = []
+        for k in keys:
+            key.append(row.get(k))
+        key = tuple(key)
+        if key not in tracker:
+            tracker[key] = []
+        tracker[key].append(row)
+    return tracker
