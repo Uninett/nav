7930c036416f74c9c1cb15a821f9a68d114b578c
Magnus Eide <magnus.eide@uninett.no>
2010-03-09 13:37:46 +0100

    Use InetAddressField where appropriate in manage.py

--- subsystem/lib-python/nav/models/manage.py
+++ subsystem/lib-python/nav/models/manage.py
@@ -27,6 +27,7 @@ from django.db import models
 from django.db.models import Q
 
 import nav.natsort
+from nav.models.fields.ip import InetAddressField, CidrAddressField
 
 # Choices used in multiple models, "imported" into the models which use them
 LINK_UP = 'y'
@@ -85,7 +86,7 @@ class Netbox(models.Model):
     TIME_FRAMES = ('day', 'week', 'month')
 
     id = models.AutoField(db_column='netboxid', primary_key=True)
-    ip = models.IPAddressField(unique=True)
+    ip = InetAddressField(unique=True)
     room = models.ForeignKey('Room', db_column='roomid')
     type = models.ForeignKey('NetboxType', db_column='typeid', null=True)
     device = models.ForeignKey('Device', db_column='deviceid')
@@ -724,7 +725,7 @@ class Arp(models.Model):
     netbox = models.ForeignKey('Netbox', db_column='netboxid')
     prefix = models.ForeignKey('Prefix', db_column='prefixid', null=True)
     sysname = models.CharField(max_length=-1)
-    ip = models.IPAddressField()
+    ip = InetAddressField()
     # TODO: Create MACAddressField in Django
     mac = models.CharField(max_length=17)
     start_time = models.DateTimeField()
