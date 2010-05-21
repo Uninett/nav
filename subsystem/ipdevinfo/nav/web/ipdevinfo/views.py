c24bdc54bfccf7f087f5f7107e3a4906275d7e5b
Morten Brekkevold <morten.brekkevold@uninett.no>
2010-05-21 11:55:37 +0200

    ipdevinfo: Use ifname to look up swports, not ifalias.
    Looking up ports by ifalias makes no sense, and breaks links from machinetracker.

--- subsystem/ipdevinfo/nav/web/ipdevinfo/views.py
+++ subsystem/ipdevinfo/nav/web/ipdevinfo/views.py
@@ -433,7 +433,7 @@ def port_details(request, netbox_sysname, module_number=None, port_type=None,
     if port_id is not None:
         port = get_object_or_404(ports, id=port_id)
     elif port_name is not None:
-        port = get_object_or_404(ports, netbox__sysname=netbox_sysname, ifalias=port_name)
+        port = get_object_or_404(ports, netbox__sysname=netbox_sysname, ifname=port_name)
 
     return render_to_response(
         'ipdevinfo/port-details.html',
