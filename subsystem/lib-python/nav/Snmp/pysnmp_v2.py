d6668812d936df462d312b0fd221ce187bffd669
Morten Brekkevold <morten.brekkevold@uninett.no>
2010-06-04 15:05:07 +0200

    nav.Snmp: Extract Exception classes to separate module.

--- subsystem/lib-python/nav/Snmp/pysnmp_v2.py
+++ subsystem/lib-python/nav/Snmp/pysnmp_v2.py
@@ -31,32 +31,7 @@ try:
     v1.RESPONSE
 except:
     v1.RESPONSE = v1.GETRESPONSE
-from nav.errors import GeneralException
-    
-class SnmpError(GeneralException):
-    """SNMP Error"""
-
-class TimeOutException(SnmpError):
-    """Timed out waiting for SNMP response"""
-    
-class NameResolverException(SnmpError):
-    """NameResolverException"""
-
-class NetworkError(SnmpError):
-    """NetworkError"""
-
-class AgentError(SnmpError):
-    """SNMP agent responded with error"""
-
-class EndOfMibViewError(AgentError):
-    """SNMP request was outside the agent's MIB view"""
-
-class UnsupportedSnmpVersionError(SnmpError):
-    """Unsupported SNMP protocol version"""
-
-class NoSuchObjectError(SnmpError):
-    """SNMP agent did not know of this object"""
-    
+from errors import *
 
 class Snmp(object):
     """Simple class that provides snmpget, snmpwalk and snmpjog(tm)
