fb2d7c2b136af1e7b327ff9f4a9f749d758ba570
Magnus Eide <magnus.eide@uninett.no>
2010-07-02 14:00:45 +0200

    Forms and template for editing

---
+++ subsystem/seeddb/nav/web/seeddb/forms.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009 UNINETT AS
+#
+# This file is part of Network Administration Visualized (NAV).
+#
+# NAV is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License version 2 as published by the Free
+# Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.  You should have received a copy of the GNU General Public License
+# along with NAV. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from django import forms
+
+from nav.models.manage import Netbox, Room, Location
+
+class NetboxForm(forms.ModelForm):
+    class Meta:
+        model = Netbox
+        fields = (
+            'ip', 'sysname', 'category', 'room', 'organisation', 'read_only',
+            'read_write'
+        )
+
+class RoomForm(forms.ModelForm):
+    description = forms.CharField(required=False)
+    class Meta:
+        model = Room
+
+class LocationForm(forms.ModelForm):
+    description = forms.CharField()
+
+    class Meta:
+        model = Location
+
+    def __init__(self, *args, **kwargs):
+        super(LocationForm, self).__init__(*args, **kwargs)
+        if kwargs.get('instance'):
+            del self.fields['id']
