cb5e9e26387a918d33b095eac4f30316ba70cefc
Morten Brekkevold <morten.brekkevold@uninett.no>
2010-08-20 15:28:01 +0200

    update file headers according to policy

--- tools/updatetypes.py
+++ tools/updatetypes.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright 2006 UNINETT AS
 #
-# This file is part of Network Administration Visualized (NAV)
+# Copyright (C) 2006, 2010 UNINETT AS
 #
-# NAV is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# This file is part of Network Administration Visualized (NAV).
 #
-# NAV is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# NAV is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License version 2 as published by the Free
+# Software Foundation.
 #
-# You should have received a copy of the GNU General Public License
-# along with NAV; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.  You should have received a copy of the GNU General Public License
+# along with NAV. If not, see <http://www.gnu.org/licenses/>.
 #
 """This script produces UPDATE statements suitable to update a NAV type table.
 
@@ -26,8 +22,6 @@ access a NAV database by way of the standard db.conf configuration file.
 """
 import sys
 import nav.db
-
-__author__ = 'Morten Vold <morten.vold@uninett.no>'
 
 selectsql = 'SELECT vendorid, typename, cdp, tftp, cs_at_vlan, chassis, ' \
             'descr, sysobjectid FROM "type"'
