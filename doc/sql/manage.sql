61cadf7ff9310b7dba82ee8a33a03b0e79a59808
Morten Brekkevold <morten.brekkevold@uninett.no>
2010-11-16 13:30:29 +0100

    Define a bunch of ordering operators and an operator class for PostgreSQL's POINT data type (fixes LP#675543)

--- doc/sql/manage.sql
+++ doc/sql/manage.sql
@@ -1051,6 +1051,92 @@ ypos double precision NOT NULL
 );
 
 ------------------------------------------------------------------------------
+-- Simple but stupid operators for POINT data type comparisons.
+-- Needed because Django will issue DISTINCT queries that require
+-- ordering operators for the room.point column.
+------------------------------------------------------------------------------
+CREATE FUNCTION point_stupid_lt(point, point) RETURNS bool
+    AS $$
+BEGIN
+  RETURN $1::text < $2::text;
+END;
+$$
+    LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE FUNCTION point_stupid_gt(point, point) RETURNS bool
+    AS $$
+BEGIN
+  RETURN $1::text > $2::text;
+END;
+$$
+    LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE FUNCTION point_stupid_gte(point, point) RETURNS bool
+    AS $$
+BEGIN
+  RETURN $1::text >= $2::text;
+END;
+$$
+    LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE FUNCTION point_stupid_lte(point, point) RETURNS bool
+    AS $$
+BEGIN
+  RETURN $1::text <= $2::text;
+END;
+$$
+    LANGUAGE plpgsql IMMUTABLE STRICT;
+
+
+CREATE OPERATOR < (
+   leftarg = point, rightarg = point, procedure = point_stupid_lt,
+   commutator = > , negator = >= ,
+   restrict = scalarltsel, join = scalarltjoinsel
+);
+
+CREATE OPERATOR > (
+   leftarg = point, rightarg = point, procedure = point_stupid_gt,
+   commutator = < , negator = <= ,
+   restrict = scalarltsel, join = scalarltjoinsel
+);
+
+CREATE OPERATOR <= (
+   leftarg = point, rightarg = point, procedure = point_stupid_lte,
+   commutator = >= , negator = > ,
+   restrict = scalarltsel, join = scalarltjoinsel
+);
+
+CREATE OPERATOR >= (
+   leftarg = point, rightarg = point, procedure = point_stupid_gte,
+   commutator = <= , negator = < ,
+   restrict = scalarltsel, join = scalarltjoinsel
+);
+
+CREATE OPERATOR = (
+   leftarg = point, rightarg = point, procedure = point_eq,
+   negator = <> ,
+   restrict = scalarltsel, join = scalarltjoinsel
+);
+
+CREATE FUNCTION point_cmp(point, point)
+    RETURNS integer
+    AS $$
+BEGIN
+  RETURN bttextcmp($1::text, $2::text);
+END
+$$
+    LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR CLASS point_abs_ops
+    DEFAULT FOR TYPE point USING btree AS
+        OPERATOR        1       < ,
+        OPERATOR        2       <= ,
+        OPERATOR        3       = ,
+        OPERATOR        4       >= ,
+        OPERATOR        5       > ,
+        FUNCTION        1       point_cmp(point, point);
+
+------------------------------------------------------------------------------
 -- simple schema version check table
 ------------------------------------------------------------------------------
 CREATE TABLE nav_schema_version (
