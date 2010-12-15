c25eb88758a38c1eab037cc02b260d3844e989f6
John-Magne Bredal <john.m.bredal@ntnu.no>
2010-12-15 10:11:13 +0100

    Added copyright headers. Also added simplejson as required python library in INSTALL

--- python/nav/portadmin/snmputils.py
+++ python/nav/portadmin/snmputils.py
@@ -1,3 +1,18 @@
+#
+# Copyright 2010 (C) Norwegian University of Science and Technology
+#
+# This file is part of Network Administration Visualized (NAV).
+#
+# NAV is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.  You should have received a copy of the GNU General Public License
+# along with NAV. If not, see <http://www.gnu.org/licenses/>.
+#
 import time
 from nav.Snmp.pysnmp_se import Snmp
 from nav.Snmp.errors import *
