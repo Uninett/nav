98e9b7e02ffebd82219dbc3f46733e934deffa39
Morten Brekkevold <morten.brekkevold@uninett.no>
2011-01-04 14:58:28 +0100

    update netmap tests to import from new names.

--- tests/integration/netmap_test.py
+++ tests/integration/netmap_test.py
@@ -1,8 +1,8 @@
 from nav.tests.cases import ModPythonTestCase
-from nav.web.netmap import handler
+from nav.web.netmap import views
 
 class NetmapServerTest(ModPythonTestCase):
-    module_under_test = handler
+    module_under_test = views
 
     def test_server_no_unicode_output(self):
         self.handler_outputs_no_unicode('/netmap/server')
