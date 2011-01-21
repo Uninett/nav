c166faf56ecf6373f7db5638c87a2459a3e4346e
Morten Brekkevold <morten.brekkevold@uninett.no>
2011-01-21 21:18:45 +0100

    modify baseline and add first schema change script to update a legacy db with the same change

---
+++ sql/changes/sc.03.08.0000.sql
@@ -0,0 +1,10 @@
+DROP TABLE nav_schema_version;
+
+CREATE TABLE schema_change_log (
+    id SERIAL PRIMARY KEY,
+    major INTEGER NOT NULL,
+    minor INTEGER NOT NULL,
+    point INTEGER NOT NULL,
+    script_name VARCHAR NOT NULL,
+    date_applied TIMESTAMP NOT NULL DEFAULT NOW()
+);
