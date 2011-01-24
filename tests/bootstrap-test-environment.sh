ca74a1a581d3e938b9b4890fdcfd0830b61ff7a4
Morten Brekkevold <morten.brekkevold@uninett.no>
2011-01-24 11:22:30 +0100

    bootstrapping the test environment now uses syncdb.py, and also exits with error status if any of the build steps fail.

--- tests/bootstrap-test-environment.sh
+++ tests/bootstrap-test-environment.sh
@@ -25,25 +25,26 @@ else
     virtualenv "$VIRTENV"
 fi
 source "$VIRTENV/bin/activate"
-easy_install pip
-pip install -r tests/requirements.txt
+easy_install pip || exit 1
+pip install -r tests/requirements.txt || exit 1
 
-
-# Cleanup any existing DB
-dropdb $PGDATABASE || true
-
-(cd sql; ./createdb.sh -d $PGDATABASE -u $PGUSER -U)
 
 # Make install code into given directory
-./autogen.sh
-./configure --prefix "$BUILDDIR"
-make
-make install
+./autogen.sh || exit 1
+./configure --prefix "$BUILDDIR" || exit 1
+make || exit 1
+make install || exit 1
 
-# Update config
+export PYTHONPATH="$BUILDDIR/lib/python"
+
+# Update db config
 sed -i'' -e "s/^db_nav\s*=\s*nav/db_nav=$PGDATABASE/" "$BUILDDIR/etc/db.conf"
 sed -i'' -e "s/^script_default\s*=\s*nav/script_default=$PGUSER/" "$BUILDDIR/etc/db.conf"
 sed -i'' -e "s/^userpw_nav\s*=.*/userpw_$PGUSER=$PGPASSWORD/" "$BUILDDIR/etc/db.conf"
+if [ -n "$PGHOST" ]; then sed -i'' -e "s/^dbhost\s*=\s*localhost/dbhost=$PGHOST/" "$BUILDDIR/etc/db.conf"; fi
 
-if [ -n "$PGHOST" ];        then sed -i'' -e "s/^dbhost\s*=\s*localhost/dbhost=$PGHOST/" "$BUILDDIR/etc/db.conf"; fi
+# Remove existing DB, if any, and create new one
+dropdb $PGDATABASE || true
+sql/syncdb.py -c || exit 1
+
 if [ -n "$ADMINPASSWORD" ]; then psql -c "UPDATE account SET password = '$ADMINPASSWORD' WHERE login = 'admin'"; fi
