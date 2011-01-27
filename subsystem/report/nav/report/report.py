ccf0653389a5db5e6c3847d0217c545e3d21c268
Morten Brekkevold <morten.brekkevold@uninett.no>
2011-01-27 15:41:32 +0100

    update copyright years for files modified since last release

--- subsystem/report/nav/report/report.py
+++ subsystem/report/nav/report/report.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2003-2005 Norwegian University of Science and Technology
-# Copyright (C) 2008-2010 UNINETT
+# Copyright (C) 2008-2011 UNINETT
 #
 # This file is part of Network Administration Visualized (NAV).
 #
