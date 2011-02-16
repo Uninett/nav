d54990b6a1b99ae7f06d1859d91b3e0407593122
Morten Brekkevold <morten.brekkevold@uninett.no>
2011-02-16 14:08:24 +0100

    (typicalMatchRe): don't match colons greedily (fixes LP#720024)

--- python/nav/logengine.py
+++ python/nav/logengine.py
@@ -115,7 +115,7 @@ typicalmatchRe = re.compile(
     ((?P<year>\d{4}) \s+ )?                          # origin year, if present
     (?P<hour>\d+) : (?P<min>\d+) : (?P<second>\d+)   # origin hour/minute/second
     .* %                                             # eat chars until % appears
-    (?P<type>.*?) :                                  # message type
+    (?P<type>[^:]+) :                                # message type
     \s* (?P<description>.*)                          # message (lstripped)
     $
     """, re.VERBOSE)
