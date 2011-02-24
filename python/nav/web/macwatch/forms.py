b57fd9b827f26072f05dd5acb46998548d22d557
Morten Brekkevold <morten.brekkevold@uninett.no>
2011-02-24 15:19:56 +0100

    clean up macwatch source header boilerplates according to policy

--- python/nav/web/macwatch/forms.py
+++ python/nav/web/macwatch/forms.py
@@ -1,29 +1,19 @@
-# -*- coding: utf-8 -*-
 #
-# Copyright 2011 UNINETT AS
+# Copyright (C) 2011 UNINETT AS
 #
-# This file is part of Network Administration Visualized (NAV)
+# This file is part of Network Administration Visualized (NAV).
 #
-# NAV is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# NAV is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
 #
-# NAV is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.  You should have received a copy of the GNU General Public License
+# along with NAV. If not, see <http://www.gnu.org/licenses/>.
 #
-# You should have received a copy of the GNU General Public License
-# along with NAV; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-#
-
-__copyright__ = "Copyright 2011 UNINETT AS"
-__license__ = "GPL"
-__author__ = "John-Magne Bredal <john.m.bredal@ntnu.no> and Trond Kandal <Trond.Kandal@ntnu.no>"
-__id__ = "$Id$"
+"""macwatch form definitions"""
 
 import re
 
