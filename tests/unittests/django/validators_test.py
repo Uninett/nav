a35399ef13c79eec1c351ecc0cfbfbb841d7dd79
Magnus Eide <magnus.eide@uninett.no>
2011-03-18 13:56:36 +0100

    Updated tests for is_valid_point_string. is_valid_point_string now accepts points without parantheses.

--- tests/unittests/django/validators_test.py
+++ tests/unittests/django/validators_test.py
@@ -8,12 +8,27 @@ class ValidPointStringTest(TestCase):
         res = is_valid_point_string(point_string)
         self.assertTrue(res)
 
-    def test_missing_parens(self):
+    def test_parens_and_sapce(self):
+        point_string = "(9.888, 3.2222)"
+        res = is_valid_point_string(point_string)
+        self.assertTrue(res)
+
+    def test_no_parens(self):
         point_string = "5.5,8.7"
         res = is_valid_point_string(point_string)
-        self.assertFalse(res)
+        self.assertTrue(res)
+
+    def test_no_parens_and_space(self):
+        point_string = "77.22134124, 543.3342"
+        res = is_valid_point_string(point_string)
+        self.assertTrue(res)
 
     def test_no_comma(self):
         point_string = "(2.4)"
+        res = is_valid_point_string(point_string)
+        self.assertFalse(res)
+
+    def test_chars(self):
+        point_string = "foo, bar"
         res = is_valid_point_string(point_string)
         self.assertFalse(res)
