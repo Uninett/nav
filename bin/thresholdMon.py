a3a8e174152f72e1081de8072930a12e5a46017c
Trond Kandal <trond.kandal@ntnu.no>
2011-04-29 10:32:18 +0200

    now using django for db-access.

--- bin/thresholdMon.py
+++ bin/thresholdMon.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
 # $Id$
 #
-# Copyright 2003, 2004 Norwegian University of Science and Technology
+# Copyright 2011 UNINETT AS
 #
 # This file is part of Network Administration Visualized (NAV)
 #
@@ -20,225 +22,235 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 #
-# Authors: John Magne Bredal <bredal@itea.ntnu.no>
 #
 """
 This is a python script that walks through the rrd-files in
 the nav-database and reports if the threshold is surpassed.
 """
 
+import re
 import time
+import sys
+import optparse
+
+# import NAV libraries
+from nav.event import Event
+from nav.rrd import presenter
+from nav.models.rrd import RrdFile, RrdDataSource
+
+pres = presenter.presentation()
 
 start = int(time.time())
 date = time.ctime()
 
-import getopt,sys
-import nav.event
-from nav.db import getConnection
-conn = getConnection('default')
-
-import re
-
-from nav.rrd import presenter
-pres = presenter.presentation()
-
 # Globals
-exceptions = ['cpu5min','c5000BandwidthMax']
-ll = 2
+exceptions = ['cpu5min', 'c5000BandwidthMax']
+log_level = 2
 downmodifier = 20
 
-# Print usage when -h option called
-def usage(name):
-    print "USAGE: %s [-h] [-l loglevel]" % name
-    print "-h\t\tthis helptext"
-    print "-l loglevel\tset the loglevel (1-silent,2-normal,3-debug)"
+def log_it(level, msg):
+    """ Just a simple logger to stderr"""
+    if log_level >= level:
+        print >> sys.stdout, msg
 
-# A simple method to set state in the rrd_datasource table
-def setState(dsid, descr, state):
-    if ll >= 2: print "Setting %s to %s" %(descr, state)
+def set_state(dsid, descr, state):
+    """ A simple method to set state in the rrd_datasource table"""
+    log_it(2, "Setting %s to %s" %(descr, state))
+    if isinstance(dsid, str) or isinstance(dsid, unicode):
+        if dsid.isdigit():
+            dsid = int(dsid)
+        else:
+            print >> sys.stderr, 'dsid is an illegal type: %s' % dsid
+            return
+    rrd_datasource = None
+    try :
+        rrd_datasource = RrdDataSource.objects.get(pk=dsid)
+    except Exception, get_ex:
+        print >> sys.stderr, get_ex
+        return
 
-    c = conn.cursor()
-    sql = """
-    UPDATE rrd_datasource SET thresholdstate = %s WHERE rrd_datasourceid = %s
-    """
-    c.execute(sql, (state, dsid))
-    conn.commit()
-    
+    rrd_datasource.thresholdstate = state
+    try :
+        rrd_datasource.save()
+    except Exception, save_ex:
+        print >> sys.stderr, save_ex
 
-# Makes the event ready for sending and updates the rrd_datasource
-# table with the correct information
-# calls sendEvent with correct values
-def makeEvent (presobject, var, val, subid, fileid, state):
-    # Fetching some data about the rrd
-    c = conn.cursor()
-    sql = """
-    SELECT netboxid, sysname, ip FROM rrd_file 
-    LEFT JOIN netbox USING (netboxid)
-    WHERE rrd_fileid = %s
-    """
-    c.execute(sql, (fileid, ))
-    
-    netboxid, sysname, ip = c.fetchone()
+def make_event(presobject, var, val, subid, fileid, state):
+    """Makes the event ready for sending and updates the rrd_datasource
+       table with the correct information
+       calls sendEvent with correct values"""
+    if isinstance(fileid, str) or isinstance(fileid, unicode):
+        if fileid.isdigit():
+            fileid = int(fileid)
+        else:
+            print >> sys.stderr, 'fileid is an illegal type: %s' % fileid
+            return
+    rrd_file = None
+    try :
+        rrd_file = RrdFile.objects.get(pk=fileid)
+    except Exception, get_ex:
+        print >> sys.stderr, get_ex
+        return
+  
+    netboxid = rrd_file.nextbox.id
+    sysname = rrd_file.nextbox.sysname
+    ip = rrd_file.nextbox.ip
 
     if netboxid:
-        if ll >= 2: print "thresholdalert regarding %s (%s)" %(sysname, ip)
+        log_it(2, "thresholdalert regarding %s (%s)" %(sysname, ip))
     if state == 'active':
-        if ll >= 2: print "Threshold on %s surpassed." %var
-        setState(subid, var, state)
-        sendEvent(var, val, netboxid, state, subid);
+        log_it(2, "Threshold on %s surpassed." %var)
+        set_state(subid, var, state)
+        send_event(var, val, netboxid, state, subid)
     elif state == 'inactive':
-        if ll >= 2: print "%s has calmed down." %var
-        setState(subid, var, state)
-        sendEvent(var, val, netboxid, state, subid);
+        log_it(2, "%s has calmed down." %var)
+        set_state(subid, var, state)
+        send_event(var, val, netboxid, state, subid)
     elif state == 'stillactive':
-        if ll >= 2: print "Alert on %s is still active." %var
+        log_it(2, "Alert on %s is still active." %var)
     else:
-        if ll >= 2: print "No such state (%s)" % state
+        log_it(2, "No such state (%s)" % state)
 
 
-# Updates the correct tables for sending the event
-def sendEvent (var, val, netboxid, state, subid):
+def send_event (var, val, netboxid, state, subid):
+    """Updates the correct tables for sending the event"""
     if state == 'active':
         state = 's'
     else:
         state = 'e'
 
-    if ll >= 2: print "sending event"
+    log_it(1, "sending event")
 
-    e = nav.event.Event(source='thresholdMon', target='eventEngine', 
-                        netboxid=netboxid, subid=subid, 
-                        eventtypeid='thresholdState', state=state)
-    e[var] = val
+    the_event = Event(source='thresholdMon', target='eventEngine', 
+                netboxid=netboxid, subid=subid, 
+                eventtypeid='thresholdState', state=state)
+    the_event[var] = val
 
     try:
-        e.post()
-    except Exception, e:
-        print e
+        the_event.post()
+    except Exception, post_ex:
+        print >> sys.stderr, post_ex
 
 
 ##################################################
 # Done with functions, let the games begin!
 ##################################################
 
-# First we get options from the commandline
-try:
-    opt,args = getopt.getopt(sys.argv[1:], "hl:")
-except getopt.GetoptError:
-    print "GetoptError %s" % str(getopt.GetoptError)
+def main(argv):
+    """ Main """
+    global log_level
+    # First we get options from the commandline
+    usage = "usage: %prog [-h|--help] [-l LEVEL|--log=LEVEL]"
+    parser = optparse.OptionParser(usage)
+    parser.add_option('-l', '--log', action='store', type='int',
+                        dest='level', default=2, help='Log level (1-3)')
+    (options, args) = parser.parse_args()
+    if options.level in range(1, 4):
+        log_level = options.level
+    else:
+        log_it(2, "No such loglevel: %d, using loglevel %d" %
+                (options.level, log_level))
+    
+    log_it(1, "Starting thresholdMon at %s" % date)
+    # For each rrd_datasource, fetch the value and compare
+    # it to the max-value.
+    for rrd_datasource in RrdDataSource.objects.filter(threshold__isnull=False):
 
-if ll >= 2: print "Starting thresholdMon at %s" % date
-for option, argument in opt:
-    if option == '-h':
-        # If -h option set, print usage, exit
-        usage(sys.argv[0])
-        sys.exit()
-    elif option == '-l':
-        # If -l option set, use the argument as loglevel if it is in range 1:4
-        argument = int(argument)
-        if argument in range(1,4):
-            ll = argument
+        rrd_fileid = rrd_datasource.rrd_file_id
+        rrd_datasourceid = rrd_datasource.id
+        descr = rrd_datasource.description
+        threshold = rrd_datasource.threshold
+        threshold_max = rrd_datasource.max
+        delimiter = rrd_datasource.delimiter
+        thresholdstate = rrd_datasource.threshold_state
+
+        log_it(3, "-- NEW DATASOURCE (%s) --" % rrd_datasourceid)
+        surpassed = 0
+
+        # These values are silly. They just show how much the maximum value
+        # ever in the life of the unit has been, which is totally useless to
+        # the thresholdMonitor
+        if exceptions.count(descr):
+            log_it(3, "%s is in exceptions" % descr)
+            continue
+
+        threshold_max = int(threshold_max)
+        log_it(3, "Adding datasource %s" % rrd_datasourceid)
+        # Getting the value from the database
+        pres.removeAllDs()
+        try:
+            pres.addDs(rrd_datasourceid)
+        except TypeError:
+            log_it(3, "Error could not add ds, continuing (%s,%s,%s)" %
+                    (rrd_datasourceid))
+            continue
+
+        
+        # We look at values over the 15 last minutes. 
+        pres.fromTime = '-15min'
+
+        log_it(3, "Getting data from %s (%s)" % (rrd_datasourceid, descr))
+        if pres.average():
+            value = pres.average().pop()
+            log_it(3, "Value returned = %s" % value)
         else:
-            if ll >= 2: print "No such loglevel: %s, using loglevel 1" % argument
-            ll = 1
+            log_it(3, "No value returned")
+            continue
 
-# For each rrd_datasource, fetch the value and compare
-# it to the max-value.
-c = conn.cursor()
-sql = """
-SELECT rrd_fileid, rrd_datasourceid, descr, threshold, max, delimiter, 
-thresholdstate
-FROM rrd_datasource 
-WHERE threshold IS NOT NULL
-"""
-c.execute(sql)
+        # Checking if it is percent or a normal value we are comparing
+        is_percent = re.compile("%$").search(threshold)
+        threshold = int(re.sub("%$", "", threshold))
 
-for (rrd_fileid, rrd_datasourceid, descr, threshold, max, delimiter, 
-     thresholdstate) in c.fetchall():
-    if ll >= 3: print "-- NEW DATASOURCE (%s) --" % rrd_datasourceid
-    surpassed = 0
-
-    # These values are silly. They just show how much the maximum value
-    # ever in the life of the unit has been, which is totally useless to
-    # the thresholdMonitor
-    if exceptions.count(descr):
-        if ll >= 3: print "%s is in exceptions" % descr
-        continue
-
-    max = int(max)
-
-    if ll >= 3: print "Adding datasource %s" % rrd_datasourceid
-    # Getting the value from the database
-    pres.removeAllDs()
-    try:
-        pres.addDs(rrd_datasourceid)
-    except TypeError:
-        if ll >= 3: 
-            print "Error could not add ds, continuing (%s,%s,%s)" \
-                %(rrd_datasourceid)
-        continue
-
-    # We look at values over the 15 last minutes. 
-    pres.fromTime = '-15min'
-
-    if ll >= 3: print "Getting data from %s (%s)" % (rrd_datasourceid, descr)
-    if pres.average():
-        value = pres.average().pop()
-        if ll >= 3: print "%s" % value
-    else:
-        if ll >= 3: print "No value returned"
-        continue
-
-    # Checking if it is percent or a normal value we are comparing
-    m = re.compile("%$").search(threshold)
-    threshold = int(re.sub("%$","",threshold))
-
-    # To prevent oscillation in case the value is just below the threshold
-    # we create a lower limit that has to be passed to really say that the
-    # crisis os over.
-    if thresholdstate == 'active':
-        if delimiter == '>':
-            threshold = threshold - downmodifier
-        elif delimiter == '<':
-            threshold = threshold + downmodifier
+        # To prevent oscillation in case the value is just below the threshold
+        # we create a lower limit that has to be passed to really say that the
+        # crisis os over.
+        if thresholdstate == 'active':
+            if delimiter == '>':
+                threshold = threshold - downmodifier
+            elif delimiter == '<':
+                threshold = threshold + downmodifier
             
-    if ll >= 3: print "Threshold is %s" % threshold
+        log_it(3, "Threshold is %s" % threshold)
 
-    if m:
-        if delimiter == '>' and (value / max  * 100) > threshold:
-            surpassed = 1
-        elif delimiter == '<' and (value / max * 100) < threshold:
-            surpassed = 1
-    else:
-        if delimiter == '<' and value < threshold:
-            surpassed = 1
-        elif delimiter == '>' and value > threshold:
-            surpassed = 1
+        if is_percent:
+            if delimiter == '>' and (value / threshold_max  * 100) > threshold:
+                surpassed = 1
+            elif delimiter == '<' and (value / threshold_max * 100) < threshold:
+                surpassed = 1
+        else:
+            if delimiter == '<' and value < threshold:
+                surpassed = 1
+            elif delimiter == '>' and value > threshold:
+                surpassed = 1
         
 
-    if surpassed and thresholdstate == 'inactive':
-        if ll >= 2: print "--------------------"
-        if ll >= 2: print "Threshold surpassed (%s,%s,%s ds:%s)" \
-                %(value, threshold, max, rrd_datasourceid)
-        # must send danger-event
-        makeEvent(pres, descr, threshold, rrd_datasourceid, rrd_fileid, 
-                  'active')
-    elif surpassed and thresholdstate == 'active':
-        if ll >= 2: print "--------------------"
-        if ll >= 2: print "Threshold still surpassed. (%s,%s,%s ds:%s)" \
-                %(value, threshold, max, rrd_datasourceid)
-	makeEvent(pres, descr, threshold, rrd_datasourceid, rrd_fileid,
-                  'stillactive')
-    elif datasource.thresholdstate == 'active':
-        if ll >= 2: print "--------------------"
-        if ll >= 2: print "Threshold below value (%s,%s,%s ds:%s)" \
-                %(value, threshold, max, rrd_datasourceid)
-        # Must send nodanger-event
-        makeEvent(pres, descr, threshold, rrd_datasourceid, rrd_fileid, 
+        if surpassed and thresholdstate == 'inactive':
+            log_it(2, "--------------------")
+            log_it(2, "Threshold surpassed (%s,%s,%s ds:%s)" %
+                    (value, threshold, threshold_max, rrd_datasourceid))
+            # must send danger-event
+            make_event(pres, descr, threshold, rrd_datasourceid, rrd_fileid, 
+                      'active')
+        elif surpassed and thresholdstate == 'active':
+            log_it(2, "--------------------")
+            log_it(2, "Threshold still surpassed. (%s,%s,%s ds:%s)" %
+                    (value, threshold, threshold_max, rrd_datasourceid))
+            make_event(pres, descr, threshold, rrd_datasourceid, rrd_fileid,
+                      'stillactive')
+        elif thresholdstate == 'active':
+            log_it(2, "--------------------")
+            log_it(2, "Threshold below value (%s,%s,%s ds:%s)" %
+                    (value, threshold, threshold_max, rrd_datasourceid))
+            # Must send nodanger-event
+            make_event(pres, descr, threshold, rrd_datasourceid, rrd_fileid, 
                   'inactive')
-    else:
-        if ll >= 3: print "Threshold not surpassed (%s,%s,%s)" \
-                %(value, threshold, max)
+        else:
+            log_it(3, "Threshold not surpassed (%s,%s,%s)" %
+                    (value, threshold, threshold_max))
 
-end = int(time.time())
-if ll >= 2: print "%s executed in %s seconds." %(sys.argv[0],end-start)
-if ll >= 2: print "------------------------------------------------------------------\n\n"
+    end = int(time.time())
+    log_it(2, "%s executed in %s seconds." %(argv[0], end-start))
+    log_it(2, "------------------------------------------------------------------\n\n")
+
+if __name__ == '__main__':
+    main(sys.argv)
