28b545e8dccd6b33471edeb52996b60d3ddbe895
Morten Brekkevold <morten.brekkevold@uninett.no>
2011-05-16 09:52:14 +0200

    Added EXTREME-VLAN-MIB

---
+++ python/nav/mibs/extreme_vlan_mib.py
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2011 UNINETT AS
+#
+# This file is part of Network Administration Visualized (NAV).
+#
+# NAV is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.  You should have received a copy of the GNU General Public
+# License along with NAV. If not, see <http://www.gnu.org/licenses/>.
+#
+"""Implements an EXTREME-VLAN-MIB MibRetriever"""
+
+import mibretriever
+from nav.mibs.qbridge_mib import PortList
+
+class ExtremeVlanMib(mibretriever.MibRetriever):
+    from nav.smidumps.extreme_vlan_mib import MIB as mib
