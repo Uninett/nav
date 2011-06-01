73e091931d488daed35e951e5d82a3f0f5a749c2
Trond Kandal <trond.kandal@ntnu.no>
2011-06-01 15:20:30 +0200

    sql for database-table sensor.

---
+++ sql/changes/sc.03.10.0001.sql
@@ -0,0 +1,8 @@
+CREATE TABLE manage.sensor (
+  sensorid SERIAL PRIMARY KEY,
+  netboxid INT REFERENCES netbox(netboxid) ON DELETE CASCADE ON UPDATE CASCADE,
+  oid VARCHAR,
+  unit_of_measurement VARCHAR,
+  data_scale VARCHAR,
+  human_readable VARCHAR
+);
