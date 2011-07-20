4277d98c644e4f3f6fc31bd7de02fdbf3f8c06c6
John-Magne Bredal <john.m.bredal@ntnu.no>
2011-07-20 14:45:03 +0200

    Refactored routers-module to use django models (and pleased pylint and pep8)

--- python/nav/mcc/routers.py
+++ python/nav/mcc/routers.py
@@ -1,29 +1,34 @@
 """
 Creates Cricket config for routers
 """
-import sys
+
 import os
 import re
 import logging
 from os.path import join
 
-from nav.db import getConnection
-
+from nav.models.manage import Netbox
+from nav.models.oid import NetboxSnmpOid
+from django.db.models import Q
 from nav.mcc import utils
 
+
+ROUTER_CATEGORIES = ['GW', 'GSW']
+
+
 def make_config(config):
+    """ Required function, run by mcc.py """
+
+    # This function should be divided in to many smaller ones.
+
     dirname = "routers"
     logger = logging.getLogger('mcc.routers')
 
     # Get path to cricket-config
     configfile = config.get('mcc', 'configfile')
     configroot = utils.get_configroot(configfile)
-    if not configroot:
-        logger.error("Could not find configroot in %s, exiting."
-                     % config.get('mcc', 'configfile'))
-        return False
-
     fullpath = join(configroot, dirname)
+
     logger.info("Creating config for %s in %s" % (dirname, fullpath))
 
     # Get views
@@ -33,7 +38,7 @@ def make_config(config):
         return False
 
     # Get datadir
-    datadir = join(utils.get_datadir(configroot), dirname)
+    datadir = join(configroot, dirname)
     logger.debug("Datadir set to %s" % datadir)
 
     # Find oids. We search all files as we cannot be certain of the name
@@ -43,102 +48,87 @@ def make_config(config):
         logger.error("Could not find oids in %s - exiting." % fullpath)
         return False
 
-    # Connect to database
-    conn = getConnection('default')
-    c = conn.cursor()
-
-    # Fetch routers
-    sql = """
-    SELECT netboxid, ro, sysname, ip, room.descr, typename
-    FROM netbox
-    JOIN room USING (roomid)
-    JOIN type USING (typeid)
-    WHERE catid IN ('GW','GSW')
-    """
-    c.execute(sql)
+    routers = Netbox.objects.filter(category__in=ROUTER_CATEGORIES)
 
     # Create target-types
     try:
-        f = open(join(fullpath, 'navTargetTypes'), 'w')
-    except Exception, e:
-        logger.error("Could not write to file: %s" % e)
+        filehandle = open(join(fullpath, 'navTargetTypes'), 'w')
+    except IOError, error:
+        logger.error("Could not write to file: %s" % error)
         return False
-    
+
     targetlist = []
     containers = []
-    for (netboxid, ro, sysname, ip, descr, typename) in c.fetchall():
-        logger.info("Creating config for %s" % sysname)
-        sql = """
-        SELECT oidkey, snmpoid FROM netboxsnmpoid
-        JOIN snmpoid USING (snmpoidid)
-        WHERE netboxid = %s
-        AND (oidsource = 'Cricket' OR snmpoid = '1.3.6.1.2.1.1.3.0')
-        """
+    for router in routers:
+        logger.info("Creating config for %s" % router.sysname)
 
-        c.execute(sql, (netboxid,))
-
-        # Check if rrd-file exists. If not, the rrd-file and corresponding
-        # datasources are removed from the database, and config is made from
-        # scratch.
-        fileexists = utils.check_file_existence(datadir, sysname)
+        snmpoids = NetboxSnmpOid.objects.filter(netbox=router).filter(
+            Q(snmp_oid__oid_source='Cricket') |
+            Q(snmp_oid__oid_key__iexact='sysuptime'))
 
         # Find the oids valid for this box that also exists in the cricket
         # config files.
         targetoids = []
-        for (oidkey, snmpoid) in c.fetchall():
-            if snmpoid in oidlist:
-                targetoids.append(oidkey)
+        for snmpoid in snmpoids:
+            if snmpoid.snmp_oid.snmp_oid in oidlist:
+                targetoids.append(snmpoid.snmp_oid.oid_key)
 
         targetoids.sort()
 
         # Skip to next netbox if no matching oids were found
-        if len(targetoids) <= 0:
-            logger.error("No oids found for %s" % sysname)
+        if not targetoids:
+            logger.error("No oids found for %s" % router.sysname)
             continue
 
-        if fileexists:
+        # Check if rrd-file exists. If not, the rrd-file and corresponding
+        # datasources are removed from the database, and config is made from
+        # scratch.
+        if utils.check_file_existence(datadir, router.sysname):
             # Compare datasources we found with the ones in the database, if
             # any.
             targetoids = utils.compare_datasources(
-                datadir, sysname, targetoids)
+                datadir, router.sysname, targetoids)
 
         # Print ds definition to file
-        targetlist.append((sysname, ip, ro, descr, typename))
-        f.write("targetType %s\n" % sysname)
-        f.write("\tds = \"%s\"\n" % ", ".join(targetoids))
-            
+        targetlist.append((router.sysname, router.ip, router.read_only,
+                           router.room.description, router.type.name))
+        filehandle.write("targetType %s\n" % router.sysname)
+        filehandle.write("\tds = \"%s\"\n" % ", ".join(targetoids))
+
         # Create view configuration. We do that by comparing the data from
         # views with the targetoids and see what intersections exists.
         intersections = []
         for entry in views:
             intersect = sorted(set(views[entry]).intersection(targetoids))
-            if len(intersect) > 0:
+            if intersect:
                 intersections.append("%s: %s"
                                      % (entry, " ".join(intersect)))
 
-        if len(intersections) > 0:
-            f.write("\tview = \"%s\"\n\n" % ", ".join(sorted(intersections)))
+        if intersections:
+            filehandle.write("\tview = \"%s\"\n\n" % ", ".join(
+                sorted(intersections)))
 
         # Create container object and fill it
-        container = utils.RRDcontainer(sysname, netboxid)
+        container = utils.RRDcontainer(router.sysname, router.id)
         counter = 0
-        for to in sorted(targetoids):
-            container.datasources.append(('ds' + str(counter), to, 'GAUGE'))
+        for targetoid in sorted(targetoids):
+            container.datasources.append(('ds' + str(counter), targetoid,
+                                          'GAUGE'))
             counter = counter + 1
 
         containers.append(container)
 
 
-    f.close()
-                
+    filehandle.close()
+
     try:
-        f = open(join(fullpath, utils.TARGETFILENAME), 'w')
-    except Exception, e:
-        logger.error("Could not write to file: %s" % e)
+        filehandle = open(join(fullpath, utils.TARGETFILENAME), 'w')
+    except IOError, error:
+        logger.error("Could not write to file: %s" % error)
         return False
-        
+
     # Create targets
-    for (sysname, ip, ro, descr, typename) in targetlist:
+    for (sysname, ip, readonly, descr, typename) in targetlist:
         displayname = utils.convert_unicode_to_latin1(sysname)
         if descr:
             typename = utils.encode_and_escape(typename)
@@ -148,14 +138,14 @@ def make_config(config):
             shortdesc = utils.encode_and_escape(typename)
 
         logger.info("Writing target %s" % sysname)
-        f.write("target \"%s\"\n" % sysname)
-        f.write("\tdisplay-name\t = \"%s\"\n" % displayname)
-        f.write("\tsnmp-host\t= %s\n" % ip)
-        f.write("\tsnmp-community\t= %s\n" % ro)
-        f.write("\ttarget-type\t= %s\n" % sysname)
-        f.write("\tshort-desc\t= \"%s\"\n\n" % shortdesc)
+        filehandle.write("target \"%s\"\n" % sysname)
+        filehandle.write("\tdisplay-name\t = \"%s\"\n" % displayname)
+        filehandle.write("\tsnmp-host\t= %s\n" % ip)
+        filehandle.write("\tsnmp-community\t= %s\n" % readonly)
+        filehandle.write("\ttarget-type\t= %s\n" % sysname)
+        filehandle.write("\tshort-desc\t= \"%s\"\n\n" % shortdesc)
 
-    f.close()
+    filehandle.close()
 
     utils.updatedb(datadir, containers)
 
@@ -164,6 +154,7 @@ def make_config(config):
 
 def find_oids(logger, path):
     """ Search all files in path for oids regarding Cricket-configuration """
+
     oidlist = []
     match = re.compile("OID\s+(\w+)\s+(\S+)")
 
@@ -172,18 +163,16 @@ def find_oids(logger, path):
         fullpath = join(path, entry)
         if os.path.isfile(fullpath):
             try:
-                f = open(fullpath, 'r')
-            except Exception, e:
-                logger.error(e)
+                filehandle = open(fullpath, 'r')
+            except IOError, error:
+                logger.error(error)
                 return oidlist
-        
-        for line in f:
-            m = match.search(line)
-            if m:
+
+        for line in filehandle:
+            matcher = match.search(line)
+            if matcher:
                 logger.debug("Found oid %s - %s"
-                             % (m.groups()[0], m.groups()[1]))
-                oidlist.append(m.groups()[1])
+                             % (matcher.groups()[0], matcher.groups()[1]))
+                oidlist.append(matcher.groups()[1])
 
     return list(set(oidlist))
-
-    
