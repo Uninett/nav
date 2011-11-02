0b697f464233a5719c13aa45abb0d1398dae210d
Magnus Eide <magnus.eide@uninett.no>
2011-11-02 15:03:50 +0100

    List view for active, planned and historic tasks

---
+++ python/nav/web/maintenance/views.py
@@ -0,0 +1,53 @@
+#
+# Copyright (C) 2011 UNINETT AS
+#
+# This file is part of Network Administration Visualized (NAV).
+#
+# NAV is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License version 2 as published by the Free
+# Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.  You should have received a copy of the GNU General Public License
+# along with NAV. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import nav.maintenance
+
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def active(request):
+    tasks = nav.maintenance.getTasks('maint_start < now() AND maint_end > now()')
+    return render_to_response(
+        'maintenance/list.html',
+        {
+            'active': {'active': True},
+            'tasks': tasks,
+        },
+        RequestContext(request)
+    )
+
+def planned(request):
+    tasks = nav.maintenance.getTasks('maint_start > now() AND maint_end > NOW()')
+    return render_to_response(
+        'maintenance/list.html',
+        {
+            'active': {'planned': True},
+            'tasks': tasks,
+        },
+        RequestContext(request)
+    )
+
+def historic(request):
+    tasks = nav.maintenance.getTasks('maint_end < now()', 'maint_end DESC')
+    return render_to_response(
+        'maintenance/list.html',
+        {
+            'active': {'planned': True},
+            'tasks': tasks,
+        },
+        RequestContext(request)
+    )
