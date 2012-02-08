c32458b48a3c8ec4c8e35ca275622d39dde90604
Morten Brekkevold <morten.brekkevold@uninett.no>
2012-02-08 13:41:56 +0100

    (BridgeMib.get_forwarding_database): new method.
    (MultiBridgeMib.get_forwarding_database): Allow multi-instance retrieval of forwarding dbs.

--- python/nav/mibs/bridge_mib.py
+++ python/nav/mibs/bridge_mib.py
@@ -1,5 +1,7 @@
 import mibretriever
 
+from twisted.internet import defer
+
 from nav.mibs import reduce_index
 
 class BridgeMib(mibretriever.MibRetriever):
@@ -14,7 +16,25 @@ class BridgeMib(mibretriever.MibRetriever):
         df = self.retrieve_column('dot1dBasePortIfIndex')
         return df.addCallback(reduce_index)
 
+    @defer.inlineCallbacks
+    def get_forwarding_database(self):
+        """Retrieves the forwarding database of the device."""
+        columns = yield self.retrieve_columns(['dot1dTpFdbPort',
+                                               'dot1dTpFdbStatus'])
+        columns = self.translate_result(columns)
+        learned = (row for row in columns.values()
+                   if row['dot1dTpFdbStatus'] == 'learned')
+        result = []
+        for row in learned:
+            mac =  ':'.join(["%02x" % o for o in row[0]])
+            port = row['dot1dTpFdbPort']
+            result.append((mac, port))
+        defer.returnValue(result)
 class MultiBridgeMib(BridgeMib, mibretriever.MultiMibMixIn):
     def get_baseport_ifindex_map(self):
         method = super(MultiBridgeMib, self).get_baseport_ifindex_map
+        return self._multiquery(method)
+
+    def get_forwarding_database(self):
+        method = super(MultiBridgeMib, self).get_forwarding_database
         return self._multiquery(method)
