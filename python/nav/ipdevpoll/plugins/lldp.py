7ec5e8bb9214abe3ca3dc325873db997048dc498
Morten Brekkevold <morten.brekkevold@uninett.no>
2012-03-02 11:48:54 +0100

    (LLDP): make actual AdjacencyCandidates out of identified neighbors.

--- python/nav/ipdevpoll/plugins/lldp.py
+++ python/nav/ipdevpoll/plugins/lldp.py
@@ -13,21 +13,26 @@
 # more details.  You should have received a copy of the GNU General Public
 # License along with NAV. If not, see <http://www.gnu.org/licenses/>.
 #
-"""ipdevpoll plugin to collect LLDP neighbors
-
-TODO: Identify known neighbors and update NAV database.
-
-"""
+"ipdevpoll plugin to collect LLDP neighbors"
 from pprint import pformat
 
 from twisted.internet import defer, threads
 
 from nav.mibs.lldp_mib import LLDPMib
-from nav.ipdevpoll import Plugin
+from nav.ipdevpoll import Plugin, shadows
 from nav.ipdevpoll.neighbor import LLDPNeighbor
 
+SOURCE = 'lldp'
+
 class LLDP(Plugin):
-    "Collects devices' table of remote LLDP devices"
+    """Collects devices' table of remote LLDP devices.
+
+    If the neighbor can be identified as something monitored by NAV, a
+    topology adjacency candidate will be registered. Otherwise, the
+    neighboring device will be noted as an unrecognized neighbor to this
+    device.
+
+    """
     remote = None
     neighbors = None
 
@@ -41,9 +46,25 @@ class LLDP(Plugin):
 
     def _process_remote(self):
         "Tries to synchronously identify LLDP entries in NAV's database"
+        shadows.AdjacencyCandidate.sentinel(self.containers, SOURCE)
+
         neighbors = [LLDPNeighbor(lldp) for lldp in self.remote]
         identified = [n for n in neighbors if n.identified]
         for neigh in identified:
             self._logger.debug("identified neighbor %r from %r",
                                (neigh.netbox, neigh.interface), neigh.record)
+            self._store_candidate(neigh)
         self.neighbors = neighbors
+
+    def _store_candidate(self, neighbor):
+        ifindex = neighbor.record.ifindex
+        ifc = self.containers.factory(ifindex, shadows.Interface)
+        ifc.ifindex = ifindex
+
+        key = (ifindex, neighbor.netbox.id, neighbor.interface.id, SOURCE)
+        cand = self.containers.factory(key, shadows.AdjacencyCandidate)
+        cand.netbox = self.netbox
+        cand.interface = ifc
+        cand.to_netbox = neighbor.netbox
+        cand.to_interface = neighbor.interface
+        cand.source = SOURCE
