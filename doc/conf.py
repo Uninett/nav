bb34b825f4bc59e14e37b809654f62517949faca
Morten Brekkevold <morten.brekkevold@uninett.no>
2012-04-30 10:23:17 +0200

    remove intersphinx plugin

--- doc/conf.py
+++ doc/conf.py
@@ -24,7 +24,7 @@ from nav import buildconf
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
