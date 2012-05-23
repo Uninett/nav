8c008957ec09ca7892c20476469f9a79e79aa369
John-Magne Bredal <john.m.bredal@uninett.no>
2012-05-23 13:54:49 +0200

    Added searchform for searching for a room in info/room

--- python/nav/web/info/room/views.py
+++ python/nav/web/info/room/views.py
@@ -1,11 +1,31 @@
+from django.db.models import Q
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from nav.models.manage import Room, Netbox
+from nav.web.info.room.forms import SearchForm
 
 def search(request):
-    return render_to_response("info/room/base.html", {}, 
+    rooms = Room.objects.none()
+
+    if request.method == "GET":
+        searchform = SearchForm()
+    elif request.method == "POST":
+        searchform = SearchForm(request.POST, auto_id=False)
+        if searchform.is_valid():
+            rooms = process_searchform(searchform)
+
+    return render_to_response("info/room/base.html", 
+                              {"searchform": searchform,
+                               "rooms": rooms},
                               context_instance=RequestContext(request))
+
+def process_searchform(form):
+    query = form.cleaned_data['query']
+    return Room.objects.filter(
+        Q(id__icontains=query) |
+        Q(description__icontains=query)
+    )
 
 
 def roominfo(request, roomid):
