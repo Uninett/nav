a753469e73b2e68ac60939d5262a01b37fd09ac1
John-Magne Bredal <john.m.bredal@uninett.no>
2012-05-23 15:20:40 +0200

    Skeleton for info search page

--- python/nav/web/info/room/urls.py
+++ python/nav/web/info/room/urls.py
@@ -1,5 +1,23 @@
-from nav.web.info.room.views import search, roominfo
+#
+# Copyright (C) 2012 UNINETT AS
+#
+# This file is part of Network Administration Visualized (NAV).
+#
+# NAV is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.  You should have received a copy of the GNU General Public License
+# along with NAV. If not, see <http://www.gnu.org/licenses/>.
+#
+"""Django URL configuration"""
+
+
 from django.conf.urls.defaults import url, patterns
+from nav.web.info.room.views import search, roominfo
 
 urlpatterns = patterns('',
     url(r'^$', search, name='room-search'),
