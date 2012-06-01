e8d0793425a397e3f290b0d97fd90ae21d681a1f
John-Magne Bredal <john.m.bredal@uninett.no>
2012-06-01 13:25:02 +0200

    Small improvements

--- python/nav/web/info/room/views.py
+++ python/nav/web/info/room/views.py
@@ -5,7 +5,14 @@ from django.template import RequestContext
 from nav.models.manage import Room, Netbox
 from nav.web.info.room.forms import SearchForm
 
+
+CATEGORIES = ("GW", "GSW", "SW")
+
+
 def search(request):
+    """
+    Controller for searching for rooms
+    """
     rooms = Room.objects.none()
 
     if request.method == "GET":
@@ -14,23 +21,43 @@ def search(request):
         searchform = SearchForm(request.POST, auto_id=False)
         if searchform.is_valid():
             rooms = process_searchform(searchform)
+            for room in rooms:
+                room.netboxes = filter_netboxes(room)
 
     return render_to_response("info/room/base.html", 
                               {"searchform": searchform,
                                "rooms": rooms},
                               context_instance=RequestContext(request))
 
+
 def process_searchform(form):
+    """
+    Find and return rooms based on searchform
+    """
     query = form.cleaned_data['query']
-    return Room.objects.filter(
-        Q(id__icontains=query) |
-        Q(description__icontains=query)
-    ).order_by("id")
+    if query is None:
+        return Room.objects.all()
+    else:
+        return Room.objects.filter(
+            Q(id__icontains=query) |
+            Q(description__icontains=query)
+        ).order_by("id")
+
+
+def filter_netboxes(room):
+    """
+    Filter netboxes based on interesting categories
+    """
+    return room.netbox_set.filter(category__in=CATEGORIES)
 
 
 def roominfo(request, roomid):
+    """
+    Controller for displaying roominfo
+    """
     room = Room.objects.get(id=roomid)
-    netboxes = room.netbox_set.all().order_by("category", "sysname")
+    netboxes = filter_netboxes(room)
+    netboxes.order_by("category", "sysname")
 
     # Filter interfaces on iftype
     for netbox in netboxes:
