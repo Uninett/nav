67820ca076509f018e3dc9dbacd49e23ea2d0910
John-Magne Bredal <john.m.bredal@uninett.no>
2012-06-15 14:00:44 +0200

    Added navigation in tabs (history, bookmark)

---
+++ media/js/info.js
@@ -0,0 +1,62 @@
+
+/* Create js namespace for info page */
+var infopage = {};
+
+infopage.tabselected = false;
+
+/* Add tabs to roomview content */
+infopage.add_roomview_tabs = function() {
+    var tabconfig = {
+        cache: true // cache loaded pages
+    };
+    var tabs = $('#infotabs').tabs(tabconfig);
+};
+
+
+/* Add navigation (bookmark and history) to tabs */
+infopage.add_navigation = function() {
+    /* Mark selected tab on page load */
+    select_tab_on_load();
+
+    /* Set hash mark with index when a tab is selected */
+    $('#infotabs').bind('tabsselect', function(event, ui) {
+        if (ui.index != 0 || window.location.hash) {
+            window.location.hash = ui.index;
+        }
+    });
+
+    /* On hash change, navigate to the tab indicated in hash mark */
+    $(window).bind('hashchange', function(event) {
+        navigate();
+    });
+
+    /* Mark selected tab on page load */
+    function select_tab_on_load() {
+        navigate();
+    }
+
+    /* Navigate to correct tab based on url hash mark */
+    function navigate() {
+        var $tabs = $('#infotabs').tabs();
+        var index = 0;
+        if (window.location.hash) {
+            index = parseInt(window.location.hash.substring(1));
+        }
+
+        if (index != $tabs.tabs('option', 'selected')) {
+            $tabs.tabs('select', index);
+        }
+    }
+
+};
+
+
+/* Run javascript at document ready */
+$(document).ready(function(){
+    if ($('#infotabs')) {
+        infopage.add_roomview_tabs();
+        infopage.add_navigation();
+    }
+});
+
+
