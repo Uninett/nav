5b960d39f46f09e47eedbff34d070c34dbd3769e
Morten Brekkevold <morten.brekkevold@uninett.no>
2012-06-19 14:01:27 +0200

    Implement simple eventhandler/plugin loader w/tests.

--- tests/unittests/eventengine/plugin_test.py
+++ tests/unittests/eventengine/plugin_test.py
@@ -1,6 +1,7 @@
 import pytest
 from mock import Mock
 from nav.eventengine.plugin import UnsupportedEvent, EventHandler
+from nav.eventengine.plugin import _find_package_modules
 
 class _EmptyHandler(EventHandler):
     pass
@@ -21,3 +22,13 @@ def test_handler_should_construct_fine_on_supported_event():
     event = Mock()
     event.event_type_id = 'boxState'
     assert _BoxStateHandler(event)
+
+def test_find_package_modules_is_list():
+    modules = _find_package_modules('nav.eventengine.plugins')
+    assert not isinstance(modules, basestring)
+    assert len(modules) >= 0
+
+def test_boxstate_plugin_should_be_found():
+    from nav.eventengine.plugins.boxstate import BoxStateHandler
+    classes = EventHandler.load_and_find_subclasses()
+    assert BoxStateHandler in classes
