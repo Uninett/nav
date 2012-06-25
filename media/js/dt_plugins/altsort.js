916ca1171aab0d43c9ddf6748b89c41beb1e9cb9
John-Magne Bredal <john.m.bredal@uninett.no>
2012-06-25 13:27:26 +0200

    (DataTables): DataTables plugin for jQuery with custom plugins

---
+++ media/js/dt_plugins/altsort.js
@@ -0,0 +1,13 @@
+jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+    "alt-string-pre": function ( a ) {
+        return a.match(/alt="(.*?)"/)[1].toLowerCase();
+    },
+
+    "alt-string-asc": function( a, b ) {
+        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+    },
+
+    "alt-string-desc": function(a,b) {
+        return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+    }
+} );
