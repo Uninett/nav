277efc5adde1f5fb35518de1fe1d6e342e262d05
John-Magne Bredal <john.m.bredal@uninett.no>
2012-06-27 09:04:53 +0200

    (tests/info): Tests changed to reflect new SearchResult structure

--- tests/unittests/info/views_test.py
+++ tests/unittests/info/views_test.py
@@ -28,7 +28,7 @@ class ViewsTest(unittest.TestCase):
     def setUp(self):
         """ Test setup """
 
-        searchresult = SearchResult('test', 'test', None)
+        searchresult = SearchResult('test', None)
         self.searchprovider0 = SearchProvider()
         self.searchprovider1 = SearchProvider()
         self.searchprovider2 = SearchProvider()
