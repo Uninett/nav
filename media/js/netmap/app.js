ccdae33806db4b83f95bd1ff6820221a747ce87a
Roy Sindre Norangshol <norangshol@uninett.no>
2012-07-30 11:32:20 +0200

    Enables emulateHTTP in Backbone as mod_python doesn't support PUT!

--- media/js/netmap/app.js
+++ media/js/netmap/app.js
@@ -1,13 +1,19 @@
 
 define([
-    'jQuery',
-    'Underscore',
-    'Backbone',
+    'jquery',
+    'underscore',
+    'backbone',
     'router' // Request router.js
 ], function($, _, Backbone, Router, posts) {
     var initialize = function () {
         self = this;
-        // Pass in our Router module and call it's initalize function
+
+        // Comment this one out when moving to mod_wsgi! mod_python
+        // does not support PUT etc , only GET and POST. Silly mod_python!
+        Backbone.emulateHTTP = true;
+
+        //Backbone.emulateJSON = true;
+        // Pass in our Router module and call it's initialize function
         Router.initialize();
     };
 
