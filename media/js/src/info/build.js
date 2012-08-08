6c1762896b59f615efdccfa75105ec1a25211587
John-Magne Bredal <john.m.bredal@uninett.no>
2012-08-08 09:40:48 +0200

    (media/js) - Merged info.js into main.js and removed it.

--- media/js/src/info/build.js
+++ media/js/src/info/build.js
@@ -1,8 +1,8 @@
 ({
-    baseUrl: ".",
+    baseUrl: "../../",
     paths: {
         'main_info': 'src/info/main'
     },
     name: "main_info",
-    out: "/usr/local/nav/share/htdocs/js/src/info/main.js"
+    out: "../../builds/info-built.js"
 })
