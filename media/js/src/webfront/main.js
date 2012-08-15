2a02e96339d9af223eb73d4c386588b840e4b3f6
John-Magne Bredal <john.m.bredal@uninett.no>
2012-08-15 16:37:49 +0200

    (webfront) - Make sure map is not shown before when not logged in.

--- media/js/src/webfront/main.js
+++ media/js/src/webfront/main.js
@@ -5,8 +5,10 @@ require(['src/info/room_mapper', 'libs/jquery-1.4.4.min'], function (RoomMapper)
 
     $(function () {
         var mapper_node = $('#room_map');
+        var wrapper = mapper_node.parent('.mapwrapper');
         if (mapper_node.length > 0) {
             $.getJSON('/info/room/positions/', function (data) {
+                wrapper.show();
                 new RoomMapper(mapper_node.get(0), data.rooms).createMap();
             });
         }
