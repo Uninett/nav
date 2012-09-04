567cee24cabd6b8fa41d3feb7b00b73ed4f1a9d8
Roy Sindre Norangshol <norangshol@uninett.no>
2012-09-04 10:05:43 +0200

    Fixed pylint errors for stubs in Netmap

--- python/nav/netmap/stubs/__init__.py
+++ python/nav/netmap/stubs/__init__.py
@@ -1,4 +1,25 @@
+#
+# Copyright (C) 2012 UNINETT AS
+#
+# This file is part of Network Administration Visualized (NAV).
+#
+# NAV is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.  You should have received a copy of the GNU General Public
+# License along with NAV. If not, see <http://www.gnu.org/licenses/>.
+#
+"""Stubs for use in Netmap"""
+
+#Ignore too few public methods, these are stubs.
+# pylint: disable=R0903
+
 class Netbox(object):
+    """Netbox stub"""
     def __str__(self):
         return str(self.sysname)
 
@@ -8,16 +29,18 @@ class Netbox(object):
     def __key(self):
         return (self.sysname)
 
-    def __eq__(x, y):
-        return x.__key() == y.__key()
+    def __eq__(self, i, j):
+        return i.__key() == j.__key()
 
     def __hash__(self):
         return hash(self.__key())
 
-    def get_absolute_url(self):
+    @classmethod
+    def get_absolute_url(cls):
         return None
 
 class GwPortPrefix(object):
+    """Gwport stub"""
     def __str__(self):
         return str(self.gw_ip)
 
@@ -27,13 +50,14 @@ class GwPortPrefix(object):
     def __key(self):
         return (self.gw_ip)
 
-    def __eq__(x, y):
-        return x.__key() == y.__key()
+    def __eq__(self, i, j):
+        return i.__key() == j.__key()
 
     def __hash__(self):
         return hash(self.__key())
 
 class Interface(object):
+    """Interface stub"""
     def __str__(self):
         return str(self.iname, self.netbox)
 
@@ -43,13 +67,14 @@ class Interface(object):
     def __key(self):
         return (self.ifname)
 
-    def __eq__(x, y):
-        return x.__key() == y.__key()
+    def __eq__(self, i, j):
+        return i.__key() == j.__key()
 
     def __hash__(self):
         return hash(self.__key())
 
-    def get_absolute_url(self):
+    @classmethod
+    def get_absolute_url(cls):
         return None
 
 
