9ac88ea614a0b57b58a94e5f3182f4bd392a110b
Roy Sindre Norangshol <norangshol@uninett.no>
2012-09-10 09:44:41 +0200

    Started working on header_footer_minimize.js plugin
    Plugin to toggle showing of header and footer.

---
+++ media/js/test/plugins/header_footer_minimize-test.js
@@ -0,0 +1,30 @@
+require(['plugins/header_footer_minimize',
+    'resources/libs/text!testResources/plugins/header_footer_minimize/header.html',
+    'libs/jquery'
+], function (TestScript, headerEl, jquery) {
+    buster.testCase("HeaderFooterMinimize:", {
+        setUp: function () {
+            this.script = new TestScript;
+            this.script.initialize({
+                header: headerEl,
+                footer: headerEl
+            });
+        },
+        "should minimize when header is showing": function () {
+            var self = this;
+
+            refute.equals(headerEl, "");
+            self.script.toggleHeader();
+            assert.equals(self.script.isHeaderShowing(), false);
+
+
+        },
+        "toggle a few times and make sure state is correct": function () {
+            var self = this;
+            assert.equals(self.script.isHeaderShowing(), true);
+            self.script.toggleHeader();
+            assert.equals(self.script.isHeaderShowing(), false);
+        }
+    });
+
+});
