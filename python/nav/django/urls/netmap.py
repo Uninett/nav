4713559496d91fd8853dfe3a2d5345d402ae8d79
Roy Sindre Norangshol <norangshol@uninett.no>
2012-09-13 01:10:37 +0200

    (Netmap) map netmapdev namespace to netmap in django's main urls.py

--- python/nav/django/urls/netmap.py
+++ python/nav/django/urls/netmap.py
@@ -20,6 +20,6 @@ from django.conf.urls.defaults import patterns, include
 def get_urlpatterns():
     urlpatterns = patterns('',
         # Give the networkexplorer namespace to the Netmap subsystem
-        (r'^netmap/', include('nav.web.netmapdev.urls')),
+        (r'^netmap/', include('nav.web.netmap.urls')),
     )
     return urlpatterns
