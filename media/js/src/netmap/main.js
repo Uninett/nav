aa7b39aa88a750ef0f4efd5cd4440f4fb1ad2a8b
Roy Sindre Norangshol <norangshol@uninett.no>
2012-09-13 00:16:58 +0200

    (Netmap) Remove dead comment in main.js

--- media/js/src/netmap/main.js
+++ media/js/src/netmap/main.js
@@ -1,39 +1,3 @@
-// Require.js configure shortcut aliases
-/*
-require.config({
-    shim: {
-        underscore: {
-            exports: '_'
-        },
-        backbone: {
-            deps: ["underscore", "jquery"],
-            exports: "Backbone"
-        },
-        spinjs: {
-            exports: "Spinner"
-        }
-    },
-    paths: {
-        //loader: 'libs/backbone/loader',
-        //jQuery: 'libs/jquery/jquery',
-        jquery: 'libs/jquery/jquery', // used by jqueryui-amd ..
-        //jQuery: 'libs/jquery/jquery', // used by jqueryui-amd ..
-        jqueryui: 'libs/jqueryui',
-        underscore: 'libs/underscore/underscore',
-        //Underscore: 'libs/underscore/underscore',
-        handlebars: 'libs/handlebars/handlebars',
-        backbone: 'libs/backbone/backbone',
-        backbone_eventbroker: 'libs/backbone/backbone-eventbroker',
-        //Backbone: 'libs/backbone/backbone',
-        //text: 'libs/require/text',
-         //order: 'libs/require/order',
-        netmapextras: '../netmap-extras',
-        spinjs: '../spin.min',
-        templates: 'templates'
-    },
-    urlArgs: "nav=" + (new Date()).getTime()
-});*/
-
 require([
 
     // Load our app module and pass it to our definition function
