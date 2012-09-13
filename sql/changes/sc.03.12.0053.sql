eafd9474d5846c5ee429becd3b4782279d3a18fd
John-Magne Bredal <john.m.bredal@uninett.no>
2012-09-13 10:22:48 +0200

    Added access to info and netmap for authenticated users. Added save version of insert for other queries.

--- sql/changes/sc.03.12.0053.sql
+++ sql/changes/sc.03.12.0053.sql
@@ -1,2 +1,6 @@
 -- Grant web access to osm map redirects
-INSERT INTO AccountGroupPrivilege (accountgroupid, privilegeid, target) VALUES (2, 2, E'^/info/osm_map_redirect/?');
+INSERT INTO AccountGroupPrivilege (accountgroupid, privilegeid, target)
+  SELECT 2, 2, '^/info/osm_map_redirect/?' WHERE NOT EXISTS (
+    SELECT * FROM AccountGroupPrivilege WHERE accountgroupid=2 AND privilegeid=2 AND target = '^/info/osm_map_redirect/?'
+  )
+;
