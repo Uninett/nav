b330db99363928b545277f51fa11bb4d0341bbd8
Roy Sindre Norangshol <norangshol@uninett.no>
2012-09-27 11:03:34 +0200

    (Netmap) Netmap traffic gradient enhancements (Fixes LP#1057373)
    Use jQuery UI Dialog NAV theme for dialog
    Add metadata for the gradient (markers for where is 10%, 20%, 30% )
    Show only 0-100% palette for the gradient (not the internal one)
    Reverse gradient, 100% at top and 0% at bottom

--- media/js/src/netmap/views/modal/traffic_gradient.js
+++ media/js/src/netmap/views/modal/traffic_gradient.js
@@ -18,8 +18,23 @@ define([
                 return (value && typeof value === 'number') ? Math.round(value) : 0;
             });
 
+            // Same as each, just including index
+            Handlebars.registerHelper('iter', function(context, options) {
+                var fn = options.fn, inverse = options.inverse;
+                var ret = "";
+
+                if(context && context.length > 0) {
+                    for(var i=0, j=context.length; i<j; i++) {
+                        ret = ret + fn(_.extend({}, context[i], { percent: context.length - 1 - i, isDecade: i%10==0 }));
+                    }
+                } else {
+                    ret = inverse(this);
+                }
+                return ret;
+            });
+
             var self = this;
-            self.el = $(self.template_post({'collection': self.collection.toJSON()})).dialog({autoOpen: false, width: 'auto'});
+            self.el = $(self.template_post({'collection': self.collection.toJSON().reverse()})).dialog({autoOpen: false, width: 'auto'});
             self.$el = $(self.el);
 
         },
