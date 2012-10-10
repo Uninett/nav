67624151ca6d79e9a583be223afd60b0f9bc343f
John-Magne Bredal <john.m.bredal@uninett.no>
2012-10-10 15:56:17 +0200

    vlaninfo - testing

--- python/nav/web/info/vlan/views.py
+++ python/nav/web/info/vlan/views.py
@@ -16,16 +16,18 @@
 """View definitions for info/vlan"""
 
 from IPy import IP
+from os.path import exists
 
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
 from nav.models.manage import Prefix
 from nav.models.rrd import RrdFile
-from nav.rrd2.presenter import Presentation, Graph
+from nav.rrd2.presenter import Graph
 
 def index(request):
-    prefixes = Prefix.objects.all()[0:5]
+    """Render all graphs for prefixes"""
+    prefixes = Prefix.objects.exclude(vlan__net_type='loopback')
     images = []
     for prefix in prefixes:
         try:
@@ -33,29 +35,50 @@ def index(request):
         except RrdFile.DoesNotExist:
             continue
 
-        datasources = rrdfile.rrddatasource_set.all()
-
-        options = {'-l': '0', '-v': 'IP-addresses'}
-        graph = Graph(title=prefix.net_address, opts=options)
-        for datasource in datasources:
-            if datasource.name == 'ip_count':
-                graph.add_datasource(datasource, 'AREA', 'IP-addresses ')
-                add_graph_info(graph, datasource.id, 'ipcount')
-            if datasource.name == 'mac_count':
-                graph.add_datasource(datasource, 'LINE2', 'MAC-addresses')
-                add_graph_info(graph, datasource.id, 'maccount')
-            if datasource.name == 'ip_range' and IP(prefix.net_address).version() == 4:
-                graph.add_datasource(datasource, 'LINE2', 'Max addresses')
-                add_graph_info(graph, datasource.id, 'iprange')
-
-        images.append(graph.get_url())
+        if exists(rrdfile.get_file_path()):
+            graph = create_graph(prefix, rrdfile)
+            images.append(graph.get_url())
 
     return render_to_response("info/vlan/base.html",
                               {'images': images},
                               context_instance=RequestContext(request))
 
 
-def add_graph_info(graph, dsid, variable):
+def create_graph(prefix, rrdfile):
+    """Create graph based on prefix and rrdfile"""
+    datasources = rrdfile.rrddatasource_set.all()
+
+    options = {'-l': '0', '-v': 'IP-addresses'}
+    graph = Graph(title=prefix.net_address, opts=options)
+    for datasource in datasources:
+        if datasource.name == 'ip_count':
+            graph.add_datasource(datasource, 'AREA', 'IP-addresses ')
+            add_graph_lines(graph, datasource.id, 'ipcount')
+        if datasource.name == 'mac_count':
+            graph.add_datasource(datasource, 'LINE2', 'MAC-addresses')
+            add_graph_lines(graph, datasource.id, 'maccount')
+        if is_add_max(datasource, prefix):
+            graph.add_datasource(datasource, 'LINE2', 'Max addresses')
+            add_graph_lines(graph, datasource.id, 'iprange')
+
+    return graph
+
+def is_add_max(datasource, prefix):
+    """Check if we should create max-values for this datasource"""
+    return datasource.name == 'ip_range' and \
+           is_ipv4(prefix) and \
+           not is_scope(prefix)
+
+def is_scope(prefix):
+    """Check if this prefix is a scope-prefix"""
+    return prefix.vlan.net_type.id == 'scope'
+
+def is_ipv4(prefix):
+    """Check if prefix netaddress is of type 4"""
+    return IP(prefix.net_address).version() == 4
+
+def add_graph_lines(graph, dsid, variable):
+    """Add lines to graph based on dsid and variable"""
     graph.add_argument("VDEF:cur_%s=%s,LAST" % (variable, dsid))
     graph.add_argument("GPRINT:cur_%s:%s" % (variable, 'Now\: %-6.0lf'))
     graph.add_argument("VDEF:avg_%s=%s,AVERAGE" % (variable, dsid))
