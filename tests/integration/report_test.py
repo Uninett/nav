2728c64d9d35833dd6dc24a78c07dc1470b1186c
Roy Sindre Norangshol <norangshol@uninett.no>
2012-10-11 14:20:20 +0200

    (report) Dead mod_python tests

--- tests/integration/report_test.py
+++ tests/integration/report_test.py
@@ -35,27 +35,6 @@ class ReportEncodingTest(ModPythonTestCase):
         transaction.rollback()
         transaction.leave_transaction_management()
 
-    def test_index_no_unicode_output(self):
-        self.handler_outputs_no_unicode("/report/")
-
-    def test_matrix_no_unicode_output(self):
-        self.handler_outputs_no_unicode("/report/matrix")
-
-    def test_reportlist_no_unicode_output(self):
-        self.handler_outputs_no_unicode("/report/reportlist")
-
-    def test_netbox_report_no_unicode_output(self):
-        self.handler_outputs_no_unicode("/report/netbox")
-
-    def test_org_report_no_unicode_output(self):
-        self.handler_outputs_no_unicode("/report/org")
-
-    def test_location_report_no_unicode_output(self):
-        self.handler_outputs_no_unicode("/report/location")
-
-    def test_room_report_no_unicode_output(self):
-        self.handler_outputs_no_unicode("/report/room")
-
     def test_admin_name_present_in_output(self):
         buffer = StringIO()
         admin_substring = '\xc3\x86\xc3\x98\xc3\x85'
