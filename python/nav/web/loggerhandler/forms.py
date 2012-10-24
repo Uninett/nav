e461d75f4a246ec38d13e8dd03f90491b6e89067
Roy Sindre Norangshol <norangshol@uninett.no>
2012-10-24 14:32:07 +0200

    (loggerhandler) Fixed forms validation

--- python/nav/web/loggerhandler/forms.py
+++ python/nav/web/loggerhandler/forms.py
@@ -3,11 +3,11 @@ from nav.models.logger import Priority, LoggerCategory, Origin, LogMessageType
 
 
 class LoggerSearchForm(forms.Form):
-    DATEFORMAT = "%Y-%m-%d %H:%M:%S"
+    DATEFORMAT = ("%Y-%m-%d %H:%M:%S",)
 
-    priority = forms.ModelMultipleChoiceField(queryset=Priority.objects.all())
-    message_type = forms.ModelMultipleChoiceField(queryset=LogMessageType.objects.all())
-    category = forms.ModelMultipleChoiceField(queryset=LoggerCategory.objects.all())
-    origin = forms.ModelMultipleChoiceField(queryset=Origin.objects.all())
+    priority = forms.ModelMultipleChoiceField(queryset=Priority.objects.all(), required=False)
+    message_type = forms.ModelMultipleChoiceField(queryset=LogMessageType.objects.all(), required=False)
+    category = forms.ModelMultipleChoiceField(queryset=LoggerCategory.objects.all(), required=False)
+    origin = forms.ModelMultipleChoiceField(queryset=Origin.objects.all(), required=False)
     timestamp_from = forms.DateTimeField(input_formats=DATEFORMAT)
     timestamp_to = forms.DateTimeField(input_formats=DATEFORMAT)
