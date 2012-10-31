0a7c6488a4c29e654b08649d73088ab93601dd68
Roy Sindre Norangshol <norangshol@uninett.no>
2012-10-31 11:37:42 +0100

    (loggerhandler) Attach button event handlers when tabloading is complete

--- media/js/src/logger.js
+++ media/js/src/logger.js
@@ -17,19 +17,32 @@ require([
     function addMainTabs() {
         var tabconfig = {
             cache: true, // cache loaded pages
-            spinner: '<img src="/images/main/process-working.gif">'
+            spinner: '<img src="/images/main/process-working.gif">',
+            load: eventLoadingComplete
         };
         var tabs = $(mainTabsSelector).tabs(tabconfig);
         tabs.show();
         TabNavigation.add(mainTabsSelector);
     }
 
-    function searchSyslog(target) {
+    function eventLoadingComplete(event, ui) {
+        if (ui.tab.text.trim() === 'Direct Search' || ui.tab.text.trim() === 'Group Search') {
+            attachButtonListeners()
+        }
+    }
 
-        $.post(target, $("#syslog_search_form").serialize(), function (data) {
+    function attachButtonListeners() {
+        $('#syslog_search_form button').on('click', function (event) {
+            event.preventDefault();
+            searchSyslog($(this).data('target'));
+        });
+    }
+
+    function searchSyslog(target) {
+        $.get(target, $("#syslog_search_form").serialize(), function (data) {
             // todo: need error checking.
-            debugger;
             $('#syslog_search').html(data);
+            attachButtonListeners()
         });
     }
 });
