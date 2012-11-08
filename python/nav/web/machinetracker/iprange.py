4e93e65c6f7978dad3b876106f9effe2cbde2eec
Christian Strand Young <christian@strandyoung.com>
2012-11-08 10:18:35 +0100

    Machine tracker no longer crashes when forcing to find prefix and none is found (LP #1074323)

--- python/nav/web/machinetracker/iprange.py
+++ python/nav/web/machinetracker/iprange.py
@@ -7,9 +7,11 @@ class MachinetrackerIPRange(util.IPRange):
     def get_mask_for_network(cls, network):
         """ Lookup prefix from NAV database """
         prefix = get_prefix_info(network)
-        prefix_address = prefix.net_address
-        prefix_cidr = prefix_address.split("/")
-        return prefix_cidr[1]
+        if prefix:
+            prefix_address = prefix.net_address
+            prefix_cidr = prefix_address.split("/")
+            return prefix_cidr[1]
+        return '24'
 
 
 
