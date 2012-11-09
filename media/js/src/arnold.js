47573352f87b1bed6edd8fbbac55b4ab6938266f
John-Magne Bredal <john.m.bredal@uninett.no>
2012-11-09 11:04:21 +0100

    (arnold): Add sorting to tables in history and detained ports via tablesorter.

--- media/js/src/arnold.js
+++ media/js/src/arnold.js
@@ -1,4 +1,4 @@
-require(['libs/jquery'], function () {
+require(['libs/jquery', 'libs/jquery.tablesorter.min'], function () {
     $(function () {
         if ($('form.profileDetentionForm').length > 0) {
             addVlanToggler('#id_detention_type_0', '#id_detention_type_1', '.vlanrow')
@@ -6,6 +6,28 @@ require(['libs/jquery'], function () {
         if ($('form.manualDetentionForm').length > 0) {
             addVlanToggler('#id_method_0', '#id_method_1', '.vlanrow')
         }
+
+        // Add tablesorter to history table
+        if ($('.arnold-history tbody').length > 0) {
+            $('.arnold-history').tablesorter({
+                headers: {
+                    0: { sorter: 'ipAddress'},
+                    8: { sorter: false}
+                }
+            });
+        }
+
+        // Add tablesorter to detained ports table
+        if ($('.arnold-detainedports tbody').length > 0) {
+            $('.arnold-detainedports').tablesorter({
+                headers: {
+                    0: { sorter: 'ipAddress'},
+                    7: { sorter: false},
+                    8: { sorter: false}
+                }
+            });
+        }
+
     });
 
     function addVlanToggler(hideNode, showNode, node) {
