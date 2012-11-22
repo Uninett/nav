47a027752bab84722924ffad6c8296e01a475865
Roy Sindre Norangshol <norangshol@uninett.no>
2012-11-22 11:55:50 +0100

    (netmap) working on getting mouseover_toggler to use the generalized checkradio model and template

--- media/js/src/netmap/views/mouseover_toggler.js
+++ media/js/src/netmap/views/mouseover_toggler.js
@@ -1,7 +1,7 @@
 define([
-    'netmap/collections/categories',
+    'netmap/collections/checkradio',
     'netmap/models/input_checkradio',
-    'libs-amd/text!netmap/templates/categories_toggler.html',
+    'libs-amd/text!netmap/templates/checkradio.html',
     'libs/handlebars',
     'libs/jquery',
     'libs/underscore',
@@ -16,40 +16,41 @@ define([
         },
         initialize: function () {
             this.template = Handlebars.compile(Template);
-            // todo: fetch collection from api.
+
             if (!this.collection) {
                 this.collection = new Collection([
-                    {name: "GSW"},
-                    {name: "GW"},
-                    {name: "SW"},
-                    {name: "OTHER"},
-                    {name: "WLAN"},
-                    {name: "SRV"},
-                    {name: "EDGE"},
-                    {name: "ELINK", 'is_selected': false}
+                    {name: "nodes"},
+                    {name: "links"}
                 ]);
             }
 
-            this.collection.bind("change", this.broadcastcategoriesFilters, this);
-            //this.model.bind("change:layer", this.updateSelection, this);
-            //this.model.bind("change", this.render, this);
+            this.collection.bind("change", this.broadcastMouseOverFilters, this);
 
             return this;
         },
 
         render: function () {
             this.$el.html(
-                this.template({collection: this.collection.toJSON()})
+                this.template({
+                    title: 'Mouseover',
+                    type: 'checkbox',
+                    identifier: 'mouseOver',
+                    collection: this.collection.toJSON()
+                })
             );
 
             return this;
         },
-        broadcastcategoriesFilters: function () {
-            this.broker.trigger("netmap:changeCategoriesFilters", this.collection);
+        broadcastMouseOverFilters: function () {
+            this.broker.trigger("netmap:changeMouseOverFilters", this.collection);
         },
         setMouseOverOptions: function (e) {
-            this.context.ui.mouseover[$(e.currentTarget).val()].state = $(e.currentTarget).prop('checked');
-            this.broker.trigger('map:ui:mouseover:'+$(e.currentTarget).val(), $(e.currentTarget).prop('checked'));
+            var itemInCollection = this.collection.get($(e.currentTarget).val());
+            if (itemInCollection) {
+                itemInCollection.is_selected = $(e.currentTarget).prop('checked');
+                //this.broker.trigger('map:ui:mouseover:'+$(e.currentTarget).val(), $(e.currentTarget).prop('checked'));
+            }
+
             //this.render();
         },
 
