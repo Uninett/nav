c9777dd28784b4091c0655f260280fc349d3dcfb
Roy Sindre Norangshol <norangshol@uninett.no>
2012-11-28 15:40:34 +0100

    (netmap) clearIsSelectedStatus on check_radio collection.

--- media/js/src/netmap/collections/checkradio.js
+++ media/js/src/netmap/collections/checkradio.js
@@ -5,6 +5,11 @@ define([
     var GeneralCheckRadioCollection = Backbone.Collection.extend({
         model: Model,
         initialize: function () {
+        },
+        clearIsSelectedStatus: function () {
+            this.each(function (element) {
+                element.set({is_selected: false}, {silent: true});
+            });
         }
     });
 
