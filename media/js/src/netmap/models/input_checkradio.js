934e5f1078ed6a9ffed6999de81009e2f07cdd5d
Roy Sindre Norangshol <norangshol@uninett.no>
2012-11-28 15:39:57 +0100

    (netmap) adds support for setting a 'value' for use in checkbox/radiobutton templating
    it also makes sure to change the idAttribute to the value if value is present.

--- media/js/src/netmap/models/input_checkradio.js
+++ media/js/src/netmap/models/input_checkradio.js
@@ -1,15 +1,24 @@
 define([
-    'libs/backbone'
+    'libs/backbone',
+    'libs/underscore'
 ], function () {
     var CheckRadioModel = Backbone.Model.extend({
         idAttribute: "name",
         defaults: {
             name: null,
+            value: null,
             is_selected: false
         },
-        initialize: function () {
+        initialize: function (attributes) {
+            // Sets the idAttribute to "value" if there a value present
+            // in the model. Normally defaults to "name"
+            if (attributes.value) {
+                this.idAttribute = "value";
+                this.id = this.get("value");
+            }
         }
     });
+
     return CheckRadioModel;
 
 });
