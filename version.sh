8988e4184caee26ab8cf2e049771baa63b5391cd
Morten Brekkevold <morten.brekkevold@uninett.no>
2012-11-30 14:09:06 +0100

    Don't make it sound like an error when the version number could not be updated

--- version.sh
+++ version.sh
@@ -2,6 +2,7 @@
 # Update the package version number
 HG=`which hg`
 VERSION=$1
+M4FILE=version.m4
 
 inhgrepo() {
   test -e .hg && test -x "$HG"
@@ -10,14 +11,26 @@ inhgrepo() {
 do_describe() {
   PYTHONPATH=tools ${HG} --config extensions.hgdescribe=tools/hgdescribe describe --single-tag --limit 9999
 }
+
+get_version() {
+  if [ -f ${M4FILE} ]; then
+    m4 -P ${M4FILE} - <<EOF
+VERSION_NUMBER
+EOF
+  else
+    echo unknown
+  fi
+}
+
 if test -z "$VERSION" && inhgrepo; then
   VERSION=`do_describe`
 fi
 
 if test -n "$VERSION"; then
-  echo "m4_define([VERSION_NUMBER], [${VERSION}])" > version.m4
+  echo "m4_define(VERSION_NUMBER, ${VERSION})" > ${M4FILE}
   echo "Updated version number to ${VERSION}"
-else 
-  echo "Cannot update version number."
+else
+  VERSION=$(get_version | tr -d "\n")
+  echo "Keeping version number: ${VERSION}"
   exit 1
 fi
