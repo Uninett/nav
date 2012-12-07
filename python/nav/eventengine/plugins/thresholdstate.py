6f7ab66d41c3b5670aca447d0c126f785e2777df
John-Magne Bredal <john.m.bredal@uninett.no>
2012-12-07 13:16:49 +0100

    (EventEngine.thresholdState): Added plugin for thresholdState

---
+++ python/nav/eventengine/plugins/thresholdstate.py
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2012 UNINETT AS
+#
+# This file is part of Network Administration Visualized (NAV).
+#
+# NAV is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.  You should have received a copy of the GNU General Public
+# License along with NAV. If not, see <http://www.gnu.org/licenses/>.
+#
+"""thresholdstate handler plugin"""
+
+from nav.eventengine import EventHandler
+from nav.eventengine.alerts import AlertGenerator
+
+
+class ThresholdStateHandler(EventHandler):
+    """Accepts thresholdState events"""
+
+    handled_types = ('thresholdState', )
+
+    def handle(self):
+        event = self.event
+
+        if event.state == event.STATE_STATELESS:
+            self._logger.info('Ignoring stateless thresholdState event')
+        else:
+            alert = AlertGenerator(event)
+            alert.alert_type = (
+                'exceededThreshold' if event.state == event.STATE_START
+                else 'belowThreshold')
+            alert.post()
+
+        event.delete()
