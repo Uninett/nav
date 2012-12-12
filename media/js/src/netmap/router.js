11aba5644c7ea61cf93f8ff6f7d575368af58ae5
Roy Sindre Norangshol <norangshol@uninett.no>
2012-12-12 10:31:32 +0100

    (netmap) use navigate instead of goTo :-)

--- media/js/src/netmap/router.js
+++ media/js/src/netmap/router.js
@@ -117,11 +117,11 @@ define([
             // for every page request not containing a map id
 
             if (context_user_default_view) {
-                Backbone.View.goTo("netmap/{0}".format(context_user_default_view.attributes.viewid));
+                Backbone.View.navigate("netmap/{0}".format(context_user_default_view.attributes.viewid));
             } else {
                 new DefaultMapModel().fetch({
                     success: function (model, attributes) {
-                        Backbone.View.goTo("netmap/{0}".format(attributes.viewid));
+                        Backbone.View.navigate("netmap/{0}".format(attributes.viewid));
                     },
                     error: function () {
                         // global not found, just do a graph
