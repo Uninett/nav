98b718ed46cfaa946b9a27a7c4dd80355a011523
John-Magne Bredal <john.m.bredal@uninett.no>
2013-01-29 12:55:16 +0100

    (info): Show the first 20 rows of any resulttable.

--- media/js/src/info.js
+++ media/js/src/info.js
@@ -2,7 +2,7 @@ require(['src/plugins/table_utils', 'libs/jquery'], function (TableUtil) {
 
     $(function () {
         $('.searchprovider').each(function (index, table) {
-            new TableUtil(table, 30).addRowToggleTrigger();
+            new TableUtil(table, 20).addRowToggleTrigger();
         })
     })
 
