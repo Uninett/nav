f4edc0eb3becc4d014ee96d71cbbfb42b261da55
Morten Brekkevold <morten.brekkevold@uninett.no>
2013-02-26 11:17:46 +0100

    The dummy checker does nothing, so it will in fact work on an IPv6 host :)

--- python/nav/statemon/checker/DummyChecker.py
+++ python/nav/statemon/checker/DummyChecker.py
@@ -21,8 +21,9 @@ from nav.statemon.event import Event
 
 class DummyChecker(AbstractChecker):
     TYPENAME = "dummy"
+    IPV6_SUPPORT = True
 
     def execute(self):
         import random
-        sleep(random.random()*10)
+        sleep(random.random() * 10)
         return Event.UP, 'OK'
