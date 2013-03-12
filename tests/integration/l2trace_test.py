0326bb5f4a4f01d1037e3b2872f555c156d40def
Morten Brekkevold <morten.brekkevold@uninett.no>
2013-03-12 15:54:27 +0100

    (ModPythonTestCase): remove obsolete class.

--- tests/integration/l2trace_test.py
+++ tests/integration/l2trace_test.py
@@ -307,26 +307,3 @@ class JunctionTests(L2TraceTestCase):
         self.assertEquals(new_path[0], self.from_path[0])
         self.assertEquals(new_path[-1], self.to_path[-1])
 
-
-from nav.tests.cases import ModPythonTestCase
-from django.test.client import Client
-
-class L2TraceWebTest(L2TraceTestCase, ModPythonTestCase):
-    module_under_test = l2trace
-
-    def test_l2trace_without_args_shoule_be_ok(self):
-        client = Client()
-        response = client.get('/l2trace/')
-        self.assertEquals(response.status_code, 200)
-
-    def test_l2trace_with_args_should_be_ok(self):
-        client = Client()
-        response = client.get('/l2trace/', {'host_from': '10.0.20.10',
-                                            'host_to': '10.0.20.90'})
-        self.assertEquals(response.status_code, 200)
-
-    def test_l2trace_with_unknown_ip_args_should_be_ok(self):
-        client = Client()
-        response = client.get('/l2trace/', {'host_from': '192.168.1.1',
-                                            'host_to': '192.168.10.12'})
-        self.assertEquals(response.status_code, 200)
