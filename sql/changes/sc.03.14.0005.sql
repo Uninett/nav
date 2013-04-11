c266f7965195104e6f2e25c84f21cde2dbd11a45
John-Magne Bredal <john.m.bredal@uninett.no>
2013-04-11 11:02:11 +0200

    (sql): Add table for images

---
+++ sql/changes/sc.03.14.0005.sql
@@ -0,0 +1,11 @@
+-- Create table for images
+
+CREATE TABLE image (
+  imageid SERIAL PRIMARY KEY,
+  roomid VARCHAR REFERENCES room(roomid) NOT NULL,
+  title VARCHAR NOT NULL,
+  path VARCHAR NOT NULL,
+  name VARCHAR NOT NULL,
+  created TIMESTAMP NOT NULL,
+  uploader VARCHAR REFERENCES account(id)
+);
