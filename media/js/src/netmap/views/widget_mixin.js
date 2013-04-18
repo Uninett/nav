a48b583e204d6eba7f96acc50bdf20b78216a8eb
Roy Sindre Norangshol <norangshol@uninett.no>
2013-04-18 16:29:51 +0200

    (netmap) remove getter method for isWidgetVisible and return true after events.

--- media/js/src/netmap/views/widget_mixin.js
+++ media/js/src/netmap/views/widget_mixin.js
@@ -1,6 +1,4 @@
 define([
-    'libs-amd/text!netmap/templates/widget_container.html',
-    'libs/handlebars',
     'libs/jquery',
     'libs/underscore',
     'libs/backbone',
@@ -11,9 +9,7 @@ define([
         toggleWidget: function () {
             this.$el.find(".body").toggle();
             this.isWidgetVisible = !this.isWidgetVisible;
-        },
-        isWidgetVisible: function () {
-            return this.isWidgetVisible;
+            return true;
         },
         setIsViewEnabled: function (boolValue) {
             this.isViewEnabled = boolValue;
