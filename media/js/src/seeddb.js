0398a0bce4de543720a16354bba0843ca0ff3fe1
John-Magne Bredal <john.m.bredal@uninett.no>
2013-04-24 13:19:42 +0200

    (SeedDB): Start making tables managable

--- media/js/src/seeddb.js
+++ media/js/src/seeddb.js
@@ -1,6 +1,20 @@
-require(['plugins/checkbox_selector', 'plugins/quickselect', 'libs/jquery'], function(CheckboxSelector, QuickSelect){
-    $(function() {
-        new CheckboxSelector('#select', '.selector').add();
-        new QuickSelect('.quickselect');
+require([
+    'plugins/checkbox_selector', 'plugins/quickselect', 'libs/jquery', 'libs/jquery.dataTables.min', 'libs/FixedColumns.min'
+], function (CheckboxSelector, QuickSelect) {
+        $(function () {
+            new CheckboxSelector('#select', '.selector').add();
+            new QuickSelect('.quickselect');
+
+            var table = $('#seeddb-content').dataTable({
+                "bPaginate": true,
+                "bLengthChange": true,
+                "bFilter": false,
+                "bSort": true,
+                "bInfo": true,
+                "bAutoWidth": true,
+                "sDom": '<"top-left-side"i><"top-right-side"pl>t<"F">',
+                "sScrollX": '100%'
+            });
+            new FixedColumns(table);
+        });
     });
-});
