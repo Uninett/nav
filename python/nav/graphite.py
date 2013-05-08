366397c1d1a3cd4dc39af598014932331183b29e
Morten Brekkevold <morten.brekkevold@uninett.no>
2013-05-08 13:16:57 +0200

    optimize horribly inefficient packet generator (we're talking about many orders of magnitude here!)

--- python/nav/graphite.py
+++ python/nav/graphite.py
@@ -17,13 +17,16 @@
 This module implements various common API to send metrics to a
 Graphite/Carbon backend. It currently only supports the UDP line protocol,
 as it's the easiest to implement, and will also work without vodoo in
-asynchronouse programs (i .e. such as ipdevpoll, which is implented using
+asynchronous programs (i .e. such as ipdevpoll, which is implented using
 Twisted).
 
 """
 import socket
 
-# with a 1500 byte MTU running on IPv4 this would be 1472, lets round it down
+# Maximum payload to allow for a UDP packet containing metrics destined for
+# Graphite. A value of 1472 should be ok to stay within the standard ethernet
+# MTU of 1500 bytes using IPv4. Larger values will cause packet
+# fragmentation, but should still work.
 MAX_UDP_PAYLOAD = 1400
 
 
@@ -37,8 +40,13 @@ def send_metrics_to(metric_tuples, host, port=2003):
     :param port: The carbon backend UDP port
 
     """
-    carbon = socket.socket(_socktype_from_addr(host), socket.SOCK_DGRAM)
-    carbon.connect((host, port))
+    global carbon
+    try:
+        carbon
+    except NameError:
+        carbon = socket.socket(_socktype_from_addr(host), socket.SOCK_DGRAM)
+        carbon.connect((host, port))
+
     for packet in metrics_to_packets(metric_tuples):
         carbon.send(packet)
 
@@ -56,29 +64,31 @@ def metrics_to_packets(metric_tuples):
 
     :param metric_tuples: A list of metric tuples in the form
                           [(path, (timestamp, value)), ...]
-    :return: A series of payload packets to send to a Carbon backend.
+
+    :return: A generator that yields a series of payload packets to send to a
+             Carbon backend.
 
     """
     assert len(metric_tuples) > 0
-    remainder = metric_tuples
-    while remainder:
-        packet, remainder = _consume_metrics_to_packet(remainder)
-        if packet:
+
+    output = []
+    size = 0
+    for metric in metric_tuples:
+        line = _metric_to_line(metric)
+        if size + len(line) > MAX_UDP_PAYLOAD:
+            packet = "".join(output)
             yield packet
+            output = []
+            size = 0
+
+        output.append(line)
+        size += len(line)
+
+    if output:
+        packet = "".join(output)
+        yield packet
 
 
-def _consume_metrics_to_packet(metric_tuples):
-    remainder = []
-    metrics = list(metric_tuples)
-    packet = _metrics_to_packet(metrics)
-    while len(packet) > MAX_UDP_PAYLOAD:
-        remainder.insert(0, metrics.pop())
-        packet = _metrics_to_packet(metrics)
-    return packet, remainder
-
-
-def _metrics_to_packet(metric_tuples):
-    payload = ["%s %s %s" % (path, value, timestamp)
-               for path, (timestamp, value) in metric_tuples]
-    return "\n".join(payload)
-
+def _metric_to_line(metric_tuple):
+    path, (timestamp, value) = metric_tuple
+    return str("%s %s %s\n" % (path, value, timestamp))
