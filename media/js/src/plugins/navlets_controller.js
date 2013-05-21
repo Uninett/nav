42fd170cccfc2aa4b2801879b157cb20a385d595
John-Magne Bredal <john.m.bredal@uninett.no>
2013-05-21 13:30:30 +0200

    (navlets): Experiment with two columns

--- media/js/src/plugins/navlets_controller.js
+++ media/js/src/plugins/navlets_controller.js
@@ -3,6 +3,8 @@ define(['plugins/navlet_controller', 'libs/jquery'], function (NavletController)
 
     function NavletsController(node) {
         this.container = node;
+        this.createLayout();
+
         this.fetch_navlets_url = this.container.attr('data-list-navlets');
         this.save_ordering_url = this.container.attr('data-save-order-url');
 
@@ -10,17 +12,27 @@ define(['plugins/navlet_controller', 'libs/jquery'], function (NavletController)
         this.saveOrderingButton = $('#navlet-ordering-save');
 
         this.navletSelector = '.navlet';
+        this.sorterSelector = '.navletColumn';
 
         this.fetchNavlets();
     }
 
     NavletsController.prototype = {
+        createLayout: function () {
+            var $row = $('<div class="row"/>').appendTo(this.container);
+            this.column1 = $('<div class="large-6 column navletColumn"/>').appendTo($row);
+            this.column2 = $('<div class="large-6 column navletColumn"/>').appendTo($row);
+        },
         fetchNavlets: function () {
             var that = this;
             $.getJSON(this.fetch_navlets_url, function (data) {
                 var navlets = data, i, l;
                 for (i = 0, l = data.length; i < l; i++) {
-                    var controller = new NavletController(that.container, data[i]);
+                    if (i % 2 === 0) {
+                        new NavletController(that.column1, data[i]);
+                    } else {
+                        new NavletController(that.column2, data[i]);
+                    }
                 }
                 that.addNavletOrdering();
             });
@@ -28,8 +40,9 @@ define(['plugins/navlet_controller', 'libs/jquery'], function (NavletController)
         addNavletOrdering: function () {
             var that = this;
 
-            this.container.sortable({
-                'disabled': true
+            this.container.find(this.sorterSelector).sortable({
+                disabled: true,
+                connectWith: '.navletColumn'
             });
 
             this.activateOrderingButton.click(function () {
@@ -42,7 +55,7 @@ define(['plugins/navlet_controller', 'libs/jquery'], function (NavletController)
 
         },
         activateOrdering: function () {
-            this.container.sortable('option', 'disabled', false);
+            this.container.find(this.sorterSelector).sortable('option', 'disabled', false);
             this.getNavlets().addClass('outline');
             this.activateOrderingButton.hide();
             this.saveOrderingButton.show();
