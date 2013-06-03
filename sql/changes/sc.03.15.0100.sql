777371fd3bb9d239c8ff766b41ff33ef1413038c
John-Magne Bredal <john.m.bredal@uninett.no>
2013-06-03 11:56:14 +0200

    SQL: Add 9.1 compliant statement

--- sql/changes/sc.03.15.0100.sql
+++ sql/changes/sc.03.15.0100.sql
@@ -1,6 +1,6 @@
 -- Create table for storing navlet information for a user
 
-CREATE TABLE profiles.account_navlet (
+CREATE TABLE IF NOT EXISTS profiles.account_navlet (
   id SERIAL PRIMARY KEY,
   navlet VARCHAR NOT NULL,
   account INT REFERENCES profiles.account(id),
