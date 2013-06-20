a6a8392ef357ec6ab5f0b550ede77d9fdc89a3c0
Roy Sindre Norangshol <norangshol@uninett.no>
2013-06-20 16:18:44 +0200

    (netmap) Remove out of date after the SQL migrations done from sc.03.14.0050.sql

--- media/js/src/netmap/models/layer_toggler.js
+++ media/js/src/netmap/models/layer_toggler.js
@@ -3,7 +3,6 @@ define([
 ], function () {
     var Layermodel = Backbone.Model.extend({
         defaults: {
-            // see nav.models.profiles TOPOLOGY_TYPES , 1 == layer 2 topology
             layer: 2,
             layer2_active: true
         },
