a59616322691a679f4b9e705100a78a382b6f625
Eivind Lysne <eivindlysne@gmail.com>
2013-06-24 10:43:51 +0200

    Made sortable column headers look clickable, added filter links, added hint for slack.

--- media/js/src/radius.js
+++ media/js/src/radius.js
@@ -2,6 +2,7 @@ require(['libs/jquery', 'libs/jquery.dataTables.min'], function () {
     $(document).ready(function () {
 
         var time_field = $('#id_time_0');
+        var slack_help = $('a.tooltip');
 
         if ($('#id_time_1 :selected').val() === '') {
             time_field.attr('disabled', 'disabled');
@@ -12,23 +13,43 @@ require(['libs/jquery', 'libs/jquery.dataTables.min'], function () {
 
             if (selected.val() === '') {
                 time_field.val('');
+                slack_help.hide();
                 time_field.attr('disabled', 'disabled');
             } else {
                 time_field.removeAttr('disabled');
                 if (selected.val() === 'timestamp') {
                     time_field.val('YYYY-MM-DD hh:mm|slack');
+                    slack_help.show();
                 } else {
                     time_field.val('');
+                    slack_help.hide();
                 }
             }
         });
 
         if ($('#resulttable').length) {
-            $('#resulttable').dataTable({
+
+            // Add classes
+            $.fn.dataTableExt.oStdClasses.sStripeOdd = 'oddrow';
+            $.fn.dataTableExt.oStdClasses.sStripeEven = 'evenrow';
+
+            // Iniialize
+            var resulttable = $('#resulttable').dataTable({
                 'iDisplayLength': 50,
-                'sStripeOdd': 'oddrow',
-                'sStripeEven': 'evenrow'
+                'sDom': '<"wrapper"lipftp>'
             });
+
+            // Hack to change the label of the filter box
+            var filter_label = $('#resulttable_filter label');
+            filter_label.contents().filter(function() {
+                return this.nodeType === Node.TEXT_NODE;
+            }).remove();
+            filter_label.prepend('Filter results:');
+
+            // Register filter function
+            $.fn.add_filter = function(text) {
+                resulttable.fnFilter(text);
+            };
         }
     });
 });
