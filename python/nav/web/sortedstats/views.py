6003d7c0a1ecffa0e09d9e57c1c315f9dafd2d69
John-Magne Bredal <john.m.bredal@uninett.no>
2013-06-25 14:26:43 +0200

    (sortedstats): Introduce more human readable metric names

--- python/nav/web/sortedstats/views.py
+++ python/nav/web/sortedstats/views.py
@@ -46,6 +46,7 @@ DEFAULT_VALUES = {
 VIEWS = {
     'cpu_routers_highestmax': {
         'title': 'CPU Highest Average',
+        'metric_name': [2],
         'data_filter': 'highestAverage({serieslist}, {rows})',
         'graph_filter': 'substr(group({serieslist}),2,3)',
         'serieslist': 'nav.devices.*.cpu.*.loadavg5min',
@@ -56,6 +57,7 @@ VIEWS = {
     },
     'uptime': {
         'title': 'Highest uptime',
+        'metric_name': [2],
         'data_filter': 'highestMax({serieslist}, {rows})',
         'graph_filter': 'substr(scale(group({serieslist}), {scale}), 2,3)',
         'serieslist': 'nav.devices.*.system.sysuptime',
@@ -66,61 +68,6 @@ VIEWS = {
         }
     }
 }
-
-
-def fetch_raw_data(viewname, rows, timeframe='-1d'):
-    """Fetch raw data from graphite
-
-    :param viewname: which of the VIEWS we are fetching data for
-    :param rows: number of rows used in filter functions
-    :param timeframe: the from-attribute used in the query
-    """
-    view = VIEWS[viewname]
-    view['rows'] = rows
-    view['from'] = timeframe
-
-    args = dict_merge(DEFAULT_VALUES, view)
-    args['target'] = view['data_filter'].format(
-        serieslist=view['serieslist'], rows=view['rows'])
-    result = get_metric_average(args['target'], args['from'], args['until'])
-    if 'scale' in view:
-        result = upscale(result, float(view['scale']))
-    return result
-
-
-def upscale(result, scale):
-    """Upscale the values of the result dictionary with scale
-
-    :param result:
-    :param scale:
-    """
-    return dict((x[0], x[1] * scale) for x in result.items())
-
-
-def create_graph_url(viewname, raw_data, timeframe='-1d'):
-    """Create url for getting a graph from Graphite
-
-    :param viewname: The VIEW we are graphing
-    :param raw_data: Data from former query
-    :param timeframe: the from attribute used in the query
-    """
-
-    view = VIEWS[viewname]
-    view_args = view['graph_args']
-    view_args['from'] = timeframe
-
-    kwargs = {'serieslist': ",".join(raw_data.keys())}
-    if 'scale' in view:
-        kwargs['scale'] = view['scale']
-
-    view_args['target'] = view['graph_filter'].format(**kwargs)
-    graph_args = dict_merge(DEFAULT_VALUES, view_args)
-    return reverse("graphite-render") + "?" + urlencode(graph_args)
-
-
-def dict_merge(a_dict, b_dict):
-    """Merge two dictionaries. b will overwrite a"""
-    return dict(a_dict.items() + b_dict.items())
 
 
 def index(request):
@@ -142,13 +89,82 @@ def index(request):
         view = request.GET['view']
         data = fetch_raw_data(view, numrows, fromtime)
         graph_url = create_graph_url(view, data, fromtime)
+        data = sort_by_value(reformat(data, view))
 
         context.update({
             'view': view,
             'view_timeframe': dict(TIMEFRAMES)[fromtime],
-            'data': sorted(data.items(), key=itemgetter(1), reverse=True),
+            'data': data,
             'graph_url': graph_url
         })
 
     return render_to_response('sortedstats/sortedstats.html', context,
                               context_instance=RequestContext(request))
+
+
+def fetch_raw_data(viewname, rows, timeframe='-1d'):
+    """Fetch raw data from graphite
+
+    :param viewname: which of the VIEWS we are fetching data for
+    :param rows: number of rows used in filter functions
+    :param timeframe: the from-attribute used in the query
+    """
+    view = VIEWS[viewname]
+    view['rows'] = rows
+    view['from'] = timeframe
+
+    args = dict_merge(DEFAULT_VALUES, view)
+    args['target'] = view['data_filter'].format(**view)
+    result = get_metric_average(args['target'], args['from'], args['until'])
+    if 'scale' in view:
+        result = upscale(result, float(view['scale']))
+    return result
+
+
+def upscale(result, scale):
+    """Upscale the values of the result dictionary with scale"""
+    return dict((x[0], x[1] * scale) for x in result.items())
+
+
+def create_graph_url(viewname, raw_data, timeframe='-1d'):
+    """Create url for getting a graph from Graphite
+
+    :param viewname: The VIEW we are graphing
+    :param raw_data: Data from former query
+    :param timeframe: the from attribute used in the query
+    """
+    view = VIEWS[viewname]
+    graph_args = dict_merge(DEFAULT_VALUES, view['graph_args'])
+    graph_args['from'] = timeframe
+
+    kwargs = {'serieslist': get_series_list(raw_data)}
+    if 'scale' in view:
+        kwargs['scale'] = view['scale']
+
+    graph_args['target'] = view['graph_filter'].format(**kwargs)
+    return reverse("graphite-render") + "?" + urlencode(graph_args)
+
+
+def get_series_list(raw_data):
+    return ",".join([x[0] for x in sort_by_value(raw_data)])
+
+
+def sort_by_value(data):
+    return sorted(data.items(), key=itemgetter(1), reverse=True)
+
+
+def dict_merge(a_dict, b_dict):
+    """Merge two dictionaries. b will overwrite a"""
+    return dict(a_dict.items() + b_dict.items())
+
+
+def reformat(data, view):
+    """Sort and reformat keys according to view"""
+    indexes = VIEWS[view]['metric_name']
+    new_data = {}
+    for key in data.keys():
+        metric_names = key.split('.')
+        metric_name = " - ".join([metric_names[index] for index in indexes])
+        new_data[metric_name] = data[key]
+
+    return new_data
