612cd8d11b933de792b7ea9e1bfafe74733e5af7
John-Magne Bredal <john.m.bredal@uninett.no>
2013-06-26 12:26:23 +0200

    (Sortedstats): Rewrite to use classes instead of dictionaries as it was messy

--- python/nav/web/sortedstats/views.py
+++ python/nav/web/sortedstats/views.py
@@ -24,7 +24,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 
-from nav.metrics.data import get_metric_average
+from nav.metrics.data import get_metric_average, get_metric_max
 
 _logger = logging.getLogger(__name__)
 
@@ -35,70 +35,164 @@ TIMEFRAMES = (
     ('-1month', 'Last Month'),
 )
 
-DEFAULT_VALUES = {
-    'width': 1046,
-    'height': 448,
-    'from': '-1d',
-    'until': 'now',
-    'template': 'nav'
-}
+# Useful constants
+BYTES_TO_MBIT = 8.0 / (1024 * 1024)
+TIMETICKS_IN_DAY = 100 * 3600 * 24
 
-VIEWS = {
-    'cpu_routers_highestmax': {
-        'title': 'CPU Highest Average',
-        'metric_name': [2],
-        'data_filter': 'highestAverage({serieslist}, {rows})',
-        'graph_filter': 'substr(group({serieslist}),2,3)',
-        'serieslist': 'nav.devices.*.cpu.*.loadavg5min',
-        'graph_args': {
-            'title': 'Routers with highest average cpu load',
-            'vtitle': 'Percent'
-        }
-    },
-    'uptime': {
-        'title': 'Highest uptime',
-        'metric_name': [2],
-        'data_filter': 'highestMax({serieslist}, {rows})',
-        'graph_filter': 'substr(scale(group({serieslist}), {scale}), 2,3)',
-        'serieslist': 'nav.devices.*.system.sysuptime',
-        'scale': '0.000000116',
-        'graph_args': {
-            'title': 'Network devices with highest uptime',
-            'vtitle': 'Days'
-        }
-    },
-    'ifinoctets': {
-        'title': 'Most traffic in to interface',
-        'metric_name': [2, 4],
-        'data_filter': 'substr(highestAverage(scaleToSeconds(nonNegativeDerivative({serieslist}),1), {rows}), 0)',
-        'graph_filter': 'substr(scale(nonNegativeDerivative(scaleToSeconds(group({serieslist}),1)),{scale}),2,5)',
-        'serieslist': 'nav.devices.*.ports.*.ifInOctets',
-        'scale': '0.000007629',  # 8 / (1024 * 1024)
-        'graph_args': {
-            'title': 'Interfaces with most average traffic in',
-            'vtitle': 'Mbit'
-        }
-    },
-    'ifoutoctets': {
-        'title': 'Most traffic out of interface',
-        'metric_name': [2, 4],
-        'data_filter': 'substr(highestAverage(scaleToSeconds(nonNegativeDerivative({serieslist}),1), {rows}), 0)',
-        'graph_filter': 'substr(scale(nonNegativeDerivative(scaleToSeconds(group({serieslist}),1)),{scale}),2,5)',
-        'serieslist': 'nav.devices.*.ports.*.ifOutOctets',
-        'scale': '0.000007629',  # 8 / (1024 * 1024)
-        'graph_args': {
-            'title': 'Interfaces with most average traffic out',
-            'vtitle': 'Mbit'
-        }
+
+class Stat(object):
+    """Superclass for statistics generation"""
+    graph_filter = None
+    graph_args = None
+    default_graph_args = {
+        'width': 1046,
+        'height': 448,
+        'template': 'nav'
     }
-}
+    scale = None
+
+    def __init__(self, timeframe='-1d', rows=5):
+        self.timeframe = timeframe
+        self.rows = rows
+        self.data = self.get_sorted_data()
+        self.graph_url = self.get_graph_url()
+        self.display_data = self.get_display_data()
+
+    def get_sorted_data(self):
+        """Returns the sorted version of the data"""
+        data = self.get_data()
+        return self.sort_by_value(data)
+
+    def get_data(self):
+        """Gets the relevant data for this statistics"""
+        target = self.data_filter.format(serieslist=self.serieslist,
+                                         rows=self.rows)
+        return get_metric_average(target, start=self.timeframe)
+
+    def get_graph_url(self):
+        """Gets the graph url to display the statistics as a graph"""
+        graph_series = self.get_graph_series()
+
+        kwargs = {'serieslist': graph_series}
+        if self.scale is not None:
+            kwargs['scale'] = "%.20f" % self.scale
+        target = self.graph_filter.format(**kwargs)
+        args = self.dict_merge(self.graph_args, {'target': target})
+        return self.create_graph_url(args)
+
+    def get_display_data(self):
+        """Gets the human readable version of the raw data"""
+        display_data = []
+        for key, value in self.data:
+            display_data.append((self.get_metric_display_name(key), value))
+        return display_data
+
+    @staticmethod
+    def get_metric_display_name(metric):
+        """Return the display name for the metric"""
+        return metric.split('.')[2]
+
+    def create_graph_url(self, args):
+        """Create url for getting a graph from Graphite"""
+        graph_args = self.dict_merge(self.default_graph_args, args)
+        return reverse("graphite-render") + "?" + urlencode(graph_args)
+
+    def get_graph_series(self):
+        """Returns the graph series usable for graphites group function"""
+        return ",".join([x[0] for x in self.data])
+
+    def upscale(self, data):
+        """Upscale data with scale"""
+        upscaled = {}
+        for key, value in data.items():
+            upscaled[key] = value * self.scale
+        return upscaled
+
+    @staticmethod
+    def sort_by_value(data):
+        """Sort dictionary by value"""
+        return sorted(data.items(), key=itemgetter(1), reverse=True)
+
+    @staticmethod
+    def dict_merge(a_dict, b_dict):
+        """Merge two dictionaries. b will overwrite a"""
+        return dict(a_dict.items() + b_dict.items())
+
+
+class StatCpuAverage(Stat):
+    """Generates statistics for the CPU view"""
+    title = 'CPU Highest Average'
+    graph_args = {
+        'title': 'Routers with highest average cpu load',
+        'vtitle': 'Percent'
+    }
+    data_filter = 'highestAverage({serieslist}, {rows})'
+    graph_filter = 'substr(group({serieslist}),2,3)'
+    serieslist = 'nav.devices.*.cpu.*.loadavg5min'
+
+
+class StatUptime(Stat):
+    """Generates statistics for the Uptime view"""
+    title = "Highest uptime"
+    data_filter = 'highestMax({serieslist}, {rows})'
+    graph_filter = 'substr(scale(group({serieslist}), {scale}), 2,3)'
+    serieslist = 'nav.devices.*.system.sysuptime'
+    graph_args = {
+        'title': 'Network devices with highest uptime',
+        'vtitle': 'Days'
+    }
+    scale = 1.0 / TIMETICKS_IN_DAY
+
+    def get_data(self):
+        target = self.data_filter.format(serieslist=self.serieslist,
+                                         rows=self.rows)
+        data = get_metric_max(target, start=self.timeframe)
+        data = self.upscale(data)
+        return data
+
+
+class StatIfInOctets(Stat):
+    """Generates statistics for the Uptime view"""
+    title = 'Most traffic in to interface'
+    data_filter = 'substr(highestAverage(scaleToSeconds(' \
+                  'nonNegativeDerivative({serieslist}),1), {rows}), 0)'
+    graph_filter = 'substr(scale(nonNegativeDerivative(scaleToSeconds(' \
+                   'group({serieslist}),1)),{scale}),2,5)'
+    serieslist = 'nav.devices.*.ports.*.ifInOctets'
+    scale = BYTES_TO_MBIT
+    graph_args = {
+        'title': 'Interfaces with most average traffic in',
+        'vtitle': 'Mbit',
+        'yUnitSystem': 'binary'
+    }
+
+
+class StatIfOutOctets(Stat):
+    """Generates statistics for the Uptime view"""
+    title = 'Most traffic out of interface'
+    data_filter = 'substr(highestAverage(scaleToSeconds(' \
+                  'nonNegativeDerivative({serieslist}),1), {rows}), 0)'
+    graph_filter = 'substr(scale(nonNegativeDerivative(scaleToSeconds(' \
+                   'group({serieslist}),1)),{scale}),2,5)'
+    serieslist = 'nav.devices.*.ports.*.ifOutOctets'
+    scale = BYTES_TO_MBIT
+    graph_args = {
+        'title': 'Interfaces with most average traffic out',
+        'vtitle': 'Mbit',
+        'yUnitSystem': 'binary'
+    }
+
+CLASSMAP = {'cpu_routers_highestmax': StatCpuAverage,
+            'uptime': StatUptime,
+            'ifinoctets': StatIfInOctets,
+            'ifoutoctets': StatIfOutOctets}
 
 
 def index(request):
     """Sorted stats search & result view"""
     numrows = int(request.GET.get('numrows', 5))
     fromtime = request.GET.get('fromtime', '-1d')
-    sectionslist = [(x[0], x[1]['title']) for x in VIEWS.items()]
+    sectionslist = [(x[0], x[1].title) for x in CLASSMAP.items()]
 
     context = {
         'title': 'Statistics',
@@ -111,84 +205,14 @@ def index(request):
 
     if 'view' in request.GET:
         view = request.GET['view']
-        data = fetch_raw_data(view, numrows, fromtime)
-        graph_url = create_graph_url(view, data, fromtime)
-        data = sort_by_value(reformat(data, view))
+        cls = CLASSMAP[view]
+        result = cls(fromtime, numrows)
 
         context.update({
             'view': view,
             'view_timeframe': dict(TIMEFRAMES)[fromtime],
-            'data': data,
-            'graph_url': graph_url
+            'result': result
         })
 
     return render_to_response('sortedstats/sortedstats.html', context,
                               context_instance=RequestContext(request))
-
-
-def fetch_raw_data(viewname, rows, timeframe='-1d'):
-    """Fetch raw data from graphite
-
-    :param viewname: which of the VIEWS we are fetching data for
-    :param rows: number of rows used in filter functions
-    :param timeframe: the from-attribute used in the query
-    """
-    view = VIEWS[viewname]
-    view['rows'] = rows
-    view['from'] = timeframe
-
-    args = dict_merge(DEFAULT_VALUES, view)
-    args['target'] = view['data_filter'].format(**view)
-    result = get_metric_average(args['target'], args['from'], args['until'])
-    if 'scale' in view:
-        result = upscale(result, float(view['scale']))
-    return result
-
-
-def upscale(result, scale):
-    """Upscale the values of the result dictionary with scale"""
-    return dict((x[0], x[1] * scale) for x in result.items())
-
-
-def create_graph_url(viewname, raw_data, timeframe='-1d'):
-    """Create url for getting a graph from Graphite
-
-    :param viewname: The VIEW we are graphing
-    :param raw_data: Data from former query
-    :param timeframe: the from attribute used in the query
-    """
-    view = VIEWS[viewname]
-    graph_args = dict_merge(DEFAULT_VALUES, view['graph_args'])
-    graph_args['from'] = timeframe
-
-    kwargs = {'serieslist': get_series_list(raw_data)}
-    if 'scale' in view:
-        kwargs['scale'] = view['scale']
-
-    graph_args['target'] = view['graph_filter'].format(**kwargs)
-    return reverse("graphite-render") + "?" + urlencode(graph_args)
-
-
-def get_series_list(raw_data):
-    return ",".join([x[0] for x in sort_by_value(raw_data)])
-
-
-def sort_by_value(data):
-    return sorted(data.items(), key=itemgetter(1), reverse=True)
-
-
-def dict_merge(a_dict, b_dict):
-    """Merge two dictionaries. b will overwrite a"""
-    return dict(a_dict.items() + b_dict.items())
-
-
-def reformat(data, view):
-    """Sort and reformat keys according to view"""
-    indexes = VIEWS[view]['metric_name']
-    new_data = {}
-    for key in data.keys():
-        metric_names = key.split('.')
-        metric_name = " - ".join([metric_names[index] for index in indexes])
-        new_data[metric_name] = data[key]
-
-    return new_data
