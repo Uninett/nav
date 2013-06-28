67d94d34d91b02710e857703d8b3ffc8499f415d
Eivind Lysne <eivindlysne@gmail.com>
2013-06-28 11:45:25 +0200

    Added marker and fixed a bug reversing latitude and longitude. Divided the code into functions

--- media/js/src/seeddb.js
+++ media/js/src/seeddb.js
@@ -12,6 +12,12 @@ require([
         // Mapstuff
         if ($('#map').length) {
 
+            var map = initMap();
+            var marker_layer = addMarkerLayer(map);
+            var marker;
+            var displayProjection = map.displayProjection;
+            var inputProjection = map.getProjectionObject();
+
             // Get coordinates if room exists
             var center;
             var position_input = $('#id_position');
@@ -22,66 +28,50 @@ require([
             else {
                 center = parseLonLat(position.slice(1, -1));
             }
-
-            var map = new OpenLayers.Map('map', {
-                displayProjection: new OpenLayers.Projection("EPSG:4326"),
-                theme: '/style/openlayers.css'
-            });
-
-            map.addLayer(new OpenLayers.Layer.OSM(
-                'OpenStreetMap',
-                '/info/osm_map_redirect/${z}/${x}/${y}.png')
-            );
-
-            var displayProjection = map.displayProjection;
-            var inputProjection = map.getProjectionObject();
-
             center.transform(displayProjection, inputProjection);
             map.setCenter(center, 14);
+            marker = addMarker(center);
+            marker_layer.addMarker(marker);
 
-            map.addControl(new OpenLayers.Control.MousePosition());
-
-            OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
-                defaultHandlerOptions: {
-                    'single': true,
-                    'double': false,
-                    'pixelTolerance': 0,
-                    'stopSingle': false,
-                    'stopDouble': false
-                },
-
-                initialize: function(options) {
-                    this.handlerOptions = OpenLayers.Util.extend(
-                        {}, this.defaultHandlerOptions
-                    );
-                    OpenLayers.Control.prototype.initialize.apply(
-                        this, arguments
-                    );
-                    this.handler = new OpenLayers.Handler.Click(
-                        this, {
-                            'click': this.trigger
-                        }, this.handlerOptions
-                    );
-                },
-
-                trigger: function(e) {
-                    var lonlat = map.getLonLatFromPixel(e.xy).transform(
-                        inputProjection,
-                        displayProjection
-                    );
-                    position_input.val(lonLatToStr(lonlat));
-                }
-            });
-
-            var click = new OpenLayers.Control.Click();
-            map.addControl(click);
-            click.activate();
+            initGetLonLatOnClickControl(
+                map, marker ,inputProjection, displayProjection, position_input);
 
         }
     });
 
+    function initMap() {
+        OpenLayers.ImgPath = '/images/openlayers/';
+        var map = new OpenLayers.Map('map', {
+            displayProjection: new OpenLayers.Projection("EPSG:4326"),
+            controls: [
+                new OpenLayers.Control.PanZoomBar(),
+                new OpenLayers.Control.Navigation()
+            ],
+            theme: '/style/openlayers.css'
+        });
+        map.addLayer(new OpenLayers.Layer.OSM(
+            'OpenStreetMap',
+            '/info/osm_map_redirect/${z}/${x}/${y}.png')
+        );
+        return map;
+    }
+
+    function addMarkerLayer(map) {
+        var marker = new OpenLayers.Layer.Markers('Marker');
+        map.addLayer(marker);
+        return marker;
+    }
+
+    function addMarker(lonlat) {
+        var size = new OpenLayers.Size(21,25);
+        var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+        var icon = new OpenLayers.Icon(
+            'http://www.openlayers.org/dev/img/marker.png', size, offset);
+        return new OpenLayers.Marker(lonlat, icon);
+    }
+
     function lonLatToStr(lonlat) {
-        return [lonlat.lon, lonlat.lat].join();
+        return [lonlat.lat, lonlat.lon].join();
     }
 
     function parseLonLat(llStr) {
@@ -92,5 +82,47 @@ require([
             llStr + '"';
         }
         return new OpenLayers.LonLat(arr[2], arr[1]);
+    }
+
+    function initGetLonLatOnClickControl(
+        map, marker, inputProjection, displayProjection, position_input) {
+        map.addControl(new OpenLayers.Control.MousePosition());
+
+        OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
+            defaultHandlerOptions: {
+                'single': true,
+                'double': false,
+                'pixelTolerance': 0,
+                'stopSingle': false,
+                'stopDouble': false
+            },
+
+            initialize: function(options) {
+                this.handlerOptions = OpenLayers.Util.extend(
+                    {}, this.defaultHandlerOptions
+                );
+                OpenLayers.Control.prototype.initialize.apply(
+                    this, arguments
+                );
+                this.handler = new OpenLayers.Handler.Click(
+                    this, {
+                        'click': this.trigger
+                    }, this.handlerOptions
+                );
+            },
+
+            trigger: function(e) {
+                var lonlat = map.getLonLatFromPixel(e.xy);
+                marker.moveTo(map.getLayerPxFromLonLat(lonlat));
+                position_input.val(lonLatToStr(lonlat.transform(
+                    inputProjection,
+                    displayProjection
+                )));
+            }
+        });
+
+        var click = new OpenLayers.Control.Click();
+        map.addControl(click);
+        click.activate();
     }
 });
