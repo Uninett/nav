2f187819c24e6ca9ad26316709cf530c563d0c71
Eivind Lysne <eivindlysne@gmail.com>
2013-07-09 14:49:23 +0200

    Refactored the radius.js and made filterable table-cells look like links

--- media/js/src/radius.js
+++ media/js/src/radius.js
@@ -1,9 +1,10 @@
 require(['libs/jquery', 'libs/jquery.dataTables.min'], function () {
-    $(document).ready(function () {
 
+    function initTimeField() {
         var time_field = $('#id_time_0');
         var slack_help = $('a.tooltip');
 
+        // Disable input if all time is pre-selected
         if ($('#id_time_1 :selected').val() === '') {
             time_field.attr('disabled', 'disabled');
         }
@@ -26,40 +27,64 @@ require(['libs/jquery', 'libs/jquery.dataTables.min'], function () {
                 }
             }
         });
+    }
+    function addTableCellListener(resulttable, datatable) {
+        resulttable.on('click', 'td', function() {
+            var filter = $(this).data('filter');
+            if (filter) {
+                datatable.fnFilter(filter);
+            }
+        });
+    }
+    function addFilterInputListener(resulttable, datatable) {
+        var filter_input = $('#resulttable_filter input');
+        filter_input.off();
+        filter_input.keypress(function(e) {
+            // i.e. enter was pressed
+            if (e.keyCode === 13) {
+                datatable.fnFilter(filter_input.val());
+            }
+        });
+    }
+    function initResulttable(resulttable) {
+        // Add classes
+        $.fn.dataTableExt.oStdClasses.sStripeOdd = 'oddrow';
+        $.fn.dataTableExt.oStdClasses.sStripeEven = 'evenrow';
+        $.fn.dataTableExt.oStdClasses.sSortAsc = 'headerSortDown';
+        $.fn.dataTableExt.oStdClasses.sSortDesc = 'headerSortUp';
+
+
+        var nosort = resulttable.children('thead').data('nosort');
+
+        // Iniialize
+        var datatable = resulttable.dataTable({
+            'iDisplayLength': 50,
+            'bPaginate': false,
+            "aaSorting": [],
+            'aoColumnDefs': [
+                {'bSortable': false, 'aTargets': nosort}
+            ],
+            'sDom': '<"caption"<"info"i><"filter"f>>t',
+            'oLanguage': {'sSearch': 'Filter results:'}
+        });
+
+        // Set caption width to the table width
+        $('.caption').css('width', function() {
+           return resulttable.css('width');
+        });
+
+        // Add listeners
+        addTableCellListener(resulttable, datatable);
+        addFilterInputListener(resulttable, datatable);
+    }
+
+    $(document).ready(function () {
+
+        initTimeField();
 
         var resulttable = $('#resulttable');
         if (resulttable.length) {
-
-            // Add classes
-            $.fn.dataTableExt.oStdClasses.sStripeOdd = 'oddrow';
-            $.fn.dataTableExt.oStdClasses.sStripeEven = 'evenrow';
-            $.fn.dataTableExt.oStdClasses.sSortAsc = 'headerSortDown';
-            $.fn.dataTableExt.oStdClasses.sSortDesc = 'headerSortUp';
-
-
-            var nosort = resulttable.children('thead').data('nosort');
-
-            // Iniialize
-            var datatable = resulttable.dataTable({
-                'iDisplayLength': 50,
-                'bPaginate': false,
-                "aaSorting": [],
-                'aoColumnDefs': [
-                    {'bSortable': false, 'aTargets': nosort}
-                ],
-                'sDom': '<"caption"<"info"i><"filter"f>>t',
-                'oLanguage': {'sSearch': 'Filter results:'}
-            });
-
-            // Set caption width to the table width
-            $('.caption').css('width', function() {
-               return resulttable.css('width');
-            });
-
-            // Register filter function
-            $.fn.add_filter = function(text) {
-                datatable.fnFilter(text);
-            };
+            initResulttable(resulttable);
         }
     });
 });
