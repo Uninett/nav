717622028246697750c7675d1eefe57a1dff1593
Roy Sindre Norangshol <norangshol@uninett.no>
2013-07-29 20:02:06 +0200

    (ipdevpoll) Refactor RFC3021 tests into it's own test case and fix error in test.

--- tests/unittests/ipdevpoll/shadows_test.py
+++ tests/unittests/ipdevpoll/shadows_test.py
@@ -3,7 +3,8 @@ from __future__ import with_statement
 from unittest import TestCase
 from nav.ipdevpoll.storage import ContainerRepository
 from nav.ipdevpoll.shadows import Vlan, Prefix, Netbox
-from mock import patch, Mock
+from mock import patch, Mock, DEFAULT
+
 
 class MixedNetTypeTest(TestCase):
     """Tests for guesstimation of net_type on Vlan's with mixed prefixes"""
@@ -36,7 +37,11 @@ class MixedNetTypeTest(TestCase):
             self.assertTrue(net_type is not None)
             self.assertEqual("elink", net_type.id)
 
-    def _setup_rfc3021(self):
+
+class RFC3021NetTypeTest(TestCase):
+    """RFC3021 Tests for guesstimation of net_type on Vlan's"""
+
+    def setUp(self):
         repo = ContainerRepository()
         netbox = repo.factory(None, Netbox)
         netbox.id = 1
@@ -53,24 +58,15 @@ class MixedNetTypeTest(TestCase):
 
 
     def test_elink_net_with_subnet_31_rfc3021_router_count_as_1(self):
-        self._setup_rfc3021()
-        import nav
-        nav.ipdevpoll.shadows.Vlan._get_router_count_for_prefix = Mock(
-            return_value=1)
-        nav.ipdevpoll.shadows.Vlan._get_my_prefixes = Mock(
-            return_value=self.shadow_prefixes)
+        with patch.object(self.vlan, '_get_router_count_for_prefix') as rcount:
+            rcount.return_value = 1
 
-
-        net_type = self.vlan._guesstimate_net_type(self.repo)
-        self.assertEqual("elink", net_type.id)
+            net_type = self.vlan._guesstimate_net_type(self.repo)
+            self.assertEqual("elink", net_type.id)
 
     def test_link_net_with_subnet_31_rfc3021_router_count_as_2(self):
-        self._setup_rfc3021()
-        import nav
-        nav.ipdevpoll.shadows.Vlan._get_router_count_for_prefix = Mock(
-            return_value=2)
-        nav.ipdevpoll.shadows.Vlan._get_my_prefixes = Mock(
-            return_value=self.shadow_prefixes)
+        with patch.object(self.vlan, '_get_router_count_for_prefix') as rcount:
+            rcount.return_value = 2
 
-        net_type = self.vlan._guesstimate_net_type(self.repo)
-        self.assertEqual('link', net_type.id)
+            net_type = self.vlan._guesstimate_net_type(self.repo)
+            self.assertEqual('link', net_type.id)
