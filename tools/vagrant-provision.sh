5f00f86e8b661e751874fff8e1f72aec3ba92de6
Roy Sindre Norangshol <norangshol@uninett.no>
2013-08-02 19:18:22 +0200

    (vagrant-provision.sh) Updated provision script
    Now it fetches build-dep for python-imaging so it can build PIL
    Create database user and empty database for nav if not available.
    
    Do some SEDing to enable django debug and set a default password
    Set --prefix so we can run make in etc and python to generated
    files that requires configured to be run. (ie buildconf.py)
    
    At the end, run syncdb so we have all SQL changesets installed.

--- tools/vagrant-provision.sh
+++ tools/vagrant-provision.sh
@@ -4,11 +4,16 @@
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get -y update
-apt-get -y build-dep -y psycopg2 python-lxml librrd-dev
+apt-get -y build-dep -y psycopg2 python-lxml librrd-dev python-imaging
 apt-get -y install mercurial subversion python-virtualenv build-essential autoconf postgresql-client cricket libapache2-mod-wsgi postgresql-9.1 librrd-dev libsnmp15
 
-virtualenv --no-site-packages .env
-. .env/bin/activate
+su -c "createuser -d -R -S nav || true" postgres
+su -c "psql -U postgres -c \"ALTER USER nav WITH PASSWORD 'nav'\"" postgres
+su -c "createdb -O nav nav || true" postgres
+
+su -l vagrant
+[ ! -d .env ] && virtualenv --no-site-packages ~vagrant/.env
+. ~vagrant/.env/bin/activate
 
 if [ -f ~vagrant/modified_bash ]; then
   echo "~vagrant/.bashrc already modified, rm ~vagrant/modified_bash if need to reprovision it"
@@ -21,5 +26,18 @@ EOF
   touch ~vagrant/modified_bash
 fi
 
+export PYTHONPATH="/vagrant/python:$PYTHONPATH"
+
 pip install -vv -r /vagrant/tools/vagrant-requirements.txt
 pip install -v -r /vagrant/tools/vagrant-requirements2.txt # Silly modules who can't be installed in same process as rest of em..
+
+cd /vagrant
+sed -e 's/^#\s*\(DJANGO_DEBUG.*\)$/\1/' -i etc/nav.conf # Enable django debug. 
+sed -e 's/userpw_nav=.*/userpw_nav=nav/g' -i etc/db.conf # Set nav as password. 
+./autogen.sh
+./configure --prefix /vagrant
+cd /vagrant/python 
+make
+cd /vagrant/etc
+make
+/vagrant/sql/syncdb.py 
