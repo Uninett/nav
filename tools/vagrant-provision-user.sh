daa6f9df3482be2c0378cae8b3fb3b23aaf5ccea
Roy Sindre Norangshol <norangshol@uninett.no>
2013-08-05 21:03:19 +0200

    (vagrant-provision) Refactoring and making it \:D/
    vagrant-provision-postgres.sh:
      Script to be invoked and run under vagrant user
      in VM which initalizes the database with sql/syncdb.py
    vagrant-provision-user.sh:
      Script which installs NAV's dependencies from pypi in
      an virtualenv, ensures .bash_profile for vagrant user
      uses it and sets python path and sets proper configure
      variables and 'minimalistic' install so you can run
      NAV in virtualenv without having to 'make install' :-)
    vagrant-provision.sh:
      The king, main wrapper who fires of the two scripts
      mentioned above and kicks in system/os dependencies
      for compiling pypi modules and runs scripts in right
      order and right user etc. Vagrantfile boots this one
      on 'vagrant up'
    
    Tutorial for vagrant:
    
    vagrant up
    ... wait around 5 minutes as the first time installs quite
    a lot of dependencies!
    Run 'vagrant ssh' from NAV VCS root and your in the VM.
    django-admin runserver 0.0.0.0:8080 to kick the web up
    and then start the daemons as you fit under /vagrant/bin/nav
    (hint, this is usually a good idea! ;-))

---
+++ tools/vagrant-provision-user.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# Second step in vagrant provisioning
+# Should be run as vagrant user inside the vm.
+
+[ ! -d .env ] && virtualenv --no-site-packages ~vagrant/.env
+. ~vagrant/.env/bin/activate
+
+if [ -f ~vagrant/.bash_profile ]; then
+  echo "~vagrant/.bash_profile already modified, rm ~vagrant/.bash_profile if need to reprovision it"
+else
+  cat << EOF >> ~vagrant/.bash_profile
+export WEBROOT=/vagrant/media
+export DJANGO_SETTINGS_MODULE="nav.django.settings"
+export PYTHONPATH="/vagrant/python:$PYTHONPATH"
+source ~/.env/bin/activate
+EOF
+fi
+
+export PYTHONPATH="/vagrant/python:$PYTHONPATH"
+
+pip install -vv -r /vagrant/tools/vagrant-requirements.txt
+pip install -v -r /vagrant/tools/vagrant-requirements2.txt # Silly modules who can't be installed in same process as rest of em..
+
+cd /vagrant
+./autogen.sh
+./configure --prefix /vagrant --localstatedir ~vagrant/var --sysconfdir ~vagrant/etc WEBROOT=/vagrant/media
+cd /vagrant/python
+make
+
+if [ $1 != 0 ]; then
+  cd /vagrant/etc
+  make
+  make install
+  sed -e 's/^#\s*\(DJANGO_DEBUG.*\)$/\1/' -i ~vagrant/etc/nav.conf # Enable django debug.
+  sed -e 's/userpw_nav=.*/userpw_nav=nav/g' -i ~vagrant/etc/db.conf # Set nav as password.
+  cd /vagrant
+  make installdirs-local
+fi
--- tools/vagrant-provision.sh
+++ tools/vagrant-provision.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
+# Provision script for vagrant
+# Installs system dependent libraries and tools
+#
+# Invokes tools/vagrant-provision-user.sh as vagrant user
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -7,38 +10,11 @@ apt-get -y update
 apt-get -y build-dep -y psycopg2 python-lxml librrd-dev python-imaging
 apt-get -y install mercurial subversion python-virtualenv build-essential autoconf postgresql-client cricket libapache2-mod-wsgi postgresql-9.1 librrd-dev libsnmp15
 
-su -c "createuser -d -R -S nav || true" postgres
-su -c "psql -U postgres -c \"ALTER USER nav WITH PASSWORD 'nav'\"" postgres
-su -c "createdb -O nav nav || true" postgres
+IS_INSTALLED=$( (test -f ~vagrant/nav_installed); echo $?)
 
-su -l vagrant
-[ ! -d .env ] && virtualenv --no-site-packages ~vagrant/.env
-. ~vagrant/.env/bin/activate
+su -l -c "/usr/bin/env sh /vagrant/tools/vagrant-provision-user.sh $IS_INSTALLED" vagrant
 
-if [ -f ~vagrant/modified_bash ]; then
-  echo "~vagrant/.bashrc already modified, rm ~vagrant/modified_bash if need to reprovision it"
-else
-  cat << EOF >> ~vagrant/.bashrc
-export WEBROOT=/vagrant/media
-export DJANGO_SETTINGS_MODULE="nav.django.settings"
-export PYTHONPATH="/vagrant/python:$PYTHONPATH"
-source ~/.env/bin/activate
-EOF
-  touch ~vagrant/modified_bash
+if [ $IS_INSTALLED != 0 ]; then
+  su -l -c "sh /vagrant/tools/vagrant-provision-postgres.sh" postgres || exit 1
+  su -l -c "touch ~vagrant/nav_installed" vagrant || exit 2
 fi
-
-export PYTHONPATH="/vagrant/python:$PYTHONPATH"
-
-pip install -vv -r /vagrant/tools/vagrant-requirements.txt
-pip install -v -r /vagrant/tools/vagrant-requirements2.txt # Silly modules who can't be installed in same process as rest of em..
-
-cd /vagrant
-sed -e 's/^#\s*\(DJANGO_DEBUG.*\)$/\1/' -i etc/nav.conf # Enable django debug. 
-sed -e 's/userpw_nav=.*/userpw_nav=nav/g' -i etc/db.conf # Set nav as password. 
-./autogen.sh
-./configure --prefix /vagrant WEBROOT=/vagrant/media
-cd /vagrant/python 
-make
-cd /vagrant/etc
-make
-/vagrant/sql/syncdb.py 
