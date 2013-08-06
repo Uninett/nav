ba3e590ed724cb330b19a5fc11c7f1c1b5b5679e
Roy Sindre Norangshol <norangshol@uninett.no>
2013-08-06 22:18:24 +0200

    (javascript-test.sh) Use node_modules/.bin

--- tests/javascript-test.sh
+++ tests/javascript-test.sh
@@ -62,7 +62,7 @@ export DISPLAY=:${DISPLAYNUM}
 export PATH="${JSDIR}/node_modules/.bin:$PATH"
 echo "Running tests"
 cd ${JSDIR}
-${JSDIR}/node_modules/karma/bin/karma start karma.conf.buildserver.js
+${JSDIR}/node_modules/.bin/karma start karma.conf.buildserver.js
 
 if [ "$?" -eq 1 ]; then
     echo "Error when testing, taking screenshot"
