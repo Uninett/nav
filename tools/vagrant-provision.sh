ee274e6fccd866ad791590f966e0d19b96f48a53
Roy Sindre Norangshol <norangshol@uninett.no>
2013-08-06 20:46:02 +0200

    (vagrant provisioning) Added provisioning for node/npm to install javascript testing utilities.
    (vagrant-provision-node.bash) Clones nvm and uses nvm to install node which again fires off 'npm install --optional' in media/js
    (vagrant-provision.sh) Added git-core to dependency, and added node provisioning script and some line breaking in apt-get install line.

--- tools/vagrant-provision.sh
+++ tools/vagrant-provision.sh
@@ -8,10 +8,13 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get -y update
 apt-get -y build-dep -y psycopg2 python-lxml librrd-dev python-imaging python-ldap
-apt-get -y install mercurial subversion python-virtualenv build-essential autoconf postgresql-client cricket libapache2-mod-wsgi postgresql-9.1 librrd-dev libsnmp15
+apt-get -y install mercurial subversion python-virtualenv build-essential \
+ autoconf postgresql-client cricket libapache2-mod-wsgi postgresql-9.1 \
+ librrd-dev libsnmp15 git-core
 
 IS_INSTALLED=$( (test -f ~vagrant/nav_installed); echo $?)
 
+su -l -c "/usr/bin/env bash /vagrant/tools/vagrant-provision-node.bash $IS_INSTALLED" vagrant
 su -l -c "/usr/bin/env sh /vagrant/tools/vagrant-provision-user.sh $IS_INSTALLED" vagrant
 
 if [ $IS_INSTALLED != 0 ]; then
