c7f269d05d5f456fa6979fe3880269ffac34219f
Morten Brekkevold <morten.brekkevold@uninett.no>
2013-08-19 10:58:57 +0200

    (LDAPUser): use a configurable encoding when talking to LDAP (fixes LP#1213818)

--- tests/unittests/general/webfront_test.py
+++ tests/unittests/general/webfront_test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from unittest import TestCase
 from mock import patch, Mock, MagicMock
 
@@ -84,3 +85,18 @@ class LdapUserTestCase(TestCase):
         })
         u = nav.web.ldapauth.LDAPUser("zaphod", conn)
         self.assertRaises(nav.web.ldapauth.UserNotFound, u.search_dn)
+
+    @patch.dict("nav.web.ldapauth._config._sections",
+                {'ldap': {'__name__': 'ldap',
+                          'basedn': 'empty',
+                          'lookupmethod': 'direct',
+                          'uid_attr': 'uid',
+                          'encoding': 'utf-8'}
+                 })
+    def test_non_ascii_password_should_work(self):
+        """LP#1213818"""
+        conn = Mock(**{
+            'simple_bind_s.side_effect': lambda x, y: (str(x), str(y)),
+        })
+        u = nav.web.ldapauth.LDAPUser(u"zaphod", conn)
+        u.bind(u"æøå")
