61eccd1113d15872f8663c7fe613e5595f64558c
Roy Sindre Norangshol <norangshol@uninett.no>
2013-08-19 19:41:27 +0200

    (Netmap)(parse) Updated parsing logic to use backbone collections and models.

--- media/js/src/netmap/models/graph.js
+++ media/js/src/netmap/models/graph.js
@@ -1,6 +1,10 @@
 define([
+    'netmap/collections/vlan',
+    'netmap/collections/nodes',
+    'netmap/collections/links',
+    'netmap/collections/edges',
     'libs/backbone'
-], function () {
+], function (VlanCollection, NodesCollection, LinksCollection, EdgesCollection) {
     var graphModel = Backbone.Model.extend({
         defaults: {
                   topology: 2
@@ -11,8 +15,40 @@ define([
             var base = 'api/graph/layer{0}'.format(this.get('topology'));
             if (this.isNew()) return base;
             return base + (base.charAt(base.length - 1) === '/' ? '' : '/') + this.get('id');
-        }
+        },
+        parse: function (response, options) {
 
+            response.vlans = new VlanCollection(VlanCollection.prototype.parse(response.vlans));
+            response.nodes = new NodesCollection(NodesCollection.prototype.parse(response.nodes));
+            response.nodes.each(function (node) {
+                if (node.has('vlans')) {
+                    var vlans = node.get('vlans');
+                    node.set({
+                        'vlans': new VlanCollection(VlanCollection.prototype.parse(_.map(vlans, function (value, key) {
+                        return response.vlans.get(value);
+                    }))) }, { 'silent': true });
+                }
+            });
+
+            response.links = new LinksCollection(LinksCollection.prototype.parse(response.links));
+
+
+            response.links.each(function (link) {
+                link.set({'edges': new EdgesCollection(link.get('edges'))}, {'silent': true});
+                link.get('edges').each(function (edge) {
+                    edge.attributes.source.netbox = response.nodes.get(edge.get('source').netbox);
+                    edge.attributes.source.vlans = new VlanCollection(VlanCollection.prototype.parse(_.map(edge.get('source').vlans, function (value, key) { return response.vlans.get(value);})));
+                    edge.attributes.target.netbox = response.nodes.get(edge.get('target').netbox);
+                    edge.attributes.target.vlans = new VlanCollection(VlanCollection.prototype.parse(_.map(edge.get('target').vlans, function (value, key) { return response.vlans.get(value);})));
+                    edge.attributes.vlans = new VlanCollection(VlanCollection.prototype.parse(_.map(edge.get('vlans'), function (value, key) { return response.vlans.get(value);})));
+                });
+                link.attributes.source = response.nodes.get(link.get('source'));
+                link.attributes.target = response.nodes.get(link.get('target'));
+                link.attributes.vlans = new VlanCollection(VlanCollection.prototype.parse(_.map(link.get('vlans'), function (value, key) { return response.vlans.get(value); })));
+            });
+
+            return response;
+        }
 
     });
     return graphModel;
