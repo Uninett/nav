9617adb561b469959cb82276cf738ba89a501303
Roy Sindre Norangshol <norangshol@uninett.no>
2013-08-19 20:54:18 +0200

    Updated test to use Node model.

--- media/js/test/netmap/netbox_info_view-test.js
+++ media/js/test/netmap/netbox_info_view-test.js
@@ -1,28 +1,18 @@
-define(["netmap/views/widgets/netbox_info", "libs/jquery"], function (View) {
+define(["netmap/models/node", "netmap/views/widgets/netbox_info", "libs/jquery"], function (Node, View) {
 
     describe("View tests", function () {
         beforeEach(function (done) {
-            var node = {
-                data:   {
-                    category:       "GW",
-                    id:             "335",
-                    ip:             "192.168.42.1",
-                    ipdevinfo_link: "/ipdevinfo/nav-gw.uninett.no/",
-                    position:       null,
-                    sysname:        "nav-gw.uninett.no",
-                    up:             "y",
-                    up_image:       "green.png"
-                },
-                fixed:  true,
-                group:  0,
-                index:  138,
-                name:   "nav-gw",
-                px:     1028.0180065733132,
-                py:     1891.7714585731676,
-                weight: 4,
-                x:      1028.0180065733132,
-                y:      1891.7714585731676
-            };
+            var node = new Node({
+                category: "GW",
+                id: "335",
+                ip: "192.168.42.1",
+                ipdevinfo_link: "/ipdevinfo/nav-gw.uninett.no/",
+                position: null,
+                sysname: "nav-gw.uninett.no",
+                up: "y",
+                up_image: "green.png"
+            });
+
             this.netbox_info_view = new View();
             this.netbox_info_view.node = node;
             assert.isNotNull(this.netbox_info_view.node, "Node should not be null!");
