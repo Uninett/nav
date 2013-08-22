82973266bb278e5b42fbd80dbfe8b7718c2d8663
Roy Sindre Norangshol <norangshol@uninett.no>
2013-08-22 09:42:19 +0200

    (karma) Change webroot to be parent folder as configuration file reside in js/test

--- htdocs/js/test/karma.conf.js
+++ htdocs/js/test/karma.conf.js
@@ -1,7 +1,7 @@
 module.exports = function (config) {
     config.set({
         // base path, that will be used to resolve files and exclude
-        basePath:       '',
+        basePath:       '..',
 
 
         // frameworks to use
