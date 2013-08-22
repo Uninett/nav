008d461135160f256f6da407b019c13105a0ef10
Morten Brekkevold <morten.brekkevold@uninett.no>
2013-08-22 08:27:00 +0200

    remove Xvfb init, Jenkins will take care of that from now on

--- tests/javascript-test.sh
+++ tests/javascript-test.sh
@@ -8,12 +8,6 @@ SLEEPTIME=8
 WORKSPACE=$1
 JSDIR="${WORKSPACE}/htdocs/js"
 
-XVFB=`which Xvfb`
-if [ "$?" -eq 1 ]; then
-    echo "Xvfb not found"
-    exit 1
-fi
-
 GOOGLECHROME=`which google-chrome`
 if [ "$?" -eq 1 ]; then
     echo "google chrome not found"
@@ -89,28 +83,6 @@ jshint --config ${JSDIR}/jshint.rc.json ${JAVASCRIPT_FILES[@]} --jslint-reporter
 # Verify that jshint was running as jshint will have non-zero exit if ANY linting errors is found.
 [ -s "${JSDIR}/javascript-jshint.xml" ]
 
-echo "Starting Xvfb"
-XVFB_TRIES=0
-XVFB_STARTED=0
-until [ ${XVFB_STARTED} -eq 1 ] || (( ${XVFB_TRIES} > 10 )) ; do
-    # Find random display number for Xvfb
-    DISPLAYNUM=$((RANDOM%10+90))
-    ${XVFB} :${DISPLAYNUM} -screen 0 1280x1024x16 > /dev/null 2>/dev/null &
-    PID_XVFB="$!"
-    sleep ${SLEEPTIME}
-    if kill -0 ${PID_XVFB}; then
-        echo "Started on display ${DISPLAYNUM} with pid ${PID_XVFB}"
-        XVFB_STARTED=1
-    else
-        DISPLAYNUM=$((RANDOM%10+90))
-        let XVFB_TRIES=${XVFB_TRIES}+1
-    fi
-done
-if [ ! ${XVFB_STARTED} ]; then
-    echo "Coult not start xvfb, exiting"
-    exit 1
-fi
-
 echo "Starting buster-server"
 BUSTER_TRIES=0
 BUSTER_STARTED=0
@@ -131,7 +103,6 @@ done
 
 if [ ! ${BUSTER_STARTED} ]; then
     echo "Could not start buster-server, exiting"
-    kill ${PID_XVFB}
     exit 1
 fi
 
@@ -158,5 +129,4 @@ fi
 echo "Cleaning up"
 kill ${PID_CHROME}
 sleep 1
-kill ${PID_XVFB}
 kill ${PID_BUSTER}
