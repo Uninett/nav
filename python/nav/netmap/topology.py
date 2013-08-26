4e25c91cb34941df1de2b42b579c21c17985eb6c
Roy Sindre Norangshol <norangshol@uninett.no>
2013-08-26 17:38:10 +0200

    (netmap.topology) Better names on short loop variables

--- python/nav/netmap/topology.py
+++ python/nav/netmap/topology.py
@@ -385,16 +385,17 @@ def build_netmap_layer3_graph(topology_without_metadata, collect_rrd=False,
     rrd_datasources = collect_rrd and _get_datasource_lookup(interfaces) or {}
 
     for source, target, metadata_dict in graph.edges_iter(data=True):
-        for a, b in metadata_dict.get('gwportprefix_pairs'):
-            rrd_traffic = get_rrd_data(rrd_datasources, (a, b))
-            additional_metadata = edge_metadata_layer3((source, target), a, b,
+        for gwpp_a, gwpp_b in metadata_dict.get('gwportprefix_pairs'):
+            rrd_traffic = get_rrd_data(rrd_datasources, (gwpp_a, gwpp_b))
+            additional_metadata = edge_metadata_layer3((source, target),
+                                                       gwpp_a,
+                                                       gwpp_b,
                                                        rrd_traffic)
-            assert a.prefix.vlan.id == b.prefix.vlan.id, ("GwPortPrefix must "
-                                                          "reside inside VLan "
-                                                          "for given Prefix, "
-                                                          "bailing!")
+            assert gwpp_a.prefix.vlan.id == gwpp_b.prefix.vlan.id, (
+                "GwPortPrefix must reside inside VLan for given Prefix, "
+                "bailing!")
             metadata = metadata_dict.setdefault('metadata', defaultdict(list))
-            metadata[a.prefix.vlan.id].append(additional_metadata)
+            metadata[gwpp_a.prefix.vlan.id].append(additional_metadata)
 
 
     if view:
