f1780118acc016c91e5362c09c6c62de177cdef0
Morten Brekkevold <morten.brekkevold@uninett.no>
2013-08-28 16:43:36 +0200

    new syncdb.py (not to be installed) to make current dev schema installable from this directory.

---
+++ sql/syncdb.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+This helper script is here for backwards compatibility; it lets the the
+current development schema be installed from anywhere within the NAV source
+code tree.
+"""
+import sys
+import os
+
+_mydir = os.path.dirname(sys.argv[0])
+_top_srcdir = os.path.abspath(os.path.join(_mydir, '..'))
+sys.path.insert(0, os.path.join(_top_srcdir, 'python'))
+os.chdir(_mydir)
+
+from nav.pgsync import main
+main()
