f559dbea6f6630d6b60712c0fa2e5f2918d88528
John-Magne Bredal <john.m.bredal@uninett.no>
2013-10-31 08:55:59 +0100

    (Netmap): Remove support for mod_python.

--- htdocs/static/js/src/netmap/app.js
+++ htdocs/static/js/src/netmap/app.js
@@ -42,7 +42,7 @@ define([
 
         // Comment this one out when moving to mod_wsgi! mod_python
         // does not support PUT etc , only GET and POST. Silly mod_python!
-        Backbone.emulateHTTP = true;
+//        Backbone.emulateHTTP = true;
 
         var headerFooterPlugin = new PluginHeaderFooter();
 
