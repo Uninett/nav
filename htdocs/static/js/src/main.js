072ead4473908a4f113a7eccaebce44f62c758bf
John-Magne Bredal <john.m.bredal@uninett.no>
2014-01-06 10:30:28 +0100

    Proof of concept for mega drop down.

--- htdocs/static/js/src/main.js
+++ htdocs/static/js/src/main.js
@@ -3,5 +3,18 @@ require(['plugins/accordion_maker', 'default', 'libs/foundation.min', 'libs/sele
         $(document).foundation();   // Apply foundation javascript on load
         accordionMaker($('.tabs')); // Apply accordionmaker for tabs
         $('select.select2').select2();
+
+        var $megadrop = $('#megadrop'),
+            $megadroptoggler = $('#megadroptoggler'),
+            $caret = $megadroptoggler.find('i');
+        $megadroptoggler.click(function () {
+            $megadrop.slideToggle(300, function () {
+                if ($(this).is(':hidden')) {
+                    $caret.removeClass('fa-caret-up').addClass('fa-caret-down');
+                } else {
+                    $caret.removeClass('fa-caret-down').addClass('fa-caret-up');
+                }
+            });
+        });
     });
 });
