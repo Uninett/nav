905b14b461e07e42b97eedfff3960aa0c3d5bec1
Morten Brekkevold <morten.brekkevold@uninett.no>
2014-01-17 09:54:10 +0100

    (login): accept a "noaccess" GET argument, meaning access was denied, and display an appropriate error message to the user if present.

--- python/nav/web/webfront/views.py
+++ python/nav/web/webfront/views.py
@@ -89,12 +89,22 @@ def login(request):
         return do_login(request)
 
     origin = request.GET.get('origin', '').strip()
+    if 'noaccess' in request.GET:
+        if request.account.is_default_account():
+            errors = ['You need to log in to access this resource']
+        else:
+            errors = ['You have insufficient privileges to access this '
+                      'resource. Please log in as another user.']
+    else:
+        errors = []
+
     return direct_to_template(
         request,
         'webfront/login.html',
         {
             'form': LoginForm(),
             'origin': origin,
+            'errors': errors,
         }
     )
 
