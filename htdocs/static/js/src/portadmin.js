dc688190be0d28f27efe7b7e0ee80b64d6ece992
John-Magne Bredal <john.m.bredal@uninett.no>
2014-01-31 15:52:26 +0100

    (PortAdmin): Fix javascript to work with new layout.

--- htdocs/static/js/src/portadmin.js
+++ htdocs/static/js/src/portadmin.js
@@ -11,7 +11,7 @@ require(['libs/spin.min', 'libs/jquery', 'libs/jquery-ui-1.8.21.custom.min'], fu
         };
     }
 
-    var nav_ajax_queue = [];  // Queue for rows we are saving
+    var nav_ajax_queue = [];  // Queue for cards we are saving
     var queue_data = {};  // Object containing data for ajax requests
 
     /* Generic spinner created for display in the middle of a cell */
@@ -44,41 +44,37 @@ require(['libs/spin.min', 'libs/jquery', 'libs/jquery-ui-1.8.21.custom.min'], fu
     }
 
     /*
-     * Add changelisteners to the tbody element to avoid adding 3
-     * listeners for each row. Also split up events to avoid acting
+     * Add changelisteners the wrapper. Also split up events to avoid acting
      * on irrelevant changes.
      */
     function addChangeListener($wrapper) {
         $wrapper.on('keyup change', '.ifalias', function (event) {
-            actOnChange($(event.target).parents('.port_row'));
+            actOnChange($(event.target).parents('.portadmin-card'));
         });
         $wrapper.on('change', '.vlanlist', function (event) {
-            actOnChange($(event.target).parents('.port_row'));
+            actOnChange($(event.target).parents('.portadmin-card'));
         });
         $wrapper.on('click', '.voicevlan', function (event) {
-            actOnChange($(event.target).parents('.port_row'));
+            actOnChange($(event.target).parents('.portadmin-card'));
         });
     }
 
     /*
-     * Mark row changed or not based on values in row
+     * Mark card changed or not based on values in card
      */
-    function actOnChange(row) {
-        if (textFieldChanged(row) || dropDownChanged(row) || voiceVlanChanged(row)) {
-            markAsChanged(row);
+    function actOnChange(card) {
+        if (textFieldChanged(card) || dropDownChanged(card) || voiceVlanChanged(card)) {
+            markAsChanged(card);
         } else {
-            markAsUnchanged(row);
+            markAsUnchanged(card);
         }
     }
 
     function addSaveListener($wrapper) {
-        /*
-        Save when clicking on the save buttons. As the save button is in
-        another row than the form, find the correct row and run save on it.
-        */
-        $wrapper.on('click', '.portadmin-save', function (event) {
-            var $row = $(event.target).parents('.port_row');
-            saveRow($row);
+        /* Save when clicking on the save buttons. */
+        $wrapper.on('click', '.save-interface', function (event) {
+            var $card = $(event.target).parents('.portadmin-card');
+            saveRow($card);
         });
     }
 
@@ -87,28 +83,28 @@ require(['libs/spin.min', 'libs/jquery', 'libs/jquery-ui-1.8.21.custom.min'], fu
     }
 
     function bulkSave() {
-        $(".changed").each(function (index, row) {
-            saveRow($(row));
+        $(".changed").each(function (index, card) {
+            saveRow($(card));
         });
     }
 
-    function textFieldChanged(row) {
-        var element = $(row).find(".ifalias");
+    function textFieldChanged(card) {
+        var element = $(card).find(".ifalias");
         return $(element).attr('data-orig') !== $(element).val();
     }
 
-    function dropDownChanged(row) {
-        var dropdown = $(row).find(".vlanlist");
+    function dropDownChanged(card) {
+        var dropdown = $(card).find(".vlanlist");
         var origOption = $('[data-orig]', dropdown)[0];
         var selectedOption = $('option:selected', dropdown)[0];
         return origOption !== selectedOption;
     }
 
-    function voiceVlanChanged(row) {
+    function voiceVlanChanged(card) {
         /*
          * XOR checkbox checked and original value to see if changed
          */
-        var $checkbox = $(row).find('.voicevlan');
+        var $checkbox = $(card).find('.voicevlan');
         if ($checkbox.length) {
             var origOption = $checkbox.attr('data-orig').toLowerCase() === 'true';
             var checkedValue = $checkbox.prop('checked');
@@ -118,103 +114,104 @@ require(['libs/spin.min', 'libs/jquery', 'libs/jquery-ui-1.8.21.custom.min'], fu
         }
     }
 
-    function markAsChanged(row) {
-        var $row = $(row);
-        if (!$row.hasClass('changed')) {
-            $row.addClass("changed");
+    function markAsChanged(card) {
+        var $card = $(card);
+        if (!$card.hasClass('changed')) {
+            $card.addClass("changed");
         }
     }
 
-    function markAsUnchanged(row) {
-        var $row = $(row);
-        if ($row.hasClass('changed')) {
-            $row.removeClass("changed");
+    function markAsUnchanged(card) {
+        var $card = $(card);
+        if ($card.hasClass('changed')) {
+            $card.removeClass("changed");
         }
     }
 
-    function clearChangedState(row) {
-        markAsUnchanged(row);
+    function clearChangedState(card) {
+        markAsUnchanged(card);
     }
 
-    function saveRow($row) {
+    function saveRow($card) {
         /*
          * This funcion does an ajax call to save the information given by the user
          * when the save-button is clicked.
          */
 
-        var rowid = $row.prop('id');
-        if (!rowid) {
-            console.log('Could not find id of row ' + $row);
+        var cardid = $card.prop('id');
+        if (!cardid) {
+            console.log('Could not find id of card ' + $card);
             return;
         }
 
         // Post data and wait for json-formatted returndata. Display status information to user
-        saveInterface($row, create_ajax_data($row));
+        saveCard($card, create_ajax_data($card));
     }
 
-    function create_ajax_data($row) {
+    function create_ajax_data($card) {
         /*
          Create the object used in the ajax call.
          */
         var data = {};
-        data.interfaceid = $row.prop('id');
-        if (textFieldChanged($row)) {
-            data.ifalias = $row.find(".ifalias").val();
+        data.interfaceid = $card.prop('id');
+        if (textFieldChanged($card)) {
+            data.ifalias = $card.find(".ifalias").val();
         }
-        if (dropDownChanged($row)) {
-            data.vlan = $row.find(".vlanlist").val();
+        if (dropDownChanged($card)) {
+            data.vlan = $card.find(".vlanlist").val();
         }
-        if (voiceVlanChanged($row)) {
-            data.voicevlan = $row.find(".voicevlan").prop('checked');
+        if (voiceVlanChanged($card)) {
+            data.voicevlan = $card.find(".voicevlan").prop('checked');
         }
-        if ($row.find(".voicevlan").prop('checked')) {
+        if ($card.find(".voicevlan").prop('checked')) {
             data.voice_activated = true;
         }
         return data;
     }
 
-    function saveInterface($row, interfaceData) {
-        var rowid = $row.prop('id');
-        // If a save on this row is already in progress, do nothing.
-        if (nav_ajax_queue.indexOf(rowid) > -1) {
+    function saveCard($card, interfaceData) {
+        var cardid = $card.prop('id');
+        // If a save on this card is already in progress, do nothing.
+        if (nav_ajax_queue.indexOf(cardid) > -1) {
             return;
         }
         disableSaveallButtons();
-        nav_ajax_queue.push(rowid);
-        queue_data[rowid] = interfaceData;
+        nav_ajax_queue.push(cardid);
+        queue_data[cardid] = interfaceData;
 
         // Do not send more than one request at the time.
         if (nav_ajax_queue.length > 1) {
             return;
         }
 
-        doAjaxRequest(rowid);
+        doAjaxRequest(cardid);
     }
 
-    function doAjaxRequest(rowid) {
-        var $row = $('#' + rowid);
-        var interfaceData = queue_data[rowid];
+    function doAjaxRequest(cardid) {
+        var $card = $('#' + cardid);
+        var interfaceData = queue_data[cardid];
         $.ajax({url: "save_interfaceinfo",
             data: interfaceData,
             dataType: 'json',
-            type: 'POST',
+//            type: 'POST',
+            type: 'GET',
             beforeSend: function () {
                 $('tr.error').remove();
-                disableButtons($row);
-                spinner.spin($row);
+                disableButtons($card);
+                spinner.spin($card);
             },
             success: function () {
-                clearChangedState($row);
-                indicateSuccess($row);
-                updateDefaults($row, interfaceData);
+                clearChangedState($card);
+                indicateSuccess($card);
+                updateDefaults($card, interfaceData);
             },
             error: function (jqXhr) {
                 console.log(jqXhr.responseText);
-                indicateError($row, $.parseJSON(jqXhr.responseText).messages);
+                indicateError($card, $.parseJSON(jqXhr.responseText).messages);
             },
             complete: function (jqXhr) {
-                removeFromQueue(rowid);
-                enableButtons($row);
+                removeFromQueue(cardid);
+                enableButtons($card);
                 spinner.stop();
                 if (nav_ajax_queue.length === 0) {
                     enableSaveallButtons();
@@ -226,74 +223,66 @@ require(['libs/spin.min', 'libs/jquery', 'libs/jquery-ui-1.8.21.custom.min'], fu
         });
     }
 
-    function disableButtons(cell) {
-        $(cell).find('button').prop('disabled', true);
+    function disableButtons(card) {
+        $(card).find('button').prop('disabled', true);
     }
 
-    function enableButtons(cell) {
-        $(cell).find('button').prop('disabled', false);
+    function enableButtons(card) {
+        $(card).find('button').prop('disabled', false);
     }
 
-    function indicateSuccess($row) {
-        /* Animate row to indicate success */
-
-        $row.addClass('success');
+    function indicateSuccess($card) {
+        /* Highlight card to indicate success */
+        removeAlerts($card);
+        $card.addClass('success');
         setTimeout(function () {
-            $row.removeClass('success');
+            $card.removeClass('success');
         }, 1500);
     }
 
-    function indicateError($row, messages) {
-        var $newRow = $('<tr/>').addClass('error'),
-            $cell = $('<td class="" colspan="10"/>'),
-            $message = $('<span/>');
-
-        var error = '';
+    function indicateError($card, messages) {
+        removeAlerts($card);
         for (var x = 0, l = messages.length; x < l; x++) {
-            error += messages[x].message + '. ';
+            $card.append(
+                $('<div class="alert-box alert"></div>').text(messages[x].message)
+            );
         }
-        $message.text(error);
-
-        $newRow.append($cell.append($message));
-        $newRow.insertAfter($row);
-
-        $newRow.click(function () {
-            $(this).hide(1000, function () {
-                $(this).remove();
-            });
-        });
     }
 
-    function updateDefaults($row, data) {
+    function removeAlerts($card) {
+        $card.find('.alert-box').remove();
+    }
+
+    function updateDefaults($card, data) {
         if ('ifalias' in data) {
-            updateIfAliasDefault($row, data.ifalias);
+            updateIfAliasDefault($card, data.ifalias);
         }
         if ('vlan' in data) {
-            updateVlanDefault($row, data.vlan);
+            updateVlanDefault($card, data.vlan);
         }
         if ('voicevlan' in data) {
-            updateVoiceDefault($row, data.voicevlan);
+            updateVoiceDefault($card, data.voicevlan);
         }
     }
 
-    function updateIfAliasDefault($row, ifalias) {
-        var old_ifalias = $row.find(".ifalias").attr('data-orig');
+    function updateIfAliasDefault($card, ifalias) {
+        var old_ifalias = $card.find(".ifalias").attr('data-orig');
         if (old_ifalias !== ifalias) {
-            $row.find(".ifalias").attr('data-orig', ifalias);
+            $card.find(".ifalias").attr('data-orig', ifalias);
         }
     }
 
-    function updateVlanDefault($row, vlan) {
-        var old_vlan = $row.find('option[data-orig]').val();
+    function updateVlanDefault($card, vlan) {
+        var old_vlan = $card.find('option[data-orig]').val();
         if (old_vlan !== vlan) {
             console.log('Updating vlan default from ' + old_vlan + ' to ' + vlan);
-            $row.find('option[data-orig]').removeAttr('data-orig');
-            $row.find('option[value=' + vlan + ']').attr('data-orig', vlan);
+            $card.find('option[data-orig]').removeAttr('data-orig');
+            $card.find('option[value=' + vlan + ']').attr('data-orig', vlan);
         }
     }
 
-    function updateVoiceDefault($row, new_value) {
-        var $voice_element = $row.find(".voicevlan");
+    function updateVoiceDefault($card, new_value) {
+        var $voice_element = $card.find(".voicevlan");
         if ($voice_element.length) {
             var old_value = $voice_element.attr('data-orig');
             if (old_value !== new_value) {
@@ -334,36 +323,6 @@ require(['libs/spin.min', 'libs/jquery', 'libs/jquery-ui-1.8.21.custom.min'], fu
                 expandButton.addClass('hidden');
                 collapseButton.removeClass('hidden');
             }
-        });
-    }
-
-    /*
-     * Undo the changes the user has done on the form fields
-     */
-    function addUndoListener(element) {
-        $('.undo', element).click(function (event) {
-            var motherId = $(event.target).parents('tr').attr('data-mother-row');
-            var $row = $('#' + motherId);
-            var $ifalias = $row.find(".ifalias");
-            var $vlan = $row.find(".vlanlist");
-            var $voicevlan = $row.find(".voicevlan");
-
-            // Set ifalias back to original value
-            $ifalias.val($ifalias.attr('data-orig'));
-
-            // Set vlan back to original value
-            $vlan.val($('[data-orig]', $vlan).val());
-
-            // Check or uncheck telephone checkbox
-            if ($voicevlan.length) {
-                if ($voicevlan.attr('data-orig').toLowerCase() === 'true') {
-                    $voicevlan.prop('checked', 'checked');
-                } else {
-                    $voicevlan.prop('checked', false);
-                }
-            }
-
-            clearChangedState($row);
         });
     }
 
