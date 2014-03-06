a68acf4abc55042f5580425367d04dbab8b60b85
John-Magne Bredal <john.m.bredal@uninett.no>
2014-03-06 11:22:18 +0100

    (webfront): Bigger searchform. Remove widget dashboard. Add button for opening widget dialog.

--- htdocs/static/js/src/webfront.js
+++ htdocs/static/js/src/webfront.js
@@ -13,20 +13,6 @@ require(['plugins/room_mapper', 'plugins/navlets_controller', 'libs/jquery', 'li
             });
         }
 
-        function addControlCenterListeners() {
-            /* Controls behavior of the elements regarding the Dashboard Control Center */
-            var $widgetContainer = $('#widgets-action-container'),
-                $widgetActions = $('.widget-actions');
-            $widgetContainer.find('.action-container-toggler').click(function () {
-                $widgetActions.toggle('slide', {'direction': 'right'});
-            });
-
-            /* Hide control panel when adding a widget */
-            $widgetContainer.find('[data-reveal-id]').click(function () {
-                $widgetActions.hide();
-            });
-        }
-
         $(function () {
             var controller = new NavletsController($navletsContainer);
             controller.container.on('navlet-rendered', function (event, node) {
@@ -37,13 +23,11 @@ require(['plugins/room_mapper', 'plugins/navlets_controller', 'libs/jquery', 'li
                 }
             });
 
-
             /* Add click listener to joyride button */
             $navletsContainer.on('click', '#joyrideme', function () {
                 $(document).foundation('joyride', 'start');
             });
 
-            addControlCenterListeners();
         });
 
     }
