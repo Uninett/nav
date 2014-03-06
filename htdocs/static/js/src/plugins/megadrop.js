3f8e2bd44e1fb49280e4ffd3718a4d6ac6ac3146
John-Magne Bredal <john.m.bredal@uninett.no>
2014-03-06 11:31:49 +0100

    (MegaDrop): Switch from toggle on mouseenter to toggle on click.

--- htdocs/static/js/src/plugins/megadrop.js
+++ htdocs/static/js/src/plugins/megadrop.js
@@ -10,41 +10,18 @@ require(['libs/jquery'], function () {
             caretUpClass = 'fa-caret-up',
             slidespeed = 300;
 
-        function hideMegaDrop() {
-            $megadrop.slideUp(slidespeed, function () {
-                $caret.removeClass(caretUpClass).addClass(caretDownClass);
-            });
-        }
-
-        function inMegadrop(element) {
-            var $element = $(element);
-            return $element.prop('id') === megadropSelector || $element.parents('#' + megadropSelector).length;
-        }
-
-        function inMegadropToggler(element) {
-            var $element = $(element);
-            return $element.prop('id') === megadropTogglerSelector || $element.parents('#' + megadropTogglerSelector).length;
-        }
-
-        /* Show megadrop on mouseover */
-        $megadroptoggler.mouseover(function () {
-            $megadrop.slideDown(slidespeed, function () {
-                $caret.removeClass(caretDownClass).addClass(caretUpClass);
-            });
-        });
-
-        /* Hide megadrop when mouse leaves link and not enters megadrop */
-        $megadroptoggler.mouseleave(function (event) {
-            if (!inMegadrop(event.relatedTarget)) {
-                hideMegaDrop();
+        /* Toggle megadrop on click */
+        $megadroptoggler.click(function () {
+            if ($megadrop.is(':visible')) {
+                $megadrop.slideUp(slidespeed, function () {
+                    $caret.removeClass(caretUpClass).addClass(caretDownClass);
+                });
+            } else {
+                $megadrop.slideDown(slidespeed, function () {
+                    $caret.removeClass(caretDownClass).addClass(caretUpClass);
+                });
             }
         });
 
-        /* Hide megadrop when mouse leaves megadrop and not enters toggler */
-        $megadrop.mouseleave(function (event) {
-            if (!inMegadropToggler(event.relatedTarget)) {
-                hideMegaDrop();
-            }
-        });
     });
 });
