efc1e804feaae65e4ec6fcde464e14d50b1543c3
Morten Brekkevold <morten.brekkevold@uninett.no>
2014-03-18 11:39:31 +0100

    (UpsMib): refactor and add support for distinguishing between scalar objects and actual column objects. A scalar object will represent a single sensor, column objects may represent multiple sensors.

--- python/nav/mibs/ups_mib.py
+++ python/nav/mibs/ups_mib.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2008 - 2011 (C) UNINETT AS
+# Copyright 2008 - 2011, 2014 (C) UNINETT AS
 #
 # This file is part of Network Administration Visualized (NAV).
 #
@@ -13,8 +13,8 @@
 # details.  You should have received a copy of the GNU General Public License
 # along with NAV. If not, see <http://www.gnu.org/licenses/>.
 #
-""" A class for extracting sensors from RFC1628-compatible UPSes.
-"""
+"""A class for extracting sensors from RFC1628 compatible UPSes"""
+
 from twisted.internet import defer
 
 from nav.mibs import reduce_index
@@ -27,66 +27,82 @@ class UpsMib(mibretriever.MibRetriever):
 
     sensor_columns = {
         'upsInputVoltage': {
+            'is_column': True,
             'u_o_m': 'Volts',
-            },
+        },
         'upsInputFrequency': {
+            'is_column': True,
             'u_o_m': 'Hz',
             'precision': 1,
-            },
+        },
         'upsOutputCurrent': {
+            'is_column': True,
             'u_o_m': 'Amperes',
             'precision': 1,
-            },
+        },
+
         'upsBatteryTemperature': {
             'u_o_m': 'Celsius',
-            },
+        },
         'upsEstimatedChargeRemaining': {
             'u_o_m': 'Percent',
-            },
+        },
         'upsEstimatedMinutesRemaining': {
             'u_o_m': 'Minutes',
-            },
-        }
+        },
+    }
 
     def get_module_name(self):
-        """return the MIB-name."""
+        """Returns the MIB module"""
         return self.mib.get('moduleName', None)
 
     def _get_named_column(self, column):
-        """ Return the named column in this mib"""
+        """Retrieves the contents of the named column from this MIB"""
         df = self.retrieve_columns([column])
         df.addCallback(reduce_index)
         return df
 
     @defer.inlineCallbacks
     def get_all_sensors(self):
-        """ Get all the interesting sensors for this UPS."""
+        """Returns a list of all the interesting sensors on this UPS."""
         result = []
-        for sensor_name in self.sensor_columns.keys():
-            sensor_params = yield self._get_named_column(sensor_name)
-            self._logger.debug('get_all_sensors: %s; %s = %s',
-                               self.agent_proxy.ip, sensor_name, sensor_params)
-            for row in sensor_params.itervalues():
-                row_oid = row.get(0, None)
-                mibobject = self.nodes.get(sensor_name, None)
-                oid = str(mibobject.oid) + str(row_oid)
-                unit_of_measurement = self.sensor_columns[sensor_name].get(
-                                                                'u_o_m', None)
-                precision = self.sensor_columns[sensor_name].get(
-                                                            'precision', None)
-                scale = self.sensor_columns[sensor_name].get('scale', None)
-                description = self.mib.get('nodes').get(sensor_name).get(
-                                                           'description', None)
-                name = sensor_name
-                internal_name = name
-                result.append({
-                    'oid': oid,
-                    'unit_of_measurement': unit_of_measurement,
-                    'precision': precision,
-                    'scale': scale,
-                    'description': description,
-                    'name': name,
-                    'internal_name': internal_name,
-                    'mib': self.get_module_name(),
-                    })
+
+        for sensor in self.sensor_columns.keys():
+            sensor_params = yield self._get_named_column(sensor)
+            result.extend(self._get_sensors(sensor, sensor_params))
+
         defer.returnValue(result)
+
+    def _get_sensors(self, object_name, sensor_params):
+        result = []
+        meta = self.sensor_columns[object_name]
+        self._logger.debug('_get_sensors: %s; %s = %s',
+                           self.agent_proxy.ip, object_name, sensor_params)
+
+        for row in sensor_params.itervalues():
+            row_oid = row.get(0, None)
+            mibobject = self.nodes.get(object_name, None)
+            oid = str(mibobject.oid) + str(row_oid)
+            unit_of_measurement = meta.get('u_o_m', None)
+            precision = meta.get('precision', None)
+            scale = meta.get('scale', None)
+            description = self.mib.get('nodes').get(object_name).get(
+                'description', None)
+            if meta.get('is_column', False):
+                name = object_name + str(row_oid)
+            else:
+                name = object_name
+            internal_name = name
+
+            result.append({
+                'oid': oid,
+                'unit_of_measurement': unit_of_measurement,
+                'precision': precision,
+                'scale': scale,
+                'description': description,
+                'name': name,
+                'internal_name': internal_name,
+                'mib': self.get_module_name(),
+            })
+
+        return result
