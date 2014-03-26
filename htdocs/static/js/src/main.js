64c0c063968724c106d56e9c5c9b002128cec479
John-Magne Bredal <john.m.bredal@uninett.no>
2014-03-26 13:37:50 +0100

    (javascript): Add redirect to login when session has timed out. Remove unused file.

--- htdocs/static/js/src/main.js
+++ htdocs/static/js/src/main.js
@@ -6,6 +6,13 @@ require([
     'plugins/megadrop'
 ], function (accordionMaker) {
     $(function () {
+        /* Add redirect to login on AJAX-requests if session has timed out */
+        $(document).ajaxError(function (event, request) {
+            if (request.status === 401) {
+                window.location = '/index/login/?origin=' + encodeURIComponent(window.location.href);
+            }
+        });
+
         $(document).foundation();   // Apply foundation javascript on load
         accordionMaker($('.tabs')); // Apply accordionmaker for tabs
         $('select.select2').select2();
