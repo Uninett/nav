0780d4dbca36ed608a886d3eda5bafd10a86c843
John-Magne Bredal <john.m.bredal@uninett.no>
2014-04-24 14:23:59 +0200

    Add template tag for verbatim (comes in Django 1.5)

---
+++ python/nav/django/templatetags/verbatim.py
@@ -0,0 +1,62 @@
+#
+# Copyright (C) 2014 UNINETT AS
+#
+# This file is part of Network Administration Visualized (NAV).
+#
+# NAV is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.  You should have received a copy of the GNU General Public
+# License along with NAV. If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Credit to user paulsmith on GitHub for this code.
+https://gist.github.com/paulsmith/1313862
+
+jQuery templates use constructs like:
+
+    {{if condition}} print something{{/if}}
+
+This, of course, completely screws up Django templates,
+because Django thinks {{ and }} mean something.
+
+Wrap {% verbatim %} and {% endverbatim %} around those
+blocks of jQuery templates and this will try its best
+to output the contents with no changes.
+"""
+
+from django import template
+
+register = template.Library()
+
+
+class VerbatimNode(template.Node):
+
+    def __init__(self, text):
+        self.text = text
+
+    def render(self, context):
+        return self.text
+
+
+@register.tag
+def verbatim(parser, token):
+    text = []
+    while 1:
+        token = parser.tokens.pop(0)
+        if token.contents == 'endverbatim':
+            break
+        if token.token_type == template.TOKEN_VAR:
+            text.append('{{')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('{%')
+        text.append(token.contents)
+        if token.token_type == template.TOKEN_VAR:
+            text.append('}}')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('%}')
+    return VerbatimNode(''.join(text))
