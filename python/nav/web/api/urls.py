9ef0043dfef36c47a58d8f994d0b53f59b39022b
John-Magne Bredal <john.m.bredal@uninett.no>
2014-05-16 11:42:38 +0200

    (API): Again

---
+++ python/nav/web/api/urls.py
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2014 UNINETT AS
+#
+# This file is part of Network Administration Visualized (NAV).
+#
+# NAV is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.  You should have received a copy of the GNU General Public
+# License along with NAV. If not, see <http://www.gnu.org/licenses/>.
+#
+"""URL mapping for the various API versions"""
+
+from django.conf.urls import include, patterns, url
+from django.shortcuts import redirect
+from nav.web.api.v1 import urls as v1_urls
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', include(v1_urls)),
+    url(r'^1/', include(v1_urls)),
+)
