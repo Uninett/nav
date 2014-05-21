001dc833623f7bbe5ce1009847784401debd9e02
Morten Brekkevold <morten.brekkevold@uninett.no>
2014-05-21 09:35:33 +0200

    (room, org): Don't copy empty opt fields into the new data attributes

--- sql/changes/sc.04.01.0002.sql
+++ sql/changes/sc.04.01.0002.sql
@@ -4,7 +4,9 @@
 ALTER TABLE org ADD COLUMN data hstore;
 
 -- Copy all information from opt-fields to hstore
-UPDATE org SET data = ('opt1' => opt1) || ('opt2' => opt2) || ('opt3' => opt3);
+UPDATE org SET data = ('opt1' => opt1) WHERE COALESCE(opt1, '') <> '';
+UPDATE org SET data = data || ('opt2' => opt2) WHERE COALESCE(opt2, '') <> '';
+UPDATE org SET data = data || ('opt3' => opt3) WHERE COALESCE(opt3, '') <> '';
 
 -- Drop useless opt columns
 ALTER TABLE org DROP COLUMN opt1, DROP COLUMN opt2, DROP COLUMN opt3;
