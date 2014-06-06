c06c4b60c1c5172bfa9146ad9169a1e5e3fc1d84
John-Magne Bredal <john.m.bredal@uninett.no>
2014-06-06 10:49:29 +0200

    Add two more sorting plugins for Datatable

---
+++ htdocs/static/js/src/dt_plugins/numeric_html.js
@@ -0,0 +1,20 @@
+/*
+    Copied from: http://legacy.datatables.net/plug-ins/sorting
+    Author: Allan Jardine
+*/
+require(['libs/jquery.dataTables.min'], function () {
+    jQuery.extend(jQuery.fn.dataTableExt.oSort, {
+        "num-html-pre": function (a) {
+            var x = String(a).replace(/<[\s\S]*?>/g, "");
+            return parseFloat(x);
+        },
+
+        "num-html-asc": function (a, b) {
+            return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+        },
+
+        "num-html-desc": function (a, b) {
+            return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+        }
+    });
+});
