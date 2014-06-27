b6954198fc742193f78afd3a1b9cceed0b3d09d9
Morten Brekkevold <morten.brekkevold@uninett.no>
2014-06-27 15:25:26 +0200

    new debian version

--- tools/build_vagrant_image.sh
+++ tools/build_vagrant_image.sh
@@ -13,7 +13,7 @@ rvm use 1.9.2@veewee --create
 # Want other templates? Keep this command commented
 # so we don't forget how to check for new templates in the future. 
 #bundle exec veewee vbox templates | grep -i debian
-bundle exec veewee vbox define 'nav-basevm' 'Debian-7.1.0-amd64-netboot' --force
+bundle exec veewee vbox define 'nav-basevm' 'Debian-7.4.0-amd64-netboot' --force
 
 # Sets the norwegian mirror as default mirror
 sed 's#\(d-i.mirror.http.hostname.string\)\(.*\)#\1 ftp.no.debian.org#g' -i $BASE_FULL_DIR/veewee/definitions/nav-basevm/preseed.cfg
