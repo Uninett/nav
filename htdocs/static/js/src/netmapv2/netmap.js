d84ab653b96da28187fc8c8794d08b6a2775f770
Eivind Lysne <eivindlysne@gmail.com>
2014-08-08 13:49:01 +0200

    Check for supported IE version

--- htdocs/static/js/src/netmapv2/netmap.js
+++ htdocs/static/js/src/netmapv2/netmap.js
@@ -7,6 +7,16 @@ require([
 
     $(function () {
 
+        // Test if browser is IE and, if so, is it supported.
+        if (navigator.appVersion.indexOf('MSIE') !== -1) {
+            var version = parseFloat(navigator.appVersion.split("MSIE")[1]);
+            var docMode = document.documentMode ? document.documentMode : 0;
+            if (version < 9 || docMode < 9) {
+                alert('Your version of Internet Explorer is too old to run Netmap.' +
+                    ' Please upgrade to IE9 or make sure DocumentMode is set to 9 or newer');
+            }
+        }
+
         // Disable caching for netmap
         $.ajaxSetup({ cache: false });
 
