4f84a5ccda7aefaeb6663039d80de762dbdee460
Eivind Lysne <eivindlysne@gmail.com>
2014-08-13 15:39:35 +0200

    Removed unused form

--- python/nav/web/netmap/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class NetmapDefaultViewForm(forms.Form):
-    """Form for setting a global netmap view"""
-    map_id = forms.IntegerField(label='Map id to use as global default view')
