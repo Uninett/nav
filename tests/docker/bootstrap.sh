45a2b5dfd22aac675b627018e28912df72245631
Morten Brekkevold <morten.brekkevold@uninett.no>
2014-08-28 15:06:47 +0200

    more ci environment cleanup, *sigh*

--- tests/docker/bootstrap.sh
+++ tests/docker/bootstrap.sh
@@ -13,33 +13,13 @@ export WORKSPACE="/source"
 export BUILDDIR="${WORKSPACE}/build"
 export PYTHONPATH="${WORKSPACE}/python"
 
-start_apache() {
-    APACHE_CONFIG="${WORKSPACE}/tests/apache.conf"
-
-    echo -n "Starting apache..."
-    a2dismod cgid
-    /usr/sbin/apache2ctl -f $APACHE_CONFIG -k start
-    echo " done"
-}
-
-start_xvfb() {
-    XVFB=/usr/bin/Xvfb
-    XVFBARGS=":99 -screen 0 1024x768x24 -fbdir /var/tmp -ac"
-    PIDFILE="/var/tmp/xvfb.pid"
-
-    echo -n "Starting Xvfb..."
-    sudo -u build -- /sbin/start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background --exec $XVFB -- $XVFBARGS
-    echo " done"
-}
 
 if [ -z "$@" ]; then
     echo Nothing to do
     exit 1
+else
+
+    su -c "$*" build
+    echo All done.
+
 fi
-
-start_apache
-start_xvfb
-
-su -c "$*" build
-
-echo Done.
