1c04d1e60f6b96485a62b38fb006c28266e11098
John-Magne Bredal <john.m.bredal@uninett.no>
2014-09-12 11:31:01 +0200

    Comment function

--- htdocs/static/js/src/plugins/jquery_ui_helpers.js
+++ htdocs/static/js/src/plugins/jquery_ui_helpers.js
@@ -3,6 +3,7 @@
 define(["libs/jquery"], function () {
 
     function cacheRequest(event, ui) {
+        /* Cache is removed as an option in jquery-ui-1.10. Reimplement it */
         if (ui.tab.data("loaded")) {
             event.preventDefault();
             return;
