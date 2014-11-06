2fef94794e3b7414a418a8a1f2f5d0316da44e08
John-Magne Bredal <john.m.bredal@uninett.no>
2014-11-06 15:27:32 +0100

    (manage.py): Unset help_text for many_to_many field.

--- python/nav/models/manage.py
+++ python/nav/models/manage.py
@@ -77,6 +77,7 @@ class Netbox(models.Model):
     category = models.ForeignKey('Category', db_column='catid')
     netboxgroups = models.ManyToManyField(
         'NetboxGroup', through='NetboxCategory', blank=True, null=True)
+    netboxgroups.help_text = ''
     organization = models.ForeignKey('Organization', db_column='orgid')
     read_only = VarcharField(db_column='ro', blank=True, null=True)
     read_write = VarcharField(db_column='rw', blank=True, null=True)
