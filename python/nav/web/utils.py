24176526ed4f1ecacd9653a35fc3bdbe998ad190
Christian Strand Young <christian@strandyoung.com>
2014-11-11 16:13:45 +0100

    (SubListView): Fixed indentation error

--- python/nav/web/utils.py
+++ python/nav/web/utils.py
@@ -24,7 +24,7 @@ def create_title(navpath):
 
 
 class SubListView(ListView):
-	""" Subclass of the generic list ListView to allow extra context """
+    """ Subclass of the generic list ListView to allow extra context """
     extra_context = {}
     def get_context_data(self,*args, **kwargs):
         context = super(SubListView, self).get_context_data(*args, **kwargs)
