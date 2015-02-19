b29b0fc15ddf124cc7b065dc99aa78c1422c6b5f
Morten Brekkevold <morten.brekkevold@uninett.no>
2015-02-19 15:19:15 +0100

    use xml output for cloc reports

--- tests/docker/cloc.sh
+++ tests/docker/cloc.sh
@@ -4,6 +4,7 @@ cloc \
     --exclude-dir=.env,.idea,tmp,build,doc,htdocs/static/js/libs,htdocs/static/js/node_modules,htdocs/static/js/geomap/projdefs,python/nav/smidumps \
     --exclude-lang=make,m4,XML \
     --not-match-f='(configure|config.status)$' \
-    --report-file="${WORKSPACE}/cloc.txt" \
+    --by-file \
+    --xml \
+    --out="${WORKSPACE}/cloc.xml" \
     .
-
