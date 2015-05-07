6c531bbb7396afa70eafe1262d52e24094abac55
Morten Brekkevold <morten.brekkevold@uninett.no>
2015-05-07 13:25:58 +0200

    (NetboxEntity.gone_since): new timestamp field for missing entities

--- python/nav/models/manage.py
+++ python/nav/models/manage.py
@@ -436,6 +436,7 @@ class NetboxEntity(models.Model):
     fru = models.NullBooleanField(verbose_name='Is a field replaceable unit')
     mfg_date = models.DateTimeField(null=True)
     uris = VarcharField(null=True)
+    gone_since = models.DateTimeField(null=True)
     data = hstore.DictionaryField()
 
     objects = hstore.HStoreManager()
