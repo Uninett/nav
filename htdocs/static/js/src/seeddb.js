d0268a404641fb9e3f791fe68ec123085a88c525
John-Magne Bredal <john.m.bredal@uninett.no>
2015-06-18 10:30:28 +0200

    SeedDB IP Device editing will now trigger a confirmation when leaving without saving (Fixes LP#1466373)

--- htdocs/static/js/src/seeddb.js
+++ htdocs/static/js/src/seeddb.js
@@ -48,6 +48,7 @@ require([
         }
 
         activateIpDeviceFormPlugins();
+        addConfirmLeavePage();
     }
 
 
@@ -223,5 +224,31 @@ require([
             }
         });
     }
+
+
+    /**
+     * Some forms are complex enough to varrant a confirmation if the user tries
+     * to leave the page without saving the changed form.
+     *
+     * This function sets the return value on the beforeunload event if the
+     * forms are changed but not saved before leaving the page, in effect
+     * triggering the browsers "Are you sure you want to leave this
+     * page"-confirmation.
+     */
+    function addConfirmLeavePage() {
+        var setReturnValue = function (event) {
+            event.returnValue = 'You have unsaved changes.';
+        };
+
+        var forms = $('#seeddb-netbox-form');
+
+        forms.one('change', function() {
+            window.addEventListener('beforeunload', setReturnValue);
+        });
+        forms.on('submit', function() {
+            window.removeEventListener('beforeunload', setReturnValue);
+        });
+    }
+
 
 });
