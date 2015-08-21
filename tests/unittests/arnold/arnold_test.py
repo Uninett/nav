98852d12caee895888581ccdeb5e316c91e628d5
John-Magne Bredal <john.m.bredal@uninett.no>
2015-08-21 15:48:04 +0200

    Add tests for find_input_type.

--- tests/unittests/arnold/arnold_test.py
+++ tests/unittests/arnold/arnold_test.py
@@ -7,10 +7,25 @@ class TestArnold(unittest.TestCase):
 
     def test_find_input_type(self):
         """Test find_input_type"""
-        ip = '158.38.129.113'
+        ip_address = '158.38.129.113'
         mac = '5c:f9:dd:78:72:8a'
-        self.assertEqual(find_input_type(ip), 'IP')
+        self.assertEqual(find_input_type(ip_address), 'IP')
         self.assertEqual(find_input_type(mac), 'MAC')
         self.assertEqual(find_input_type(123), 'SWPORTID')
+
+    def test_typo_not_accepted(self):
+        """Tests for weakness in IPy library"""
+        ip_address = '158.38.129'
+        self.assertEqual(find_input_type(ip_address), 'UNKNOWN')
+
+    def test_end_on_zero(self):
+        """Tests that IP-addresses that ends on zero are accepted"""
+        ip_address = '158.38.129.0'
+        self.assertEqual(find_input_type(ip_address), 'IP')
+
+    def test_ipv6(self):
+        """Tests that a simple ipv6 address is recognized"""
+        ip_address = 'FE80:0000:0000:0000:0202:B3FF:FE1E:8329'
+        self.assertEqual(find_input_type(ip_address), 'IP')
 
 
