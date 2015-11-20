9d3cd7130b04afc6c2080120d858be8f15e25a6c
Morten Brekkevold <morten.brekkevold@uninett.no>
2015-11-20 11:39:32 +0100

    test data causes web crawler to spend too much time. disabling the whole thing until we have time to work on it.

--- tests/docker/create-db.sh
+++ tests/docker/create-db.sh
@@ -75,7 +75,12 @@ update_nav_db_conf() {
 create_nav_db() {
 
     # Create and populate database
+    echo Creating and populating initial database
     "$BUILDDIR/bin/navsyncdb" -c
+
+    ## Now do it again, with test data
+    #echo Loading test database from scratch
+    #"$BUILDDIR/bin/navsyncdb" --drop-database --create --out-of-order --restore "${WORKSPACE}/tests/test_dump.sql"
 
     if [ -n "$ADMINPASSWORD" ]; then
       psql -c "UPDATE account SET password = '$ADMINPASSWORD' WHERE login = 'admin'";
