25004d6235cd70c6feb5ce39c7a8be8a2939dda3
Pär Stolpe <par@stolpe.se>
2015-11-25 14:40:33 +0000

    Last time I do this in web-editor. Have to use something else.

--- python/nav/web/ldapauth.py
+++ python/nav/web/ldapauth.py
@@ -18,26 +18,6 @@
 Contains ldap authentication functionality for NAV web.
 """
 import sys
-import logging#
-# Copyright (C) 2004 Norwegian University of Science and Technology
-# Copyright (C) 2007, 2010, 2011, 2014 UNINETT AS
-#
-# This file is part of Network Administration Visualized (NAV).
-#
-# NAV is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License version 2 as published by
-# the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-# more details.  You should have received a copy of the GNU General Public
-# License along with NAV. If not, see <http://www.gnu.org/licenses/>.
-#
-"""
-Contains ldap authentication functionality for NAV web.
-"""
-import sys
 import logging
 import ConfigParser
 from os.path import join
