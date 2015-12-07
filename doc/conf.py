3c8f0ca4c597fc53435094e2dcd889982415c3bc
Morten Brekkevold <morten.brekkevold@uninett.no>
2015-12-07 10:16:06 +0100

    fix incorrect doc version for trunk

--- doc/conf.py
+++ doc/conf.py
@@ -47,7 +47,7 @@ copyright = u'2012-2015, UNINETT AS'
 # built documents.
 #
 # The short X.Y version.
-version = '4.3'
+version = 'dev'
 #version = '.'.join(buildconf.VERSION.split('.')[:2])
 # The full version, including alpha/beta/rc tags.
 release = buildconf.VERSION
