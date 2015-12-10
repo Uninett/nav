a120d501cb62da339bb5365f3ef1a124b211b413
Morten Brekkevold <morten.brekkevold@uninett.no>
2015-12-10 13:51:30 +0100

    rewrite mainly to allow specifying multiple event subjects.
    also add transaction handling, fix copyright statement and use constants for state values.

--- tools/eventgenerators/boxevent.py
+++ tools/eventgenerators/boxevent.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #
-# $Id$
-#
-# Copyright 2015 Norwegian University of Science and Technology
+# Copyright (C) 2015 UNINETT AS
 #
 # This file is part of Network Administration Visualized (NAV)
 #
@@ -27,6 +25,8 @@ import argparse
 
 from nav.models.event import EventQueue as Event, Subsystem, EventType
 from nav.models.manage import Netbox
+from django.db import transaction
+
 
 DEFAULT_KWARGS = {
     'source': Subsystem.objects.get(pk='pping'),
@@ -34,21 +34,25 @@ DEFAULT_KWARGS = {
     'event_type': EventType.objects.get(pk='boxState')
 }
 
+
+@transaction.atomic
 def main():
     """Main script controller"""
     args = create_parser().parse_args()
 
-    for netbox in Netbox.objects.filter(sysname__icontains=args.sysname):
-        send_event(netbox, args.event, send=args.dry_run)
+    for sysname in args.sysname:
+        for netbox in Netbox.objects.filter(sysname__icontains=sysname):
+            send_event(netbox, args.event, send=args.dry_run)
 
 
 def create_parser():
     """Create a parser for the script arguments"""
     parser = argparse.ArgumentParser(
         description='Script to simulate up/down events from pping')
-    parser.add_argument('sysname', help='Sysname used to filter netboxes')
     parser.add_argument('event', help='Type of event to simulate',
                         choices=['up', 'down'])
+    parser.add_argument('sysname', nargs='+',
+                        help='Sysname used to filter netboxes')
     parser.add_argument('--dry-run', action='store_false',
                         help='Print the events to be sent without sending them')
     return parser
@@ -58,7 +62,7 @@ def send_event(netbox, event_spec, send=True):
     """Send a boxstate event for a given netbox"""
     event = Event(**DEFAULT_KWARGS)
     event.netbox = netbox
-    event.state = 'e' if event_spec == 'up' else 's'
+    event.state = Event.STATE_END if event_spec == 'up' else Event.STATE_START
     print(event)
 
     if send:
