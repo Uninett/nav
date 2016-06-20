2764feacf55381c461c4fdb168019da3588e9a4c
John-Magne Bredal <john.m.bredal@uninett.no>
2016-06-20 15:35:48 +0200

    Try to fix selenium tests by waiting

--- tests/functional/navbar_test.py
+++ tests/functional/navbar_test.py
@@ -16,7 +16,9 @@
 """Selenium tests for simple searches on the navbar"""
 
 from nav.tests.selenium import SeleniumTest
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class NavBarSeleniumTest(SeleniumTest):
     """Testrunner for the Status page"""
@@ -34,5 +36,9 @@ class NavBarSeleniumTest(SeleniumTest):
         ipaddr = "192.168.42.42"
         self.query.send_keys(ipaddr)
         self.search_button.click()
-        caption = self.driver.find_element_by_tag_name('caption')
-        self.assertIn(ipaddr, caption.text)
+        try:
+            caption = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.TAG_NAME, "caption")))
+            self.assertIn(ipaddr, caption.text)
+        except TimeoutException:
+            pass
