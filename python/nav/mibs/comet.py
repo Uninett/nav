dd042e80fc7f9802d5de03c25bbef574ec5b7227
Morten Brekkevold <morten.brekkevold@uninett.no>
2016-06-23 14:12:03 +0200

    (Comet): also collect the binary inputs and their alarms from Comet probes

--- python/nav/mibs/comet.py
+++ python/nav/mibs/comet.py
@@ -22,6 +22,7 @@ implementation for multiple MIBs must be implemented.
 """
 
 from twisted.internet import defer
+from twisted.internet.defer import returnValue
 from nav.mibs.mibretriever import MibRetriever
 
 DEGREES_CELSIUS = "\xb0C"
@@ -41,8 +42,14 @@ class Comet(MibRetriever):
 
     @defer.inlineCallbacks
     def get_all_sensors(self):
+        channels = yield self.get_channels()
+        bin_inputs = yield self.get_binary_inputs()
+        returnValue(channels + bin_inputs)
+
+    @defer.inlineCallbacks
+    def get_channels(self):
         """Returns the temperature sensor channels for this probe."""
-        self._logger.warning("collecting Comet channels")
+        self._logger.info("collecting Comet channels")
         result = []
         for channel in range(1, 5):
             o_name = "ch%dName" % channel
@@ -53,7 +60,8 @@ class Comet(MibRetriever):
             name = yield self.get_next(o_name)
             unit = yield self.get_next(o_unit)
             unit = UNIT_MAP.get(unit, unit)
-            self._logger.debug("channel name/unit: %r/%r", name, unit)
+            self._logger.debug("channel %s name/unit: %r/%r",
+                               channel, name, unit)
 
             result.append(dict(
                 oid=str(value_oid) + '.0',
@@ -65,4 +73,41 @@ class Comet(MibRetriever):
                 internal_name="channel%s" % channel,
                 mib=self.get_module_name(),
             ))
-        defer.returnValue(result)
+        returnValue(result)
+
+    @defer.inlineCallbacks
+    def get_binary_inputs(self):
+        """Returns the binary inputs of this probe, also their alarm status"""
+        self._logger.info("collecting Comet binary sensors")
+        result = []
+        for binary in range(1, 4):
+            o_name = "bin%dName" % binary
+            o_value = "bin%dIntVal" % binary
+            o_alarm = "bin%dAlarm" % binary
+            value_oid = self.nodes[o_value].oid
+            alarm_oid = self.nodes[o_alarm].oid
+
+            name = yield self.get_next(o_name)
+            self._logger.debug("BIN input %s name: %r", binary, name)
+
+            result.append(dict(
+                oid=str(value_oid) + '.0',
+                unit_of_measurement="boolean",
+                precision=0,
+                scale=None,
+                description=name,
+                name="BIN %s" % binary,
+                internal_name="bin%s" % binary,
+                mib=self.get_module_name(),
+            ))
+            result.append(dict(
+                oid=str(alarm_oid) + '.0',
+                unit_of_measurement="boolean",
+                precision=0,
+                scale=None,
+                description="%s alarm" % name,
+                name="BIN %s Alarm" % binary,
+                internal_name="bin%sAlarm" % binary,
+                mib=self.get_module_name(),
+            ))
+        returnValue(result)
