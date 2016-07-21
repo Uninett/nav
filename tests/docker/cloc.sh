df2428fd55e3b1024565b1a1c21bf5d9c360bfb9
Morten Brekkevold <morten.brekkevold@uninett.no>
2016-07-21 15:09:30 +0200

    explicitly ignore generated css

--- tests/docker/cloc.sh
+++ tests/docker/cloc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 cd ${WORKSPACE}
 cloc \
-    --exclude-dir=.env,.idea,tmp,build,doc,htdocs/static/js/libs,htdocs/static/js/node_modules,htdocs/static/js/geomap/projdefs,python/nav/smidumps,python/nav/enterprise \
+    --exclude-dir=.env,.idea,tmp,build,doc,htdocs/static/css,htdocs/static/js/libs,htdocs/static/js/node_modules,htdocs/static/js/geomap/projdefs,python/nav/smidumps,python/nav/enterprise \
     --exclude-lang=make,m4,XML \
     --not-match-f='(configure|config.status)$' \
     --by-file \
