1f149290be9d808e0badee741e7028328b72c870
Emil Henry <emilhf@stud.ntnu.no>
2016-07-27 12:12:27 +0200

    Improved error messages in statist.js, more lax conventions on event/state naming, support passing event data

--- htdocs/static/js/libs/statist.js
+++ htdocs/static/js/libs/statist.js
@@ -1,9 +1,10 @@
 // Simple FSM shim. It simply maps states to state maps, which in turn map
-// events to the next state. The next state might also be a function, which
-// upon receiving the previous state returns the new state. The state map is
-// validated upon initialization, to ensure that we never enter an undefined
+// events to the next state. The next state might also be a function, which is
+// called (and passed any event data) and returns the next state. The state map
+// is validated upon initialization, to ensure that we never enter an undefined
 // or invalid state. It also supports listening to when a a state is entered
 // ('.on', '.once'), usually for asynchronous operations like global resets.
+// These listeners are passed any event data leading into the desired state.
 //
 // The purpose of this shim is to make sure somewhat more complex components
 // (typically views) always have a well-defined, verified state, which
@@ -30,16 +31,16 @@ define(function (require, exports, module) {
   var _ = require("libs/underscore");
 
   var FSM = function(stateMap) {
-    var defaultMap = { init: {} };
+    var defaultMap = { INIT: {} };
     // Maps states to event handlers (state maps), which map events to new states
     this.fsm = Object.assign(defaultMap, stateMap);
-    // By convention, "init" is the default state and always defined
-    this.state = "init";
+    // By convention, "INIT" is the default state and always defined
+    this.state = "INIT";
     // List of all possible actions (enum-like) for each state
     this.events = _.reduce(this.fsm, function(memo, stateMap, state) {
       var actions = _.keys(stateMap);
-      memo[state.toUpperCase()] = _.reduce(actions, function(enums, action) {
-        enums[action.toUpperCase()] = action;
+      memo[state] = _.reduce(actions, function(enums, action) {
+        enums[action] = action;
         return enums;
       }, {});
       return memo;
@@ -47,7 +48,7 @@ define(function (require, exports, module) {
     // Friendly list of all possible states (enum-like), probably useful
     var states = _.keys(this.fsm);
     this.states = _.reduce(states, function(memo, state) {
-      memo[state.toUpperCase()] = state;
+      memo[state] = state;
       return memo;
     }, {});
     // Initialize listeners
@@ -65,18 +66,21 @@ define(function (require, exports, module) {
 
   // Validate the whole stateMap to ensure we are able to reach all possible
   // states.
-  var noStateTemplate = _.template("FSM error: Trying to move from '<%= currentState %>' to state '<%= state %>', which is not defined");
+  var noStateTemplate = _.template("FSM error: Trying to move from '<%= currentState %>' to '<%= state %>' (via <%= via %>), which is not defined");
   FSM.prototype.validate = function() {
     var self = this;
     _.each(self.fsm, function(stateMap, state) {
-      var nextStates = _.values(stateMap);
-      _.each(nextStates, function(nextState) {
+      _.each(stateMap, function(nextState, action) {
         // Cannot validate functions just now
         if (_.isFunction(nextState)) {
           return;
         }
         if (!_.has(self.fsm, nextState)) {
-          var s = noStateTemplate({ state: nextState, currentState: self.state });
+          var s = noStateTemplate({
+            state: nextState,
+            currentState: state,
+            via: "event '" + action + "'"
+          });
           throw new Error(s);
         }
       });
@@ -84,47 +88,48 @@ define(function (require, exports, module) {
   };
 
   // Notify all listeners of the current state
-  FSM.prototype.notifyAll = function() {
+  FSM.prototype.notifyAll = function(eventData) {
     var self = this;
     var state = this.state;
-    // Return false if we want to remove the listener
+    // Return true if we want to remove the listener
     this.listeners[state] = _.reject(self.listeners[state], function(callbackMap) {
-      callbackMap.fn();
+      callbackMap.fn(eventData);
       return callbackMap.type === "once";
     });
   };
 
   // Update current state to nextState and notify listeners
-  FSM.prototype.setState = function(nextState) {
+  FSM.prototype.setState = function(nextState, eventData) {
     var self = this;
     if (_.isFunction(nextState)) {
-      nextState = nextState(this.state);
+      nextState = nextState(eventData);
     }
     if (!_.has(self.fsm, nextState)) {
-      var s = noStateTemplate({ state: nextState, currentState: self.state });
+      var s = noStateTemplate({ state: nextState, currentState: self.state, via: "setState()" });
       throw new Error(s);
     }
     this.state = nextState;
-    this.notifyAll();
+    this.notifyAll(eventData);
     return nextState;
   };
 
   // Make the FSM react to some event
-  FSM.prototype.step = function(event) {
+  FSM.prototype.step = function(event, eventData) {
     var currentStateMap = this.fsm[this.state];
     // Ignore events for which no next state is defined
     if (!_.has(currentStateMap, event)) {
+      console.log("Warning: Couldn't catch event", event, "looping...");
       return this.currentState;
     }
     var nextState = currentStateMap[event];
-    return this.setState(nextState);
+    return this.setState(nextState, eventData);
   };
 
   // Utility function to register functions
   FSM.prototype.__addListener = function(desiredState, callbackMap) {
     var self = this;
     if (!_.has(self.listeners, desiredState)) {
-      console.log("Warning: State", desiredState, "is not defined. Ignoring.");
+      console.log("Warning: Trying to listen to state", desiredState, ", which is undefined. Ignoring.");
       return;
     }
     this.listeners[desiredState].push(callbackMap);
