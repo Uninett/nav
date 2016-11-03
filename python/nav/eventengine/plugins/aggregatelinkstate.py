37a415dae412f4278364c6e08a6ba873e363696e
Morten Brekkevold <morten.brekkevold@uninett.no>
2016-11-03 11:37:38 +0100

    (AggregateLinkStateHandler): treat duplicate events as further degradation, by posting alert messages without altering the state

--- python/nav/eventengine/plugins/aggregatelinkstate.py
+++ python/nav/eventengine/plugins/aggregatelinkstate.py
@@ -26,16 +26,18 @@ class AggregateLinkStateHandler(EventHandler):
         event = self.event
         alert = AlertGenerator(event)
         interface = event.get_subject()
+        is_unresolved = unresolved.refers_to_unresolved_alert(event)
 
         if event.state == event.STATE_START:
             alert.alert_type = 'linkDegraded'
+            if is_unresolved:
+                return self._handle_duplicate(alert, is_unresolved)
             if not interface.is_degraded():
                 return self._ignore("Got aggregateLinkState start event, but "
                                     "the interface is not currently degraded.")
 
         elif event.state == event.STATE_END:
             alert.alert_type = 'linkRestored'
-            is_unresolved = unresolved.refers_to_unresolved_alert(event)
             if not is_unresolved:
                 return self._ignore("Got aggregateLinkState end event, but "
                                     "there is no currently active alert to "
@@ -50,6 +52,16 @@ class AggregateLinkStateHandler(EventHandler):
             alert.post()
 
         event.delete()
+
+    def _handle_duplicate(self, alert, existing_alert):
+        interface = self.event.get_subject()
+        self._logger.info("%s: Got another linkDegraded event, interface is "
+                          "possibly degraded further", interface)
+        self._logger.debug("%s: The unresolved AlertHist entry is %r",
+                           interface, vars(existing_alert))
+        # Post just an alertq entry, but don't touch alerthist
+        alert.post_alert(history=existing_alert)
+        self.event.delete()
 
     def _ignore(self, msg):
         interface = self.event.get_subject()
