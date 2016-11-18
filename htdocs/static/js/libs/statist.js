333640cabad2b4df352dea75524df2854f785d56
Emil Henry <emilhf@stud.ntnu.no>
2016-11-18 08:55:37 +0100

    Add copyright notices

--- htdocs/static/js/libs/statist.js
+++ htdocs/static/js/libs/statist.js
@@ -1,3 +1,20 @@
+//
+// Copyright (C) 2016 UNINETT AS
+//
+// This file is part of Network Administration Visualized (NAV).
+//
+// NAV is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License version 2 as published by
+// the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.  You should have received a copy of the GNU General Public
+// License along with NAV. If not, see <http://www.gnu.org/licenses/>.
+//
+// == STATIST, A SIMPLE FSM LIBRARY
+//
 // Simple FSM shim. It simply maps states to state maps, which in turn map
 // events to the next state. The next state might also be a function, which is
 // called (and passed any event data) and returns the next state. The state map
