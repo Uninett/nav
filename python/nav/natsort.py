4150e6b770787f603dfec86a55562d74d5d4b000
Sigmund Augdal <sigmund.augdal@uninett.no>
2017-01-27 12:09:45 +0100

    Remove python 2.4 support in natsort

--- python/nav/natsort.py
+++ python/nav/natsort.py
@@ -18,21 +18,11 @@
 Natural sorting of strings implies that e.g. the string 'foo20bar' will sort
 after 'foo3bar' instead of before.
 
-An example of how to naturally sort a directory listing (case insensitive):
+An example of how to naturally sort a directory listing:
 
   import os, natsort
   foo = os.listdir('/path/to/bar')
-  foo.sort(natsort.inatcmp)
-
-Note that this example is highly inefficient, as the inatcmp function has to be
-called at least O(n log n) times.  If you are running Python 2.4 or higher, the
-more effective way to sort the list foo would be:
-
   foo.sort(key=natsort.split)
-
-This will pre-generate the list of sorting keys.  If you do not have Python 2.4
-or higher, you should use the decorate-sort-undecorate pattern to achieve
-maximum efficiency.
 """
 import re
 
@@ -48,28 +38,3 @@ def split(string):
             return n
 
     return [intcast(x) for x in _split_pattern.findall(string)]
-
-
-def natcmp(a, b):
-    """Replacement for cmp, performing natural comparison between a
-    and b."""
-    return cmp(split(a), split(b))
-
-
-def inatcmp(a, b):
-    """Case insensitive version of natcmp."""
-    return natcmp(a.lower(), b.lower())
-
-
-def decorated_sort(l):
-    """
-    Sort the list of strings l naturally, using the decorate-sort-undecorate
-    pattern.  NB: Does not sort l in-place, but returns a new list with the
-    elements of l sorted naturally.
-
-    If using Python 2.4 or newer, use l.sort(key=natsort.split) instead.
-    """
-    deco = [(split(element), i, element) for i, element in enumerate(l)]
-    deco.sort()
-    new_list = [element for _, _, element in deco]
-    return new_list
