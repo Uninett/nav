febc0bce87d58102d9805137eabcbf1745d6810e
Sigmund Augdal <sigmund.augdal@uninett.no>
2017-01-27 12:14:46 +0100

    Use json library rather than hand-crafting json strings

--- python/nav/web/geomap/output_formats.py
+++ python/nav/web/geomap/output_formats.py
@@ -22,7 +22,7 @@ http://code.google.com/apis/kml/
 
 """
 
-
+import json
 from django.template.loader import render_to_string
 
 from nav.web.geomap.utils import (
@@ -42,7 +42,7 @@ def make_geojson(featurelist):
     """
     geojson = {'type': 'FeatureCollection',
                'features': map(make_geojson_feature, featurelist)}
-    return write_json(geojson)
+    return json.dumps(geojson)
 
 
 def make_geojson_feature(feature):
@@ -64,45 +64,6 @@ def make_geojson_feature(feature):
                             'size': feature.size,
                             'popup': popup},
                            feature.properties)}
-
-
-def write_json(obj):
-    """Convert an object to a JSON string.
-
-    Dictionaries, lists, strings, numbers, booleans and None are
-    recognized and represented as JSON values in the obvious way. The
-    occurence of objects of any other type in obj is an error.
-
-    This function is only needed in pre-2.6 versions of Python. From
-    version 2.6, it may be replaced by the standard library's
-    json.dumps.
-
-    """
-    json_escapes = [('\\', '\\\\'),
-                    ('"', '\\"'),
-                    ('\n', '\\n'),
-                    ('\r', '\\r'),
-                    ('\t', '\\t'),
-                    ]
-
-    if isinstance(obj, list):
-        return '[' + ', '.join(map(write_json, obj)) + ']'
-    if isinstance(obj, dict) or isinstance(obj, lazy_dict):
-        return '{' + ', '.join(map(lambda kv: kv[0]+':'+kv[1],
-                                   zip(map(write_json, obj.keys()),
-                                       map(write_json, obj.values())))) + '}'
-    if isinstance(obj, bool):
-        if obj:
-            return 'true'
-        return 'false'
-    if isinstance(obj, basestring):
-        return '"%s"' % reduce(lambda s, esc: s.replace(esc[0], esc[1]),
-                               json_escapes, obj)
-    if numeric(obj):
-        return str(obj)
-    if obj is None:
-        return 'null'
-    return '"ERROR: unrecognized type ' + str(type(obj)) + '"'
 
 # KML
 
