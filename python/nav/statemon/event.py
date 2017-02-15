3aff810683647719836c70d513fef409fd8b30ba
Hanne Moa <hanne.moa@uninett.no>
2017-02-15 09:18:03 +0100

    Get rid of CVS-cruft

--- python/nav/statemon/event.py
+++ python/nav/statemon/event.py
@@ -17,8 +17,6 @@
 # along with NAV; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-#
-# $Id: $
 # Authors: Magnus Nordseth <magnun@itea.ntnu.no>
 #
 
--- python/nav/statemon/service.py
+++ python/nav/statemon/service.py
@@ -18,7 +18,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 #
-# $Id: $
 # Authors: Magnus Nordseth <magnun@itea.ntnu.no>
 #          Erik Gorset     <erikgors@itea.ntnu.no>
 #
