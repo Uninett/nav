715a5120d62c74774c1f57c891ba795d4bdc194e
Morten Brekkevold <morten.brekkevold@uninett.no>
2017-02-22 15:38:52 +0100

    (GatewayPeerSession): rework and post events on significant state changes

--- python/nav/ipdevpoll/shadows/gwpeers.py
+++ python/nav/ipdevpoll/shadows/gwpeers.py
@@ -17,9 +17,19 @@
 
 from __future__ import absolute_import
 
+from functools import partial
+
+from django.db import transaction
+
+from nav.event2 import EventFactory
 from nav.ipdevpoll.storage import Shadow, DefaultManager
 from nav.models import manage
 from .netbox import Netbox
+
+
+EVENT = EventFactory(source='ipdevpoll', target='eventEngine',
+                     event_type='bgpState',
+                     start_type='bgpDown', end_type='bgpEstablished')
 
 
 class GatewayPeerSessionManager(DefaultManager):
@@ -70,14 +80,88 @@ class GatewayPeerSession(Shadow):
     manager = GatewayPeerSessionManager
 
     def save(self, containers):
-        model = self.get_existing_model(containers)
-        if model:
-            proto = self._protocol_map.get(self.protocol, None)
-            if model.state != self.state:
-                self._logger.info("%s STATE CHANGE DETECTED: %s -> %s",
-                                  proto, model.state, self.state)
-            if model.adminstatus != self.adminstatus:
-                self._logger.info("%s ADMINSTATUS CHANGE DETECTED: %s -> %s",
-                                  proto, model.adminstatus, self.adminstatus)
-
+        self._verify_state_changes(containers)
         return super(GatewayPeerSession, self).save(containers)
+
+    def _verify_state_changes(self, containers):
+        """
+        Verifies potential state changes and posts a single alert if the
+        state change(s) warrants it.
+
+        If admin status has changed, primarily an event about that. If not,
+        post events if session state switches between 'established' and
+        non-established states.
+
+        """
+        self._log_all_state_changes(containers)
+        model = self.get_existing_model(containers)
+        if not model:
+            return
+
+        if model.adminstatus != self.adminstatus:
+            new_alert = self.adminstatus == 'stop'
+            self._make_bgpstate_event(start=new_alert, is_adminstatus=True)
+            return
+
+        states = set((model.state, self.state))
+        if len(states) > 1 and 'established' in states:
+            new_alert = self.state != 'established'
+            self._make_bgpstate_event(start=new_alert)
+
+    def _log_all_state_changes(self, containers):
+        model = self.get_existing_model(containers)
+        if not model:
+            return
+        proto = self._protocol_map.get(self.protocol, None)
+        peerid = self._get_peer_id()
+
+        if model.adminstatus != self.adminstatus:
+            self._logger.debug("%s %s adminstatus change detected: %s -> %s",
+                               proto, peerid, model.adminstatus,
+                               self.adminstatus)
+        if model.state != self.state:
+            self._logger.debug("%s %s state change detected: %s -> %s",
+                               proto, peerid, model.state, self.state)
+
+    @transaction.atomic
+    def _make_bgpstate_event(self, start=True, is_adminstatus=False):
+        model = self.get_existing_model()
+        peername = self._get_peer_name() or str(model.peer)
+        peerid = self._get_peer_id()
+        varmap = {
+            'peer': str(model.peer),
+            'peername': peername,
+            'state': self.state,
+            'adminstatus': self.adminstatus,
+        }
+        event = EVENT.start if start else EVENT.end
+        if start and is_adminstatus:
+            event = partial(event, alert_type='bgpAdmDown')
+        event = event(netbox=self.netbox.id, subid=model.id, varmap=varmap)
+
+        proto = self._protocol_map.get(self.protocol, None)
+        self._logger.info("dispatching event (%s) for %s %s state change"
+                          " from %s to %s",
+                          event.varmap['alerttype'], proto, peerid,
+                          model.state, self.state)
+        event.save()
+
+    def _get_peer_name(self):
+        if not self.peer:
+            return
+
+        if not getattr(self, '_peername', None):
+            try:
+                peers = manage.Netbox.objects.filter(ip=self.peer)
+                self._peername = peers.values_list('sysname', flat=1)[0]
+            except (IndexError, AttributeError):
+                self._peername = None
+
+        return self._peername
+
+    def _get_peer_id(self):
+        peername = self._get_peer_name()
+        if peername:
+            return "{} ({})".format(self.peer, peername)
+        else:
+            return self.peer
