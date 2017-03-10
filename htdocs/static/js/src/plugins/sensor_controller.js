159d65f3fcd69a3bc09480e0e84a1532f73dbea3
Sigmund Augdal <sigmund.augdal@uninett.no>
2017-03-10 12:52:24 +0100

    Fix typo in comment

--- htdocs/static/js/src/plugins/sensor_controller.js
+++ htdocs/static/js/src/plugins/sensor_controller.js
@@ -78,7 +78,7 @@ define(function(require) {
         parseThresholds: function () {
             var input = this.$node.attr('data-thresholds');
             if (input) {
-                /* TOOD: Actually care about the bigger than, smaller than issue */
+                /* TODO: Actually care about the bigger than, smaller than issue */
                 var values = input.split(',');
                 for (var i = 0, j = values.length; i < j; i++) {
                     values[i] = parseFloat(values[i].replace(/\D/g, ''));
