c49c2e7ff982b892dac99adb4fbbef1e67166aad
Morten Brekkevold <morten.brekkevold@uninett.no>
2017-03-15 08:52:00 +0100

    rework Rack models to use JSON serialization for Rack configuration.
    This work is incomplete, and not all functions in the UI work as
    expected yet.

--- python/nav/models/rack.py
+++ python/nav/models/rack.py
@@ -15,6 +15,8 @@
 # License along with NAV. If not, see <http://www.gnu.org/licenses/>.
 #
 """Models for racks and rack items"""
+import json
+from itertools import chain
 
 from django.db import models
 
@@ -22,42 +24,97 @@ from nav.models.fields import VarcharField
 from nav.models.manage import Room, Sensor
 
 
+class RackManager(models.Manager):
+    def get_all_sensor_pks_in_room(self, room):
+        """Returns an exhaustive list of the primary keys of sensors added to
+        all racks in the given room.
+
+        :type room: nav.models.manage.Room
+
+        """
+        sensor_pks = (rack.get_all_sensor_pks()
+                      for rack in self.filter(room=room))
+        return set(*chain(sensor_pks))
+
+
 class Rack(models.Model):
     """A physical rack placed in a room."""
+
+    objects = RackManager()
 
     id = models.AutoField(primary_key=True, db_column='rackid')
     room = models.ForeignKey(Room, db_column='roomid')
     rackname = VarcharField(blank=True)
     ordering = models.IntegerField()
+    _configuration = VarcharField(default='{}', db_column='configuration')
+    __configuration = None
 
     class Meta(object):
         db_table = 'rack'
 
     def __unicode__(self):
-        return self.rackname or self.rackid
+        return "Rack %r in %r" % (self.rackname or self.id, self.room)
 
-    def sensors_left(self):
-        return RackSensor.objects.filter(rack=self, col=0).order_by('row')
+    @property
+    def configuration(self):
+        if self.__configuration is None:
+            self.__configuration = json.loads(self._configuration,
+                                              object_hook=rack_decoder)
+            if self.__configuration is None:
+                self.__configuration = {}
+        return self.__configuration
 
-    def sensors_center(self):
-        return RackSensor.objects.filter(rack=self, col=1).order_by('row')
+    def save(self, *args, **kwargs):
+        self._configuration = json.dumps(self.__configuration, cls=RackEncoder)
+        return super(Rack, self).save(*args, **kwargs)
 
-    def sensors_right(self):
-        return RackSensor.objects.filter(rack=self, col=2).order_by('row')
+    def add_left_item(self, item):
+        """
+        :type item: RackItem
+        """
+        self.configuration.setdefault('left', []).append(item)
+
+    def add_center_item(self, item):
+        """
+        :type item: RackItem
+        """
+        self.configuration.setdefault('center', []).append(item)
+
+    def add_right_item(self, item):
+        """
+        :type item: RackItem
+        """
+        self.configuration.setdefault('right', []).append(item)
+
+    def get_all_sensor_pks(self):
+        """Returns an exhaustive list of the primary keys of sensors in this
+        rack
+        """
+        return []
 
 
-class RackSensor(models.Model):
-    """A sensor placed in a rack"""
+def rack_decoder(obj):
+    if '__type__' in obj:
+        if obj['__type__'] == 'RackItem':
+            return RackItem(obj['sensor'])
+    return obj
 
-    id = models.AutoField(primary_key=True, db_column='racksensorid')
-    rack = models.ForeignKey(Rack, db_column='rackid')
-    sensor = models.ForeignKey(Sensor, db_column='sensorid')
-    col = models.IntegerField()
-    row = models.IntegerField()
-    sensortype = VarcharField(blank=True)
 
-    class Meta(object):
-        db_table = 'racksensor'
+class RackEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, RackItem):
+            return {'__type__': 'RackItem',
+                    'sensor': getattr(obj.sensor, 'pk', obj.sensor)
+                    }
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
 
-    def __unicode__(self):
-        return "Sensor {} in rack {}".format(self.sensor, self.rack)
+
+class RackItem(object):
+    def __init__(self, sensor=None):
+        self.sensor = sensor
+        if isinstance(sensor, int):
+            try:
+                self.sensor = Sensor.objects.get(pk=sensor)
+            except Sensor.DoesNotExist:
+                pass
--- sql/changes/sc.04.07.0060.sql
+++ sql/changes/sc.04.07.0060.sql
@@ -1,17 +1,9 @@
--- Definition of a rack and racksensor to display in the rack
+-- Definition of a rack with environment sensors
 
 CREATE TABLE IF NOT EXISTS rack (
   rackid SERIAL PRIMARY KEY,
   roomid VARCHAR REFERENCES room ON DELETE CASCADE,
   rackname VARCHAR,
-  ordering INTEGER
-);
-
-CREATE TABLE IF NOT EXISTS racksensor (
-  racksensorid SERIAL PRIMARY KEY,
-  rackid INTEGER REFERENCES rack ON DELETE CASCADE,
-  sensorid INTEGER REFERENCES sensor ON DELETE CASCADE,
-  col INTEGER NOT NULL,
-  row INTEGER NOT NULL,
-  sensortype VARCHAR
+  ordering INTEGER,
+  configuration JSONB DEFAULT NULL
 );
