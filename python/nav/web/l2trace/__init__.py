389a972dffd2ef97934bd1dfa8ba5a1dcbe2bae0
Morten Brekkevold <morten.brekkevold@uninett.no>
2017-03-16 09:18:14 +0100

    (get_vlan_gateway): ensure we only consider GSW and GW as router ports!

--- python/nav/web/l2trace/__init__.py
+++ python/nav/web/l2trace/__init__.py
@@ -232,6 +232,7 @@ def get_vlan_gateway(vlan):
     gwport_prefixes = GwPortPrefix.objects.filter(
         prefix__vlan=vlan).order_by('prefix__net_address')
     gateways = Netbox.objects.filter(
+        category__in=('GW', 'GSW'),
         interface__gwportprefix__in=gwport_prefixes).distinct()
     if gateways:
         return gateways[0]
