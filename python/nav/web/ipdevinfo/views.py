3b0a6f9b61ea96d87c907a7b2129da0c28d530f3
Sigmund Augdal <sigmund.augdal@uninett.no>
2017-03-16 13:06:13 +0100

    Fix graph and threshold view on sensors page

--- python/nav/web/ipdevinfo/views.py
+++ python/nav/web/ipdevinfo/views.py
@@ -288,12 +288,16 @@ def ipdev_details(request, name=None, addr=None, netbox_id=None):
         except GraphiteUnreachableError:
             graphite_error = True
 
-    sensor_metric_ids = {sensor.get_metric_name(): sensor for sensor
-                         in netbox.sensor_set.all()}
-    sensor_metrics = {}
-    for metric in system_metrics:
-        if metric['id'] in sensor_metric_ids:
-            sensor_metrics[sensor_metric_ids[metric['id']].id] = metric
+    sensor_metrics = []
+    for sensor in netbox.sensor_set.all():
+        metric = {
+            'id': sensor.get_metric_name(),
+            'sensor': sensor,
+            'graphite_data_url': Graph(
+                magic_targets=[metric['id']], format='json'),
+        }
+        sensor_metrics.append(metric)
+    find_rules(sensor_metrics)
     # Display info about current and scheduled maintenance tasks
     # related to this device
     current_tasks = MaintenanceTask.objects.current()
