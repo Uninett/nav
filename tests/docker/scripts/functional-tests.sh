bdba5386e599f28c34e44ab026bcdde098ee8825
Morten Brekkevold <morten.brekkevold@uninett.no>
2017-06-12 14:54:57 +0200

    be explicit about the geckodriver's location

--- tests/docker/scripts/functional-tests.sh
+++ tests/docker/scripts/functional-tests.sh
@@ -8,6 +8,7 @@ export TARGETURL=http://$TARGETHOST:$APACHE_PORT/
 py.test --junitxml=functional-results.xml \
         --verbose \
         --driver Firefox \
+        --driver-path=/usr/local/bin/geckodriver \
         --base-url "$TARGETURL" \
         --sensitive-url "nothing to see here" \
         --html functional-report.html --self-contained-html \
