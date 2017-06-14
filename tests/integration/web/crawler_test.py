44df47f3b7638fca89565235476824ddf493d5ea
Morten Brekkevold <morten.brekkevold@uninett.no>
2017-06-14 13:12:28 +0200

    Rewrite webcrawler tests, as pylint has deprecated yield tests

--- tests/integration/web/crawler_test.py
+++ tests/integration/web/crawler_test.py
@@ -1,15 +1,21 @@
-#!/usr/bin/env python
-"""Crawls test instance of NAV and report any errors.
+"""Webcrawler tests for reachability and HTML validation.
 
-The basic crawler code enumerates all pages paths that can be reached by GET
-queries without parameters while logged in as an administrator. HTML for all
-pages that report Content-type html are stored an validated by tidy in an
-additional test.
+The crawler attempts to retrieve any NAV web UI page that can be reached with
+parameterless GET requests, while logged in as an administrator.
+
+We want one test for each such URL, but since generating more tests while
+running existing tests isn't easily supported under pytest (yield tests are
+becoming deprecated under pytest 4), the crawler is the de-facto reachability
+tester. A dummy test will be generated for each seen URL, and the dummy tests
+will assert that the response code of the URL was 200 OK.
+
+In addition, HTML validation tests (using libtidy) will be generated for all
+URLs that report a Content-Type of text/html.
 
 """
-
 from __future__ import print_function
 
+from collections import namedtuple
 from lxml.html import fromstring
 import os
 import pytest
@@ -17,12 +23,11 @@ from tidylib import tidy_document
 import urllib
 import urllib2
 import urlparse
-import time
-from itertools import chain
 
 HOST_URL = os.environ.get('TARGETURL', None)
 USERNAME = os.environ.get('ADMINUSERNAME', 'admin')
 PASSWORD = os.environ.get('ADMINPASSWORD', 'admin')
+TIMEOUT = 90  # seconds?
 
 TIDY_OPTIONS = {
     'doctype': 'auto',
@@ -49,108 +54,156 @@ if not HOST_URL:
     pytest.skip(msg="Missing environment variable TARGETURL "
                     "(ADMINUSERNAME, ADMINPASSWORD) , skipping crawler tests!")
 
-TIMEOUT = 90
-HOST = urlparse.urlsplit(HOST_URL).hostname
 
-seen_paths = {}
-html_store = {}
-queue = [HOST_URL]
+#
+# Web Crawler code and related utility functions
+#
 
-def test_webpages():
-    result = login()
-    if result:
-        func, args = result[0], result[1:]
-        func(*args)
-    register_seen(HOST_URL)
-    while queue:
-        url = queue.pop()
-        yield ('is %s reachable' % url2path(url),) + check_response(url)
-
-    for url in html_store.keys():
-        yield "does %s validate" % url2path(url), check_validates, url
+Page = namedtuple('Page', 'url response content_type content')
 
 
-def url2path(url):
-    """Strips everything but the path, query and fragment of a full URL"""
-    uri = urlparse.urlsplit(url)
-    uri = urlparse.SplitResult('', '', uri.path, uri.query, uri.fragment)
-    uri = urlparse.urlunsplit(uri)
-    return uri
-
-
-def handle_http_error(func):
-    def _decorator(*args, **kwargs):
-        starttime = time.time()
-        funcinfo = "%s(%s)" % (func.__name__,
-                               ", ".join(map(repr,
-                                             chain(args, kwargs.values()))))
-        fmt = "%s - %s used %s seconds"
-        try:
-            return func(*args, **kwargs)
-        except urllib2.HTTPError, error:
-            print(fmt % ('HttpError', funcinfo, time.time() - starttime))
-            print("%s :" % error.url)
-            print("-" * (len(error.url)+2))
-            return failure, error.url, error.code, error
-        except urllib2.URLError, error:
-            print(fmt % ('URLError', funcinfo, time.time() - starttime))
-            return urlerror, error
-
-    return _decorator
-
-def failure(url, code, error):
-    print(error.fp.read())
-    assert code == 200
-
-def success(url):
-    assert True
-
-def urlerror(error):
-    raise error
-
-@handle_http_error
-def login():
-    login_url = '%sindex/login/' % HOST_URL
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-    data = urllib.urlencode({'username': USERNAME, 'password': PASSWORD})
-    opener.open(login_url, data, TIMEOUT)
-    urllib2.install_opener(opener)
-    return success, login_url
-
-def get_path(url):
+def normalize_path(url):
     return urlparse.urlsplit(url).path.rstrip('/')
 
-def is_html(resp):
-    return 'html' in resp.info()['Content-type'].lower()
+
+class WebCrawler(object):
+    blacklist = set(normalize_path(path) for path in BLACKLISTED_PATHS)
+
+    def __init__(self, base_url, username, password):
+        self.base_url = base_url
+        self.netloc = urlparse.urlsplit(base_url).netloc
+        self.username = username
+        self.password = password
+        self.seen_pages = {}
+        self.queue = []
+
+    def crawl(self):
+        if self.seen_pages:
+            for page in self.seen_pages.values():
+                yield page
+            return
+
+        self.login()
+        self.queue.append(self.base_url)
+
+        while self.queue:
+            url = self.queue.pop()
+            page = self._visit_with_error_handling(url)
+            if page:
+                yield page
+
+    def _visit_with_error_handling(self, url):
+        try:
+            page = self._visit(url)
+        except urllib2.HTTPError as error:
+            content = error.fp.read()
+            page = Page(url, error.code, error, content)
+            self._add_seen(page)
+
+        except urllib2.URLError as error:
+            page = Page(url, None, error, None)
+            self._add_seen(page)
+
+        return page
+
+    def _visit(self, url):
+        if self._is_seen(url):
+            return
+
+        resp = urllib2.urlopen(url, timeout=TIMEOUT)
+        content_type = resp.info()['Content-type']
+
+        if 'html' in content_type.lower():
+            content = resp.read()
+            self._queue_links_from(content, url)
+        else:
+            content = None
+
+        page = Page(url, resp.getcode(), content_type, content)
+        self._add_seen(page)
+        return page
+
+    def _queue_links_from(self, content, base_url):
+        html = fromstring(content)
+        html.make_links_absolute(base_url)
+
+        for element, attribute, link, pos in html.iterlinks():
+            url = urlparse.urlsplit(link)
+            path = normalize_path(link)
+
+            if url.scheme not in ['http', 'https']:
+                continue
+            elif url.netloc != self.netloc:
+                continue
+            elif element.tag in ('form', 'object') or attribute == 'style':
+                continue
+            elif self._is_blacklisted(path):
+                continue
+            elif not self._is_seen(path):
+                self.queue.append('%s://%s%s' % (url.scheme, url.netloc, url.path))
+
+    def login(self):
+        login_url = '%sindex/login/' % self.base_url
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+        data = urllib.urlencode({'username': self.username,
+                                 'password': self.password})
+        opener.open(login_url, data, TIMEOUT)
+        urllib2.install_opener(opener)
+
+    def _add_seen(self, page, url=None):
+        if not url:
+            url = page.url
+        url = normalize_path(url)
+        self.seen_pages[url] = page
+
+    def _is_seen(self, url):
+        return normalize_path(url) in self.seen_pages
+
+    def _is_blacklisted(self, url):
+        return normalize_path(url) in self.blacklist
+
+
+#
+# test functions
+#
+
+crawler = WebCrawler(HOST_URL, USERNAME, PASSWORD)
+
+
+def page_id(page):
+    """Extracts a URL as a test id from a page"""
+    return normalize_path(page.url)
+
+
+@pytest.mark.parametrize("page", crawler.crawl(), ids=page_id)
+def test_link_should_be_reachable(page):
+    assert page.response == 200, page.content
+
+
+@pytest.mark.parametrize("page", crawler.crawl(), ids=page_id)
+def test_page_should_be_valid_html(page):
+    if page.response != 200:
+        pytest.skip("not validating non-reachable page")
+    if not page.content_type or 'html' not in page.content_type.lower():
+        pytest.skip("not attempting to validate non-html page")
+    if not should_validate(page.url):
+        pytest.skip("skip validation of blacklisted page")
+    if not page.content:
+        pytest.skip("page has no content")
+
+    document, errors = tidy_document(page.content, TIDY_OPTIONS)
+    errors = filter_errors(errors)
+
+    assert not errors, "Found following validation errors:\n" + errors
+
 
 def should_validate(url):
-    path = get_path(url)
+    path = normalize_path(url)
     for blacklisted_path in TIDY_BLACKLIST:
         if path.startswith(blacklisted_path):
             return False
     return True
 
-def retrieve_links(current_url):
-    root = fromstring(html_store[current_url])
-    root.make_links_absolute(current_url)
-
-    for element, attribute, link, pos in root.iterlinks():
-        url = urlparse.urlsplit(link)
-        path = get_path(link)
-
-        if url.scheme not in ['http', 'https']:
-            continue
-        elif url.hostname != HOST:
-            continue
-        elif element.tag in ('form', 'object') or attribute == 'style':
-            continue
-        elif path in BLACKLISTED_PATHS:
-            continue
-        elif not has_been_seen(path):
-            queue.append('%s://%s%s' % (url.scheme, url.netloc, url.path))
-
-
-        register_seen(path, current_url)
 
 def filter_errors(errors):
     if errors:
@@ -164,32 +217,3 @@ def _should_ignore(msg):
             return True
     return False
 
-
-@handle_http_error
-def check_response(current_url):
-    resp = urllib2.urlopen(current_url, timeout=TIMEOUT)
-
-    if is_html(resp):
-        html_store[current_url] = resp.read()
-        retrieve_links(current_url)
-    return success, current_url
-
-def has_been_seen(url):
-    return get_path(url) in seen_paths
-
-def register_seen(seen_url, source_url=None):
-    seen_url = get_path(seen_url)
-    if seen_url not in seen_paths:
-        seen_paths[seen_url] = []
-    if source_url:
-        seen_paths[seen_url].append(source_url)
-
-
-def check_validates(url):
-    if not should_validate(url):
-        return
-
-    document, errors = tidy_document(html_store[url], TIDY_OPTIONS)
-    errors = filter_errors(errors)
-
-    assert not errors, "Found following validation errors:\n" + errors
