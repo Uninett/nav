2b5f1985eac82e5b3372dee702940e53716d0965
Morten Brekkevold <morten.brekkevold@uninett.no>
2017-07-24 13:18:32 +0200

    refactor from optparse to argparse
    w/additional refactors for cleanliness...

--- bin/mailin.py
+++ bin/mailin.py
@@ -22,14 +22,91 @@ import re
 import sys
 import email
 import ConfigParser
-import logging
-import traceback
-from optparse import OptionParser
+import argparse
+
 import nav
 import nav.mailin
+from nav import logs
 
-logger = None
+import logging
+logging.raiseExceptions = False
+logger = logging.getLogger('nav.mailin')
 conf = None
+
+
+def main():
+    global conf
+
+    args = parse_args()
+
+    configfile = nav.buildconf.sysconfdir + '/mailin.conf'
+    logfile = nav.buildconf.localstatedir + '/log/mailin.log'
+
+    # Todo: fail if config file is not found
+    conf = ConfigParser.ConfigParser()
+    conf.read(configfile)
+
+    # Must do this after config, so logfile can be configurable
+    if args.test:
+        init_logging('-')
+    else:
+        init_logging(logfile)
+
+    add_mailin_subsystem()
+
+    plugins = conf.get('main', 'plugins').split()
+    plugins = load_plugins(plugins)
+
+    if args.init:
+        logger.info('Initialization done. Exiting.')
+        return
+
+    read_and_process_input(plugins, test=args.test)
+    logger.info('Done')
+
+
+def parse_args():
+    """Parse program arguments"""
+    parser = argparse.ArgumentParser(
+        description="Parse RFC822 formatted mail messages from 3rd party "
+                    "software and convert them to NAV events, using plugins",
+    )
+    parser.add_argument('-i', '--init', action='store_true',
+                        help='only load plugins and create event/alert types')
+    parser.add_argument('-t', '--test', action='store_true',
+                        help='write events to stdout instead of posting them.')
+    return parser.parse_args()
+
+
+def read_and_process_input(plugins, test=False):
+    """Parses stdin as e-mail and passes it to each plugin in order"""
+    msg = email.message_from_file(sys.stdin)
+    logger.info('---')
+    logger.info('Got message: From=%r Subject=%r', msg['From'], msg['Subject'])
+
+    for plugin in plugins:
+        if plugin.accept(msg):
+            logger.info('%s accepted the message', plugin.name)
+
+            if authorize_match(plugin, msg):
+                if plugin.authorize(msg):
+                    events = plugin.process(msg)
+                    if isinstance(events, nav.event.Event):
+                        # Only one event, put it in a list
+                        events = [events]
+
+                    if test:
+                        print('Would have posted these events:')
+                        for event in events:
+                            print(event)
+                    else:
+                        for event in events:
+                            event.post()
+                            logger.info('Posted %r', event)
+                else:
+                    logger.error('Message not authorized')
+
+            break  # Only one message gets to play
 
 
 def add_mailin_subsystem():
@@ -40,29 +117,24 @@ def add_mailin_subsystem():
 
     cursor.execute("select * from subsystem where name='mailin'")
     if cursor.rowcount == 0:
-        cursor.execute("INSERT INTO subsystem (name, descr) VALUES ('mailin', '')")
+        cursor.execute("INSERT INTO subsystem (name, descr) "
+                       "VALUES ('mailin', '')")
     conn.commit()
-
-
-def indent_msg(msg):
-    """Indent every line in msg."""
-    return re.sub(r'(^|\n)', r'\1    ', msg)
 
 
 def load_plugins(paths):
     plugins = []
 
     for path in paths:
-        logger.info('Loading plugin %s ...' % path)
+        logger.info('Loading plugin %s ...', path)
         parent = path.split('.')[:-1]
 
         try:
             mod = __import__(path, globals(), locals(), [parent])
         except ImportError:
-            logger.error('Plugin not found: %s' % path)
-        except Exception, arg:
-            logger.error('Failed to load plugin %s\n%s' % (path,
-                                                           indent_msg(traceback.format_exc())))
+            logger.error('Plugin not found: %s', path)
+        except Exception:
+            logger.exception('Failed to load plugin %s', path)
             continue
 
         plugin = mod.Plugin(path, conf, logger)
@@ -74,19 +146,17 @@ def load_plugins(paths):
 def authorize_match(plugin, msg):
     """Test message headers against a pattern in the configuration file"""
 
-    # Try to get 'authorization' option from plugin section
-    # or from main section.
+    # Try to get 'authorization' option from plugin section or main section
     if conf.has_option(plugin.name, 'authorization'):
-        p = conf.get(plugin.name, 'authorization')
+        raw = conf.get(plugin.name, 'authorization')
     elif conf.has_option('main', 'authorization'):
-        p = conf.get('main', 'authorization')
+        raw = conf.get('main', 'authorization')
     else:
         # No authorization pattern, so just let it through
         return True
 
-    # Return True if the patterm matches at least one
-    # header line
-    pattern = re.compile(p)
+    # Return True if the pattern matches at least one header line
+    pattern = re.compile(raw)
     for key in msg.keys():
         value = msg[key]
         line = key + ': ' + value
@@ -94,101 +164,17 @@ def authorize_match(plugin, msg):
             return True
 
     # Should this logged as error or warning?
-    logger.error("Message doesn't match auth pattern %s" % repr(p))
+    logger.error("Message doesn't match auth pattern %r", raw)
     return False
 
 
-def make_logger(filename):
-    logger = logging.getLogger('nav.mailin')
-    logger.setLevel(logging.DEBUG)
-
+def init_logging(filename):
     if filename == '-':
-        # Log to stdout
-        #     logging.basicConfig(level=logging.DEBUG)
-        handler = logging.StreamHandler()
+        logs.init_stderr_logging()
     else:
         if conf.has_option('main', 'logfile'):
             filename = conf.get('main', 'logfile')
-            # logging.basicConfig(filename=logfile, level=logging.DEBUG)
-
-        handler = logging.FileHandler(filename)
-
-    handler.setFormatter(logging.Formatter("[%(asctime)s] - %(name)s - %(levelname)s - %(message)s"))
-    logger.addHandler(handler)
-
-    return logger
-
-
-def main():
-    global logger
-    global conf
-
-    parser = OptionParser()
-    parser.add_option('-i',
-                      '--init',
-                      dest='init',
-                      action='store_true',
-                      default=False,
-                      help='Only load plugins and create event/alert types')
-    parser.add_option('-t',
-                      '--test',
-                      dest='test',
-                      action='store_true',
-                      default=False,
-                      help='Write events to stdout instead of posting them.')
-    (options, args) = parser.parse_args()
-
-    configfile = nav.buildconf.sysconfdir + '/mailin.conf'
-    logfile = nav.buildconf.localstatedir + '/log/mailin.log'
-
-    # Todo: fail if config file is not found
-    conf = ConfigParser.ConfigParser()
-    conf.read(configfile)
-
-    # Must do this after config, so logfile can be configurable
-    if options.test:
-        logger = make_logger('-')
-    else:
-        logger = make_logger(logfile)
-
-    add_mailin_subsystem()
-
-    plugins = conf.get('main', 'plugins').split()
-    plugins = load_plugins(plugins)
-
-    if options.init:
-        logger.info('Initialization done. Exiting.')
-        return
-
-    msg = email.message_from_file(sys.stdin)
-    logger.info('---')
-    logger.info('Got message: From=%s Subject=%s' % (repr(msg['From']), repr(msg['Subject'])))
-
-    for plugin in plugins:
-        if plugin.accept(msg):
-            logger.info('%s accepted the message' % plugin.name)
-
-            if authorize_match(plugin, msg):
-                if plugin.authorize(msg):
-                    events = plugin.process(msg)
-                    if isinstance(events, nav.event.Event):
-                        # Only one event, put it in a list
-                        events = [events]
-
-                    if options.test:
-                        print('Would have posted these events:')
-                        for event in events:
-                            print(event)
-                    else:
-                        for event in events:
-                            event.post()
-                            logger.info('Posted %s' % repr(event))
-                else:
-                    logger.error('Message not authorized')
-
-            break  # Only one message gets to play
-
-    logger.info('Done')
+        logs.init_generic_logging(logfile=filename, stderr=False)
 
 if __name__ == '__main__':
     main()
