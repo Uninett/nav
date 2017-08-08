73c9d109c54e498cb16d81dd85a1896ca5f1534d
Sigmund Augdal <sigmund.augdal@uninett.no>
2017-08-08 09:27:57 +0200

    Attempt to fix flash of unstyled content and test failing because of it

--- tests/functional/status_test.py
+++ tests/functional/status_test.py
@@ -16,6 +16,9 @@
 """Selenium tests for Status"""
 from time import sleep
 import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 
 @pytest.fixture()
@@ -25,12 +28,14 @@ def statuspage(selenium, base_url):
     filter_toggle = selenium.find_element_by_class_name('toggle-panel')
     return panel, filter_toggle
 
+
 def test_panel_should_toggle_when_clicked(statuspage):
     """Test if panel toggles when clicked"""
     panel, filter_toggle = statuspage
     initial_state = panel.is_displayed()
     filter_toggle.click()
     assert initial_state != panel.is_displayed(), 'Clicking filter_toggle did not do anything'
+
 
 def test_remember_last_panel_state(selenium, statuspage):
     """Test if the panel stays in the same state after page refresh"""
@@ -40,8 +45,8 @@ def test_remember_last_panel_state(selenium, statuspage):
     filter_toggle.click()
     sleep(1)
     selenium.refresh()
+    WebDriverWait(selenium, 3).until(
+        expected_conditions.visibility_of_element_located((By.ID, 'status-page')))
     # We need to fetch panel element again after a refresh
     panel = selenium.find_element_by_id('status-panel')
-    if not panel.is_displayed():
-        sleep(1)  # stupid attempt to workaround potential FOUC
     assert panel.is_displayed(), 'Panel did not stay in same state after page refresh'
