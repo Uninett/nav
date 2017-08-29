9a23289ca6e574ccbb8f94a9061710be9b5b524c
Morten Brekkevold <morten.brekkevold@uninett.no>
2017-08-29 14:06:31 +0200

    sync baseline and changescripts
    new baseline fixes some issues that would remain unfixed when upgrading
    from older NAV versions. this changescript gets upgrades in sync with
    the new baseline.

---
+++ sql/changes/sc.04.08.0001.sql
@@ -0,0 +1,14 @@
+-- delete obsolete function used as trigger on removed rrd tables
+DROP FUNCTION IF EXISTS close_thresholdstate_on_threshold_delete();
+
+
+-- Ensure functions are in correct schema, in a roundabout way:
+-- Since bootstrapping from a new baseline already puts it in the correct
+-- schema, and moving it to the schema it already is in raises an error, we
+-- move it around a bit first :-D
+ALTER FUNCTION create_new_dashboard() SET SCHEMA public;
+ALTER FUNCTION public.create_new_dashboard() SET SCHEMA profiles;
+
+ALTER FUNCTION insert_default_navlets_for_existing_users() SET SCHEMA public;
+ALTER FUNCTION public.insert_default_navlets_for_existing_users() SET SCHEMA profiles;
+
