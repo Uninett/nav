dd7a3427f150a66bd3fc616220c3558611ec6467
Sigmund Augdal <sigmund.augdal@uninett.no>
2017-09-07 15:37:39 +0200

    Explicitly mark byte string as such to fix a test on python3

--- tests/unittests/smsd/gammudispatcher_test.py
+++ tests/unittests/smsd/gammudispatcher_test.py
@@ -23,7 +23,7 @@ class EncodingTests(TestCase):
         self.assertEquals(sms, unicode_sms)
 
     def test_utf_8_string_should_properly_decode(self):
-        sms = 'A m\xc3\xb8\xc3\xb8se once bit my sister'
+        sms = b'A m\xc3\xb8\xc3\xb8se once bit my sister'
         unicode_sms = decode_sms_to_unicode(sms)
         expected = u'A m\xf8\xf8se once bit my sister'
         self.assertEquals( unicode_sms, expected)
