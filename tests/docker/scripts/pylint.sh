c2f0a218849646afd395eeb9603378e217f75533
Morten Brekkevold <morten.brekkevold@uninett.no>
2017-10-03 13:40:10 +0200

    attempt to parallelize pylint itself, since pylint is the slow part

--- tests/docker/scripts/pylint.sh
+++ tests/docker/scripts/pylint.sh
@@ -5,4 +5,4 @@
 cd "${WORKSPACE}"
 echo "Running pylint"
 export PYLINTHOME="${WORKSPACE}"
-pylint python/nav --rcfile=python/pylint.rc --disable=I,similarities --output-format=parseable || true
+pylint python/nav --jobs=4 --rcfile=python/pylint.rc --disable=I,similarities --output-format=parseable || true
