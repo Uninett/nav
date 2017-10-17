2ef7ea84b019f92fc5160464b94dec20e2adf26a
Morten Brekkevold <morten.brekkevold@uninett.no>
2017-10-17 12:45:00 +0200

    We should be able to use Django's TestCase directly by now
    but we want all NAV tests to have the serialized_rollback option
    enabled.

--- python/nav/tests/cases.py
+++ python/nav/tests/cases.py
@@ -14,61 +14,16 @@
 # License along with NAV. If not, see <http://www.gnu.org/licenses/>.
 #
 """NAV test cases"""
-import sys
-import os
-
 import django.test
-from django.db import transaction
 
 
 class DjangoTransactionTestCase(django.test.TestCase):
-    """Runs tests inside a Django transaction, with the ability to load Django
-    fixtures at the start of each test.
-
-    django.test.TestCase's _pre_setup() and _post_teardown() methods
-    are overridden, since NAV doesn't use Django models to initialize
-    the database, and generally does funny things with Django.
-
-    """
-    fixtures = []
+    serialized_rollback = True
 
     def _pre_setup(self):
-        transaction.enter_transaction_management()
-        transaction.managed(True)
-        if hasattr(self, 'fixtures'):
-            self._load_fixtures(self.fixtures)
+        print "PRE SETUP!!!!"
+        return super(DjangoTransactionTestCase, self)._pre_setup()
 
-    def _post_teardown(self):
-        transaction.rollback()
-        transaction.leave_transaction_management()
-
-    def _load_fixtures(self, fixtures):
-        from django.core import serializers
-        fixture_paths = self._find_fixtures(fixtures)
-        if fixtures and not fixture_paths:
-            raise Exception("No fixtures found")
-        for fixture_path in fixture_paths:
-            fixture = file(fixture_path, "r")
-            try:
-                objects = serializers.deserialize("xml", fixture)
-                for obj in objects:
-                    obj.save()
-            except (SystemExit, KeyboardInterrupt):
-                raise
-            except Exception:
-                fixture.close()
-                transaction.rollback()
-                transaction.leave_transaction_management()
-                raise
-            fixture.close()
-
-    def _find_fixtures(self, fixtures):
-        my_module = sys.modules[self.__module__]
-        search_path = [os.path.dirname(my_module.__file__)]
-        result = []
-        for f in fixtures:
-            for dir in search_path:
-                filename = os.path.join(dir, f)
-                if os.path.exists(filename):
-                    result.append(filename)
-        return result
+    def _fixture_setup(self):
+        print "LOADING FIXTURES!! " + repr(self.fixtures)
+        return super(DjangoTransactionTestCase, self)._fixture_setup()
