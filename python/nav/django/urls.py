f4e5e2bfcac7cd1f24db887df91f21c1fbfa5b51
John-Magne Bredal <john.m.bredal@uninett.no>
2018-01-22 11:02:03 +0100

    Basic setup for PortList page

--- python/nav/django/urls.py
+++ python/nav/django/urls.py
@@ -61,6 +61,7 @@ urlpatterns += [
     url(r'^styleguide/', styleguide_index),
     url(r'^refresh_session/', refresh_session, name='refresh-session'),
     url(r'^auditlog/', include('nav.auditlog.urls')),
+    url(r'^portlist/', include('nav.web.portlist.urls')),
 ]
 
 # Load local url-config
