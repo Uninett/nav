5faf25f459916d1a14b67b201d005f5883ba6b94
Morten Brekkevold <morten.brekkevold@uninett.no>
2018-02-14 13:01:24 +0100

    set PYTHONPATH based on WORKSPACE

--- tests/docker/scripts/entrypoint.sh
+++ tests/docker/scripts/entrypoint.sh
@@ -2,4 +2,5 @@
 # Remap the container's build user entry to the currently running UID
 gosu root usermod -u $UID build
 gosu root chown -R $UID /home/build
+export PYTHONPATH="$WORKSPACE/python"
 exec "$@"
