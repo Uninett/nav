5efd0fe6e200bb1947b38223b270e34cc8e897be
Morten Brekkevold <morten.brekkevold@uninett.no>
2018-02-14 13:47:46 +0100

    let tox set PYTHONPATH instead, relative to where its init file resides.

--- tests/docker/scripts/entrypoint.sh
+++ tests/docker/scripts/entrypoint.sh
@@ -2,5 +2,4 @@
 # Remap the container's build user entry to the currently running UID
 gosu root usermod -u $UID build
 gosu root chown -R $UID /home/build
-export PYTHONPATH="$WORKSPACE/python"
 exec "$@"
