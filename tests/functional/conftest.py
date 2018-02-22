84bd2577dab1367f480296e7ccb63df10a089349
Morten Brekkevold <morten.brekkevold@uninett.no>
2018-02-22 15:48:57 +0100

    setup test environment as part of integration tests using pytest hooks

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,12 +1,46 @@
 import os
+import subprocess
+
 import pytest
-from nav.django.auth import create_session_cookie
 
 USERNAME = 'admin'
 
+########################################################################
+#                                                                      #
+# Set up the required components for an integration test. Components   #
+# such as PostgreSQL and Apache are assumed to already be installed on #
+# the system. The system is assumed to be Debian. See                  #
+# tests/docker/Dockerfile.                                             #
+#                                                                      #
+########################################################################
+
+if os.environ.get('WORKSPACE'):
+    SCRIPT_PATH = os.path.join(os.environ['WORKSPACE'], 'tests/docker/scripts')
+else:
+    SCRIPT_PATH = '/'
+SCRIPT_CREATE_DB = os.path.join(SCRIPT_PATH, 'create-db.sh')
+SCRIPT_START_SERVICES = os.path.join(SCRIPT_PATH, 'start-services.sh')
+SCRIPT_STOP_SERVICES = os.path.join(SCRIPT_PATH, 'stop-services.sh')
+
+
+def pytest_configure(config):
+    subprocess.check_call([SCRIPT_CREATE_DB])
+    os.environ['TARGETURL'] = "http://localhost:8000/"
+    subprocess.check_call([SCRIPT_START_SERVICES])
+
+def pytest_unconfigure(config):
+    subprocess.check_call([SCRIPT_STOP_SERVICES])
+
+############
+#          #
+# Fixtures #
+#          #
+############
 
 @pytest.fixture
 def selenium(selenium, base_url):
+    from nav.django.auth import create_session_cookie
+
     selenium.implicitly_wait(10)
     cookie = create_session_cookie(USERNAME)
     selenium.get('{}/400'.format(base_url))
