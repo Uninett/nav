a9b576ca456fba5b8dee4537b7991452b98d59d0
Morten Brekkevold <morten.brekkevold@uninett.no>
2018-02-28 08:24:15 +0100

    remove obsolete files and scripts related to the CI environment

--- tests/docker/scripts/python-unit-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -xe
-cd "${WORKSPACE}/tests"
-py.test \
-    --cov=/opt/nav/lib/python --cov-report=xml:coverage.xml \
-    --junitxml=unit-results.xml \
-    --verbose \
-    unittests
-sed -i 's!filename="nav/!filename="python/nav/!' coverage.xml
