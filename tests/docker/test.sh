50dcf23f8f997df371418e15a9c2593c456aa8b9
Morten Brekkevold <morten.brekkevold@uninett.no>
2018-02-28 08:33:35 +0100

    offload pylint to tox

--- tests/docker/test.sh
+++ tests/docker/test.sh
@@ -1,16 +1,11 @@
 #!/bin/bash -xe
 
-run_pylint() {
-    time "/pylint.sh" > "${WORKSPACE}/pylint.txt"
-}
-
-
 # MAIN EXECUTION POINT
 cd "$WORKSPACE"
 tox
 
 # Code analysis steps
-run_pylint
+tox -e pylint
 /count-lines-of-code.sh
 
 echo "test.sh done"
