6fc821618ea788adb8c5a7bfe17b2f22870c6332
Morten Brekkevold <morten.brekkevold@uninett.no>
2018-02-28 08:44:36 +0100

    be more picky about what to actually count using cloc
    using just ignores and no includes appears to slow down cloc considerably, so
    this way we just include the "interesting" parts.

--- tests/docker/scripts/count-lines-of-code.sh
+++ tests/docker/scripts/count-lines-of-code.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 cd ${WORKSPACE}
 cloc \
+    --list-file=tests/.clocinclude \
     --exclude-list-file=tests/.clocignore \
     --exclude-lang=make,m4,XML \
     --not-match-f='(configure|config.status)$' \
