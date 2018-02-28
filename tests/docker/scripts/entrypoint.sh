d9bd354aee3c0e8b96a2a0becab9153750612004
Morten Brekkevold <morten.brekkevold@uninett.no>
2018-02-28 09:53:26 +0100

    Jenkins may mount a different volume than /source, so pay attention to that

--- tests/docker/scripts/entrypoint.sh
+++ tests/docker/scripts/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Remap the container's build user entry to the currently running UID
 gosu root usermod -u $UID build
-mkdir -p /source/.cache/pip
+if [ -n "$WORKSPACE" ]; then
+    export HOME="$WORKSPACE"
+fi
+mkdir -p "$HOME/.cache/pip"
 exec "$@"
