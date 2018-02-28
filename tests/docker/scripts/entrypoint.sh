f3294c39175a161fc5186b1dfbc4552c66a1f559
Morten Brekkevold <morten.brekkevold@uninett.no>
2018-02-28 08:01:03 +0100

    ensure pip's cache goes into /source/.cache/pip
    simply do it by using /source as the build user's home directory.

--- tests/docker/scripts/entrypoint.sh
+++ tests/docker/scripts/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Remap the container's build user entry to the currently running UID
 gosu root usermod -u $UID build
-gosu root chown -R $UID /home/build
+mkdir -p /source/.cache/pip
 exec "$@"
