6bc4a1fd073d20dafc64d7da0ba573ae4ba2d249
John-Magne Bredal <john.m.bredal@uninett.no>
2018-03-01 08:14:18 +0100

    Add tests for dependencies function

--- tests/integration/seeddb_test.py
+++ tests/integration/seeddb_test.py
@@ -1,10 +1,14 @@
+from __future__ import print_function
+
 from django.core.urlresolvers import reverse
 from django.http import Http404
 from django.test.client import RequestFactory
 from mock import MagicMock
 
+from nav.models.manage import Netbox, Room
 from nav.models.profiles import Account, AlertProfile
 from nav.web.seeddb.page.netbox.edit import netbox_edit
+from nav.web.seeddb.utils.delete import dependencies
 
 import pytest
 
@@ -23,3 +27,31 @@ def test_editing_deleted_netboxes_should_raise_404():
 
     with pytest.raises(Http404):
         netbox_edit(request, netboxid)
+
+
+@pytest.fixture()
+def netbox():
+    box = Netbox(ip='10.254.254.254', sysname='downhost.example.org',
+                 organization_id='myorg', room_id='myroom', category_id='SRV',
+                 snmp_version=2)
+    box.save()
+    yield box
+    print("teardown test device")
+    box.delete()
+
+
+def test_dependencies(netbox):
+    """Tests the related objects listed when deleting objects in seeddb"""
+    qs = Room.objects.filter(pk='myroom')
+    deps = dependencies(qs, [Netbox])
+    assert 'myroom' in deps
+    assert [netbox] == deps.get('myroom')
+
+
+def test_dependencies_no_whitelist(netbox):
+    """Tests the related objects listed when deleting objects in seeddb"""
+    qs = Room.objects.filter(pk='myroom')
+    deps = dependencies(qs, [])
+    assert Netbox.objects.get(pk=netbox.pk)
+    assert deps == {}
+
