f2d56e5b5a011da616ebde892a84b83ba4a80de9
Hanne Moa <hanne.moa@uninett.no>
2018-03-06 10:37:04 +0100

    app-modernize macwatch and put it in INSTALLED_APPS

---
+++ python/nav/web/macwatch/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'nav.web.macwatch.apps.MacWatchConfig'
---
+++ python/nav/web/macwatch/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MacWatchConfig(AppConfig):
+    name = 'nav.web.macwatch'
+    verbose_name = 'Mac Watch'
