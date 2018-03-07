2487516f0bd5a11cd4edcd79db6bc248ad520f0e
Morten Brekkevold <morten.brekkevold@uninett.no>
2018-03-07 15:18:11 +0100

    WIP: refactor business reports util functions completely

--- python/nav/web/business/utils.py
+++ python/nav/web/business/utils.py
@@ -5,10 +5,8 @@ from datetime import datetime, timedelta
 from collections import defaultdict, namedtuple
 
 from nav.models.event import AlertHistory
-from nav.models.manage import Interface
+from nav.models.manage import Interface, Netbox
 from nav.models.profiles import ReportSubscription
-from django.db.models import Q
-
 AvailabilityRecord = namedtuple(
     'AvailabilityRecord',
     ['subject', 'incidents', 'downtime', 'availability', 'maintenances'])
@@ -73,54 +71,31 @@ def get_months(number_of_months=12):
     return months
 
 
-def compute_downtime(alerts, start, end):
-    """Computes the total downtime for the given alerts"""
-    downtime = timedelta()
-    for alert in alerts:
-        start_inside_interval = start <= alert.start_time <= end
-        end_inside_interval = start <= alert.end_time <= end
-
-        if start_inside_interval or end_inside_interval:
-            # I want one liners dammit! :p
-            # pylint: disable=C0301
-            interval_start = alert.start_time if start_inside_interval else start
-            interval_end = alert.end_time if end_inside_interval else end
-            downtime += (interval_end - interval_start)
-        elif alert.start_time <= start and alert.end_time >= end:
-            # If the alert covers the whole interval
-            downtime += (end - start)
-
-    return downtime
-
-
 def compute_availability(downtime, interval):
     """Computes the availability given downtime and interval"""
     if downtime.total_seconds() == 0 or not interval:
         return 100
     availability = 1.0
     fraction = downtime.total_seconds() / interval.total_seconds()
-    availability = availability - fraction
-
-    # For special cases this may go in the negative, fix that.
-    if availability < 0:
-        availability = 0;
-
+    availability = max(availability - fraction, 0.0)
     return availability * 100
 
 
-def create_record(subject, alerts, start, end, exclude_maintenance=False):
-    """Creates an availability record based on a subject' alerts in a period"""
+def create_record(subject, alerts, start, end, maintenances=None):
+    """Creates an availability record based on a subject's alerts in a period"""
     now = datetime.now()
     if end > now:
         end = now
 
-    maintenances = (get_maintenances(start, end, subject)
-                    if exclude_maintenance else [])
-    intervals = find_intervals(start, end, maintenances)
-    downtimes = [compute_downtime(alerts, *interval) for interval in intervals]
-    downtime = sum(downtimes, timedelta(0))
-    total_interval = sum([e-s for s, e in intervals], timedelta(0))
-    availability = compute_availability(downtime, total_interval)
+    def duration(alert):
+        return alert.end_within - alert.start_within
+
+    downtime = sum((duration(a) for a in alerts), timedelta())
+    if maintenances:
+        maintenancetime = sum((duration(m) for m in maintenances), timedelta())
+        downtime -= maintenancetime
+
+    availability = compute_availability(downtime, end-start)
 
     # Cheekily remove microseconds
     downtime = downtime - timedelta(microseconds=downtime.microseconds)
@@ -137,81 +112,65 @@ def get_subject(subject):
     return subject
 
 
-def get_maintenances(start, end, subject):
-    subject = subject.netbox if isinstance(subject, Interface) else subject
-    return AlertHistory.objects.filter(
-        netbox=subject, event_type='maintenanceState',
-        end_time__isnull=False).filter(
-            Q(end_time__range=(start, end)) |
-            Q(start_time__range=(start, end)) |
-            (Q(start_time__lte=start) & Q(end_time__gte=end)
-            )).order_by('-start_time')
-
-
-def get_alerts(start, end, eventtype='boxState', alerttype='boxDown'):
-    """Gets the alerts for the given start-end interval"""
-
-    return AlertHistory.objects.filter(
-        event_type=eventtype, end_time__isnull=False,
-        alert_type__name=alerttype).filter(
-            Q(end_time__range=(start, end)) |
-            Q(start_time__range=(start, end)) |
-            (Q(start_time__lte=start) & Q(end_time__gte=end)
-            )).order_by('-start_time')
-
-
-def find_intervals(start, end, maintenances):
-    """Assert that maintenances are sorted"""
-    intervals = []
-    temp_start = start
-    temp_end = end
-    for maintenance in maintenances:
-        # If a maintenance overlaps the whole interval, there is no downtime
-        if maintenance.start_time <= start and maintenance.end_time >= end:
-            return []
-
-        if maintenance.start_time > start and maintenance.end_time < end:
-            intervals.append((temp_start, maintenance.start_time))
-            temp_start = maintenance.end_time
-
-        if maintenance.start_time < start:
-            temp_start = maintenance.end_time
-        if maintenance.end_time > end:
-            temp_end = maintenance.start_time
-
-    intervals.append((temp_start, temp_end))
-    return intervals
-
-
-def group_by_netbox(alerts):
-    """Group alerts by netbox"""
+def group_by_subject(alerts):
     grouped_alerts = defaultdict(list)
     for alert in alerts:
-        grouped_alerts[alert.netbox].append(alert)
-    return grouped_alerts
-
-
-def group_by_interface(alerts):
-    grouped_alerts = defaultdict(list)
-    for alert in alerts:
-        try:
-            interface = Interface.objects.get(pk=alert.subid)
-        except Interface.DoesNotExist:
-            continue
-        else:
-            grouped_alerts[interface].append(alert)
+        grouped_alerts[alert.get_subject()].append(alert)
     return grouped_alerts
 
 
 def get_netbox_records(start, end, exclude_maintenance=False):
-    alerts = get_alerts(start, end, 'boxState', 'boxDown')
-    grouped_alerts = group_by_netbox(alerts)
-    return [create_record(subject, alerts, start, end, exclude_maintenance)
-            for subject, alerts in grouped_alerts.items() if subject]
+    alerts = get_alert_periods_by_type(start, end, 'boxState', ['boxDown'])
+    grouped_alerts = group_by_subject(alerts)
+
+    if exclude_maintenance:
+        maintenances = get_alert_periods_by_type(start, end, 'maintenanceState',
+                                                 ['onMaintenance'])
+        grouped_maintenance = group_by_subject(maintenances)
+    else:
+        grouped_maintenance = {}
+
+    records = (create_record(netbox, alerts, start, end,
+                             grouped_maintenance.get(netbox))
+               for netbox, alerts in grouped_alerts.items() if netbox)
+    return [record for record in records if record.downtime > timedelta(0)]
 
 
-def get_interface_records(start, end, exclude_mainteannce=False):
-    alerts = get_alerts(start, end, 'linkState', 'linkDown')
-    grouped_alerts = group_by_interface(alerts)
+def get_interface_records(start, end, exclude_maintenance=False):
+    alerts = get_alert_periods_by_type(start, end, 'linkState', ['linkDown'])
+    grouped_alerts = group_by_subject(alerts)
     return [create_record(subject, alerts, start, end)
             for subject, alerts in grouped_alerts.items() if subject]
+
+
+def get_alert_periods_by_type(period_start, period_end, event_type_id,
+                              alert_type_ids):
+    """Returns AlertHistory objects of a specific event type, overlapping with
+    a given time period.
+
+    The AlertHistory objects will be augmented with the attributes
+    `start_within` and `end_within`, which represent the intersection of (
+    `period_start`, `period_end`) and (`start_time`, `end_time`) (for proper
+    calculation of which duration of earch AlertHistory object is inside the
+    specified time period.
+
+    """
+    return AlertHistory.objects.raw("""
+    WITH period AS (
+  SELECT
+    %s AS "start",
+    %s AS "end"
+)
+SELECT
+  alerthist.*,
+  GREATEST("start", start_time) AS start_within,
+  LEAST("end", end_time) AS end_within
+FROM alerthist
+JOIN period ON ((start_time, end_time) OVERLAPS ("start", "end"))
+JOIN alerttype USING (alerttypeid)
+WHERE
+  alerthist.eventtypeid = %s
+  AND end_time IS NOT NULL
+  AND alerttype IN %s
+ORDER BY netboxid, subid, start_time
+    """, [period_start, period_end, event_type_id, tuple(alert_type_ids)])
--- tests/unittests/business/business_test.py
+++ tests/unittests/business/business_test.py
@@ -29,31 +29,6 @@ class TestBusinessUtils(unittest.TestCase):
         self.downtime2 = self.alert2.end_time - self.alert2.start_time
         self.alerts = [self.alert1, self.alert2]
 
-    def test_zero_downtime(self):
-        start = datetime(2014, 2, 1)
-        end = datetime(2014, 2, 27)
-        downtime = utils.compute_downtime(self.alerts, start, end)
-        self.assertEqual(downtime, timedelta(0))
-
-    def test_zero_downtime2(self):
-        start = datetime(2014, 1, 4)
-        end = datetime(2014, 1, 10)
-        downtime = utils.compute_downtime(self.alerts, start, end)
-        self.assertEqual(downtime, timedelta(0))
-
-
-    def test_single_downtime(self):
-        start = datetime(2014, 1, 1)
-        end = datetime(2014, 1, 31)
-        downtime = utils.compute_downtime([self.alert1], start, end)
-        self.assertEqual(downtime, self.downtime1)
-
-    def test_multiple_downtimes(self):
-        start = datetime(2014, 1, 1)
-        end = datetime(2014, 1, 31)
-        downtime = utils.compute_downtime(self.alerts, start, end)
-        self.assertEqual(downtime, self.downtime1 + self.downtime2)
-
     def test_full_availability(self):
         start = datetime(2014, 2, 1)
         end = datetime(2014, 2, 27)
@@ -67,73 +42,3 @@ class TestBusinessUtils(unittest.TestCase):
         interval = end - start
         avail = utils.compute_availability(timedelta(days=1), interval)
         self.assertEqual(avail, 0)
-
-    def test_find_intervals_one_maintenance(self):
-        start = datetime(2014, 1, 1)
-        end = datetime(2014, 1, 31)
-        intervals = utils.find_intervals(start, end, [self.maintenance1])
-        self.assertEqual(len(intervals), 2)
-        a, b = intervals
-        self.assertEqual(a, (start, self.maintenance1.start_time))
-        self.assertEqual(b, (self.maintenance1.end_time, end))
-
-    def test_find_intervals_two_maintenances(self):
-        start = datetime(2014, 1, 1)
-        end = datetime(2014, 1, 31)
-        intervals = utils.find_intervals(start, end, [self.maintenance1, self.maintenance2])
-        self.assertEqual(len(intervals), 3)
-        a, b, c = intervals
-        self.assertEqual(a, (start, self.maintenance1.start_time))
-        self.assertEqual(b, (self.maintenance1.end_time, self.maintenance2.start_time))
-        self.assertEqual(c, (self.maintenance2.end_time, end))
-
-    def test_find_intervals_three_maintenances(self):
-        start = datetime(2014, 1, 1)
-        end = datetime(2014, 1, 31)
-        intervals = utils.find_intervals(start, end, [self.maintenance1, self.maintenance2, self.maintenance3])
-        self.assertEqual(len(intervals), 4)
-        a, b, c, d = intervals
-        self.assertEqual(a, (start, self.maintenance1.start_time))
-        self.assertEqual(b, (self.maintenance1.end_time, self.maintenance2.start_time))
-        self.assertEqual(c, (self.maintenance2.end_time, self.maintenance3.start_time))
-        self.assertEqual(d, (self.maintenance3.end_time, end))
-
-    def test_find_interval_overlapping_start(self):
-        start = datetime(2014, 1, 1)
-        end = datetime(2014, 1, 31)
-        intervals = utils.find_intervals(start, end, [self.maintenance0])
-        self.assertEqual(len(intervals), 1)
-        a, = intervals
-        self.assertEqual(a, (self.maintenance0.end_time, end))
-
-    def test_find_interval_overlapping_end(self):
-        start = datetime(2014, 1, 1)
-        end = datetime(2014, 1, 31)
-        intervals = utils.find_intervals(start, end, [self.maintenance5])
-        self.assertEqual(len(intervals), 1)
-        a, = intervals
-        self.assertEqual(a, (start, self.maintenance5.start_time))
-
-    def test_find_interval_cover_all(self):
-        """Test the case where:
-
-        - the maintenance starts before the start
-        - the maintenance ends after the end
-        """
-        maintenance = AlertHistory(start_time=datetime(2013, 1, 29),
-                                   end_time=datetime(9999, 12, 31))
-        start = datetime(2014, 1, 1)
-        end = datetime(2014, 1, 31)
-        intervals = utils.find_intervals(start, end, [maintenance])
-        self.assertEqual(len(intervals), 0)
-
-    def test_find_interval_combined(self):
-        start = datetime(2014, 1, 1)
-        end = datetime(2014, 1, 31)
-        intervals = utils.find_intervals(start, end, [
-            self.maintenance0, self.maintenance1, self.maintenance2, self.maintenance5])
-        self.assertEqual(len(intervals), 3)
-        a, b, c = intervals
-        self.assertEqual(a, (self.maintenance0.end_time, self.maintenance1.start_time))
-        self.assertEqual(b, (self.maintenance1.end_time, self.maintenance2.start_time))
-        self.assertEqual(c, (self.maintenance2.end_time, self.maintenance5.start_time))
