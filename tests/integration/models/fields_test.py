c63e5d211cb2dc3535fc5c2f4a84faa8db61a94f
Hanne Moa <hanne.moa@uninett.no>
2018-03-08 12:18:44 +0100

    split the get/set test

--- tests/integration/models/fields_test.py
+++ tests/integration/models/fields_test.py
@@ -36,10 +36,23 @@ class LegacyGenericForeignKeyTest(TestCase):
         self.assertEqual(mc.key, self.ee1._meta.db_table)
         self.assertEqual(mc.value, str(self.ee1.id))
 
-    def test_set_get_via_lgfk(self):
+    def test_get_via_lgfk(self):
         mc = MaintenanceComponent(
             maintenance_task=self.task,
-            component=self.ee1,
+            key=self.ee1._meta.db_table,
+            value=str(self.ee1.id),
         )
         mc.save()
         self.assertEqual(mc.component, self.ee1)
+
+    def test_set_via_lgfk(self):
+        ee2 = ErrorError(message='another message')
+        ee2.save()
+        mc = MaintenanceComponent(
+            maintenance_task=self.task,
+            key=self.ee1._meta.db_table,
+            value=str(self.ee1.id),
+        )
+        mc.component = ee2
+        mc.save()
+        self.assertEqual(mc.component, ee2)
