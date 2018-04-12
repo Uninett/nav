b283c14a84a08e963c992aebb18b82b58423bc81
Hanne Moa <hanne.moa@uninett.no>
2018-04-12 11:20:32 +0200

    add tests for validate_hstore

--- tests/unittests/django/validators_test.py
+++ tests/unittests/django/validators_test.py
@@ -33,3 +33,28 @@ class ValidPointStringTest(TestCase):
         point_string = "foo, bar"
         res = is_valid_point_string(point_string)
         self.assertFalse(res)
+
+
+class ValidHStoreFieldTest(TestCase):
+
+    def test_empty_hstore_field(self):
+        inputs = ('', None,{})
+        for input in inputs:
+            result = validate_hstore(input)
+            self.assertEqual(result, {})
+
+    def test_non_dict(self):
+        inputs = ((), [], 0)
+        for input in inputs:
+            with self.assertRaises(ValidationError):
+               validate_hstore(input)
+
+    def test_bytes_json(self):
+        input = b'{"a": "b"}'
+        result = validate_hstore(input)
+        self.assertEqual(result, {u'a': u'b'})
+
+    def test_good_json(self):
+        input = '{"a": "b"}'
+        result = validate_hstore(input)
+        self.assertEqual(result, {u'a': u'b'})
