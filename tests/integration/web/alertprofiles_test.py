e7cfc4c344ba3d84e321bdcc61af233af8a1950a
Morten Brekkevold <morten.brekkevold@uninett.no>
2018-04-19 11:59:35 +0200

    parametrized test for simple GET requests against some alertprofiles views

--- tests/integration/web/alertprofiles_test.py
+++ tests/integration/web/alertprofiles_test.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from mock import MagicMock
-from nav.tests.cases import DjangoTransactionTestCase
-from django.test.client import RequestFactory
+import pytest
 
+from django.test.client import RequestFactory
+from django.core.urlresolvers import reverse
+
+from nav.tests.cases import DjangoTransactionTestCase
 from nav.models.profiles import AlertProfile, Account
 from nav.web.alertprofiles.views import set_active_profile
 
@@ -20,3 +23,25 @@ class ProfileTest(DjangoTransactionTestCase):
         profile.save()
 
         self.assertIsNone(set_active_profile(request, profile))
+
+
+@pytest.mark.parametrize("view", [
+    'alertprofiles-overview',
+    'alertprofiles-profile',
+    'alertprofiles-profile-new',
+    'alertprofiles-sms',
+    'alertprofiles-address',
+    'alertprofiles-address-new',
+    'alertprofiles-filters',
+    'alertprofiles-filters-new',
+    'alertprofiles-filter_groups',
+    'alertprofiles-filter_groups-new',
+    'alertprofiles-matchfields',
+    'alertprofiles-matchfields-new',
+    'alertprofiles-permissions',
+])
+def test_alertprofiles_view(client, view):
+    """Simple GET tests for various non-modifying alertprofiles views"""
+    url = reverse(view)
+    response = client.get(url)
+    assert "admin" in response.content
