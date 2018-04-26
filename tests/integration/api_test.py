1f6b805e9a01debe8370e25009da98e440fc115e
John-Magne Bredal <john.m.bredal@uninett.no>
2018-04-26 15:18:59 +0200

    Don't forget Python 3 :)

--- tests/integration/api_test.py
+++ tests/integration/api_test.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+from django.utils.encoding import force_text
+
 import json
 
 from nav.bootstrap import bootstrap_django
@@ -34,7 +37,7 @@ def create_token_endpoint(token, name):
 def get(api_client, endpoint, id=None):
     endpoint = ENDPOINTS[endpoint]
     if id:
-        endpoint = endpoint + unicode(id) + '/'
+        endpoint = endpoint + force_text(id) + '/'
     return api_client.get(endpoint)
 
 
@@ -45,12 +48,12 @@ def create(api_client, endpoint, data):
 
 def update(api_client, endpoint, id, data):
     """Sends a patch request to endpoint with data"""
-    return api_client.patch(ENDPOINTS[endpoint] + unicode(id) + '/', data, format='json')
+    return api_client.patch(ENDPOINTS[endpoint] + force_text(id) + '/', data, format='json')
 
 
 def delete(api_client, endpoint, id):
     """Sends a delete request to endpoint"""
-    return api_client.delete(ENDPOINTS[endpoint] + unicode(id) + '/')
+    return api_client.delete(ENDPOINTS[endpoint] + force_text(id) + '/')
 
 
 # Generic tests
@@ -93,7 +96,7 @@ def test_create_account(db, api_client, token):
     endpoint = 'account'
     create_token_endpoint(token, endpoint)
     response = create(api_client, endpoint, _account_data)
-    print response
+    print(response)
     assert response.status_code == 201
 
 
@@ -102,12 +105,12 @@ def test_update_org_on_account(db, api_client, token):
     create_token_endpoint(token, endpoint)
     data = {"organizations": ["myorg"]}
     response = update(api_client, endpoint, 1, data)
-    print response
+    print(response)
     assert response.status_code == 200
 
     data = {"organizations": []}
     response = update(api_client, endpoint, 1, data)
-    print response
+    print(response)
     assert response.status_code == 200
 
 
@@ -117,7 +120,7 @@ def test_update_group_on_org(db, api_client, token):
     # Only admin group
     data = {"accountgroups": [1]}
     response = update(api_client, endpoint, 1, data)
-    print response
+    print(response)
     assert response.status_code == 200
 
 
@@ -129,10 +132,10 @@ def test_delete_account(db, api_client, token):
     response_delete = delete(api_client, endpoint, res.get('id'))
     response_get = get(api_client, endpoint, res.get('id'))
 
-    print response_delete
+    print(response_delete)
     assert response_delete.status_code == 204
 
-    print response_get
+    print(response_get)
     assert response_get.status_code == 404
 
 
@@ -142,7 +145,7 @@ def test_create_netbox(db, api_client, token):
     endpoint = 'netbox'
     create_token_endpoint(token, endpoint)
     response = create(api_client, endpoint, _netbox_data)
-    print response
+    print(response)
     assert response.status_code == 201
 
 
@@ -153,7 +156,7 @@ def test_update_netbox(db, api_client, token):
     res = json.loads(response_create.content)
     data = {'categoryid': 'GW'}
     response_update = update(api_client, endpoint, res['id'], data)
-    print response_update
+    print(response_update)
     assert response_update.status_code == 200
 
 
@@ -166,8 +169,8 @@ def test_delete_netbox(db, api_client, token):
     response_get = get(api_client, endpoint, json_create['id'])
     json_get = json.loads(response_get.content)
 
-    print response_delete
-    print json_get['deleted_at']
+    print(response_delete)
+    print(json_get['deleted_at'])
 
     assert response_delete.status_code == 204
     assert json_get['deleted_at'] != None
@@ -179,7 +182,7 @@ def test_delete_netbox(db, api_client, token):
 def test_get_wrong_room(db, api_client, token):
     create_token_endpoint(token, 'room')
     response = api_client.get('{}blapp/'.format(ENDPOINTS['room']))
-    print response
+    print(response)
     assert response.status_code == 404
 
 
@@ -187,7 +190,7 @@ def test_create_new_room(db, api_client, token):
     endpoint = 'room'
     create_token_endpoint(token, endpoint)
     response = create(api_client, endpoint, _room_data)
-    print response
+    print(response)
     assert response.status_code == 201
 
 
@@ -196,7 +199,7 @@ def test_get_new_room(db, api_client, token):
     create_token_endpoint(token, endpoint)
     create(api_client, endpoint, _room_data)
     response = api_client.get('/api/1/room/blapp/')
-    print response
+    print(response)
     assert response.status_code == 200
 
 
@@ -204,7 +207,7 @@ def test_patch_room_not_found(db, api_client, token):
     create_token_endpoint(token, 'room')
     data = {'location': 'mylocation'}
     response = api_client.patch('/api/1/room/blapp/', data, format='json')
-    print response
+    print(response)
     assert response.status_code == 404
 
 
@@ -214,7 +217,7 @@ def test_patch_room_wrong_location(db, api_client, token):
     create(api_client, endpoint, _room_data)
     data = {'location': 'mylocatio'}
     response = api_client.patch('/api/1/room/blapp/', data, format='json')
-    print response
+    print(response)
     assert response.status_code == 400
 
 
@@ -225,7 +228,7 @@ def test_patch_room(db, api_client, token):
     data = {'location': 'mylocation'}
     response = api_client.patch('/api/1/room/blapp/', data, format='json')
 
-    print response
+    print(response)
     assert response.status_code == 200
 
 
@@ -235,7 +238,7 @@ def test_delete_room_wrong_room(db, api_client, token):
     create(api_client, endpoint, _room_data)
     response = api_client.delete('/api/1/room/blap/')
 
-    print response
+    print(response)
     assert response.status_code == 404
 
 
@@ -246,8 +249,8 @@ def test_delete_room(db, api_client, token):
     response1 = delete(api_client, endpoint, 'blapp')
     response2 = get(api_client, endpoint, 'blapp')
 
-    print response1
+    print(response1)
     assert response1.status_code == 204
 
-    print response2
+    print(response2)
     assert response2.status_code == 404
