41f7a29ea0e7ff4af24f05c0c872270db34abab1
Morten Brekkevold <morten.brekkevold@uninett.no>
2018-05-14 15:04:57 +0200

    add regression test for #1592 and #1711

--- tests/integration/report/generator_test.py
+++ tests/integration/report/generator_test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #
 # This file is part of Network Administration Visualized (NAV).
 #
@@ -18,12 +19,16 @@ These tests simply enumerate all known reports and ensure that the dbresult is
 error free. This only ensures that the SQL can be run, no further verification
 is performed.
 """
+from __future__ import unicode_literals
 import os.path
 import pytest
+
+from django.http import QueryDict
+from django.core.urlresolvers import reverse
+
 from nav import db
 from nav.report.generator import ReportList, Generator
 from nav.buildconf import sysconfdir
-from django.http import QueryDict
 
 config_file = os.path.join(sysconfdir, 'report', 'report.conf')
 config_file_local = os.path.join(sysconfdir, 'report', 'report.local.conf')
@@ -46,3 +51,10 @@ def test_report(report_name):
 
     assert dbresult, 'dbresult is None'
     assert not dbresult.error, dbresult.error + '\n' + dbresult.sql
+
+
+def test_non_ascii_filter_should_work(client):
+    url = reverse('report-by-name', kwargs={'report_name': 'room'})
+    url = "{}?roomid=æøå".format(url)
+    response = client.get(url, follow=True)
+    assert response.status_code == 200
