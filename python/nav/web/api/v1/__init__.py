afcc77e3a275ae3283adc007ac6e4e742e2ace78
John-Magne Bredal <john.m.bredal@uninett.no>
2018-05-15 14:23:02 +0200

    Override and set default pagination class
    The setting of options has mostly moved to custom classes, thus we need one to
    set pagination options.

---
+++ python/nav/web/api/v1/__init__.py
@@ -0,0 +1,9 @@
+from rest_framework import pagination
+
+class NavPageNumberPagination(pagination.PageNumberPagination):
+    """Custom pagination class for NAV API
+
+    See http://www.django-rest-framework.org/api-guide/pagination/
+    """
+    page_size = 100
+    page_size_query_param = 'page_size'
