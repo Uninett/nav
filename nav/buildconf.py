b59175ec0e5250fb7a63a8f154cad4b993cd8bbb
Morten Brekkevold <morten.brekkevold@uninett.no>
2018-07-16 14:03:49 +0200

    add static (non-generated) buildconf module for now.

---
+++ nav/buildconf.py
@@ -0,0 +1,25 @@
+"""NAV build configuration variables."""
+# pylint: disable=invalid-name
+import os
+import sys
+import getpass
+import pkg_resources
+import sysconfig
+
+#prefix = sys.prefix
+#exec_prefix = sys.exec_prefix
+#bindir = "@bindir@"
+#libdir = "@libdir@"
+#pythonlibdir = "@pythonlibdir@"
+#pythondir = "@pythondir@"
+datadir = os.path.join(sysconfig.get_config_var('datarootdir'), 'nav')
+sysconfdir = os.path.join(datadir, 'conf')
+localstatedir = os.path.join(datadir, 'var')
+webrootdir = os.path.join(datadir, "www")
+initdir = os.path.join(sysconfdir, "init.d")
+crondir = os.path.join(sysconfdir, "cron.d")
+djangotmpldir = os.path.join(datadir, "templates")
+docdir = os.path.join(datadir, "doc")
+nav_user = (getpass.getuser() if os.geteuid() != 0
+            else os.getenv("NAV_USER", "navcron"))
+VERSION = pkg_resources.get_distribution("nav").version
