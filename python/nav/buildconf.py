c4cd4c7d3fb0800cf4e9fdc3598c3d35f5f44241
Morten Brekkevold <morten.brekkevold@uninett.no>
2018-11-09 11:03:54 +0100

    remove more unused buildconf vars

--- python/nav/buildconf.py
+++ python/nav/buildconf.py
@@ -8,7 +8,6 @@ datadir = os.path.join(sysconfig.get_config_var('datarootdir'), 'nav')
 sysconfdir = os.path.join(datadir, 'conf')
 localstatedir = os.path.join(datadir, 'var')
 webrootdir = os.path.join(datadir, "www")
-crondir = os.path.join(sysconfdir, "cron.d")
 djangotmpldir = os.path.join(datadir, "templates")
 docdir = os.path.join(datadir, "doc")
 VERSION = pkg_resources.get_distribution("nav").version
