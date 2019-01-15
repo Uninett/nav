7de9f44b852087cd611cd320907a99d98a745239
Morten Brekkevold <morten.brekkevold@uninett.no>
2019-01-15 15:30:09 +0100

    clean up issues with python and sass change watchers in dev env
    there was a path typo in source-watch.sh, and sass-watch.sh had not been
    committed to the repo at all.

---
+++ tools/docker/sass-watch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# ensures CSS files are rebuilt on SASS file changes
+#
+cd /source
+while inotifywait -e modify -e move -e create -e delete -r --exclude \# /source/python/nav/web/sass
+do
+  python setup.py build_sass
+done
--- tools/docker/source-watch.sh
+++ tools/docker/source-watch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # ensures Apache restarts the WSGI process on changes to /source/python
 #
-while inotifywait -e modify -e move -e create -e delete -r --exclude \# /source/nav
+while inotifywait -e modify -e move -e create -e delete -r --exclude \# /source/python
 do
   supervisorctl restart web
 done
