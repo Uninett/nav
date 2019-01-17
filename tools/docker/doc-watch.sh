66e8cedd703eb073d4530216f3325555692f31d1
Morten Brekkevold <morten.brekkevold@uninett.no>
2019-01-17 09:24:55 +0100

    Always build the docs once before watching for changes.

--- tools/docker/doc-watch.sh
+++ tools/docker/doc-watch.sh
@@ -2,6 +2,9 @@
 # Rebuilds Sphinx documentation on changes
 #
 cd /source
+# Build once first
+python setup.py build_sphinx
+# Then re-build on any changes to the doc directory
 while inotifywait -e modify -e move -e create -e delete -r --exclude \# /source/doc
 do
   python setup.py build_sphinx
