e8159bbd7e2107feedcd8772cfd6e8cd67ec8e91
Morten Brekkevold <morten.brekkevold@uninett.no>
2019-02-15 14:31:29 +0100

    Make logging from web interface work both on WSGI and Django runserver
    This will ensure the loginit for web is run as part of the nav.web
    AppConfig when it appears that the code is running inside the Django
    runserver instead of under WSGI.
    
    the loginit() function is moved to the nav.web module so it can
    easily be reused both by nav.wsgi and nav.web.apps

--- python/nav/wsgi.py
+++ python/nav/wsgi.py
@@ -15,50 +15,11 @@
 #
 """A NAV wsgi application"""
 from __future__ import absolute_import
-import os
-import sys
-import logging
 
 from nav.bootstrap import bootstrap_django
+from nav.web import loginit
+
 bootstrap_django(__file__)
-import nav.logs
-
-
-def loginit():
-    """Initialize a logging setup for the NAV web interface.
-
-    All logging is directed to stderr, which should end up in Apache's
-    error log.
-
-    """
-    # pylint: disable=W0601
-    global _loginited
-    try:
-        # Make sure we don't initialize logging setup several times (in case
-        # of module reloads and such)
-        if _loginited:
-            return
-    except NameError:
-        pass
-
-    root = logging.getLogger('')
-
-    # Attempt to mimic Apache's standard log time format
-    formatter = logging.Formatter(
-        "[%(asctime)s] [%(levelname)s] [pid=%(process)d %(name)s] %(message)s",
-        "%a %b %d %H:%M:%S %Y")
-    try:
-        handler = logging.StreamHandler(sys.stderr)
-    except IOError:
-        # Something went terribly wrong. Maybe stderr is closed?
-        # We silently ignore it and log nothing :-P
-        pass
-    else:
-        handler.setFormatter(formatter)
-
-        root.addHandler(handler)
-        nav.logs.set_log_config()
-        _loginited = True
 
 loginit()
 from django.core.wsgi import get_wsgi_application
