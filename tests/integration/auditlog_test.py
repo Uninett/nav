164b29e9405b27f0b0f2f01f198f2d494bda4f46
Hanne Moa <hanne.moa@uninett.no>
2019-02-25 13:34:13 +0100

    add missing tests for auditlog model

--- tests/integration/auditlog_test.py
+++ tests/integration/auditlog_test.py
@@ -10,17 +10,67 @@ from nav.auditlog.models import LogEntry
 from nav.auditlog.utils import get_auditlog_entries
 
 
-class AuditlogTestCase(TestCase):
+class AuditlogModelTestCase(TestCase):
 
     def setUp(self):
         # This specific model is used because it is very simple
         self.justification = Justification.objects.create(name='testarossa')
+
+    def test_str(self):
+        LogEntry.add_log_entry(self.justification, u'str test', 'foo')
+        l = LogEntry.objects.filter(verb='str test').get()
+        self.assertEqual(str(l), 'foo')
+        l.delete()
+
+    def test_add_log_entry_bad_template(self):
+        LogEntry.add_log_entry(self.justification, u'bad template test',
+                                  u'this is a {bad} template')
+        l = LogEntry.objects.filter(verb='bad template test').get()
+        self.assertEqual(l.summary, u'Error creating summary - see error log')
+        l.delete()
+#         # When on python3:
+#         with self.assertLogs(level='ERROR') as log:
+#             # run body
+#             self.assertEqual(len(log.output), 1)
+#             self.assertEqual(len(log.records), 1)
+#             self.assertIn('KeyError when creating summary:', log.output[0])
 
     def test_add_log_entry_actor_only(self):
         LogEntry.add_log_entry(self.justification, u'actor test',
                                   u'actor "{actor}" only is tested')
         l = LogEntry.objects.filter(verb='actor test').get()
         self.assertEqual(l.summary, u'actor "testarossa" only is tested')
+        l.delete()
+
+    def test_add_create_entry(self):
+        LogEntry.add_create_entry(self.justification, self.justification)
+        l = LogEntry.objects.filter(verb=u'create-justification').get()
+        self.assertEqual(l.summary, u'testarossa created testarossa')
+        l.delete()
+
+    def test_add_delete_entry(self):
+        LogEntry.add_delete_entry(self.justification, self.justification)
+        l = LogEntry.objects.filter(verb=u'delete-justification').get()
+        self.assertEqual(l.summary, u'testarossa deleted testarossa')
+        l.delete()
+
+    def test_compare_objects(self):
+        j1 = Justification.objects.create(name='ferrari', description='Psst!')
+        j2 = Justification.objects.create(name='lambo', description='Hush')
+        LogEntry.compare_objects(self.justification, j1, j2,
+                                 ('name', 'description'),
+                                 ('description',)
+        )
+        l = LogEntry.objects.filter(verb=u'edit-justification-name').get()
+        self.assertEqual(l.summary,
+                         u'testarossa edited lambo: name changed'
+                         u" from 'ferrari' to 'lambo'")
+        l.delete()
+        l = LogEntry.objects.filter(
+            verb=u'edit-justification-description'
+        ).get()
+        self.assertEqual(l.summary,
+                         u'testarossa edited lambo: description changed')
         l.delete()
 
     def test_addLog_entry_before(self):
