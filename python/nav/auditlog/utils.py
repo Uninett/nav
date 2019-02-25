d4b2269dba1eb3a9e5b8d9e07a32528edcb3bc40
Hanne Moa <hanne.moa@uninett.no>
2019-02-25 13:33:43 +0100

    make get_auditlog_entries work as intended

--- python/nav/auditlog/utils.py
+++ python/nav/auditlog/utils.py
@@ -9,14 +9,36 @@ from .models import LogEntry
 LATEST_N_AUDITLOG_ENTRIES = 15
 
 
-def get_auditlog_entries(iterable, limit=LATEST_N_AUDITLOG_ENTRIES, subsystem=None):
-    modelname = find_modelname(list(iterable)[0])
-    pks = [force_text(i.pk) for i in iterable]
-    object_query = Q(object_pk__in=pks, object_model=modelname)
-    target_query = Q(target_pk__in=pks, object_model=modelname)
-    actor_query = Q(actor_pk__in=pks, object_model=modelname)
+def get_auditlog_entries(modelname=None, queryset=None, pks=None,
+                         limit=LATEST_N_AUDITLOG_ENTRIES, subsystem=None):
+    """Get up to <limit> entries involving <modelname>
+
+    Limit results to <pks>, <queryset> and/or <subsystem>
+    """
+    assert modelname or queryset, ("At least one of <modelname> or "
+                                   "<queryset> must be given")
+    if pks is None:
+        pks = []
+    if queryset is not None and queryset.exists():
+        qs_pks = set(force_text(o.pk) for o in queryset)
+        if qs_pks:
+            if pks:
+                pks = qs_pks.intersect(pks)
+            else:
+                pks = qs_pks
+        if modelname is None:
+            modelname = find_modelname(queryset[0])
+    object_query = Q(object_model=modelname)
+    target_query = Q(target_model=modelname)
+    actor_query = Q(actor_model=modelname)
     filter_query = object_query | target_query | actor_query
     qs = LogEntry.objects.filter(filter_query)
+    if pks:
+        object_query = Q(object_pk__in=pks)
+        target_query = Q(target_pk__in=pks)
+        actor_query = Q(actor_pk__in=pks)
+        filter_query = object_query | target_query | actor_query
+        qs = qs.filter(filter_query)
     if subsystem:
         qs = qs.filter(subsystem=subsystem)
     entries = qs.distinct().order_by('-timestamp')[:limit]
--- tests/integration/auditlog_test.py
+++ tests/integration/auditlog_test.py
@@ -7,6 +7,7 @@ from nav.models.arnold import Justification
 
 from nav.auditlog import find_modelname
 from nav.auditlog.models import LogEntry
+from nav.auditlog.utils import get_auditlog_entries
 
 
 class AuditlogTestCase(TestCase):
@@ -32,3 +33,28 @@ class AuditlogTestCase(TestCase):
     def test_find_name(self):
         name = find_modelname(self.justification)
         self.assertEqual(name, 'blocked_reason')
+
+
+class AuditlogUtilsTestCase(TestCase):
+    def setUp(self):
+        # This specific model is used because it is very simple
+        self.justification = Justification.objects.create(name='testarossa')
+
+
+    def test_get_auditlog_entries(self):
+        modelname = 'blocked_reason'  # Justification's db_table
+        j1 = Justification.objects.create(name='j1')
+        j2 = Justification.objects.create(name='j2')
+        LogEntry.add_create_entry(self.justification, j1)
+        LogEntry.add_log_entry(self.justification, u'greet',
+                               u'{actor} greets {object}',
+                               object=j2, subsystem="hello")
+        LogEntry.add_log_entry(self.justification, u'deliver',
+                               u'{actor} delivers {object} to {target}',
+                               object=j1, target=j2, subsystem='delivery')
+        entries = get_auditlog_entries(modelname=modelname)
+        self.assertEqual(entries.count(), 3)
+        entries = get_auditlog_entries(modelname=modelname, subsystem='hello')
+        self.assertEqual(entries.count(), 1)
+        entries = get_auditlog_entries(modelname=modelname, pks=[j1.pk])
+        self.assertEqual(entries.count(), 2)
