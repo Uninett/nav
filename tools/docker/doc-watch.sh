979bb52f93579a047f1e1985fadda55117dcbf34
Morten Brekkevold <morten.brekkevold@uninett.no>
2019-02-27 10:04:55 +0100

    Make the docbuild container more reliable.
    I.e. fix the annoying "distribution not found" errors which cause the container
    to fail.

--- tools/docker/doc-watch.sh
+++ tools/docker/doc-watch.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash -e
 # Rebuilds Sphinx documentation on changes
 #
 cd /source
 # Build once first
-python setup.py build_sphinx
+python setup.py develop
+sudo -u nav python setup.py build_sphinx
 # Then re-build on any changes to the doc directory
 while inotifywait -e modify -e move -e create -e delete -r --exclude \# /source/doc
 do
-  python setup.py build_sphinx
+  sudo -u nav python setup.py build_sphinx
 done
