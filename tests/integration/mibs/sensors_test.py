98420ebaae34a26722839dac06c555d8fe54838c
Sigmund Augdal <sigmund.augdal@uninett.no>
2019-05-20 15:56:31 +0200

    Added integration test for getting sensors from APC PDUs
    This uses snmpsim and a recording of a snmpwalk of such a device to
    create a simulated snmp agent that the mibretriever code can query

---
+++ tests/integration/mibs/sensors_test.py
@@ -0,0 +1,78 @@
+from itertools import cycle
+import subprocess
+
+import pytest
+import pytest_twisted
+
+from nav.ipdevpoll.snmp import snmpprotocol, AgentProxy
+from nav.ipdevpoll.snmp.common import SNMPParameters
+from nav.mibs import powernet_mib
+
+
+ports = cycle([snmpprotocol.port() for i in range(50)])
+
+
+@pytest.fixture(scope='session')
+def snmpsim():
+    proc = subprocess.Popen([
+        '/usr/local/bin/snmpsimd.py',
+        '--data-dir=/source/tests/integration/snmp_fixtures',
+        '--log-level=error',
+        '--agent-udpv4-endpoint=127.0.0.1:1024'], env={'HOME': '/source'})
+    yield
+    proc.kill()
+
+
+@pytest.fixture()
+def snmp_agent(snmpsim):
+    port = next(ports)
+    agent = AgentProxy(
+        '127.0.0.1', 1024,
+        community='placeholder',
+        snmpVersion='v2c',
+        protocol=port.protocol,
+        snmp_parameters=SNMPParameters(timeout=0.5, max_repetitions=5,
+                                       throttle_delay=0)
+    )
+    return agent
+
+
+@pytest.mark.twisted
+@pytest_twisted.inlineCallbacks
+def test_apc_pdu(snmp_agent):
+    snmp_agent.community = 'apc-pdu'
+    snmp_agent.open()
+    mib = powernet_mib.PowerNetMib(snmp_agent)
+    res = yield mib.get_all_sensors()
+    assert res == [
+        {
+            'description': 'PDU Phase 1 ampere load',
+            'internal_name': 'rPDULoadStatusLoad1',
+            'mib': 'PowerNet-MIB',
+            'name': 'PDU Phase 1',
+            'oid': '.1.3.6.1.4.1.318.1.1.12.2.3.1.1.2.1',
+            'precision': 1,
+            'scale': None,
+            'unit_of_measurement': 'Ampere'
+        },
+        {
+            'description': 'PDU Bank 1 ampere load',
+            'internal_name': 'rPDULoadStatusLoad2',
+            'mib': 'PowerNet-MIB',
+            'name': 'PDU Bank 1',
+            'oid': '.1.3.6.1.4.1.318.1.1.12.2.3.1.1.2.2',
+            'precision': 1,
+            'scale': None,
+            'unit_of_measurement': 'Ampere'
+        },
+        {
+            'description': 'PDU Bank 2 ampere load',
+            'internal_name': 'rPDULoadStatusLoad3',
+            'mib': 'PowerNet-MIB',
+            'name': 'PDU Bank 2',
+            'oid': '.1.3.6.1.4.1.318.1.1.12.2.3.1.1.2.3',
+            'precision': 1,
+            'scale': None,
+            'unit_of_measurement': 'Ampere'
+        },
+    ]
