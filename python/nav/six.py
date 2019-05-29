a4a4787053fed58b98808cc586021414e195d250
Sigmund Augdal <sigmund.augdal@uninett.no>
2019-05-29 17:59:20 +0200

    Put symbols imported only to be importable in __all__ to indicate what they are imported for

--- python/nav/six.py
+++ python/nav/six.py
@@ -21,12 +21,10 @@ same that six lacks.
 from __future__ import absolute_import
 
 from django.utils import six
-
 try:
     from django.urls import reverse, NoReverseMatch, reverse_lazy
 except ImportError:
     from django.core.urlresolvers import reverse, NoReverseMatch, reverse_lazy
-
 
 if six.PY3:
     def encode_array(array):
@@ -34,3 +32,10 @@ if six.PY3:
 else:
     def encode_array(array):
         return array.tostring()
+
+__all__ = [
+    'reverse',
+    'NoReverseMatch',
+    'reverse_lazy',
+    'encode_array',
+]
