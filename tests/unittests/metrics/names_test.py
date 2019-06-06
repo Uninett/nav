46a28c90042ef8a61cf571e75bc4058a785187c7
Morten Brekkevold <morten.brekkevold@uninett.no>
2019-06-06 09:28:50 +0200

    Add unittests for #1958

--- tests/unittests/metrics/names_test.py
+++ tests/unittests/metrics/names_test.py
@@ -1,5 +1,6 @@
+import pytest
 from unittest import TestCase
-from nav.metrics.names import join_series
+from nav.metrics.names import join_series, escape_metric_name
 
 
 class MetricNamingTests(TestCase):
@@ -12,3 +13,21 @@ class MetricNamingTests(TestCase):
     def test_join_single_series_should_return_same(self):
         series = 'oh.freddled.gruntbuggly'
         self.assertEqual(join_series([series]), series)
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        (None, None),
+        ('', ''),
+        ('foobar', 'foobar'),
+        ('strangeness\x00', 'strangeness'),
+        ('dot.warner', 'dot_warner'),
+        ('ge-1/0/0.1', 'ge-1_0_0_1'),
+        ('temperature [chassis 1]', 'temperature__chassis_1_'),
+        ('something in (parens)', 'something_in__parens_'),
+        ('temperature, top', 'temperature__top'),
+    ]
+)
+def test_escape_metric_name(test_input, expected):
+    assert escape_metric_name(test_input) == expected
