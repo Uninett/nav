5a42b309ddca30ddf24bcbbbc5f8a1cd01813fc9
Morten Brekkevold <morten.brekkevold@uninett.no>
2019-06-14 09:06:20 +0200

    Reinstate the docs sidebar with local TOC.

--- doc/conf.py
+++ doc/conf.py
@@ -198,7 +198,7 @@ html_theme_options = {
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {'**': ['localtoc.html', 'sourcelink.html', 'searchbox.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
