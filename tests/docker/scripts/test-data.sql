233c115e465115a63463dfd7c158b69206b3ae29
Morten Brekkevold <morten.brekkevold@uninett.no>
2019-09-27 15:03:19 +0200

    Add a netbox with 1 interface to test data.
    This is done mainly to increase the reach of the web crawler; this
    should reveal some yet-to-be-discovered issues.

--- tests/docker/scripts/test-data.sql
+++ tests/docker/scripts/test-data.sql
@@ -9,3 +9,9 @@ UPDATE account SET name = 'Administrator ÆØÅ' WHERE login = 'admin';
 -- the shift to Python 3
 INSERT INTO location (locationid) VALUES ('bø');
 INSERT INTO room (roomid, locationid) VALUES ('bø-123', 'bø');
+
+-- Add a test netbox
+INSERT INTO netbox (ip, typeid, sysname, catid, orgid, roomid)
+VALUES ('192.168.0.42', (SELECT typeid FROM TYPE WHERE typename = 'hp2626A'), 'test-gsw.example.org', 'GSW', 'myorg', 'bø-123');
+INSERT INTO interface (netboxid, ifindex, ifname, ifdescr, iftype, speed, ifphysaddress, ifadminstatus, ifoperstatus, ifalias, vlan)
+VALUES ((SELECT netboxid FROM netbox WHERE sysname='test-gsw.example.org'), 1000, '1', 'Port #1', 6, 1000, '0c:0f:fe:eb:00:b5', 1, 1, 'Uplink to space', 42);
