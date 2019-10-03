9f5e2ed63f1765d1eda8c62d1ca3352a01531db5
Morten Brekkevold <morten.brekkevold@uninett.no>
2019-10-03 10:02:19 +0200

    Don't attempt to load unsupported dispatchers.
    Because:
    - A FatalDispatcherException is gracefully handled and logged by
      AlertEngine, while an ImportError is not.

--- python/nav/models/profiles.py
+++ python/nav/models/profiles.py
@@ -444,6 +444,8 @@ class AlertSender(models.Model):
     @transaction.atomic
     def send(self, *args, **kwargs):
         """Sends an alert via this medium."""
+        if not self.supported:
+            raise FatalDispatcherException("{} is not supported".format(self.name))
         if self.handler not in self._handlers:
             dispatcher_class = self._load_dispatcher_class()
             dispatcher = dispatcher_class(
