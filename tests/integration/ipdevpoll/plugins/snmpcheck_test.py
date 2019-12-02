db816b3c5c11b690c8ee0e235a4b497a0f25f7ae
Morten Brekkevold <morten.brekkevold@uninett.no>
2019-12-02 13:45:59 +0100

    Rewrite tests to match new snmpcheck behavior
    Because:
    - We now need to expect the plugin to always send a start event when the
      device isn't responding, even though we already know it to be down.

--- tests/integration/ipdevpoll/plugins/snmpcheck_test.py
+++ tests/integration/ipdevpoll/plugins/snmpcheck_test.py
@@ -1,4 +1,5 @@
 from mock import Mock
+from datetime import datetime
 
 import pytest
 import pytest_twisted
@@ -6,7 +7,8 @@ from twisted.internet import defer
 
 from nav.ipdevpoll.jobs import JobHandler, SuggestedReschedule
 from nav.ipdevpoll.plugins import snmpcheck, plugin_registry
-from nav.models.event import EventQueue
+from nav.models.event import EventQueue, AlertHistory
+from nav.models.fields import INFINITY
 
 
 @pytest.mark.twisted
@@ -32,7 +34,8 @@ def test_short_outage(localhost, db):
         event_type='snmpAgentState',
         netbox_id=localhost.pk,
         state=EventQueue.STATE_START).count() == 1
-    yield job.run()
+    with pytest.raises(SuggestedReschedule):
+        yield job.run()
     assert localhost.info_set.filter(
         key=snmpcheck.INFO_KEY_NAME,
         variable=snmpcheck.INFO_VARIABLE_NAME,
@@ -42,8 +45,20 @@ def test_short_outage(localhost, db):
         target_id='eventEngine',
         event_type='snmpAgentState',
         netbox_id=localhost.pk,
-        state=EventQueue.STATE_START).count() == 1
+        state=EventQueue.STATE_START).count() == 2
 
+    # now fake an AlertHist entry from event engine
+    AlertHistory(
+        source_id='ipdevpoll',
+        event_type_id='snmpAgentState',
+        netbox_id=localhost.pk,
+        start_time=datetime.now(),
+        end_time=INFINITY,
+        value=100,
+        severity=50,
+    ).save()
+
+    # and make sure snmpcheck tries to resolve it when the box is up
     agent.walk.return_value = defer.succeed(True)
     yield job.run()
     assert localhost.info_set.filter(
