01a7c7744fb0f6bbd6da8dee24aad28e8d1ec017
Morten Brekkevold <morten.brekkevold@uninett.no>
2020-01-09 12:27:14 +0100

    Rewrite unit tests to pytest
    Because:
    - pytest rules
    - All test cases were lumped in with a common setUp that was only common
      to three of the tests.

--- tests/unittests/report/util_test.py
+++ tests/unittests/report/util_test.py
@@ -1,37 +1,39 @@
-"""Tests"""
-from unittest import TestCase
+"""Tests for report view utility functions"""
 from nav.web.report.views import find_page_range
 
 
-class PageRangeTest(TestCase):
+class TestPageRangeForPage1(object):
     """Tests for find_page_range"""
 
-    def setUp(self):
-        self.page_num = 1
-        self.pages = range(1, 28)
-        self.result = find_page_range(self.page_num, self.pages, 5)
+    @classmethod
+    def setup_class(cls):
+        cls.page_num = 1
+        cls.pages = range(1, 28)
+        cls.result = find_page_range(cls.page_num, cls.pages, 5)
 
     def test_returns_list(self):
-        self.assertTrue(isinstance(self.result, list))
+        assert isinstance(self.result, list)
 
     def test_current_page_is_in_range(self):
-        self.assertTrue(self.page_num in self.result)
+        assert self.page_num in self.result
 
     def test_returns_num_visible_pages(self):
-        self.assertEqual(len(self.result), 5)
+        assert len(self.result) == 5
 
+
+class TestGenericPageRangeCalls(object):
     def test_except_when_total_is_less(self):
         result = find_page_range(1, range(1, 3), 3)
-        self.assertEqual(len(result), 2)
+        assert len(result) == 2
 
     def test_start_at_first_returns_correct_number(self):
         result = find_page_range(1, range(1, 11), 5)
-        self.assertEqual(len(result), 5)
+        assert len(result) == 5
 
     def test_start_at_last_returns_correct_number(self):
         result = find_page_range(11, range(1, 11), 5)
-        self.assertEqual(len(result), 5)
+        assert len(result) == 5
 
     def test_page_should_be_center(self):
         result = find_page_range(3, range(1, 11), 5)
-        self.assertEqual(result[2], 3)
+        assert result[2] == 3
