5394c343b8e6dd5c68a235c9eabfaeba80e61bda
Morten Brekkevold <morten.brekkevold@uninett.no>
2020-02-21 09:32:39 +0100

    Partially implement CNaaS NMS API handler methods

--- python/nav/portadmin/cnaas_nms/proxy.py
+++ python/nav/portadmin/cnaas_nms/proxy.py
@@ -16,7 +16,11 @@
 from nav.models import manage
 from nav.portadmin.config import CONFIG
 from nav.portadmin.cnaas_nms.lowlevel import get_api
-from nav.portadmin.handlers import ManagementHandler
+from nav.portadmin.handlers import (
+    ManagementHandler,
+    DeviceNotConfigurableError,
+    ProtocolError,
+)
 
 
 class CNaaSNMSMixIn(ManagementHandler):
@@ -32,13 +36,47 @@ class CNaaSNMSMixIn(ManagementHandler):
         self._api = get_api(config.url, config.token)
 
     def set_interface_description(self, interface: manage.Interface, description: str):
-        raise NotImplementedError
+        data = {"description": interface.ifalias}
+        payload = {"interfaces": {interface.ifdescr: data}}
+        self._api.interfaces.configure(self.netbox.sysname, body=payload)
 
     def set_interface_down(self, interface: manage.Interface):
-        raise NotImplementedError
+        self._set_interface_enabled(interface, enabled=False)
 
     def set_interface_up(self, interface: manage.Interface):
-        raise NotImplementedError
+        self._set_interface_enabled(interface, enabled=True)
+
+    def _set_interface_enabled(self, interface: manage.Interface, enabled=True):
+        data = {"enabled": enabled}
+        payload = {"interfaces": {interface.ifdescr: data}}
+        self._api.interfaces.configure(self.netbox.sysname, body=payload)
 
     def commit_configuration(self):
-        raise NotImplementedError
+        payload = {"hostname": self.netbox.sysname, "dry_run": False, "auto_push": True}
+        self._api.device_sync.syncto(body=payload)
+        # TODO: Get a job number from the syncto call
+        # TODO: Poll the job API for "status": "FINISHED"
+
+    def raise_if_not_configurable(self):
+        response = self._api.devices.retrieve(self.netbox.sysname)
+        payload = response.body
+        if response.status_code == 200 and payload.get("status") == "success":
+            if len(payload.get("devices", []) < 0):
+                raise CNaaSNMSApiError("No devices match this name in CNaaS NMS")
+            device = payload["devices"][0]
+            if not (
+                device.get("state") == "MANAGED"
+                and device.get("device_type") == "ACCESS"
+                and device.get("synchronized")
+            ):
+                raise DeviceNotConfigurableError(
+                    "CNaaS NMS will not permit configuration of this device"
+                )
+        else:
+            raise CNaaSNMSApiError("Cannot verify device status in CNaaS NMS")
+
+
+class CNaaSNMSApiError(ProtocolError):
+    """An exception raised whenever there is a problem with the responses from the
+    CNaaS NMS API
+    """
