b8b63fde6efa910b144c2063bf64911dbf9cf840
Morten Brekkevold <morten.brekkevold@uninett.no>
2020-03-27 13:40:39 +0100

    Handle unexpected IDNA encoding errors
    Because:
    - We never know what kind of garbage comes as input. Not every string in
      the world can be encoded to IDNA for DNS lookup, and if it can't then
      we might as well treat it as a non-answerable DNS lookup.

--- python/nav/web/ipdevinfo/host_information.py
+++ python/nav/web/ipdevinfo/host_information.py
@@ -51,7 +51,12 @@ def _get_host_info(host):
     if is_valid_ip(host):
         addresses = list(reverse_lookup([host]))
     else:
-        addresses = forward_lookup(host) or []
+        try:
+            addresses = forward_lookup(host) or []
+        except UnicodeError:
+            # Probably just some invalid string that cannot be represented using IDNA
+            # encoding. Let's just pretend it never happened (i.e. we can't look it up)
+            addresses = []
         if addresses:
             addresses = list(reverse_lookup(addresses))
 
