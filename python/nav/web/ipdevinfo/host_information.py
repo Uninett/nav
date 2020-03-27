cdced2c6e5e935d6f839083d9df69f1f94fbba2e
Morten Brekkevold <morten.brekkevold@uninett.no>
2020-03-27 13:42:01 +0100

    Use strict IP address validation in ipdevinfo.
    Because:
    - We never know what kind of garbage is used as input. It might be
      parseable as an IP address to IP.IPy, but not necessarily to
      everything else we feed the string to, such as directly to PostgreSQL
      queries. The string 02.44.02 would cause ipdevinfo to crash.

--- python/nav/web/ipdevinfo/host_information.py
+++ python/nav/web/ipdevinfo/host_information.py
@@ -48,7 +48,7 @@ def reverse_lookup(addresses):
 
 def _get_host_info(host):
     """Returns a dictionary containing DNS information about the host"""
-    if is_valid_ip(host):
+    if is_valid_ip(host, use_socket_lib=True):
         addresses = list(reverse_lookup([host]))
     else:
         try:
