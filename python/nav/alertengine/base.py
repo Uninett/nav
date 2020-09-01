Uninett/nav

commit 61bd1716b53f5e8a7f856132b77ea1dc07ea2346

Calculate the real start time of a looping period
Because:
- When the current time period is a looping one, it will in most cases cross a
  date line. It made no sense to just compare the time the alert was inserted
  with the start time of the period if no dates were considered in the
  comparison.
- The old logic would cause notifications to stay permanently delayed if the
  alerts were generated earlier in the day than when the looping timeperiod
  started.

This factors out the proper calculation of the start *datetime* of a period,
and refactors the logic of _verify_next_dispatch to be more succinct.

Fixes #2165

python/nav/alertengine/base.py
@@ -449,26 +449,22 @@ def _verify_next_dispatch(queued_alert, now, _logger=_logger):
 
         insertion_time = queued_alert.insertion_time
         queued_alert_time_period = subscription.time_period
-        is_different_timeperiod = queued_alert_time_period.id != current_time_period.id
 
-        # Send if we are in a different time period than the one that
-        # the message was inserted with.
-        _logger.debug('Tests: different time period %s', is_different_timeperiod)
-
-        # Check if the message was inserted on a previous day and
-        # that the start period of the time period it was inserted in
-        # has passed. This check should catch the corner case where
-        # a user only has one timeperiod that loops.
         timeperiod_count = _get_number_of_timeperiods_today(active_profile, now)
-        _logger.debug(
-            'Tests: only one time period %s, insertion time %s',
-            timeperiod_count == 1,
-            insertion_time.time() < queued_alert_time_period.start)
+        if timeperiod_count == 1:  # we have a looping time period
+            start_time = _calculate_timeperiod_start(queued_alert_time_period, now)
+            was_inserted_before = insertion_time < start_time
+            _logger.debug(
+                "Tests: Only one time period, inserted in previous iteration: %s",
+                was_inserted_before
+            )
+        else:
+            was_inserted_before = queued_alert_time_period.id != current_time_period.id
+            _logger.debug(
+                "Tests: Inserted in different time period: %s", was_inserted_before
+            )
 
-        return is_different_timeperiod or (
-            timeperiod_count == 1
-            and insertion_time.time() < queued_alert_time_period.start
-        )
+        return was_inserted_before
 
 
 def _get_number_of_timeperiods_today(alertprofile, now):
@@ -481,6 +477,24 @@ def _get_number_of_timeperiods_today(alertprofile, now):
     else:
         valid_during = [TimePeriod.ALL_WEEK, TimePeriod.WEEKDAYS]
     return alertprofile.timeperiod_set.filter(valid_during__in=valid_during).count()
+
+
+def _calculate_timeperiod_start(timeperiod, now=None):
+    """Given a timeperiod that is looping, i.e. it is the only timeperiod in a
+    profile, this calculates the full datetime when the current time period started.
+
+    :type timeperiod: TimePeriod
+    :type now: datetime
+    """
+    if not now:
+        now = datetime.now()
+
+    if now.time() < timeperiod.start:
+        date = now.date() - timedelta(days=1)  # yesterday
+    else:
+        date = now.date()  # today
+
+    return datetime.combine(date, timeperiod.start)
 
 
 def alert_should_be_ignored(queued_alert, subscription, now):

commit d121ca5617c62b43f7f44b82a869bb249cfd5426

Add missing import
Fixes a regression caused by 61bd1716b53f5e8a7f856132b77ea1dc07ea2346, which attempted to fix #2165

Closes #2182

python/nav/alertengine/base.py
@@ -20,7 +20,7 @@
 
 import gc
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django.db import transaction, reset_queries
 from django.utils.lru_cache import lru_cache