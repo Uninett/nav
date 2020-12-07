Uninett/nav

commit 065ee61aff5eab9524d0fe5d4ffa6eaf877e0172

Validate and cleanly bulk import room positions
Because:
- For some reason, proper validation of the room bulk import column
  "position" was never implemented.
- The bulk importer also seemed to expect the model to validate the
  value itself, but it doesn't. This may have broken due to changes
  in the way Django models work, IDK.

Fixes #2213

python/nav/bulkparse.py
@@ -25,6 +25,7 @@ import json
 from django.utils import six
 from IPy import IP
 
+from nav.django.validators import is_valid_point_string
 from nav.errors import GeneralException
 from nav.models.manage import Location, Room, Organization, Vendor, Usage, ManagementProfile
 
@@ -295,6 +296,10 @@ class RoomBulkParser(BulkParser):
     @classmethod
     def _validate_roomid(cls, value):
         return len(value) <= cls.roomid_maxlength
+
+    @classmethod
+    def _validate_position(cls, value):
+        return is_valid_point_string(value)
 
 
 class ServiceBulkParser(BulkParser):

python/nav/bulkimport.py
@@ -24,6 +24,7 @@ import json
 from django.core.exceptions import ValidationError
 from django.utils import six
 
+from nav.models.fields import PointField
 from nav.models.manage import Netbox, Room, Organization
 from nav.models.manage import Category, NetboxInfo, NetboxGroup
 from nav.models.manage import NetboxCategory, Interface
@@ -233,7 +234,7 @@ class RoomImporter(BulkImporter):
         room = Room(id=row['roomid'], location=location,
                     description=row['descr'], data=attributes)
         try:
-            room.position = row['position']
+            room.position = PointField().to_python(row['position'])
         except (ValidationError, ValueError):
             raise InvalidValue(row['position'])
         return [room]

commit 226262b8b1565cdd15f40dcfce015509a97f402f

Only validate real values
Because:
- Empty position values are allowed
- The validator doesn't support validating None values and will crash
  if one is given.

This regression was introduced by 065ee61.
Fixes #2234

python/nav/bulkparse.py
@@ -299,7 +299,7 @@ class RoomBulkParser(BulkParser):
 
     @classmethod
     def _validate_position(cls, value):
-        return is_valid_point_string(value)
+        return is_valid_point_string(value) if value is not None else True
 
 
 class ServiceBulkParser(BulkParser):