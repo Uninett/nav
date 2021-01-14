ad0f8f5d59e2f27d5b15c2912585b3bded1f3c7d
Morten Brekkevold <morten.brekkevold@uninett.no>
2021-01-14 13:25:22 +0100

    Test siNumbers formatter from rickshaw-utils

---
+++ python/nav/web/static/js/test/plugins/rickshaw-utils-test.js
@@ -0,0 +1,42 @@
+define([
+    'rickshaw-utils',
+    'libs/jquery'], function (plugin) {
+    describe("siNumbers", function () {
+        it("should format two-digit numbers right", function() {
+            assert.equal(plugin.siNumbers(42.0), "42.00");
+        });
+
+        it("should format milli-scale numbers right", function() {
+            assert.equal(plugin.siNumbers(0.0042), "4.20 m");
+        });
+
+        it("should format micro-scale numbers right", function() {
+            assert.equal(plugin.siNumbers(0.0000042), "4.20 µ");
+        });
+
+        it("should format nano-scale numbers right", function() {
+            assert.equal(plugin.siNumbers(0.0000000042), "4.20 n");
+        });
+
+        it("should format pico-scale numbers right", function() {
+            assert.equal(plugin.siNumbers(0.0000000000042), "4.20 p");
+        });
+
+        it("should format mega-scale numbers right", function() {
+            assert.equal(plugin.siNumbers(42420000), "42.42 M");
+        });
+
+        it("should format giga-scale numbers right", function() {
+            assert.equal(plugin.siNumbers(42420000000), "42.42 G");
+        });
+
+        it("should format tera-scale numbers right", function() {
+            assert.equal(plugin.siNumbers(42420000000000), "42.42 T");
+        });
+
+        it("should format negative numbers right", function() {
+            assert.equal(plugin.siNumbers(-0.0042), "-4.20 m");
+        });
+
+    });
+});
