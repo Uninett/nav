409d57ff65cfa88b7c0482f5c7c0fa3aba51b683
Morten Brekkevold <morten.brekkevold@uninett.no>
2021-05-05 14:16:04 +0200

    Add decorator for SNMP error translations
    Because:
    - Most of the SNMPErrors raised by the underlying SNMP library would
      bleed through to the PortAdmin API views.
    - The PortAdmin API views are not equipped to handle arbitrary
      exceptions, but any ProtocolError raised will be reported to the API
      client using JSON objects and shown to the end user in the PortAdmin
      UI.
    - The latter is a better approach than just saying "Failed" to the end
      user and mailing a huge traceback to the site admin, as most
      SNMPErrors are timeouts or error messages from SNMP agents that will
      not accept the values PortAdmin attempt to set, and do not stem
      from actual code problems in NAV.

--- python/nav/portadmin/snmp/base.py
+++ python/nav/portadmin/snmp/base.py
@@ -14,6 +14,7 @@
 # along with NAV. If not, see <http://www.gnu.org/licenses/>.
 #
 import time
+from functools import wraps
 from operator import attrgetter
 import logging
 from typing import Dict, Sequence, List, Any
@@ -43,6 +44,28 @@ from nav.smidumps import get_mib
 
 
 _logger = logging.getLogger(__name__)
+
+
+def translate_protocol_errors(func):
+    """Decorator that translates SNMPErrors into PortAdmin ProtocolErrors.
+
+    The PortAdmin API will handle ProtocolErrors gracefully, but other exceptions
+    will bleed through and be handled by Django's 500 handler.
+    """
+
+    @wraps(func)
+    def _wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except SnmpError as error:
+            logmsg = "An SnmpError was raised:"
+            if args and isinstance(args[0], SNMPHandler):
+                sysname = args[0].netbox.sysname
+                logmsg = f"{sysname}: {logmsg}"
+            _logger.exception(logmsg)
+            raise ProtocolError(error)
+
+    return _wrapper
 
 
 class SNMPHandler(ManagementHandler):
