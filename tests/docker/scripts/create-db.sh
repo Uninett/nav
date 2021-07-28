76e45834b913f5fa826c277e09bab3d2e46e8cc7
Morten Brekkevold <morten.brekkevold@uninett.no>
2021-07-28 14:49:00 +0200

    Support multiple methods of PostgreSQL test init
    Because:
    - When running on GitHub Actions, we do not run inside our self-defined
      Docker container.
    - CI environments like GitHub Actions can provide the test suite with an
      external PostgreSQL instance, in which case we don't need root access
      to manipulate a PostgreSQL installation on the current system/container.

--- tests/docker/scripts/create-db.sh
+++ tests/docker/scripts/create-db.sh
@@ -1,9 +1,22 @@
 #!/bin/bash -xe
+# Creates and initializes a NAV database for the test runner
+
+maybesudo() {
+    # Run command using gosu or sudo, unless we're running on GitHub Actions
+    user="$1"
+    shift
+    if [ -n "$GITHUB_ACTIONS" ]; then
+        $@
+    else
+        gosu "$user" $@
+    fi
+}
+
 update_nav_db_conf() {
     # Update db config
     DBCONF="${BUILDDIR}/etc/db.conf"
     echo "Updating $DBCONF"
-    gosu root tee "$DBCONF" <<EOF
+    maybesudo root tee "$DBCONF" <<EOF
 dbhost=${PGHOST:-localhost}
 dbport=${PGPORT:-5432}
 db_nav=${PGDATABASE:-nav}
@@ -17,20 +30,23 @@ create_nav_db() {
 
     # Create and populate database
     echo Creating and populating initial database
-    gosu postgres:postgres "${BUILDDIR}/bin/navsyncdb" -c
+    maybesudo postgres:postgres "${BUILDDIR}/bin/navsyncdb" -c
 
     if [ -n "$ADMINPASSWORD" ]; then
-      gosu postgres:postgres psql -c "UPDATE account SET password = '$ADMINPASSWORD' WHERE login = 'admin'" nav
+      maybesudo postgres:postgres psql -c "UPDATE account SET password = '$ADMINPASSWORD' WHERE login = 'admin'" nav
     fi
 
     # Add generic test data set
-    gosu postgres:postgres psql -f "$(dirname $0)/test-data.sql" nav
+    maybesudo postgres:postgres psql -f "$(dirname $0)/test-data.sql" nav
 
 }
 
-PGVERSION=$(gosu root pg_lsclusters -h|awk '{print $1}')
-gosu root pg_dropcluster --stop ${PGVERSION} main || true
-gosu root pg_createcluster --locale=C.UTF-8 --start ${PGVERSION} main -- --nosync
+if [ -z "$GITHUB_ACTIONS" ]; then
+    # If not on GitHub actions, we manipulate PostgreSQL clusters directly
+    PGVERSION=$(gosu root pg_lsclusters -h|awk '{print $1}')
+    gosu root pg_dropcluster --stop ${PGVERSION} main || true
+    gosu root pg_createcluster --locale=C.UTF-8 --start ${PGVERSION} main -- --nosync
+fi
 
 update_nav_db_conf
 create_nav_db
