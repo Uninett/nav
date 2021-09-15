a96eb45005c99f4e1075c7677c6521d80c87069e
Morten Brekkevold <morten.brekkevold@uninett.no>
2021-09-15 14:23:48 +0200

    Configure Sphinx docs for 5.2.x branch

--- doc/conf.py
+++ doc/conf.py
@@ -58,7 +58,7 @@ copyright = u'2012-2021, Uninett AS'
 # built documents.
 #
 # The short X.Y version.
-version = 'dev'
+version = '5.2'
 # version = '.'.join(buildconf.VERSION.split('.')[:2])
 # The full version, including alpha/beta/rc tags.
 release = buildconf.VERSION
