e3e0bade98102e7a7b71457b0dde8c14d78d7f88
Morten Brekkevold <morten.brekkevold@uninett.no>
2021-12-03 17:42:46 +0100

    Convert test to pure pytest

--- tests/unittests/smsd/gammudispatcher_test.py
+++ tests/unittests/smsd/gammudispatcher_test.py
@@ -1,5 +1,3 @@
-from unittest import TestCase
-
 # mock/patch/hack/workaround: gammudispatcher normally requires gammu to be
 # installed, but we're only testing functionality from the module that is not
 # dependent on this. If there is no gammu module, we fake it so we can import
@@ -10,22 +8,22 @@ except ImportError:
     import sys
 
     gammu = sys.modules['gammu'] = type(sys)('gammu')
-from nav.smsd.gammudispatcher import decode_sms_to_unicode
+from nav.smsd.gammudispatcher import decode_sms_to_unicode, GammuDispatcher
 
 
-class EncodingTests(TestCase):
-    def test_ascii_string_should_decode_to_equal_object(self):
+class TestThat_decode_sms_to_unicode:
+    def test_should_decode_ascii_bytes_to_comparable_string(self):
         sms = 'Hello'
         unicode_sms = decode_sms_to_unicode(sms)
-        self.assertEqual(sms, unicode_sms)
+        assert sms == unicode_sms
 
-    def test_unicode_object_should_be_decoded_to_equal_object(self):
+    def test_should_decode_string_to_comparable_string(self):
         sms = u'A m\xf8\xf8se once bit my sister'
         unicode_sms = decode_sms_to_unicode(sms)
-        self.assertEqual(sms, unicode_sms)
+        assert sms == unicode_sms
 
-    def test_utf_8_string_should_properly_decode(self):
+    def test_should_decode_utf8_string_properly(self):
         sms = b'A m\xc3\xb8\xc3\xb8se once bit my sister'
         unicode_sms = decode_sms_to_unicode(sms)
         expected = u'A m\xf8\xf8se once bit my sister'
-        self.assertEqual(unicode_sms, expected)
+        assert unicode_sms == expected
