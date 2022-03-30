c4d125438ee30dcd8533d5a7359c12cd753529be
Hanne Moa <hanne.moa@sikt.no>
2022-03-30 13:32:57 +0200

    Make directory to hold contrib scripts
    - Include a todo for a script to push dhcpd-metrics to graphite

---
+++ contrib/scripts/isc-dhpcd-graphite.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Docs here"""
+
+# run command and store json output
+# pickle the data
+# send the data
+
+if __name__ == '__main__':
+    pass
+    # get command-line flags
+    # - metric-prefix
+    # - graphite server name
+    # - port (2004 by default)
+    # - location (optional, string)
+    # - pickle protocol (default: 2?)
