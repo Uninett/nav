4015c1d3a76ff20014510fc5aa5a1e99cee49af2
Johanna England <johanna.england@sikt.no>
2022-07-07 13:36:46 +0200

    Test naventity with ip address and port as input

--- tests/integration/bin_test.py
+++ tests/integration/bin_test.py
@@ -1,3 +1,4 @@
+import pytest
 import sys
 import subprocess
 
@@ -9,5 +10,29 @@ def test_binary_runs(binary):
     retcode = proc.wait()
 
     print(done.decode('utf-8'))
+
+    assert retcode == 0
+
+
+@pytest.mark.timeout(10)
+def test_naventity_runs_without_error_with_arguments(localhost, snmpsim):
+    """
+    Verifies that naventity runs with a zero exit code when given an
+    ip address and a port
+
+    Added in regards to: https://github.com/Uninett/nav/issues/2433
+    """
+    proc = subprocess.Popen(
+        ["./bin/naventity", localhost.ip, "-p", "1024"],
+        stderr=subprocess.STDOUT,
+        stdout=subprocess.PIPE,
+    )
+    (done, fail) = proc.communicate()
+    retcode = proc.wait()
+
+    if done:
+        print(done.decode('utf-8'))
+    if fail:
+        print(fail.decode('utf-8'))
 
     assert retcode == 0
