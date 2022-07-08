e3b372ed31807a2b6b4b6ad16dc9b4e2f12e95f0
Morten Brekkevold <morten.brekkevold@sikt.no>
2022-07-08 13:13:37 +0200

    Add regression tests for #2447

---
+++ tests/unittests/metrics/thresholds_test.py
@@ -0,0 +1,24 @@
+"""Unit tests for nav.metrics.thresholds"""
+from nav.metrics.thresholds import ThresholdEvaluator
+
+
+class TestThatThresholdEvaluator:
+    def test_evaluates_zero_as_less_than_positive_number(self):
+        """A regression test for #2447"""
+        t = ThresholdEvaluator('data.*')
+        t.result = {
+            'data.good': {'value': 200.0},
+            'data.zero': {'value': 0.0},
+        }
+
+        assert ('data.zero', 0.0) in t.evaluate('<120')
+
+    def test_evaluates_zero_as_greater_than_negative_number(self):
+        """A regression test for #2447"""
+        t = ThresholdEvaluator('data.*')
+        t.result = {
+            'data.good': {'value': -50.0},
+            'data.zero': {'value': 0.0},
+        }
+
+        assert ('data.zero', 0.0) in t.evaluate('>-120')
