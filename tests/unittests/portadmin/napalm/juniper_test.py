9a22db30171a240b5bbb5989f236111a1c789127
Morten Brekkevold <morten.brekkevold@sikt.no>
2022-07-21 12:30:55 +0200

    Add regression test for #2452

--- tests/unittests/portadmin/napalm/juniper_test.py
+++ tests/unittests/portadmin/napalm/juniper_test.py
@@ -13,12 +13,14 @@
 # more details.  You should have received a copy of the GNU General Public
 # License along with NAV. If not, see <http://www.gnu.org/licenses/>.
 #
+from unittest.mock import Mock, patch
+
 import pytest
 
 from jnpr.junos.exception import RpcError
 
 from nav.portadmin.handlers import ProtocolError
-from nav.portadmin.napalm.juniper import wrap_unhandled_rpc_errors
+from nav.portadmin.napalm.juniper import wrap_unhandled_rpc_errors, Juniper
 
 
 class TestWrapUnhandledRpcErrors:
@@ -37,3 +39,18 @@ class TestWrapUnhandledRpcErrors:
 
         with pytest.raises(TypeError):
             wrapped_function()
+
+
+class TestJuniper:
+    @patch('nav.models.manage.Vlan.objects', Mock(return_value=[]))
+    def test_get_netbox_vlans_should_ignore_vlans_with_non_integer_tags(self):
+        """Regression test for #2452"""
+
+        class MockedJuniperHandler(Juniper):
+            @property
+            def vlans(self):
+                """Mock a VLAN table response from the device"""
+                return [Mock(tag='NA'), Mock(tag='10')]
+
+        m = MockedJuniperHandler(Mock())
+        assert len(m.get_netbox_vlans()) == 1
