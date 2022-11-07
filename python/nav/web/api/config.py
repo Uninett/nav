7880b57cd1c63a1192e869db835ce31554a63ad7
Simon Oliver Tveit <simtve@uninett.no>
2022-11-07 11:07:10 +0100

    Raise exception if no public key
    Raise exception if public key not configured

--- python/nav/web/api/config.py
+++ python/nav/web/api/config.py
@@ -1,12 +1,12 @@
-from os.path import isabs, join
-from nav.config import NAVConfigParser, find_config_dir
+from os.path import isabs, join, dirname
+from nav.config import ConfigurationError, NAVConfigParser, find_config_file
 
 
 class APIConfig(NAVConfigParser):
     DEFAULT_CONFIG_FILES = (join("api", "api.conf"),)
     DEFAULT_CONFIG = u"""
 [keys]
-public_key=jwtRS256.key.pub
+public_key=/etc/nav/api/jwtRS256.key.pub
 """
 
     def get_public_key(self):
@@ -14,8 +14,11 @@ public_key=jwtRS256.key.pub
 
     def get_key(self, key_name):
         path = self.get('keys', key_name)
+        if not path:
+            raise ConfigurationError(f"{key_name} is not configured")
         if not isabs(path):
-            path = join(find_config_dir(), "api", path)
+            config_file = find_config_file(self.DEFAULT_CONFIG_FILES[0])
+            path = join(dirname(config_file), path)
         with open(path) as f:
             return f.read()
 
