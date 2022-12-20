1d054ccde8c72912f2f878c5d306a5b252714e2d
Simon Oliver Tveit <simtve@uninett.no>
2022-12-20 09:37:01 +0100

    Restructure jwt config parser

--- python/nav/jwtconf.py
+++ python/nav/jwtconf.py
@@ -17,25 +17,34 @@ class JWTConf(NAVConfigParser):
         for section in self.sections():
             try:
                 get = partial(self.get, section)
-                if get('type') not in ['JWKS', 'PEM']:
-                    raise ConfigurationError(
-                        "Invalid 'type' in section %s: 'type' must be either 'JWKS' or 'PEM'"
-                        % section
-                    )
-                if not get('key'):
-                    raise ConfigurationError(
-                        "Invalid 'key' in section %s: 'key' must not be empty" % section
-                    )
-                if not get('audience'):
-                    raise ConfigurationError(
-                        "Invalid 'audience' in section %s: 'audience' must not be empty"
-                        % section
-                    )
+                key = self._validate_key(get('key'))
+                aud = self._validate_key(get('audience'))
+                key_type = self._validate_key(get('type'))
+                if key == 'PEM':
+                    with open("demofile.txt", "r") as f:
+                        key = f.read()
                 issuers_settings[section] = {
-                    'type': get('type'),
-                    'key': get('key'),
-                    'aud': get('audience'),
+                    'key': key,
+                    'type': key_type,
+                    'aud': aud,
                 }
-            except (configparser.Error) as error:
+            except (configparser.Error, ConfigurationError) as error:
                 _logger.error('Error collecting stats for %s: %s', section, error)
         return issuers_settings
+
+    def _validate_key(self, key):
+        if not key:
+            raise ConfigurationError("Invalid 'key': 'key' must not be empty")
+        return key
+
+    def _validate_type(self, key_type):
+        if key_type not in ['JWKS', 'PEM']:
+            raise ConfigurationError(
+                "Invalid 'type': 'type' must be either 'JWKS' or 'PEM'"
+            )
+        return key_type
+
+    def _validate_audience(self, audience):
+        if not audience:
+            raise ConfigurationError("Invalid 'audience': 'audience' must not be empty")
+        return audience
