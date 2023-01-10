c3e378e3605d66721e5f80d4e3552d677a65ad6b
Johanna England <johanna.england@sikt.no>
2023-01-10 15:37:22 +0100

    Change assumptions to be more robust

--- tests/integration/ipdevpoll/plugins/snmpcheck_test.py
+++ tests/integration/ipdevpoll/plugins/snmpcheck_test.py
@@ -21,37 +21,32 @@ def test_short_outage(localhost, db):
     job._create_agentproxy = Mock()
     job._destroy_agentproxy = Mock()
     agent.walk.return_value = defer.succeed(False)
+    events = EventQueue.objects.filter(
+        source_id='ipdevpoll',
+        target_id='eventEngine',
+        event_type='snmpAgentState',
+        netbox_id=localhost.pk,
+    )
+    start_events = events.filter(state=EventQueue.STATE_START)
+    end_events = events.filter(state=EventQueue.STATE_END)
+    start_events_first_count = start_events.count()
+    end_events_first_count = end_events.count()
+
     with pytest.raises(SuggestedReschedule):
         yield job.run()
+
     assert agent.walk.called
     assert localhost.info_set.filter(
         key=snmpcheck.INFO_KEY_NAME, variable=snmpcheck.INFO_VARIABLE_NAME, value="down"
     ).exists()
-    assert (
-        EventQueue.objects.filter(
-            source_id='ipdevpoll',
-            target_id='eventEngine',
-            event_type='snmpAgentState',
-            netbox_id=localhost.pk,
-            state=EventQueue.STATE_START,
-        ).count()
-        == 1
-    )
+    assert start_events.count() == start_events_first_count + 1
+
     with pytest.raises(SuggestedReschedule):
         yield job.run()
     assert localhost.info_set.filter(
         key=snmpcheck.INFO_KEY_NAME, variable=snmpcheck.INFO_VARIABLE_NAME, value="down"
     ).exists()
-    assert (
-        EventQueue.objects.filter(
-            source_id='ipdevpoll',
-            target_id='eventEngine',
-            event_type='snmpAgentState',
-            netbox_id=localhost.pk,
-            state=EventQueue.STATE_START,
-        ).count()
-        == 2
-    )
+    assert start_events.count() == start_events_first_count + 2
 
     # now fake an AlertHist entry from event engine
     AlertHistory(
@@ -70,13 +65,4 @@ def test_short_outage(localhost, db):
     assert localhost.info_set.filter(
         key=snmpcheck.INFO_KEY_NAME, variable=snmpcheck.INFO_VARIABLE_NAME, value="up"
     ).exists()
-    assert (
-        EventQueue.objects.filter(
-            source_id='ipdevpoll',
-            target_id='eventEngine',
-            event_type='snmpAgentState',
-            netbox_id=localhost.pk,
-            state=EventQueue.STATE_END,
-        ).count()
-        == 1
-    )
+    assert end_events.count() == end_events_first_count + 1
