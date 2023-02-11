fd078b64c6b8c73f7cd33a4a6ce14cc9ae018dbe
Simon Oliver Tveit <simtve@uninett.no>
2023-02-11 02:11:39 +0100

    Change field to refer to oid of other sensor

--- python/nav/models/sql/changes/sc.05.05.0002.sql
+++ python/nav/models/sql/changes/sc.05.05.0002.sql
@@ -1,6 +1,5 @@
 ALTER TABLE sensor
   ADD threshold_type INT,
   ADD threshold_alert_type INT,
-  ADD threshold_for_id INT,
-  ADD CONSTRAINT FOREIGN KEY(threshold_for_id) REFERENCES sensor(threshold_for_id)
-;
+  ADD threshold_for_oid_id INT,
+  ADD CONSTRAINT fk_threshold_for_oid_id FOREIGN KEY (threshold_for_oid_id) REFERENCES sensor(sensorid);
