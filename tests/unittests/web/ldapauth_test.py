8a76e8e5b49c3fa0da36f96c49d0c1340a53cd57
Morten Brekkevold <morten.brekkevold@sikt.no>
2023-02-21 12:00:12 +0100

    Improve unit test coverage of ldapauth.py
    Adds unit tests for open_ldap() function, with mocked LDAP connections

--- tests/unittests/web/ldapauth_test.py
+++ tests/unittests/web/ldapauth_test.py
@@ -1,5 +1,5 @@
 from nav.config import NAVConfigParser
-from nav.web.ldapauth import LDAPUser
+from nav.web.ldapauth import LDAPUser, open_ldap
 from mock import Mock, patch
 
 
@@ -33,3 +33,39 @@ def test_ldapuser_search_dn_decode_regression():
     user = LDAPUser('zaphod', connection)
     dn, uid = user.search_dn()
     assert dn == u'CN=Zaphod Beeblebr\xf6x,CN=people,DC=example,DC=org'
+
+
+class LdapOpenTestConfig(NAVConfigParser):
+    DEFAULT_CONFIG_FILES = []
+    DEFAULT_CONFIG = u"""
+[ldap]
+server=ldap.example.org
+port=636
+encryption=tls
+timeout=3
+debug=true
+"""
+
+
+@patch('nav.web.ldapauth._config', LdapOpenTestConfig())
+def test_open_ldap_should_run_without_error():
+    with patch('ldap.initialize') as initialize:
+        assert open_ldap()
+
+
+class LdapOpenTestInvalidEncryptionConfig(NAVConfigParser):
+    DEFAULT_CONFIG_FILES = []
+    DEFAULT_CONFIG = u"""
+[ldap]
+server=ldap.example.org
+port=636
+encryption=invalid
+timeout=3
+debug=true
+"""
+
+
+@patch('nav.web.ldapauth._config', LdapOpenTestInvalidEncryptionConfig())
+def test_when_encryption_setting_is_invalid_open_ldap_should_run_without_encryption():
+    with patch('ldap.initialize') as initialize:
+        assert open_ldap()
