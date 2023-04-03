a4bd85952e1c13165b96ce530f9cabd5a6f04337
Johanna England <johanna.england@sikt.no>
2023-04-03 13:41:55 +0200

    Catch Validation error in API prefix filtering

--- tests/integration/web/prefixviewset_test.py
+++ tests/integration/web/prefixviewset_test.py
@@ -49,6 +49,28 @@ class TestVlanFilter:
         assert prefix.id in prefix_ids
 
 
+class TestIsNetAddressFilter:
+    def test_prefix_with_given_address_is_returned(self, client, prefix):
+        response = client.get(prefix_url, {"net_address": prefix.net_address})
+        assert response.status_code == 200
+        content = json.loads(response.content.decode('utf-8'))
+        prefix_ids = [prefix['id'] for prefix in content['results']]
+        assert prefix.id in prefix_ids
+
+    def test_prefix_that_is_not_given_address_is_not_returned(self, client, prefix):
+        response = client.get(prefix_url, {"net_address": "20.4.111.0/24"})
+        assert response.status_code == 200
+        content = json.loads(response.content.decode('utf-8'))
+        prefix_ids = [prefix['id'] for prefix in content['results']]
+        assert prefix.id not in prefix_ids
+
+    def test_error_is_returned_if_given_address_is_not_valid_cidr_address(
+        self, client, prefix
+    ):
+        response = client.get(prefix_url, {"net_address": "invalid_address"})
+        assert response.status_code == 400
+
+
 ###
 #
 # Fixtures
