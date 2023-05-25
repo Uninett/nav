7f9f30d77438837877b6c0ad1dafaa4f72496d8b
Morten Brekkevold <morten.brekkevold@sikt.no>
2023-05-25 15:46:38 +0200

    Add simple event logger function as event trigger.
    This is just for testing that the notification mechanism works.

--- python/nav/ipdevpoll/daemon.py
+++ python/nav/ipdevpoll/daemon.py
@@ -125,7 +125,9 @@ class IPDevPollProcess(object):
             self.work_pool,
             self.options.onlyjob,
         )
-        reactor.callWhenRunning(db.subscribe_to_event_notifications)
+        reactor.callWhenRunning(
+            db.subscribe_to_event_notifications, schedule.log_received_events
+        )
 
         def log_scheduler_jobs():
             JobScheduler.log_active_jobs(logging.INFO)
@@ -195,7 +197,9 @@ class IPDevPollProcess(object):
             self.work_pool,
             self.options.onlyjob,
         )
-        reactor.callWhenRunning(db.subscribe_to_event_notifications)
+        reactor.callWhenRunning(
+            db.subscribe_to_event_notifications, schedule.log_received_events
+        )
 
         def log_scheduler_jobs():
             JobScheduler.log_active_jobs(logging.INFO)
