Uninett/nav

commit 17ef4e00c02f787a453f785e6049866ec8598a9c

Change lookups to new related name in Netbox model

python/nav/web/info/room/views.py
@@ -117,7 +117,7 @@ def process_searchform(form):
 
 def filter_netboxes(room):
     """Filter netboxes based on interesting categories"""
-    return room.netbox_set.filter(category__in=CATEGORIES)
+    return room.netboxes.filter(category__in=CATEGORIES)
 
 
 def roominfo(request, roomid):
@@ -143,7 +143,7 @@ def get_room_meta(room):
     """Find meta data for the room"""
     room_interfaces = Interface.objects.filter(netbox__room=room)
     return {
-        'devices': room.netbox_set.count(),
+        'devices': room.netboxes.count(),
         'interfaces': room_interfaces.count(),
         'interfaces_with_link': room_interfaces.filter(
             ifoperstatus=Interface.OPER_UP
@@ -155,7 +155,7 @@ def get_room_meta(room):
 def render_deviceinfo(request, roomid):
     """Controller for rendering device info"""
     room = get_object_or_404(Room, id=roomid)
-    all_netboxes = room.netbox_set.select_related(
+    all_netboxes = room.netboxes.select_related(
         "type", "category", "organization"
     ).order_by("sysname")
     availabilities = {}
@@ -254,7 +254,7 @@ def create_csv(request):
 def render_sensors(request, roomid):
     """Gets the environment devices for a room"""
     room = get_object_or_404(Room, pk=roomid)
-    netboxes = room.netbox_set.filter(category='ENV')
+    netboxes = room.netboxes.filter(category='ENV')
 
     for netbox in netboxes:
         netbox.env_sensors = netbox.sensor_set.filter(

python/nav/web/ajax/views.py
@@ -69,7 +69,7 @@ def netbox_down_in(room):
     """
     Returns True if a netbox is down on the room, otherwise False
     """
-    return len(room.netbox_set.filter(up='n'))
+    return len(room.netboxes.filter(up='n'))
 
 
 def get_neighbors(_request, netboxid):

python/nav/web/info/netboxgroup/views.py
@@ -95,7 +95,7 @@ def group_detail(request, groupid):
 
     """
     group = get_object_or_404(NetboxGroup, pk=groupid)
-    netboxes = group.netbox_set.select_related('organization', 'category', 'type')
+    netboxes = group.netboxes.select_related('organization', 'category', 'type')
     availabilities = {}
     graphite_error = False
 
@@ -137,7 +137,7 @@ def group_edit(request, groupid):
         return handle_edit_request(request, group)
 
     netboxes = Netbox.objects.exclude(
-        pk__in=group.netbox_set.all().values_list('id', flat=True)
+        pk__in=group.netboxes.all().values_list('id', flat=True)
     )
 
     return render(

python/nav/models/msgmaint.py
@@ -150,7 +150,7 @@ class MaintenanceTask(models.Model):
         subjects = []
         for component in self.get_components():
             if isinstance(component, (manage.Room, manage.NetboxGroup)):
-                subjects.extend(component.netbox_set.all())
+                subjects.extend(component.netboxes.all())
             elif isinstance(component, manage.Location):
                 for location in component.get_descendants(include_self=True):
                     subjects.extend(

python/nav/web/info/searchproviders.py
@@ -276,5 +276,5 @@ class DevicegroupSearchProvider(SearchProvider):
             SearchResult(g.get_absolute_url(), g)
             for g in NetboxGroup.objects.filter(id__icontains=self.query)
             .order_by('id')
-            .annotate(num_netboxes=Count('netbox'))
+            .annotate(num_netboxes=Count('netboxes'))
         ]

python/nav/web/navlets/pdu.py
@@ -54,7 +54,7 @@ class PduWidget(Navlet):
             return context
 
         pdus = (
-            room.netbox_set.filter(category='POWER')
+            room.netboxes.filter(category='POWER')
             .filter(sensor__internal_name__startswith='rPDULoadStatusLoad')
             .prefetch_related('sensor_set')
             .distinct()

python/nav/web/seeddb/forms/__init__.py
@@ -340,7 +340,7 @@ class DeviceGroupForm(forms.ModelForm):
         # netboxes field with netboxes from the many to many relationship
         if 'instance' in kwargs and kwargs['instance'] is not None:
             initial = kwargs.setdefault('initial', {})
-            initial['netboxes'] = [n.pk for n in kwargs['instance'].netbox_set.all()]
+            initial['netboxes'] = [n.pk for n in kwargs['instance'].netboxes.all()]
         forms.ModelForm.__init__(self, *args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_tag = False
@@ -362,7 +362,7 @@ def to_choice_format(objects, key, value):
 
 def get_netboxes_in_group(group):
     if group:
-        return group.netbox_set.all()
+        return group.netboxes.all()
     else:
         return Netbox.objects.none()
 
@@ -370,7 +370,7 @@ def get_netboxes_in_group(group):
 def get_netboxes_not_in_group(group):
     if group:
         return Netbox.objects.exclude(
-            pk__in=group.netbox_set.all().values_list('id', flat=True)
+            pk__in=group.netboxes.all().values_list('id', flat=True)
         )
     else:
         return Netbox.objects.all()

python/nav/web/seeddb/page/management_profile/__init__.py
@@ -92,7 +92,7 @@ def management_profile_list(request):
     info = ManagementProfileInfo()
     value_list = ('name', 'description', 'get_protocol_display', 'related')
     netbox_link = reverse('seeddb-netbox')
-    queryset = ManagementProfile.objects.annotate(related=Count('netbox'))
+    queryset = ManagementProfile.objects.annotate(related=Count('netboxes'))
     filter_form = ManagementProfileFilterForm(request.GET)
     return render_list(
         request,
@@ -134,7 +134,7 @@ def management_profile_edit(request, management_profile_id=None):
     """
     try:
         profile = ManagementProfile.objects.get(id=management_profile_id)
-        num_netboxes = profile.netbox_set.distinct().count()
+        num_netboxes = profile.netboxes.distinct().count()
     except ManagementProfile.DoesNotExist:
         profile = None
         num_netboxes = 0

commit 50de7f034cdbe13e1574a64f3bb631b299a48aec

Fix backwards relation references in room search
17ef4e0 broke things by adding a related name that was used otherwise

python/nav/web/info/room/views.py
@@ -86,7 +86,7 @@ def search(request):
             titles.append(("Search for %s" % request.GET['query'],))
             rooms = process_searchform(searchform)
             for room in rooms:
-                room.netboxes = filter_netboxes(room)
+                room.filtered_netboxes = filter_netboxes(room)
     else:
         searchform = RoomSearchForm()
 
@@ -111,7 +111,7 @@ def process_searchform(form):
         return Room.objects.filter(
             Q(id__icontains=query)
             | Q(description__icontains=query)
-            | Q(child_locations__id__icontains=query)
+            | Q(location__id__icontains=query)
         ).order_by("id")
 
 

python/nav/web/templates/info/room/base.html
@@ -45,7 +45,7 @@
                   {{ room.description }}
                 </td>
                 <td>
-                  {{ room.netboxes.count }}
+                  {{ room.filtered_netboxes.count }}
                 </td>
               </tr>
             {% endfor %}