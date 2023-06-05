a8bc7b557237a30fe80d7f84b29ca39230e8ac61
Morten Brekkevold <morten.brekkevold@sikt.no>
2023-06-05 14:08:58 +0200

    "Un-parametrize" webcrawler tests
    While pytest can accomplish a lot of exciting things, it cannot use
    fixtures as input to test parametrization.  While we can make a test
    depend on a fixture for getting a running webserver instance, the test
    discovery phase that generates the tests cannot.
    
    I.e. we cannot get our test parameters from the webcrawler unless the
    web server was already up and running when the test discovery phase
    started.  Sad, but true.
    
    This changes the webcrawler into a fixture, and changes the web link
    reachability and HTML validation tests to iterate the pages provided
    by this session-scoped crawler.
    
    This also considerably shortens the discovery phase, since the crawling
    actually takes place during the running of the first test that uses
    the fixture.

--- tests/integration/web/crawler_test.py
+++ tests/integration/web/crawler_test.py
@@ -221,32 +221,27 @@ def _quote_url(url):
 
 
 #
+# fixtures
+#
+
+
+@pytest.fixture(scope="session")
+def webcrawler(gunicorn):
+    crawler = WebCrawler(HOST_URL, USERNAME, PASSWORD)
+    yield crawler
+
+
+#
 # test functions
 #
 
-# just one big, global crawler instance to ensure it's results are cached
-# throughout all the tests in a single session
-if HOST_URL:
-    crawler = WebCrawler(HOST_URL, USERNAME, PASSWORD)
-else:
-    crawler = Mock()
-    crawler.crawl.return_value = []
 
-
-def page_id(page):
-    """Extracts a URL as a test id from a page"""
-    return normalize_path(page.url)
-
-
-@pytest.mark.skipif(
-    not HOST_URL,
-    reason="Missing environment variable TARGETURL "
-    "(ADMINUSERNAME, ADMINPASSWORD) , skipping crawler "
-    "tests!",
-)
-@pytest.mark.parametrize("page", crawler.crawl(), ids=page_id)
-def test_link_should_be_reachable(page):
-    assert page.response == 200, _content_as_string(page.content)
+def test_all_links_should_be_reachable(webcrawler):
+    for page in webcrawler.crawl():
+        if page.response != 200:
+            # No need to fill up the test report files with contents of OK pages
+            print(_content_as_string(page.content))
+        assert page.response == 200, "{} is not reachable".format(page.url)
 
 
 def _content_as_string(content):
@@ -256,23 +251,17 @@ def _content_as_string(content):
         return content.decode('utf-8')
 
 
-@pytest.mark.skipif(
-    not HOST_URL,
-    reason="Missing environment variable TARGETURL "
-    "(ADMINUSERNAME, ADMINPASSWORD) , skipping crawler "
-    "tests!",
-)
-@pytest.mark.parametrize("page", crawler.crawl_only_html(), ids=page_id)
-def test_page_should_be_valid_html(page):
-    if page.response != 200:
-        pytest.skip("not validating non-reachable page")
-    if not page.content:
-        pytest.skip("page has no content")
+def test_page_should_be_valid_html(webcrawler):
+    for page in webcrawler.crawl_only_html():
+        if page.response != 200 or not page.content:
+            continue
 
-    document, errors = tidy_document(page.content, TIDY_OPTIONS)
-    errors = filter_errors(errors)
+        document, errors = tidy_document(page.content, TIDY_OPTIONS)
+        errors = filter_errors(errors)
+        if errors:
+            print(errors)
 
-    assert not errors, "Found following validation errors:\n" + errors
+        assert not errors, "{} did not validate as HTML".format(page.url)
 
 
 def should_validate(page: Page):
