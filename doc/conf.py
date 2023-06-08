f10a79f6c2d9e3fe63c20b51cee1839d42bb99b3
Morten Brekkevold <morten.brekkevold@sikt.no>
2023-06-08 16:41:14 +0200

    Add sphinx-bootstrap-theme to doc requirements
    To build the documentation, the sphinx-bootstrap-theme package is now
    needed, as the old, vendored-in version has been thrown out.

--- doc/conf.py
+++ doc/conf.py
@@ -10,7 +10,7 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
-
+import sphinx_bootstrap_theme
 import sys, os
 
 # If extensions (or modules to document with autodoc) are in another directory,
