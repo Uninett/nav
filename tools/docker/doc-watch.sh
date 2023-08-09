933bd18adc3159d117cc3d70079cbbde0581f4ae
Hanne Moa <hanne.moa@sikt.no>
2023-08-09 10:38:08 +0200

    Use "sphinx-build" instead of "setup.py build_sphinx"

--- tools/docker/doc-watch.sh
+++ tools/docker/doc-watch.sh
@@ -3,11 +3,11 @@
 #
 cd /source
 # Build once first
-sudo -u nav python3 setup.py build  # ensure build data and .eggs aren't stored as root
-python3 setup.py develop
-sudo -u nav python3 setup.py build_sphinx
+sudo -u nav python3 -m build  # ensure build data and .eggs aren't stored as root
+pip install -e .
+sudo -u nav sphinx-build doc/ doc/_build
 # Then re-build on any changes to the doc directory
 while inotifywait -e modify -e move -e create -e delete -r --exclude \# /source/doc /source/NOTES.rst
 do
-  sudo -u nav python3 setup.py build_sphinx
+  sudo -u nav sphinx-build doc/ doc/_build
 done
