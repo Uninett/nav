1e54014dd674769408703a5ff2368e31b9c78fe8
Simon Oliver Tveit <simtve@uninett.no>
2023-08-30 12:31:13 +0200

    Replace camel-case

--- tests/unittests/portadmin/portadmin_test.py
+++ tests/unittests/portadmin/portadmin_test.py
@@ -24,30 +24,32 @@ class TestPortadminManagementFactory:
 class TestPortadminResponseHP:
     def test_get_vlan_hp(self, handler_hp):
         # get hold of the read-only Snmp-object
-        snmpReadOnlyHandler = handler_hp._get_read_only_handle()
+        snmp_read_only_handler = handler_hp._get_read_only_handle()
         # replace get-method on Snmp-object with a mock-method
         # this get-method returns a vlan-number
-        snmpReadOnlyHandler.get = Mock(return_value=666)
+        snmp_read_only_handler.get = Mock(return_value=666)
         ifc = Mock(baseport=1)
         assert handler_hp.get_interface_native_vlan(ifc) == 666, "getVlan-test failed"
-        snmpReadOnlyHandler.get.assert_called_with(OID('.1.3.6.1.2.1.17.7.1.4.5.1.1.1'))
+        snmp_read_only_handler.get.assert_called_with(
+            OID('.1.3.6.1.2.1.17.7.1.4.5.1.1.1')
+        )
 
     def test_get_ifaliases_hp(self, handler_hp):
         # get hold of the read-only Snmp-object
-        snmpReadOnlyHandler = handler_hp._get_read_only_handle()
+        snmp_read_only_handler = handler_hp._get_read_only_handle()
         # replace get-method on Snmp-object with a mock-method
         # for getting all IfAlias
         walkdata = [('.1', b'hjalmar'), ('.2', b'snorre'), ('.3', b'bjarne')]
         expected = {1: 'hjalmar', 2: 'snorre', 3: 'bjarne'}
-        snmpReadOnlyHandler.bulkwalk = Mock(return_value=walkdata)
+        snmp_read_only_handler.bulkwalk = Mock(return_value=walkdata)
         assert handler_hp._get_all_ifaliases() == expected, "getAllIfAlias failed."
 
     def test_set_ifalias_hp(self, handler_hp):
         # get hold of the read-write Snmp-object
-        snmpReadWriteHandler = handler_hp._get_read_write_handle()
+        snmp_read_only_handler = handler_hp._get_read_write_handle()
         # replace set-method on Snmp-object with a mock-method
         # all set-methods return None
-        snmpReadWriteHandler.set = Mock(return_value=None)
+        snmp_read_only_handler.set = Mock(return_value=None)
         interface = Mock()
         interface.ifindex = 1
         assert (
@@ -58,22 +60,22 @@ class TestPortadminResponseHP:
 class TestPortadminResponseCisco:
     def test_get_vlan_cisco(self, handler_cisco):
         # get hold of the read-only Snmp-object
-        snmpReadOnlyHandler = handler_cisco._get_read_only_handle()
+        snmp_read_only_handler = handler_cisco._get_read_only_handle()
         # replace get-method on Snmp-object with a mock-method
         # this get-method returns a vlan-number
-        snmpReadOnlyHandler.get = Mock(return_value=77)
+        snmp_read_only_handler.get = Mock(return_value=77)
         ifc = Mock(ifindex=1)
         assert handler_cisco.get_interface_native_vlan(ifc) == 77, "getVlan-test failed"
-        snmpReadOnlyHandler.get.assert_called_with('1.3.6.1.4.1.9.9.68.1.2.2.1.2.1')
+        snmp_read_only_handler.get.assert_called_with('1.3.6.1.4.1.9.9.68.1.2.2.1.2.1')
 
     def test_get_ifaliases_cisco(self, handler_cisco):
         # get hold of the read-only Snmp-object
-        snmpReadOnlyHandler = handler_cisco._get_read_only_handle()
+        snmp_read_only_handler = handler_cisco._get_read_only_handle()
         # replace get-method on Snmp-object with a mock-method
         # for getting all IfAlias
         walkdata = [('.1', b'jomar'), ('.2', b'knut'), ('.3', b'hjallis')]
         expected = {1: 'jomar', 2: 'knut', 3: 'hjallis'}
-        snmpReadOnlyHandler.bulkwalk = Mock(return_value=walkdata)
+        snmp_read_only_handler.bulkwalk = Mock(return_value=walkdata)
         assert handler_cisco._get_all_ifaliases() == expected, "getAllIfAlias failed."
 
 
