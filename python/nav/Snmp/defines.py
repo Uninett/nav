ebe881377085134900b3e0fced17c634db74831f
Morten Brekkevold <morten.brekkevold@sikt.no>
2023-11-01 16:12:24 +0100

    Define SNMPv3 protocol config enums
    These could be re-used elsewhere, like in the `SNMPv3Form`.

---
+++ python/nav/Snmp/defines.py
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2023 Sikt
+#
+# This file is part of Network Administration Visualized (NAV).
+#
+# NAV is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 3 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.  You should have received a copy of the GNU General Public
+# License along with NAV. If not, see <http://www.gnu.org/licenses/>.
+#
+"""Defines types and enumerations for SNMP communication parameters"""
+from enum import Enum
+
+
+class SecurityLevel(Enum):
+    """SNMPv3 security levels"""
+
+    NO_AUTH_NO_PRIV = "noAuthNoPriv"
+    AUTH_NO_PRIV = "authNoPriv"
+    AUTH_PRIV = "authPriv"
+
+
+class AuthenticationProtocol(Enum):
+    """SNMPv3 authentication protocols supported by NET-SNMP"""
+
+    MD5 = "MD5"
+    SHA = "SHA"
+    SHA512 = "SHA-512"
+    SHA384 = "SHA-384"
+    SHA256 = "SHA-256"
+    SHA224 = "SHA-224"
+
+
+class PrivacyProtocol(Enum):
+    """SNMPv3 privacy protocols supported by NET-SNMP"""
+
+    DES = "DES"
+    AES = "AES"
