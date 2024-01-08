a2ccc9cc92f4576b562c35da4c5675f106819d44
Hanne Moa <hanne.moa@sikt.no>
2024-01-08 10:32:46 +0100

    Test _config_resource_walk

---
+++ tests/unittests/config_test.py
@@ -0,0 +1,16 @@
+from nav.config import _config_resource_walk
+
+
+from unittest import TestCase
+
+
+class TestConfigResourceWalk(TestCase):
+    def test_should_read_relative_paths_as_strings_from_nav_package_and_return_a_long_list_of_strings(
+        self,
+    ):
+        # result should be many, many relative paths as strings
+        result = tuple(_config_resource_walk())  # generator
+        self.assertTrue(len(result) > 20)
+        for relpath in result:
+            self.assertIsInstance(relpath, str)
+            self.assertFalse(relpath.startswith('/'))
