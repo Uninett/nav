32459e05ca81d2ab228e95537c29f2cf4e28020c
Hanne Moa <hanne.moa@sikt.no>
2024-02-21 13:03:13 +0100

    Add CSP: frame-ancestors support

--- python/nav/web/security.py
+++ python/nav/web/security.py
@@ -25,3 +25,26 @@ allow_frames=self
         if frames_flag == 'none':
             return 'DENY'
         return 'SAMEORIGIN'
+
+    def get_frame_ancestors(self):
+        """Return a list of sources
+
+        A single 'none' or a string of one or more of self, source-scheme and
+        host-scheme are valid. There is currently no validator for host-scheme,
+        so source-scheme and host-scheme are both outputted as-is.
+
+        To be set in django settings and used by the django-csp middleware.
+        """
+        default = "'self'"
+        frames_flag = self.get(self.FRAMES_OPTION) or self.FRAMES_DEFAULT
+        pieces = frames_flag.split()
+        valid_pieces = []
+        for piece in pieces:
+            if piece == 'none':
+                valid_pieces.append("'none'")
+                break
+            if piece == 'self':
+                valid_pieces.append(default)
+            else:
+                valid_pieces.append(piece)
+        return valid_pieces or [default]
