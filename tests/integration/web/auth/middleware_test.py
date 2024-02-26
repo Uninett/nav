96190ff696512f14dadd38df97b8e61a488e6731
Simon Oliver Tveit <simtve@uninett.no>
2024-02-26 13:14:01 +0100

    Test session cycling on remote login

---
+++ tests/integration/web/auth/middleware_test.py
@@ -0,0 +1,28 @@
+import pytest
+from mock import patch
+
+from nav.web.auth.middleware import AuthenticationMiddleware
+from nav.models.profiles import Account
+
+
+def test_remote_user_being_logged_in_should_change_session_id(
+    db, session_request, remote_account
+):
+    pre_login_session_id = session_request.session.session_key
+    with patch(
+        'nav.web.auth.remote_user.get_username', return_value=remote_account.login
+    ):
+        middleware = AuthenticationMiddleware()
+        middleware.process_request(session_request)
+    assert session_request.account == remote_account
+    post_login_session_id = session_request.session.session_key
+    assert pre_login_session_id != post_login_session_id
+
+
+@pytest.fixture()
+def remote_account(db):
+    account = Account(login="remote")
+    account.set_password("supersecret")
+    account.save()
+    yield account
+    account.delete()
