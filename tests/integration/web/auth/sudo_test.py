3c46e2b0445dab0b03d52b0eeef9ca605460d733
Simon Oliver Tveit <simtve@uninett.no>
2024-02-26 12:32:26 +0100

    Move fixture to conftest for auth directory

--- tests/integration/web/auth/sudo_test.py
+++ tests/integration/web/auth/sudo_test.py
@@ -42,16 +42,3 @@ def other_account(db):
     account.save()
     yield account
     account.delete()
-
-
-@pytest.fixture()
-def session_request(db):
-    """Request object with a real session"""
-    r = RequestFactory()
-    session_request = r.post('/anyurl')
-
-    # use middleware to make session for session_request
-    middleware = SessionMiddleware()
-    middleware.process_request(session_request)
-    session_request.session.save()
-    return session_request
