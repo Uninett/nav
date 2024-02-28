39f33945087c66844c7c2ffa5202b5535ced5a7c
Johanna England <johanna.england@sikt.no>
2024-02-28 13:46:28 +0100

    Override template to show logo in mobile view

--- doc/conf.py
+++ doc/conf.py
@@ -38,7 +38,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
