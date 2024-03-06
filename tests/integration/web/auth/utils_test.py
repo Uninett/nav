d11f301f65f6b20470be901e0317f033a34f4f48
Simon Oliver Tveit <simtve@uninett.no>
2024-03-06 14:33:22 +0100

    Put tests in class

--- tests/integration/web/auth/utils_test.py
+++ tests/integration/web/auth/utils_test.py
@@ -1,26 +1,25 @@
 from nav.web.auth.utils import set_account, clear_session
 
 
-def test_clear_session_should_create_new_session_id(db, session_request):
-    pre_session_id = session_request.session.session_key
-    clear_session(session_request)
-    post_session_id = session_request.session.session_key
-    assert pre_session_id != post_session_id
+class TestClearSession:
+    def test_should_create_new_session_id(self, db, session_request):
+        pre_session_id = session_request.session.session_key
+        clear_session(session_request)
+        post_session_id = session_request.session.session_key
+        assert pre_session_id != post_session_id
 
+    def test_should_remove_account_from_request(
+        self, db, session_request, admin_account
+    ):
+        # login with admin acount
+        set_account(session_request, admin_account)
+        assert session_request.account
+        clear_session(session_request)
+        assert not hasattr(session_request, "account")
 
-def test_clear_session_should_remove_account_from_request(
-    db, session_request, admin_account
-):
-    # login with admin acount
-    set_account(session_request, admin_account)
-    assert session_request.account
-    clear_session(session_request)
-    assert not hasattr(session_request, "account")
-
-
-def test_clear_session_should_clear_session_dict(db, session_request, admin_account):
-    set_account(session_request, admin_account)
-    # Make sure there is something to be deleted
-    assert session_request.session.keys()
-    clear_session(session_request)
-    assert not session_request.session.keys()
+    def test_should_clear_session_dict(self, db, session_request, admin_account):
+        set_account(session_request, admin_account)
+        # Make sure there is something to be deleted
+        assert session_request.session.keys()
+        clear_session(session_request)
+        assert not session_request.session.keys()
