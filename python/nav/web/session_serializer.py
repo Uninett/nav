2234aa7f406be49b0e3142642bb338eb3f91ebba
Hanne Moa <hanne.moa@sikt.no>
2024-03-08 11:12:01 +0100

    Add meaty docstring to the serializer

--- python/nav/web/session_serializer.py
+++ python/nav/web/session_serializer.py
@@ -6,8 +6,22 @@ from django.core.exceptions import ImproperlyConfigured
 
 class PickleSerializer:
     """
-    Simple wrapper around pickle to be used in signing.dumps()/loads() and
-    cache backends.
+    Simple wrapper around pickle to be used for serializing data to be put in
+    cookies.
+
+    This was vendored from the version found in Django 4.2. JSONSerializer has
+    been the default in Django since 1.6, deprecated since 4.1 and purged from
+    the codebase since 5.0. What Django did not provide is a migration path:
+    a test showed that any access of a cookie after the serializer had been
+    changed lead to a rather useless exception.
+
+    PickleSerializer was removed due to it being danegerous in the
+    signed_cookie session backend. NAV doesn't use that see we can keep the old
+    serializer.
+
+    Changes from the original: A deprecation warning has been removed and
+    a check that it is not used with the signed_cookie session backend has been
+    added.
     """
 
     def __init__(self, protocol=None):
