efb9faca9539e78096f21650ca008ace1d04b62e
Morten Brekkevold <morten.brekkevold@sikt.no>
2024-03-08 13:03:01 +0100

    Update Sphinx doc version

--- doc/conf.py
+++ doc/conf.py
@@ -58,7 +58,7 @@ copyright = u'2012-2021 Uninett AS, 2022-2024 Sikt'
 # built documents.
 #
 # The short X.Y version.
-version = 'dev'
+version = '5.9'
 # version = '.'.join(buildconf.VERSION.split('.')[:2])
 # The full version, including alpha/beta/rc tags.
 release = buildconf.VERSION
