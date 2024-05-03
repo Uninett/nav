f22185444b136593c8ad96c5332733326032d7fc
Simon Oliver Tveit <simtve@uninett.no>
2024-05-03 14:50:52 +0200

    Constraint last 3 bytes to be 0

--- python/nav/models/sql/changes/sc.05.08.0001.sql
+++ python/nav/models/sql/changes/sc.05.08.0001.sql
@@ -1,5 +1,6 @@
 CREATE TABLE manage.oui (
     id SERIAL PRIMARY KEY,
     vendor VARCHAR NOT NULL,
-    oui MACADDR NOT NULL UNIQUE
+    oui MACADDR NOT NULL UNIQUE,
+    CHECK (oui=trunc(oui))
 );
