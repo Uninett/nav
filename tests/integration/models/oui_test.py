8067ff1a30713276ca893329b0589de1f5c3b959
Simon Oliver Tveit <simtve@uninett.no>
2024-05-03 14:51:18 +0200

    Update tests

--- tests/integration/models/oui_test.py
+++ tests/integration/models/oui_test.py
@@ -1,26 +1,35 @@
 import pytest
 
-from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
 
 from nav.models.oui import OUI
 
 
 class TestOUI:
-    def test_validation_should_raise_error_if_oid_is_less_than_6_characters(self):
-        with pytest.raises(ValidationError):
-            instance = OUI(oui="AABB", vendor="myvendor")
-            instance.full_clean()
+    def test_string_representation_should_match_oid(self, valid_oui):
+        assert str(valid_oui) == valid_oui.oui
 
-    def test_validation_should_raise_error_if_oid_is_more_than_6_characters(self):
-        with pytest.raises(ValidationError):
-            instance = OUI(oui="AABBCCDD", vendor="myvendor")
-            instance.full_clean()
+    def test_save_should_raise_error_if_last_3_bytes_are_not_zero(self, invalid_oui):
+        with pytest.raises(IntegrityError):
+            invalid_oui.save()
 
-    def test_validation_should_allow_oid_with_6_characters(self):
-        instance = OUI(oui="AABBCC", vendor="myvendor")
-        instance.full_clean()
+    def test_save_should_allow_oui_if_last_3_bytes_are_zero(self, valid_oui):
+        valid_oui.save()
 
-    def test_string_representation_should_match_oid(self):
-        oui = "AABBCC"
-        instance = OUI(oui=oui, vendor="myvendor")
-        assert str(instance) == oui
+
+@pytest.fixture()
+def valid_oui():
+    oui = "aa:bb:cc:00:00:00"
+    instance = OUI(oui=oui, vendor="myvendor")
+    yield instance
+    if instance.id:
+        instance.delete()
+
+
+@pytest.fixture()
+def invalid_oui():
+    oui = "aa:bb:cc:dd:ee:ff"
+    instance = OUI(oui=oui, vendor="myvendor")
+    yield instance
+    if instance.id:
+        instance.delete()
