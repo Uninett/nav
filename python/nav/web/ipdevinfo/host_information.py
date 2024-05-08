09bf5c36e6890f50bc2f320bb54682dcf1ea48f7
Johanna England <johanna.england@sikt.no>
2024-05-08 15:59:04 +0200

    Use lru_cache directly as decorator
    This was added in Python 3.8

--- python/nav/web/ipdevinfo/host_information.py
+++ python/nav/web/ipdevinfo/host_information.py
@@ -46,7 +46,8 @@ def reverse_lookup(addresses):
                 yield {'addr': addr, 'name': name}
 
 
-def _get_host_info(host):
+@lru_cache
+def get_host_info(host):
     """Returns a dictionary containing DNS information about the host"""
     if is_valid_ip(host, strict=True):
         addresses = list(reverse_lookup([host]))
@@ -61,6 +62,3 @@ def _get_host_info(host):
             addresses = list(reverse_lookup(addresses))
 
     return {'host': host, 'addresses': addresses}
-
-
-get_host_info = lru_cache()(_get_host_info)
