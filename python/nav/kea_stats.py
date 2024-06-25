85823db20d2ac14b1850b42125b6be56f21996d6
Jørund Hellebø <jorund.hellebo@sikt.no>
2024-06-25 15:20:16 +0200

    Add dataclasses for Kea subnets and Kea DHCP config files

--- python/nav/kea_stats.py
+++ python/nav/kea_stats.py
@@ -2,21 +2,33 @@
 Functions for querying the Kea Control Agent for statistics from Kea DHCP
 servers.
 
-Stork (https://gitlab.isc.org/isc-projects/stork) is used as a guiding
-implementation for interacting with the Kea Control Agent.  See also the Kea
-Control Agent documentation
-(https://kea.readthedocs.io/en/kea-2.6.0/arm/agent.html).
+     RESTful-queries                      IPC
+nav <---------------> Kea Control Agent <=====> Kea DHCP4 server / Kea DHCP6 server
+        (json)
+
+No additional hook libraries are assumed to be included with the Kea Control
+Agent that is queried, meaning this module will be able to gather DHCP
+statistics from any Kea Control Agent.
+
+* Stork (https://gitlab.isc.org/isc-projects/stork) is used as a guiding
+implementation for interacting with the Kea Control Agent.
+* See also the Kea Control Agent documentation. This script assumes Kea versions
+>= 2.2.0 are used.  (https://kea.readthedocs.io/en/kea-2.2.0/arm/agent.html).
 """
 from dataclasses import dataclass, asdict
-from enum import Enum
+from enum import IntEnum
 import logging
 import requests
+import json
 from requests.exceptions import JSONDecodeError, HTTPError
-from typing import Union
+from typing import Union, Optional
+from IPy import IP
 
 logger = logging.getLogger(__name__)
 
-class KeaStatus(Enum):
+
+class KeaStatus(IntEnum):
+    """Status of a REST response."""
     # Successful operation.
     SUCCESS = 0
     # General failure.
@@ -28,51 +40,234 @@ class KeaStatus(Enum):
     # Unsuccessful operation due to a conflict between the command arguments and the server state.
     CONFLICT = 4
 
+
 @dataclass
 class KeaResponse:
     """
-    Class for defining a REST response on a REST query sent to a kea-ctrl-agent
-    process.
+    Class representing a REST response on a REST query sent to a Kea Control
+    Agent.
     """
     result: int
     text: str
     arguments: dict[str: Union[str, int]]
     service: str
 
+    @property
+    def success(self):
+        return self.result == KeaStatus.SUCCESS
+
+
 @dataclass
 class KeaQuery:
-    """
-    Class for defining a REST query to be sent to a kea-ctrl-agent
-    process.
-    """
+    """Class representing a REST query to be sent to a Kea Control Agent."""
     command: str
     arguments: dict[str: Union[str, int]]
-    services: list[str] # The server(s) at which the command is targeted. Usually ["dhcp4", "dhcp6"] or ["dhcp4"] or ["dhcp6"].
+    service: list[str] # The server(s) at which the command is targeted. Usually ["dhcp4", "dhcp6"] or ["dhcp4"] or ["dhcp6"].
 
-    def send(self, address) -> list[KeaResponse]:
+
+@dataclass
+class KeaDhcpSubnet:
+    """Class representing information about a subnet managed by a Kea DHCP server."""
+    id: int # either specified in the server config or assigned automatically by the dhcp server
+    prefix: IP # e.g. 192.0.2.1/24
+    pools: list[tuple[IP]] # e.g. [(192.0.2.10, 192.0.2.20), (192.0.2.64, 192.0.2.128)]
+
+    @classmethod
+    def from_json(cls, subnetjson: dict):
         """
-        Send this query to a kea-ctrl-agent located at address
+        Initialize and return a Subnet instance based on json
+
+        :param json: python dictionary that is structured the same way as the
+        json object representing a subnet in the Kea DHCP config file.
+        Example:
+            {
+                "id": 0
+                "subnet": "192.0.2.0/24",
+                "pools": [
+                    {
+                        "pool": "192.0.2.1 - 192.0.2.100"
+                    },
+                    {
+                        "pool": "192.0.2.128/26"
+                    }
+                ]
+            }
         """
-        logger.debug("KeaQuery.send: sending request to %s with query %r", address, self)
-        try:
-            r = requests.post(address, data=asdict(self))
-        except HTTPError as err:
-            logger.error("KeaQuery.send: request to %s yielded an error: %d %s", err.url, err.status_code, err.reason)
-            raise err
+        if "id" not in subnetjson:
+            raise ValueError("Expected subnetjson['id'] to exist")
+        id = subnetjson["id"]
 
-        try:
-            json = r.json()
-        except JSONDecodeError as err:
-            logger.error("KeaQuery.send: expected json from %s, got %s", address, r.text)
-            raise err
+        if "subnet" not in subnetjson:
+            raise ValueError("Expected subnetjson['subnet'] to exist")
+        prefix = IP(subnetjson["subnet"]),
 
-        responses = []
-        for obj in json:
-            response = KeaResponse(
-                obj.get("result", KeaStatus.ERROR),
-                obj.get("text", ""),
-                obj.get("arguments", {}),
-                obj.get("service", ""),
-            )
-            responses.append(response)
-        return responses
+        pools = []
+        for obj in subnetjson.get("pools", []):
+            pool = obj["pool"]
+            if "-" in pool: # TODO: Error checking?
+                # pool == "x.x.x.x - y.y.y.y"
+                start, end = (IP(ip) for ip in pool.split("-"))
+            else:
+                # pool == "x.x.x.x/nn"
+                pool = IP(pool)
+                start, end = pool[0], pool[-1]
+            pools.append((start, end))
+
+        return cls(
+            id=id,
+            prefix=prefix,
+            pools=pools,
+        )
+
+
+@dataclass
+class KeaDhcpConfig:
+    """
+    Class representing information found in the configuration of a Kea DHCP
+    server. Most importantly, this class contains:
+    * A list of the shared networks managed by the DHCP server
+      * A shared network furthermore contain a list of subnets
+        assigned to that network
+    * The IP version of the DCHP server
+    """
+    _config_hash: Optional[str] # Used to check if there's a new config on the Kea DHCP server
+    ip_version: int
+    subnets: list[KeaDhcpSubnet]
+
+
+    @classmethod
+    def from_json(cls, configjson: dict, hash: Optional[str] = None):
+        """
+        Initialize and return a Config instance based on json
+
+        :param json: a dictionary that is structured the same way as a
+        Kea DHCP configuration.
+        Example:
+        {
+            "Dhcp4": {
+                "subnet4": [{
+                "id": 1,
+                "subnet": "192.0.2.0/24",
+                "pools": [
+                    {
+                        "pool": "192.0.2.1 - 192.0.2.200",
+                    },
+                ],
+                }]
+            }
+        }
+
+        :param hash: hash of the Kea DHCP config file as returned by a
+        `config-hash-get` query on the kea-ctrl-agent REST server.
+        """
+        if len(configjson) > 1:
+            raise ValueError("Did not expect len(configjson) > 1")
+
+        ip_version, json = configjson.popitem()
+        if ip_version == "Dhcp4":
+            ip_version = 4
+        elif ip_version == "Dhcp6":
+            ip_version == 6
+        else:
+            raise ValueError(f"Unsupported DHCP IP version '{ip_version}'")
+
+        subnets = []
+        for obj in json.get(f"subnet{ip_version}", []):
+            subnet = KeaDhcpSubnet.from_json(obj)
+            subnets.append(subnet)
+
+        return cls(
+            _config_hash=hash,
+            ip_version=ip_version,
+            subnets=subnets,
+        )
+
+
+def send_query(query: KeaQuery, address: str, port: int, https: bool = True, session: requests.Session = None) -> list[KeaResponse]:
+    """
+    Internal function.
+    Send `query` to a Kea Control Agent listening to `port`
+    on IP address `address`, using either http or https
+
+    :param session: optional session to be used when sending the query. Assumed
+    to not be closed. Session is not closed after the query, so that the session
+    can be used for persistent connections among differend send_query calls.
+    """
+    scheme = "https" if https else "http"
+    location = f"{scheme}://{address}:{port}/"
+    logger.debug("send_query: sending request to %s with query %r", location, query)
+    try:
+        if session is None:
+            r = requests.post(location, data=json.dumps(asdict(query)), headers={"Content-Type": "application/json"})
+        else:
+            r = session.post(location, data=json.dumps(asdict(query)), headers={"Content-Type": "application/json"})
+    except HTTPError as err:
+        logger.error("send_query: request to %s yielded an error: %d %s", err.url, err.status_code, err.reason)
+        raise err
+
+    try:
+        responsejson = r.json()
+    except JSONDecodeError as err:
+        logger.error("send_query: expected json from %s, got %s", address, r.text)
+        raise err
+
+    if isinstance(responsejson, dict):
+        logger.error("send_query: expected a json list of objects from %s, got %r", address, responsejson)
+        raise ValueError(f"bad response from {address}: {responsejson!r}")
+
+    responses = []
+    for obj in responsejson:
+        response = KeaResponse(
+            obj.get("result", KeaStatus.ERROR),
+            obj.get("text", ""),
+            obj.get("arguments", {}),
+            obj.get("service", ""),
+        )
+        responses.append(response)
+    return responses
+
+
+def get_dhcp_config(address: str, port: int, https: bool = True, ip_version: int = 4) -> KeaDhcpConfig:
+    """ Fetch the config of a Kea DHCP server that manages addresses of IP
+    version `ip_version` from a Kea Control Agent listening to `port` on
+    `address`.
+
+    :param address: the IP address or DNS addressable hostname of the Kea
+    Control Agent.
+    :param port: the port that the Kea Control Agent listens to.
+    :param https: whether or not to use https. If not using https, http is used.
+    :param ip_version: the IP version of the Kea DHCP server
+    """
+    query = KeaQuery(
+        command="config-get",
+        service=[f"dhcp{ip_version}"],
+        arguments={},
+    )
+    responses = send_query(query, address, port, https)
+    if len(responses) != 1:
+        raise Exception(f"Received invalid amount of responses from '{address}'") # TODO: Change Exception
+
+    response = responses[0]
+    if not response.success:
+        raise Exception("Did not receive config file from DHCP server")
+
+    return KeaDhcpConfig.from_json(responses[0].arguments)
+
+def get_dhcp_statistics(address: str, port: int, https: bool = True, ip_version: int = 4) -> KeaDhcpSubnet:
+    query = KeaQuery(
+        command="statistic-get",
+        service=[f"dhcp{ip_version}"],
+        arguments={
+            "name": f"subnet[1].assigned-addresses",
+        },
+    )
+
+    with requests.Session() as s:
+        responses = send_query(query, address, port, https, session=s)
+        if len(responses) != 1:
+            raise Exception(f"Received invalid amount of responses from '{address}'") # TODO: Change Exception
+
+        response = responses[0]
+        if not response.success:
+            raise Exception("Did not receive statistics from DHCP server")
+    return response
--- tests/unittests/kea_stats_test.py
+++ tests/unittests/kea_stats_test.py
@@ -1,6 +1,99 @@
+from nav.kea_stats import *
 import pytest
-from nav.kea_stats import KeaQuery, KeaResponse
 import requests
+from IPy import IP
+import json
+from requests.exceptions import JSONDecodeError
+
+DHCP4_CONFIG = '''
+    {
+        "Dhcp4": {
+            "subnet4": [{
+            "4o6-interface": "eth1",
+            "4o6-interface-id": "ethx",
+            "4o6-subnet": "2001:db8:1:1::/64",
+            "allocator": "iterative",
+            "authoritative": false,
+            "boot-file-name": "/tmp/boot",
+            "client-class": "foobar",
+            "ddns-generated-prefix": "myhost",
+            "ddns-override-client-update": true,
+            "ddns-override-no-update": true,
+            "ddns-qualifying-suffix": "example.org",
+            "ddns-replace-client-name": "never",
+            "ddns-send-updates": true,
+            "ddns-update-on-renew": true,
+            "ddns-use-conflict-resolution": true,
+            "hostname-char-replacement": "x",
+            "hostname-char-set": "[^A-Za-z0-9.-]",
+            "id": 1,
+            "interface": "eth0",
+            "match-client-id": true,
+            "next-server": "0.0.0.0",
+            "store-extended-info": true,
+            "option-data": [
+                {
+                    "always-send": true,
+                    "code": 3,
+                    "csv-format": true,
+                    "data": "192.0.3.1",
+                    "name": "routers",
+                    "space": "dhcp4"
+                }
+            ],
+            "pools": [
+                {
+                    "client-class": "phones_server1",
+                    "option-data": [],
+                    "pool": "192.1.0.1 - 192.1.0.200",
+                    "pool-id": 7,
+                    "require-client-classes": [ "late" ]
+                },
+                {
+                    "client-class": "phones_server2",
+                    "option-data": [],
+                    "pool": "192.3.0.1 - 192.3.0.200",
+                    "require-client-classes": []
+                }
+            ],
+            "rebind-timer": 40,
+            "relay": {
+                "ip-addresses": [
+                    "192.168.56.1"
+                ]
+            },
+            "renew-timer": 30,
+            "reservations-global": true,
+            "reservations-in-subnet": true,
+            "reservations-out-of-pool": true,
+            "calculate-tee-times": true,
+            "t1-percent": 0.5,
+            "t2-percent": 0.75,
+            "cache-threshold": 0.25,
+            "cache-max-age": 1000,
+            "reservations": [
+                {
+                    "circuit-id": "01:11:22:33:44:55:66",
+                    "ip-address": "192.0.2.204",
+                    "hostname": "foo.example.org",
+                    "option-data": [
+                        {
+                            "name": "vivso-suboptions",
+                            "data": "4491"
+                        }
+                    ]
+                }
+            ],
+            "require-client-classes": [ "late" ],
+            "server-hostname": "myhost.example.org",
+            "subnet": "192.0.0.0/8",
+            "valid-lifetime": 6000,
+            "min-valid-lifetime": 4000,
+            "max-valid-lifetime": 8000
+            }]
+        }
+    }
+    '''
 
 def custom_post_response(func):
     """
@@ -30,18 +123,118 @@ def custom_post_response(func):
 
 @pytest.fixture
 @custom_post_response
-def simple_response():
-    return '[{"result": 0, "text": "b", "arguments": {"arg1": "val1"}, "service": "d"}]'
+def success_responses():
+    return '''[
+    {"result": 0, "text": "b", "arguments": {"arg1": "val1"}, "service": "d"},
+    {"result": 0, "arguments": {"arg1": "val1"}, "service": "d"},
+    {"result": 0, "service": "d"},
+    {"result": 0}
+    ]'''
 
 @pytest.fixture
 @custom_post_response
-def lackluster_response():
-    return '[{"result": "a"}]'
+def error_responses():
+    return '''[
+    {"result": 1, "text": "b", "arguments": {"arg1": "val1"}, "service": "d"},
+    {"result": 2, "text": "b", "arguments": {"arg1": "val1"}, "service": "d"},
+    {"result": 3, "text": "b", "arguments": {"arg1": "val1"}, "service": "d"},
+    {"result": 4, "text": "b", "arguments": {"arg1": "val1"}, "service": "d"},
+    {"text": "b", "arguments": {"arg1": "val1"}, "service": "d"}
+    ]'''
 
 @pytest.fixture
 @custom_post_response
-def large_response():
+def invalid_json_responses():
+    return '''[
+    {"result": 1, "text": "b", "arguments": {"arg1": "val1"}, "service": "d"},
+    {"result": 2, "text": "b", "arguments": {"arg1": "val1"}, "service": "d"},
+    {"result": 3, "text": "b", "arguments": {"arg1": "val1"}, "service": "d"},
+    {"result": 4, "text": "b", "arguments": {"arg1": "val1"}, "service": "d"},
+    {"text": "b", "arguments": {"arg1": "val1"}, "service": "d"
+    ]'''
+
+@pytest.fixture
+@custom_post_response
+def large_responses():
     return '''
 
     '''
 
+def test_success_responses_does_succeed(success_responses):
+    query = KeaQuery("command", {}, [])
+    responses = send_query(query, "example.org")
+    assert len(responses) == 4
+    for response in responses:
+        assert response.success
+
+def test_error_responses_does_not_succeed(error_responses):
+    query = KeaQuery("command", {}, [])
+    responses = send_query(query, "example.org")
+    assert len(responses) == 5
+    for response in responses:
+        assert not response.success
+
+def test_invalid_json_responses_raises_jsonerror(invalid_json_responses):
+    query = KeaQuery("command", {}, [])
+    with pytest.raises(JSONDecodeError):
+        responses = send_query(query, "example.org")
+
+def test_correct_subnet_from_json(dhcp4_config):
+    j = json.loads(dhcp4_config)
+    subnet = Subnet.from_json(j["Dhcp4"]["subnet4"][0])
+    assert subnet.id == 1
+    assert subnet.prefix == IP("192.0.0.0/8")
+    assert len(subnet.pools) == 2
+    assert subnet.pools[0] == (IP("192.1.0.1"), IP("192.1.0.200"))
+    assert subnet.pools[1] == (IP("192.3.0.1"), IP("192.3.0.200"))
+
+def test_correct_config_from_json(dhcp4_config):
+    j = json.loads(dhcp4_config)
+    config = KeaDhcpConfig.from_json(j)
+    assert len(config.subnets) == 1
+    subnet = config.subnets[0]
+    assert subnet.id == 1
+    assert subnet.prefix == IP("192.0.0.0/8")
+    assert len(subnet.pools) == 2
+    assert subnet.pools[0] == (IP("192.1.0.1"), IP("192.1.0.200"))
+    assert subnet.pools[1] == (IP("192.3.0.1"), IP("192.3.0.200"))
+    assert config.ip_version == 4
+
+@pytest.fixture
+@custom_post_response
+def dhcp4_config_response():
+    return f'''
+    {{
+        "result": 0,
+        "arguments": {{
+            {DHCP4_CONFIG}
+        }}
+    }}
+    '''
+
+def test_get_dhcp_config(dhcp4_config_response):
+    config = get_dhcp_config("example.org", ip_version=4)
+    assert len(config.subnets) == 1
+    subnet = config.subnets[0]
+    assert subnet.id == 1
+    assert subnet.prefix == IP("192.0.0.0/8")
+    assert len(subnet.pools) == 2
+    assert subnet.pools[0] == (IP("192.1.0.1"), IP("192.1.0.200"))
+    assert subnet.pools[1] == (IP("192.3.0.1"), IP("192.3.0.200"))
+    assert config.ip_version == 4
+
+@pytest.fixture
+@custom_post_response
+def dhcp4_config_response_result_is_1():
+    return f'''
+    {{
+        "result": 1,
+        "arguments": {{
+            {DHCP4_CONFIG}
+        }}
+    }}
+    '''
+
+def test_get_dhcp_config_result_is_1(dhcp4_config_result_is_1):
+    with pytest.raises(Exception): # TODO: Change
+        get_dhcp_config("example-org", ip_version=4)
