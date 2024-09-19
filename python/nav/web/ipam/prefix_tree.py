cedfaee60fe82704ffce492e0de0b1a26596f94d
Morten Brekkevold <morten.brekkevold@sikt.no>
2024-09-19 11:15:02 +0000

    Fix incorrect handling of non-existent scopes
    The old code seemed to assume that `scope` would be `None` if no
    `Prefix` matching the network address could be found in the DB.  But,
    of course, Django raises a `DoesNotExist` exception when using
    `Prefix.objects.get()` for this purpose.
    
    This changes the logic to catch the exception, but otherwise produce
    the originally intended response.

--- python/nav/web/ipam/prefix_tree.py
+++ python/nav/web/ipam/prefix_tree.py
@@ -467,10 +467,10 @@ def make_tree(prefixes, family=None, root_ip=None, show_all=None, sort_by="ip"):
     init = []
 
     if root_ip is not None and root_ip:
-        scope = Prefix.objects.get(net_address=root_ip)
-        if scope is not None:
+        try:
+            scope = Prefix.objects.get(net_address=root_ip)
             node = PrefixNode(scope)
-        else:
+        except Prefix.DoesNotExist:
             node = FauxNode(root_ip, "scope", "scope")
         init.append(node)
 
