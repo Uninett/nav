04a7333ca2f9a47de0f8fde80188ce4281e12bab
Morten Brekkevold <morten.brekkevold@sikt.no>
2024-09-25 13:55:05 +0000

    Build uncrispyfied versions of all /search forms
    This builds an alternate display of all search/info forms based on
    `nav.web.info.forms.SearchForm`, by moving form layout hints into a
    non-crispy-forms based structure.
    
    All forms are displayed twice in a row in order to be able to compare
    them "side-by-side" (i.e. this is just an intermediate commit)

--- python/nav/web/info/forms.py
+++ python/nav/web/info/forms.py
@@ -15,6 +15,7 @@
 # along with NAV. If not, see <http://www.gnu.org/licenses/>.
 #
 """Forms for use in the info subsystem"""
+from types import SimpleNamespace
 
 from django import forms
 from crispy_forms.helper import FormHelper
@@ -25,12 +26,18 @@ class SearchForm(forms.Form):
     """The searchform used for base searches"""
 
     query = forms.CharField(max_length=100, label='', required=False)
+    hints = SimpleNamespace(
+        placeholder='Search', method='get', form_action='', form_class='search-form'
+    )
 
     def __init__(self, *args, **kwargs):
-        self.helper = get_formhelper(
-            kwargs.pop('form_action', ''), kwargs.pop('placeholder', 'Search')
-        )
+        self.hints.form_action = kwargs.pop('form_action', '')
+        self.hints.placeholder = kwargs.pop('placeholder', 'Search')
+        self.helper = get_formhelper(self.hints.form_action, self.hints.placeholder)
         super(SearchForm, self).__init__(*args, **kwargs)
+        self.fields['query'].widget = forms.TextInput(
+            attrs={'placeholder': self.hints.placeholder}
+        )
 
     def clean_query(self):
         """Remove whitespace from searchterm"""
