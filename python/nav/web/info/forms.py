8e3ee641f7d945e3dfbe1f5cca5f595ad2b2e79b
Morten Brekkevold <morten.brekkevold@sikt.no>
2024-09-27 12:51:33 +0000

    Switch info/search over to new anti-crispy system
    This uses the form mechanisms that were introduced in #3034 to replace
    crispy-forms.

--- python/nav/web/info/forms.py
+++ python/nav/web/info/forms.py
@@ -15,30 +15,48 @@
 # along with NAV. If not, see <http://www.gnu.org/licenses/>.
 #
 """Forms for use in the info subsystem"""
-from types import SimpleNamespace
-
+from crispy_forms.templatetags.crispy_forms_field import css_class
 from django import forms
+
+from nav.web.crispyforms import (
+    set_flat_form_attributes,
+    SubmitField,
+    FormRow,
+    FormColumn,
+)
 
 
 class SearchForm(forms.Form):
     """The searchform used for base searches"""
 
     query = forms.CharField(max_length=100, label='', required=False)
-    hints = SimpleNamespace(
-        placeholder='Search',
-        method='get',
-        form_id='',
-        form_action='',
-        form_class='search-form',
-    )
 
-    def __init__(self, *args, **kwargs):
-        self.hints.form_action = kwargs.pop('form_action', '')
-        self.hints.placeholder = kwargs.pop('placeholder', 'Search')
-        super(SearchForm, self).__init__(*args, **kwargs)
-        self.fields['query'].widget = forms.TextInput(
-            attrs={'placeholder': self.hints.placeholder}
+    def __init__(
+        self, *args, form_action: str = '', placeholder: str = 'Search', **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.attrs = set_flat_form_attributes(
+            form_action=form_action,
+            form_method='get',
+            form_class='search-form',
+            form_fields=[
+                FormRow(
+                    [
+                        FormColumn([self['query']], css_classes='medium-9'),
+                        FormColumn(
+                            [
+                                SubmitField(
+                                    value='Search', css_classes='submit button postfix'
+                                )
+                            ],
+                            css_classes='medium-3',
+                        ),
+                    ],
+                    css_classes='collapse',
+                ),
+            ],
         )
+        self.fields['query'].widget.attrs.update({"placeholder": placeholder})
 
     def clean_query(self):
         """Remove whitespace from searchterm"""
