d53016d3086796eb3f90b338db4e10377833774b
Ilona Podliashanyk <ilona.podliashanyk@sikt.no>
2024-10-04 13:10:12 +0200

    Remove build_sass from setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,5 @@
-from glob import glob
 from setuptools import setup
-from setuptools.command.build import build
-
-
-# Ensure CSS files are built every time build is invoked
-build.sub_commands = [('build_sass', None)] + build.sub_commands
-
 
 setup(
-    setup_requires=['libsass', 'setuptools_scm'],
-    sass_manifests={
-        'nav.web': {
-            'sass_path': 'sass',
-            'css_path': 'static/css',
-            'strip_extension': True,
-        },
-    },
+    setup_requires=['setuptools_scm'],
 )
