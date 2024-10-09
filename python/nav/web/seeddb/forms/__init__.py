b4db6dcc79ee962e935e021a1559f0364594c1e0
Johanna England <johanna.england@sikt.no>
2024-10-09 14:17:48 +0200

    Use new uncrispified templates in filtering seedDB

--- python/nav/web/seeddb/forms/__init__.py
+++ python/nav/web/seeddb/forms/__init__.py
@@ -129,25 +129,6 @@ def cut_branch(field, klass, pk):
 # non-crispy helpers
 
 
-def set_filter_form_attributes(
-    legend,
-    submit_value='Filter',
-    form_action='',
-    form_method='get',
-    form_class='custom',
-):
-    class Obj:
-        pass
-
-    obj = Obj()
-    obj.legend = legend
-    obj.submit_value = submit_value
-    obj.action = form_action
-    obj.method = form_method
-    obj.form_class = form_class
-    return obj
-
-
 def get_single_layout(heading, row):
     """Get default layout for a single filter"""
     return set_flat_form_attributes(
@@ -184,10 +165,13 @@ class RoomFilterForm(forms.Form):
     """Form for filtering rooms"""
 
     location = forms.ModelChoiceField(
-        Location.objects.order_by('id').all(), required=False, label_suffix=''
+        Location.objects.order_by('id').all(), required=False
     )
     location.widget.attrs.update({"class": "select"})
-    no_crispy = set_filter_form_attributes('Filter rooms')
+
+    def __init__(self, *args, **kwargs):
+        super(RoomFilterForm, self).__init__(*args, **kwargs)
+        self.attrs = get_single_layout(heading="Filter rooms", row=self["location"])
 
 
 class RoomForm(forms.ModelForm):
@@ -302,10 +286,11 @@ class OrganizationMoveForm(forms.Form):
 class NetboxTypeFilterForm(forms.Form):
     """Form for filtering a netbox type by vendor"""
 
-    vendor = forms.ModelChoiceField(
-        Vendor.objects.order_by('id').all(), required=False, label_suffix=''
-    )
-    no_crispy = set_filter_form_attributes('Filter types')
+    vendor = forms.ModelChoiceField(Vendor.objects.order_by('id').all(), required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(NetboxTypeFilterForm, self).__init__(*args, **kwargs)
+        self.attrs = get_single_layout(heading="Filter types", row=self["vendor"])
 
 
 class NetboxTypeForm(forms.ModelForm):
