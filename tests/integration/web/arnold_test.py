e3f27c4d8ffc61507388ccccc17f9fde7cbb0cc4
Johanna England <johanna.england@sikt.no>
2025-02-19 12:49:04 +0100

    Check that vlan is not already quarantined in Arnold

--- tests/integration/web/arnold_test.py
+++ tests/integration/web/arnold_test.py
@@ -1,4 +1,7 @@
 from django.urls import reverse
+from django.utils.encoding import smart_str
+
+from nav.models.arnold import QuarantineVlan
 
 
 def test_arnold_manualdetention_should_not_crash(client):
@@ -14,3 +17,24 @@ def test_arnold_manualdetention_should_not_crash(client):
     )
 
     assert response.status_code == 200
+
+
+def test_arnold_quarantine_vlan_twice_should_show_error_message(client):
+    url = reverse('arnold-quarantinevlans')
+
+    vlan_id = 1
+    QuarantineVlan.objects.create(vlan=vlan_id, description='')
+
+    response = client.post(
+        url,
+        follow=True,
+        data={
+            'vlan': vlan_id,
+            'description': '',
+            'qid': '',
+            'submit': 'Add+vlan',
+        },
+    )
+
+    assert response.status_code == 200
+    assert "This vlan is already quarantined." in smart_str(response.content)
