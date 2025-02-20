1dc5ad8f5f57baecb5a61ec7879bdd187df8b127
Johanna England <johanna.england@sikt.no>
2025-02-20 13:24:12 +0100

    Clean number of days in account log search form

--- python/nav/web/radius/forms.py
+++ python/nav/web/radius/forms.py
@@ -247,6 +247,17 @@ class AccountLogSearchForm(forms.Form):
             ],
         )
 
+    def clean_time(self):
+        time_type, time = self.cleaned_data["time"]
+        if time_type == "days":
+            try:
+                datetime.now() - timedelta(days=int(time))
+            except OverflowError:
+                raise forms.ValidationError(
+                    "They did not have computers %s days ago" % time
+                )
+        return time
+
 
 class AccountChartsForm(forms.Form):
     """Form for displaying top talkers"""
