463fbfa7f75cce00b9b93142c001d54130485002
Johanna England <johanna.england@sikt.no>
2025-02-25 15:18:11 +0100

    Use validate timedelta in ipdevinfo

--- python/nav/web/ipdevinfo/forms.py
+++ python/nav/web/ipdevinfo/forms.py
@@ -15,8 +15,6 @@
 # along with NAV. If not, see <http://www.gnu.org/licenses/>.
 #
 
-import datetime as dt
-
 from django import forms
 
 from nav.models.manage import Sensor
@@ -26,6 +24,7 @@ from nav.web.crispyforms import (
     SubmitField,
     set_flat_form_attributes,
 )
+from ..utils import validate_timedelta_for_overflow
 
 
 class SearchForm(forms.Form):
@@ -86,13 +85,7 @@ class ActivityIntervalForm(forms.Form):
 
     def clean_interval(self):
         interval = self.cleaned_data["interval"]
-        try:
-            dt.datetime.now() - dt.timedelta(days=interval)
-        except OverflowError:
-            raise forms.ValidationError(
-                "They did not have computers %s days ago" % interval
-            )
-
+        validate_timedelta_for_overflow(days=interval)
         return interval
 
 
