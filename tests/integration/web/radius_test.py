8a2851aba114d554680554e95ca1043009b6d9d9
Johanna England <johanna.england@sikt.no>
2025-02-26 13:31:42 +0100

    Validate number of hours in error log search form

--- tests/integration/web/radius_test.py
+++ tests/integration/web/radius_test.py
@@ -23,3 +23,15 @@ def test_account_log_search_should_not_crash_on_big_days(db, client, admin_usern
 
     assert response.status_code == 200
     assert 'They did not have computers' in smart_str(response.content)
+
+
+def test_error_log_search_should_not_crash_on_big_hours(db, client, admin_username):
+    url = reverse('radius-log_search')
+    url = (
+        url
+        + f'?query_0=username&query_1={admin_username}&log_entry_type=&time_0=hours&time_1=123123123123123&send=Search'
+    )
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert 'They did not have computers' in smart_str(response.content)
