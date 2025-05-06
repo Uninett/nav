60189addc7118fd38f2b6146b358dba01e6a77a6
Morten Brekkevold <morten.brekkevold@sikt.no>
2025-05-06 16:25:53 +0200

    Create a graphite service for devcontainer
    This adds the existing graphite image definition from the source
    code tree and configures NAV in the devcontainer to use it.

--- .devcontainer/scripts/init-nav-config.sh
+++ .devcontainer/scripts/init-nav-config.sh
@@ -25,9 +25,23 @@ update_nav_conf() {
   sed -i "s,^#UPLOAD_DIR=.*,UPLOAD_DIR=${NAV_UPLOAD_DIR}," "$NAV_CONF" || echo "UPLOAD_DIR=${NAV_UPLOAD_DIR}" >> "$NAV_CONF"
 }
 
+update_graphite_conf() {
+  GRAPHITE_CONF="${NAV_CONFIG_DIR}/graphite.conf"
+  echo "Updating $GRAPHITE_CONF"
+  cat > "$GRAPHITE_CONF" <<EOF
+[carbon]
+host=graphite
+port=2003
+
+[graphiteweb]
+base=http://graphite:8000/
+EOF
+}
+
 nav config install "$NAV_CONFIG_DIR"
 update_nav_conf
 update_nav_db_conf
+update_graphite_conf
 
 # Ensure the default virtualenv is in the secure_path when running sudo
 echo "Defaults        secure_path=\"${UV_PROJECT_ENVIRONMENT}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\"" | sudo tee /etc/sudoers.d/secure_path_virtualenv
