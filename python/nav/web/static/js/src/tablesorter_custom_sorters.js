b605954061e3c7d31e55a4f265e0644750afefe1
Jørund Hellebø <jorund.hellebo@sikt.no>
2025-07-23 21:46:15 +0200

    Add custom tablesorter.js parser for sorting iso datetimes

---
+++ python/nav/web/static/js/src/tablesorter_custom_sorters.js
@@ -0,0 +1,40 @@
+/**
+ * Adds custom parsers to the global $.tablesorter object
+ * See https://mottie.github.io/tablesorter/docs/example-parsers.html
+ */
+define(["libs/jquery.tablesorter.min"], function() {
+  /**
+   * Returns data-sort-value attribute of a DOM node if the attribute is
+   * defined.  If not, returns fallback
+   *
+   * @param {Element} node A DOM node
+   * @param {String} fallback Value to return if node does not have
+   *                          attribute
+   */
+  function getSortValue(node, fallback) {
+    if (node.hasAttribute("data-sort-value")) {
+      return node.getAttribute("data-sort-value");
+    } else {
+      return fallback;
+    }
+  }
+
+  $.tablesorter.addParser({
+    id: "custom-iso-datetime",
+    is: function(text, table, node) {
+      return false;
+    },
+    format: function(text, table, node) {
+      const value = getSortValue(node, text);
+      if (value === "last") {
+        return Number.NEGATIVE_INFINITY;
+      }
+      if (value === "first") {
+        return Number.POSITIVE_INFINITY;
+      }
+      return new Date(value).getTime();
+    },
+    parsed: false,
+    type: "numeric"
+  });
+});
