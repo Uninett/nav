fa28707e0b262244f755dcb21e1232e958d042d6
Johanna England <johanna.england@sikt.no>
2025-07-30 11:53:25 +0200

    Add util functions to zip QR codes
    Co-authored-by: Jørund Hellebø <jorund.hellebo@sikt.no>

--- tests/unittests/web/qrcode_test.py
+++ tests/unittests/web/qrcode_test.py
@@ -1,4 +1,11 @@
-from nav.web.utils import generate_qr_code, generate_qr_code_as_string
+import io
+import zipfile
+
+from nav.web.utils import (
+    generate_qr_code,
+    generate_qr_code_as_string,
+    generate_qr_codes_as_zip_response,
+)
 
 
 def test_generate_qr_code_returns_bytes():
@@ -11,3 +18,20 @@ def test_generate_qr_code_as_string_returns_string():
         url="www.example.com", caption="buick.lab.uninett.no"
     )
     assert isinstance(qr_code, str)
+
+
+def test_generate_qr_codes_as_zip_response_should_return_zip_with_correct_filenames():
+    response = generate_qr_codes_as_zip_response(
+        {
+            "buick.lab.uninett.no": "www.example.com",
+            "buick2.lab.uninett.no": "www2.example.com",
+        }
+    )
+
+    buf = io.BytesIO(b"".join(response.streaming_content))
+    zip_ = zipfile.ZipFile(buf, "r")
+
+    actual_filenames = set(zip_.namelist())
+    expected_filenames = set(["buick.lab.uninett.no.png", "buick2.lab.uninett.no.png"])
+
+    assert actual_filenames == expected_filenames
