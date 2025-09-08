ceb4da3b31e242a275bca9f2f3ce32b2f9edc441
Hanne Moa <hanne.moa@sikt.no>
2025-09-08 11:08:02 +0200

    Replace urls re_path with path where possible, 3

--- python/nav/web/info/location/urls.py
+++ python/nav/web/info/location/urls.py
@@ -16,12 +16,13 @@
 #
 """Django URL configuration"""
 
+from django.urls import path
 from django.urls import re_path
 from nav.web.info.location import views
 
 
 urlpatterns = [
-    re_path(r'^$', views.search, name='location-search'),
+    path('', views.search, name='location-search'),
     re_path(
         r'^(?P<locationid>.+)/upload/', views.upload_image, name='location-info-upload'
     ),
