Uninett/nav

commit c221f67fd2456cf82501894bd4f5c673e1e44f6b

Catch explicit errors instead of bare except

python/nav/web/navlets/status2.py
@@ -117,7 +117,7 @@ class Status2Widget(Navlet):
                 navlet.preferences['refresh_interval'] = (
                     int(request.POST['interval']) * 1000
                 )
-            except Exception:
+            except (TypeError, ValueError):
                 pass
             navlet.save()
             return JsonResponse(self.preferences)

python/nav/ipdevpoll/timestamps.py
@@ -103,7 +103,12 @@ class TimestampChecker(object):
                 return None
             try:
                 return json.loads(info.value)
-            except Exception:
+            except (
+                AttributeError,
+                json.JSONDecodeError,
+                UnicodeDecodeError,
+                TypeError,
+            ):
                 return None
 
         self.loaded_times = yield db.run_in_thread(_deserialize)

python/nav/web/macwatch/views.py
@@ -79,7 +79,7 @@ def delete_macwatch(request, macwatchid):
         macwatchid = int(macwatchid)
         try:
             m = MacWatch.objects.get(id=macwatchid)
-        except Exception as e:
+        except MacWatch.DoesNotExist as e:
             messages = [e]
             return do_list(request, messages)
 

python/nav/web/maintenance/views.py
@@ -126,7 +126,7 @@ def active(request):
                 netbox_id = maint_components[0].value
                 try:
                     netbox = Netbox.objects.get(pk=int(netbox_id))
-                except Exception as error:
+                except Netbox.DoesNotExist as error:
                     _logger.error(
                         'Get netbox %s failed; Exception = %s', netbox_id, error
                     )

python/nav/web/netmap/common.py
@@ -146,7 +146,7 @@ def get_status_image_link(status):
     """uplink icon for status"""
     try:
         return STATUS_IMAGE_MAP[status]
-    except Exception:
+    except KeyError:
         return STATUS_IMAGE_MAP[Netbox.UP_DOWN]
 
 

commit 662d3e34a77ce350d1d573bd4ae77e0c6c256230

Catch additional error when editing status widget
This was broken by c221f67

python/nav/web/navlets/status2.py
@@ -20,6 +20,7 @@ from operator import itemgetter
 
 from django.http import QueryDict, JsonResponse
 from django.test.client import RequestFactory
+from django.utils.datastructures import MultiValueDictKeyError
 from django.utils.dateparse import parse_datetime
 
 from nav.models.profiles import Account
@@ -118,7 +119,7 @@ class Status2Widget(Navlet):
                 navlet.preferences['refresh_interval'] = (
                     int(request.POST['interval']) * 1000
                 )
-            except (TypeError, ValueError):
+            except (TypeError, ValueError, MultiValueDictKeyError):
                 pass
             navlet.save()
             return JsonResponse(self.preferences)