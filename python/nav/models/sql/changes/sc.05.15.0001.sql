c04dd64c2eccebf887a2ae0690e7624e93306833
Simen Abelsen <simen.abelsen@sikt.no>
2025-09-25 11:44:10 +0200

    fixup! Add shared dashboard property and subscription model

--- python/nav/models/sql/changes/sc.05.15.0001.sql
+++ python/nav/models/sql/changes/sc.05.15.0001.sql
@@ -1,10 +1,9 @@
-ALTER TABLE profiles.account_dashboard
+ALTER TABLE account_dashboard
 ADD COLUMN is_shared BOOLEAN NOT NULL DEFAULT FALSE;
 
 CREATE TABLE profiles.account_dashboard_subscription (
     id SERIAL PRIMARY KEY,
     account_id INT REFERENCES account(id) ON UPDATE CASCADE ON DELETE CASCADE,
     dashboard_id INT REFERENCES account_dashboard(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    subscribed_at TIMESTAMP NOT NULL DEFAULT NOW(),
     CONSTRAINT unique_account_dashboard_subscription UNIQUE(account_id, dashboard_id)
 )
