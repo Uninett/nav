7079547714067f66e46af377c7c90c3a6cf0c3ba
Simen Abelsen <simen.abelsen@sikt.no>
2025-10-08 09:27:56 +0200

    Add search preview tests

--- tests/integration/web/info/search_test.py
+++ tests/integration/web/info/search_test.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+import pytest
 from django.urls import reverse
+
+from nav.models.manage import Interface, Netbox
 
 
 def test_search_for_ip_devices_should_not_crash(client):
@@ -36,3 +39,74 @@ def test_search_for_device_groups_should_not_crash(client):
     url = reverse('netbox-group') + '?query=a'
     response = client.get(url)
     assert response.status_code == 200
+
+
+class TestIndexSearchPreviewView:
+    """Tests for the search preview feature."""
+
+    def test_given_empty_query_it_should_return_no_results(self, client):
+        response = self._handle_search_request(client, '')
+        assert response.status_code == 200
+        assert response.context['results'] is None
+
+    def test_given_empty_query_it_should_return_close_popover_event(self, client):
+        response = self._handle_search_request(client, '')
+        assert 'HX-Trigger' in response.headers
+        assert 'popover.close' in response.headers['HX-Trigger']
+
+    def test_given_existing_resource_it_should_return_results(self, client):
+        response = self._handle_search_request(client, "myroom")
+        assert response.status_code == 200
+        assert len(response.context['results']) > 0
+
+    def test_given_valid_query_it_should_return_results(self, client, db, netbox):
+        response = self._handle_search_request(client, netbox.sysname)
+        assert response.status_code == 200
+        results = response.context['results']
+        assert results is not None
+        # At least one provider should have results
+        assert any(len(provider.results) > 0 for provider in results)
+
+    def test_given_more_than_five_results_then_it_should_truncate_results(
+        self, client, db, netbox
+    ):
+        # Create more than 5 interfaces to trigger truncation
+        for i in range(10):
+            create_interface(netbox, ifname=f'GigabitEthernet0/{i}')
+        response = self._handle_search_request(client, "Test Interface")
+        results = response.context['results']
+        # Find the Interfaces provider
+        provider = next((p for p in results if p.name == 'Interfaces'), None)
+        assert provider is not None
+        assert provider.count == 10
+        assert len(provider.results) == 5  # Should be truncated to 5
+        assert provider.truncated is True
+        assert provider.truncated_count == 5
+
+    @staticmethod
+    def _handle_search_request(client, query):
+        url = reverse('info-search-preview') + f'?query={query}'
+        return client.get(url)
+
+
+@pytest.fixture()
+def netbox(management_profile):
+    box = Netbox(
+        ip='10.254.254.254',
+        sysname='box.example.org',
+        organization_id='myorg',
+        room_id='myroom',
+        category_id='SRV',
+    )
+    box.save()
+    yield box
+    box.delete()
+
+
+def create_interface(
+    netbox, ifname='GigabitEthernet0/1', ifalias='Test Interface', **kwargs
+):
+    """Create a test interface"""
+    interface = Interface(netbox=netbox, ifname=ifname, ifalias=ifalias, **kwargs)
+    interface.save()
+    return interface
