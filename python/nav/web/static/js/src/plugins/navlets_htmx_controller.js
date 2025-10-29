5b7c3c53e0eae580def82d32e86f1b9021839870
Simen Abelsen <simen.abelsen@sikt.no>
2025-10-29 10:41:23 +0100

    Refactor navlet handlers in separate plugin

--- python/nav/web/static/js/src/plugins/navlets_htmx_controller.js
+++ python/nav/web/static/js/src/plugins/navlets_htmx_controller.js
@@ -12,23 +12,15 @@
  * - Visual feedback for user interactions
  */
 define([
-    'plugins/room_mapper',
-    'plugins/sensors_controller',
-    'src/getting_started_wizard'
-], function (RoomMapper, SensorsController, GettingStartedWizard) {
+    'plugins/navlet_handlers'
+], function (NavletHandlers) {
 
     const NAVLETS_CONTAINER_ID = 'navlets-htmx';
-
-    const SELECT2_REINIT_DELAY_MS = 30;
 
     const CSS_CLASSES = {
         NAVLET: 'navlet',
         OUTLINE: 'outline',
         MARK_NEW: 'mark-new',
-        // If the element has the `select2-offscreen` class, it means select2 was previously initialized.
-        // TODO: Update class detection when upgrading to select2 v4.
-        //  See: https://select2.org/programmatic-control/methods#checking-if-the-plugin-is-initialized
-        SELECT2_INITIALIZED: 'select2-offscreen'
     };
 
     const SELECTORS = {
@@ -136,7 +128,7 @@ define([
                 this.reinitializeSortable();
             }
             if (this.isNavlet(swappedNode)) {
-                handleNavletSwap(swappedNode);
+                NavletHandlers.handle(swappedNode);
             }
         },
 
@@ -161,66 +153,6 @@ define([
             this.updateOrder();
         }
     };
-
-    function createRoomMap(mapwrapper, room_map) {
-        mapwrapper.show();
-        new RoomMapper(room_map.get(0));
-    }
-
-    function handleNavletSwap(swappedNode) {
-        const isNavlet = swappedNode?.dataset?.id && swappedNode.classList.contains(CSS_CLASSES.NAVLET);
-        if (!isNavlet) return;
-        const $node = $(swappedNode);
-
-        // Initialize RoomMapNavlet
-        if ($node.hasClass('RoomMapNavlet')) {
-            const room_map = $node.find('#room_map');
-            if (!room_map.length) return;
-            const map_wrapper = $node.find('.mapwrapper');
-            createRoomMap(map_wrapper, room_map);
-        }
-
-        // Initialize SensorWidget
-        if ($node.hasClass('SensorWidget')) {
-            const sensors = $node.find('.room-sensor');
-            if (!sensors.length) return;
-            new SensorsController($node.find('.room-sensor'));
-        }
-
-        // Handle wizard button for GettingStartedWidget
-        if ($node.hasClass('GettingStartedWidget')) {
-            $node.on('click', '#getting-started-wizard', function () {
-                GettingStartedWizard.start();
-            })
-        }
-
-        // Handle list expansion for WatchDogWidget
-        if ($node.hasClass('WatchDogWidget')) {
-            $node.on('click', '.watchdog-tests .label.alert', function (event) {
-                $(event.target).closest('li').find('ul').toggle();
-            });
-        }
-
-        handleSelect2Initialization(swappedNode);
-    }
-
-    function handleSelect2Initialization(swappedNode) {
-        const $selectElements = $(swappedNode).find('select');
-
-        if ($selectElements.length > 0) {
-            $selectElements.each((_, element) => {
-                if ($(element).hasClass(CSS_CLASSES.SELECT2_INITIALIZED)) {
-                    // Re-initialize after a short delay to allow destroy to complete
-                    // Timeout value selected based on manual testing
-                    setTimeout(() => {
-                        $(element).select2();
-                    }, SELECT2_REINIT_DELAY_MS);
-                } else {
-                    $(element).select2();
-                }
-            });
-        }
-    }
 
     function initialize() {
         const controller = new NavletsHtmxController();
