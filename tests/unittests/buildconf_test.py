0d5a829099edb1448d2eeb9da663f22c4f553b2a
Aleksander Fløtten <aleksander.flotten@sikt.no>
2025-10-30 18:09:47 +0100

    Add version info to debug view

--- tests/unittests/buildconf_test.py
+++ tests/unittests/buildconf_test.py
@@ -2,6 +2,8 @@
 
 from unittest import TestCase
 
+import nav.buildconf
+
 
 class TestBuildconf(TestCase):
     def test_VERSION_can_be_imported(self):
@@ -9,3 +11,10 @@ class TestBuildconf(TestCase):
             from nav.buildconf import VERSION
         except ImportError:
             self.fail('VERSION could not be imported')
+
+    def test_VERSION_contains_dot(self):
+        """
+        Assumes that VERSION containing a '.' implies
+        it is set to a valid version string
+        """
+        assert "." in nav.buildconf.VERSION
--- tests/unittests/django/utils_test.py
+++ tests/unittests/django/utils_test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from nav.django.utils import get_verbose_name, reverse_with_query
+from nav.django.utils import get_verbose_name, reverse_with_query, get_os_version
+from unittest.mock import patch
 
 
 def test_verbose_name():
@@ -13,3 +14,35 @@ def test_verbose_name():
 def test_reverse_with_query_should_work_with_unicode():
     """Reveals issues with PY2/PY3 co-compatibility"""
     assert reverse_with_query("maintenance-new", roomid="bø-123")
+
+
+def test_get_os_version_examples(subtests):
+    with subtests.test("Linux"):
+        with (
+            patch("platform.system", return_value="Linux"),
+            patch("distro.name", return_value="Ubuntu 22.04"),
+        ):
+            assert get_os_version() == "Linux Ubuntu 22.04"
+
+    with subtests.test("macOS"):
+        with (
+            patch("platform.system", return_value="Darwin"),
+            patch("platform.mac_ver", return_value=("13.2", ("", "", ""), "")),
+        ):
+            assert get_os_version() == "macOS 13.2"
+
+    with subtests.test("Windows"):
+        with (
+            patch("platform.system", return_value="Windows"),
+            patch("platform.release", return_value="10"),
+            patch("platform.version", return_value="10.0.19042"),
+        ):
+            assert get_os_version() == "Windows 10 (10.0.19042)"
+
+    with subtests.test("FreeBSD"):
+        with (
+            patch("platform.system", return_value="FreeBSD"),
+            patch("platform.release", return_value="13.0"),
+            patch("platform.version", return_value="GENERIC"),
+        ):
+            assert get_os_version() == "FreeBSD 13.0 (GENERIC)"
