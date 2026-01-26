ead93106227d6a3c589f315fce2f6c469eed5da1
Simen Abelsen <simen.abelsen@sikt.no>
2026-01-26 15:06:16 +0100

    Upgrade library to version 4

--- python/nav/web/static/js/require_config.js
+++ python/nav/web/static/js/require_config.js
@@ -26,7 +26,7 @@ var require = {
         "driver": "libs/driver-1.3.6.min",
         "tinysort": "libs/tinysort-3.1.4.min",
         "flatpickr": "libs/flatpickr-4.6.13.min",
-        "jquery": "libs/jquery-3.7.1.min",
+        "jquery": "libs/jquery-4.0.0.min",
         "jquery-ui": "libs/jquery-ui-1.14.0.min",
         "jquery-tablesorter": "libs/jquery.tablesorter-2.32.0.min",
         "jquery-sparkline": "libs/jquery.sparkline-2.4.0.min",
