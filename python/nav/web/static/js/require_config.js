d958915b583c81e761a52ad43b90becd8206efd3
Simen Abelsen <simen.abelsen@sikt.no>
2026-01-29 23:36:58 +0100

    Update JavaScript to use DataTables 2.x API

--- python/nav/web/static/js/require_config.js
+++ python/nav/web/static/js/require_config.js
@@ -61,7 +61,7 @@ var require = {
             deps: ['jquery-migrate']
         },
         'libs/datatables.min': {
-            deps: ['jquery-migrate']
+            deps: ['jquery']
         },
         'select2': {
             deps: ['jquery-migrate']
