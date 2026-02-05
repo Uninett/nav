46ede6d2fd2432b8a5581adbe120efd68016b540
Simen Abelsen <simen.abelsen@sikt.no>
2026-02-05 23:08:07 +0100

    Update test configuration and fixtures for Playwright

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,8 @@
 
 import os
 import platform
-import subprocess
 
 import pytest
-import requests
-from requests.adapters import HTTPAdapter, Retry
 
 
 def pytest_configure(config):
@@ -15,7 +12,6 @@ def pytest_configure(config):
 
     bootstrap_django('pytest')
 
-    # Setup test environment for Django
     from django.test.utils import setup_test_environment
 
     setup_test_environment()
@@ -35,39 +31,6 @@ def admin_username():
 @pytest.fixture(scope='session')
 def admin_password():
     return os.environ.get('ADMINPASSWORD', 'admin')
-
-
-@pytest.fixture(scope='session')
-def gunicorn():
-    """Sets up NAV to be served by a gunicorn instance.
-
-    Useful for tests that need to make external HTTP requests to NAV.
-
-    Returns the (expected) base URL of the gunicorn instance.
-    """
-    workspace = os.path.join(os.environ.get('WORKSPACE', ''), 'reports')
-    errorlog = os.path.join(workspace, 'gunicorn-error.log')
-    accesslog = os.path.join(workspace, 'gunicorn-access.log')
-    gunicorn = subprocess.Popen(
-        [
-            'gunicorn',
-            '--error-logfile',
-            errorlog,
-            '--access-logfile',
-            accesslog,
-            'navtest_wsgi:application',
-        ]
-    )
-    # Allow for gunicorn to become ready to serve requests before handing off to a test
-    session = requests.Session()
-    retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
-    base_url = "http://localhost:8000"
-    session.mount(base_url, HTTPAdapter(max_retries=retries))
-    session.get(base_url)
-
-    yield base_url
-
-    gunicorn.terminate()
 
 
 @pytest.fixture(scope="session")
