ac344e5e591e085abc63dd3527e4399164f0201d
Hanne Moa <hanne.moa@sikt.no>
2026-03-12 09:04:30 +0100

    Update tests for login_required
    Co-authored-by: Morten Brekkevold <morten.brekkevold@sikt.no>

---
+++ tests/unittests/general/modpython_test.py
@@ -0,0 +1,35 @@
+from unittest.mock import patch
+
+from django.test import RequestFactory
+
+from nav.models import profiles
+from nav.web.modpython import ModPythonAuthorizationMiddleware
+
+
+PLAIN_ACCOUNT = profiles.Account(id=101, login='tim', password='wizard', locked=False)
+
+
+class TestModPythonAuthorizationMiddleware:
+    def test_when_authorized_then_process_request_should_return_none(self):
+        r = RequestFactory()
+        fake_request = r.get('/')
+        fake_request.user = PLAIN_ACCOUNT
+        with patch('nav.web.modpython.authorize_request', return_value=True):
+            result = ModPythonAuthorizationMiddleware(lambda x: x).process_request(
+                fake_request
+            )
+            assert result is None
+
+    def test_when_not_authorized_process_request_should_redirect_to_login(self):
+        r = RequestFactory()
+        fake_request = r.get('/')
+        fake_request.user = PLAIN_ACCOUNT
+        with patch('nav.web.modpython.authorize_request', return_value=False):
+            with patch(
+                'nav.web.modpython.redirect_to_login',
+                return_value='here',
+            ):
+                result = ModPythonAuthorizationMiddleware(lambda x: x).process_request(
+                    fake_request
+                )
+                assert result == 'here'
