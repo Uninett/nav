ce2627575ce803c86c3a48368cf6093b25aaae5a
Hanne Moa <hanne.moa@sikt.no>
2026-05-18 14:31:18 +0200

    fixup! Add Django 5.2 upgrade verification tests

--- tests/unittests/django/django52_upgrade_test.py
+++ tests/unittests/django/django52_upgrade_test.py
@@ -1,12 +1,5 @@
 """Tests to verify Django 5.2 upgrade requirements are met."""
 
-import django
-
-
-class TestDjangoVersion:
-    def test_when_checking_version_then_it_should_be_52_or_higher(self):
-        assert django.VERSION >= (5, 2)
-
 
 class TestLoginNotRequired:
     def test_when_importing_then_it_should_exist_natively(self):
